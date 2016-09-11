69c0a0dbf583e32ee0734bf75e1e4784389bde7a
Samuel Hassine <samuel@hassine.fr>
2016-09-11 03:54:45 +0200

    New serialization bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new APIBundle\APIBundle(),
             new FrontendBundle\FrontendBundle(),
         ];
