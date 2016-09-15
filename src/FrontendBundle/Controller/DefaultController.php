75aa4c8a0ea310faf544bf66cbb6129d48f656e2
Samuel Hassine <samuel@hassine.fr>
2016-09-15 22:46:12 +0200

    Try the frontend bundle

--- src/FrontendBundle/Controller/DefaultController.php
+++ src/FrontendBundle/Controller/DefaultController.php
@@ -3,19 +3,17 @@
 namespace FrontendBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="home")
+     * @Template("FrontendBundle:index.html.twig")
      */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+    public function indexAction() {
+        return [];
     }
 }
--- src/FrontendBundle/FrontendBundle.php
+++ src/FrontendBundle/FrontendBundle.php
@@ -6,4 +6,5 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class FrontendBundle extends Bundle
 {
+
 }
