f1bef18f753185da3c98588edf2c2311eff4b159
Samuel Hassine <samuel@hassine.fr>
2016-09-30 21:50:39 +0200

    Refactor UserPopover

---
+++ src/app/components/Popover.js
@@ -0,0 +1,23 @@
+import React, {PropTypes} from 'react';
+import MUIPopover from 'material-ui/Popover';
+
+export const Popover = (props) => (
+  <MUIPopover
+    open={props.open}
+    anchorEl={props.anchorEl}
+    anchorOrigin={props.anchorOrigin}
+    targetOrigin={props.targetOrigin}
+    onRequestClose={props.onRequestClose}
+  >
+    {props.children}
+  </MUIPopover>
+)
+
+Popover.propTypes = {
+  open: PropTypes.bool,
+  anchorEl: PropTypes.object,
+  anchorOrigin: PropTypes.object,
+  targetOrigin: PropTypes.object,
+  onRequestClose: PropTypes.func,
+  children: PropTypes.node
+}
