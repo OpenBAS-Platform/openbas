d1bdcccae76c0af9f7abae6847e521a57e641bb9
Samuel Hassine <samuel@hassine.fr>
2016-10-02 02:16:46 +0200

    Sanitize

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App';
-import './www/css/main.css'
+import '../public/css/main.css'
 
 ReactDOM.render(
   <App />,
