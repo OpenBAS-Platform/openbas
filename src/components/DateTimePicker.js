15508cb4265b0a99f1a839f3359ef8c85ed649d3
Samuel Hassine <samuel@hassine.fr>
2016-10-10 00:35:33 +0200

    Sanitize

--- src/components/DateTimePicker.js
+++ src/components/DateTimePicker.js
@@ -1,14 +1,68 @@
 import React, {Component, PropTypes} from 'react'
+import {Dialog} from './Dialog'
 import {DatePicker} from './DatePicker'
 import {TimePicker} from './TimePicker'
 
 class DateTimePicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      dateStyle: 'block',
+      timeStyle: 'hidden',
+      datetime: '',
+    }
+  }
+
+  handleOpen() {
+    this.setState({open: true})
+  }
+
+  handleClose() {
+    this.setState({open: false})
+  }
+
+  handleFinish() {
+    this.handleReplace()
+    this.props.replaceValue(this.state.datetime)
+  }
+
+  handleDateChange(event, date) {
+    this.setState({
+      datetime: date,
+      dateStyle: 'hidden',
+      timeStyle: 'block'
+    })
+  }
+
+  handleTimeChange(event, time) {
+    this.setState({
+      datetime: this.state.datetime + ' ' + time,
+    })
+    this.handleClose()
+  }
+
   render() {
-    console.log('EXERCISE', this.props.exercise)
-    let image = this.props.exercise ? this.props.exercise.get('exercise_image') : ''
-
     return (
-      <div>
-
+      <Dialog
+        title="Select the date"
+        modal={false}
+        open={this.state.open}
+        onRequestClose={this.handleClose.bind(this)}
+      >
+        <div style={{display: this.state.dateStyle}}>
+          <DatePicker onChange={this.handleDateChange.bind(this)}/>
+        </div>
+        <div style={{display: this.state.timeStyle}}>
+          <TimePicker onChange={this.handleTimeChange.bind(this)}/>
+        </div>
+      </Dialog>
+    )
+  }
 }
 
+DateTimePicker.propTypes = {
+  replaceValue: PropTypes.func,
+}
+
+export default DateTimePicker;
