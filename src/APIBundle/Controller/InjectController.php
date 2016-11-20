eead74f12c9b2c7ec3e8f11a9852b66dfc0949e9
Samuel Hassine <samuel@hassine.fr>
2016-11-21 00:42:03 +0100

    Select all injects for the worker

--- src/APIBundle/Controller/InjectController.php
+++ src/APIBundle/Controller/InjectController.php
@@ -8,8 +8,10 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use APIBundle\Entity\Audience;
 use APIBundle\Entity\Exercise;
 use APIBundle\Form\Type\InjectType;
+use APIBundle\Entity\InjectStatus;
 use APIBundle\Entity\Event;
 use APIBundle\Entity\Incident;
 use APIBundle\Entity\Inject;
@@ -34,7 +36,7 @@ class InjectController extends Controller
         $injects = array();
         $exercises = $em->getRepository('APIBundle:Exercise')->findAll();
         /* @var $exercises Exercise[] */
-        foreach($exercises as $exercise) {
+        foreach ($exercises as $exercise) {
             $events = $em->getRepository('APIBundle:Event')->findBy(['event_exercise' => $exercise]);
             /* @var $events Event[] */
             foreach ($events as $event) {
@@ -42,11 +44,51 @@ class InjectController extends Controller
                 /* @var $incidents Incident[] */
 
                 foreach ($incidents as $incident) {
-                    $injects = array_merge($injects, $em->getRepository('APIBundle:Inject')->findBy(['inject_incident' => $incident]));
+                    if ($request->get('worker')) {
+                        // get pending id
+                        $status = $em->getRepository('APIBundle:InjectStatus')->findOneBy(['status_name' => 'PENDING']);
+                        /* @var $status InjectStatus */
+                        $dateStart = new \DateTime();
+                        $dateStart->modify('-60 minutes');
+                        $dateEnd = new \DateTime();
+
+                        $injects = array_merge($injects, $em->getRepository('APIBundle:Inject')->createQueryBuilder('i')
+                            ->orderBy('i.inject_date', 'ASC')
+                            ->where('i.inject_incident = :incident')
+                            ->andWhere('i.inject_status = :status')
+                            ->andWhere('i.inject_date BETWEEN :start AND :end')
+                            ->setParameter('incident', $incident->getIncidentId())
+                            ->setParameter('status', $status->getStatusId())
+                            ->setParameter('start', $dateStart)
+                            ->setParameter('end', $dateEnd)
+                            ->getQuery()
+                            ->getResult());
+
+                    } else {
+                        $injects = array_merge($injects, $em->getRepository('APIBundle:Inject')->findBy(['inject_incident' => $incident]));
+                    }
                 }
             }
         }
 
-        return $injects;
+        if ($request->get('worker')) {
+            $output = array();
+            foreach ($injects as $inject) {
+                $content = json_decode($inject->getInjectContent(), true);
+                $content['inject_id'] = $inject->getInjectId();
+                $content['users'] = array();
+                foreach ($inject->getInjectAudiences() as $audience) {
+                    /* @var $audience Audience */
+                    foreach(  $audience->getAudienceUsers() as $user ) {
+                        $content['users'][] = $user;
+                    }
+                }
+                $output[] = $content;
+            }
+
+            return $output;
+        } else {
+            return $injects;
+        }
     }
 }
