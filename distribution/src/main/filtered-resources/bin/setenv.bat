42558d7e0e0a634f42831849cd26dcbe336a87e7
Julien Richard <richard.julien@gmail.com>
2016-11-23 01:01:12 +0100

    Adapt worker to always start correctly

---
+++ distribution/src/main/filtered-resources/bin/setenv.bat
@@ -0,0 +1,13 @@
+@echo off
+
+rem Karaf home folder
+SET KARAF_HOME=
+
+rem Window name of the windows console
+SET KARAF_TITLE=Openex
+
+rem Minimum memory for the JVM
+SET JAVA_MIN_MEM=512m
+
+rem Maximum memory for the JVM
+SET JAVA_MAX_MEM=1024m
