cd245a168034fc674fe2db60072118d6fdbb2777
Julien Richard <richard.julien@gmail.com>
2016-12-23 15:04:04 +0100

    Handle secure download and PGP attachments

--- email/src/main/java/io/openex/email/EmailPgp.java
+++ email/src/main/java/io/openex/email/EmailPgp.java
@@ -1,5 +1,6 @@
 package io.openex.email;
 
+import io.openex.email.attachment.EmailAttachment;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.bouncycastle.bcpg.ArmoredOutputStream;
@@ -12,13 +13,16 @@ import org.bouncycastle.util.encoders.Base64;
 
 import java.io.*;
 import java.nio.charset.Charset;
-import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Spliterator;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
+import static io.openex.email.attachment.EmailDownloader.ATTACHMENTS_CONTENT;
 import static java.util.Spliterators.spliteratorUnknownSize;
 
 /**
@@ -26,47 +30,65 @@ import static java.util.Spliterators.spliteratorUnknownSize;
  */
 @SuppressWarnings("PackageAccessibility")
 public class EmailPgp {
-
-	@SuppressWarnings("unused")
+	
+	public static final String GPG = ".gpg";
+	
+	@SuppressWarnings({"unused", "unchecked"})
 	public void process(Exchange exchange) {
 		Message in = exchange.getIn();
-		String base64PgpKey = in.getHeader("PgpKey", String.class);
+		PGPPublicKey pgpKey = pgpKeyFromExchange(exchange);
+		if (pgpKey != null) {
+			//encode message
+			in.setBody(encrypt(in.getBody().toString(), pgpKey));
+			//encode attachments
+			List<EmailAttachment> filesContent = (List) exchange.getProperty(ATTACHMENTS_CONTENT, new ArrayList<>());
+			List<EmailAttachment> encodedFilesContent = filesContent.stream()
+					.map(e -> new EmailAttachment(e.getName() + GPG, encrypt(e.getData(), pgpKey), e.getContentType()))
+					.collect(Collectors.toList());
+			exchange.setProperty(ATTACHMENTS_CONTENT, encodedFilesContent);
+		}
+	}
+	
+	private PGPPublicKey pgpKeyFromExchange(Exchange exchange) {
+		String base64PgpKey = exchange.getIn().getHeader("PgpKey", String.class);
 		if (base64PgpKey != null) {
 			byte[] pgpKey = Base64.decode(base64PgpKey);
+			InputStream in = new ByteArrayInputStream(pgpKey);
 			try {
-				in.setBody(encrypt(in.getBody().toString(), keyFromPgp(pgpKey)));
+				in = PGPUtil.getDecoderStream(in);
+				PGPPublicKeyRing keyRing = new JcaPGPPublicKeyRing(in);
+				Spliterator<PGPPublicKey> splitIterator = spliteratorUnknownSize(keyRing.getPublicKeys(), Spliterator.ORDERED);
+				Stream<PGPPublicKey> targetStream = StreamSupport.stream(splitIterator, false);
+				return targetStream.filter(pgpPublicKey -> pgpPublicKey.isEncryptionKey() && !pgpPublicKey.isMasterKey())
+						.findFirst().orElseThrow(() -> new IllegalArgumentException("Invalid PGP public key"));
 			} catch (Exception e) {
 				throw new RuntimeException(e);
 			}
 		}
+		return null;
 	}
 	
-	private PGPPublicKey keyFromPgp(byte[] publicKey) throws IOException {
-		InputStream in = new ByteArrayInputStream(publicKey);
-		in = org.bouncycastle.openpgp.PGPUtil.getDecoderStream(in);
-		PGPPublicKeyRing keyRing = new JcaPGPPublicKeyRing(in);
-		Spliterator<PGPPublicKey> splitIterator = spliteratorUnknownSize(keyRing.getPublicKeys(), Spliterator.ORDERED);
-		Stream<PGPPublicKey> targetStream = StreamSupport.stream(splitIterator, false);
-		return targetStream.filter(pgpPublicKey -> pgpPublicKey.isEncryptionKey() && !pgpPublicKey.isMasterKey())
-				.findFirst().orElseThrow(() -> new IllegalArgumentException("Invalid PGP public key"));
+	private String encrypt(String clearData, PGPPublicKey encKey) {
+		return new String(encrypt(clearData.getBytes(), encKey), Charset.forName("UTF-8"));
 	}
 	
-	private String encrypt(String clearData, PGPPublicKey encKey)
-			throws IOException, PGPException, NoSuchProviderException {
-		
-		byte[] compressedData = compress(clearData.getBytes(), CompressionAlgorithmTags.ZIP);
-		ByteArrayOutputStream bOut = new ByteArrayOutputStream();
-		OutputStream out = new ArmoredOutputStream(bOut);
-		PGPEncryptedDataGenerator encGen = new PGPEncryptedDataGenerator(
-				new BcPGPDataEncryptorBuilder(PGPEncryptedDataGenerator.CAST5)
-						.setSecureRandom(new SecureRandom()));
-		encGen.addMethod(new BcPublicKeyKeyEncryptionMethodGenerator(encKey));
-		OutputStream encOut = encGen.open(out, compressedData.length);
-		encOut.write(compressedData);
-		encOut.close();
-		out.close();
-		
-		return new String(bOut.toByteArray(), Charset.forName("UTF-8"));
+	private byte[] encrypt(byte[] clearData, PGPPublicKey encKey) {
+		try {
+			byte[] compressedData = compress(clearData, CompressionAlgorithmTags.ZIP);
+			ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+			OutputStream out = new ArmoredOutputStream(bOut);
+			PGPEncryptedDataGenerator encGen = new PGPEncryptedDataGenerator(
+					new BcPGPDataEncryptorBuilder(PGPEncryptedDataGenerator.CAST5)
+							.setSecureRandom(new SecureRandom()));
+			encGen.addMethod(new BcPublicKeyKeyEncryptionMethodGenerator(encKey));
+			OutputStream encOut = encGen.open(out, compressedData.length);
+			encOut.write(compressedData);
+			encOut.close();
+			out.close();
+			return bOut.toByteArray();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
 	private byte[] compress(byte[] clearData, int algorithm) throws IOException {
---
+++ email/src/main/java/io/openex/email/attachment/EmailDownloader.java
@@ -0,0 +1,49 @@
+package io.openex.email.attachment;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.commons.io.IOUtils;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Julien on 19/12/2016.
+ */
+@SuppressWarnings({"PackageAccessibility", "WeakerAccess"})
+public class EmailDownloader {
+	
+	public static final String X_AUTH_TOKEN = "X-Auth-Token";
+	public static final String FILE_NAME = "file_name";
+	public static final String FILE_URL = "file_url";
+	public static final String ATTACHMENTS = "attachments";
+	public static final String ATTACHMENTS_CONTENT = "attachments_content";
+	
+	@SuppressWarnings({"unused", "unchecked"})
+	public void process(Exchange exchange) {
+		Message in = exchange.getIn();
+		List attachments = in.getHeader(ATTACHMENTS, List.class);
+		if (attachments != null) {
+			List<EmailAttachment> filesContent = (List) exchange.getProperty(ATTACHMENTS_CONTENT, new ArrayList<>());
+			for (Object attachment : attachments) {
+				Map attachmentMap = (Map) attachment;
+				String file_name = attachmentMap.get(FILE_NAME).toString();
+				String file_url = attachmentMap.get(FILE_URL).toString();
+				try {
+					String openexToken = in.getHeader(X_AUTH_TOKEN, String.class);
+					HttpURLConnection urlConnection = (HttpURLConnection) new URL(file_url).openConnection();
+					urlConnection.setRequestProperty(X_AUTH_TOKEN, openexToken);
+					byte[] content = IOUtils.toByteArray(urlConnection.getInputStream());
+					filesContent.add(new EmailAttachment(file_name, content, urlConnection.getContentType()));
+					
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			}
+			exchange.setProperty(ATTACHMENTS_CONTENT, filesContent);
+		}
+	}
+}
