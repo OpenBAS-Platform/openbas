9b60ee890d438f47d7d6d0ffe6123e2a7c047712
Samuel Hassine <samuel@hassine.fr>
2017-01-05 21:25:21 +0100

    In progress

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -2,6 +2,7 @@ import React, {Component, PropTypes} from 'react'
 import {ONE_SECOND, dateFromNow, now, parse} from '../utils/Time'
 import Rx from 'rxjs/Rx'
 import {injectIntl} from 'react-intl'
+import {T} from '../components/I18n'
 import countdown from 'countdown'
 import * as Constants from '../constants/ComponentTypes'
 import {i18nRegister} from '../utils/Messages'
@@ -11,6 +12,7 @@ i18nRegister({
     ' millisecond| second| minute| hour| day| week| month| year| decade| century| millennium': ' ms| s| m| h| j| s| m| a| d| s| m',
     ' milliseconds| seconds| minutes| hours| days| weeks| months| years| decades| centuries| millennia': ' ms| s| m| h| j| s| m| a| d| s| m',
     'now': 'maintenant',
+    'in progress': 'en cours'
   },
   en: {
     ' millisecond| second| minute| hour| day| week| month| year| decade| century| millennium': ' ms| s| m| h| d| w| m| y| d| c| m',
@@ -46,19 +48,21 @@ class Countdown extends Component {
     const initialStream = Rx.Observable.of(1);
     const intervalStream = Rx.Observable.interval(ONE_SECOND)
     this.subscription = initialStream
-        .merge(intervalStream)
-        .do(() => this.setState({startDate: new Date()}))
-        .subscribe()
+      .merge(intervalStream)
+      .do(() => this.setState({startDate: new Date()}))
+      .subscribe()
   }
 
   componentWillUnmount() {
-      this.subscription.unsubscribe()
+    this.subscription.unsubscribe()
   }
 
   render() {
-      if(!now().isAfter(parse(this.props.targetDate))) {
-          return <span style={styles[this.props.type]}>({dateFromNow(this.props.targetDate)})</span>
-      }
+    if (now().isAfter(parse(this.props.targetDate))) {
+      return <span style={styles[this.props.type]}>(<T>in progress</T>)</span>
+    } else {
+      return <span style={styles[this.props.type]}>({dateFromNow(this.props.targetDate)})</span>
+    }
   }
 }
 
