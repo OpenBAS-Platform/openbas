ee9f1b5e4d43d4180661babe801c4ec7f35c7328
Julien Richard <richard.julien@gmail.com>
2017-01-19 18:36:40 +0100

    Remove auth token from route to handle it directly in file downloader

--- email/src/main/java/io/openex/email/attachment/EmailDownloader.java
+++ email/src/main/java/io/openex/email/attachment/EmailDownloader.java
@@ -1,27 +1,24 @@
 package io.openex.email.attachment;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.*;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.commons.io.IOUtils;
-
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Julien on 19/12/2016.
  */
 @SuppressWarnings({"PackageAccessibility", "WeakerAccess"})
 public class EmailDownloader {
-	
+
 	public static final String AUTHORIZATION = "Authorization";
 	public static final String FILE_NAME = "file_name";
 	public static final String FILE_URL = "file_url";
 	public static final String ATTACHMENTS = "attachments";
 	public static final String ATTACHMENTS_CONTENT = "attachments_content";
-	
+
 	@SuppressWarnings({"unused", "unchecked"})
 	public void process(Exchange exchange) {
 		Message in = exchange.getIn();
@@ -33,12 +30,12 @@ public class EmailDownloader {
 				String file_name = attachmentMap.get(FILE_NAME).toString();
 				String file_url = attachmentMap.get(FILE_URL).toString();
 				try {
-					String openexToken = in.getHeader(AUTHORIZATION, String.class);
-					HttpURLConnection urlConnection = (HttpURLConnection) new URL(file_url).openConnection();
+                    String openexToken = exchange.getContext().resolvePropertyPlaceholders("openex.token");
+                    HttpURLConnection urlConnection = (HttpURLConnection) new URL(file_url).openConnection();
 					urlConnection.setRequestProperty(AUTHORIZATION, openexToken);
 					byte[] content = IOUtils.toByteArray(urlConnection.getInputStream());
 					filesContent.add(new EmailAttachment(file_name, content, urlConnection.getContentType()));
-					
+
 				} catch (Exception e) {
 					throw new RuntimeException(e);
 				}
