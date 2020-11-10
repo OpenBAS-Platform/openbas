53e285ed1ae615ef82d38d366c904e9e7660fe6c
Samuel Hassine <samuel@hassine.fr>
2020-11-10 10:33:31 +0100

    [all] Reorganize the repository

--- worker/management/src/main/java/io/openex/management/contract/ContractCardinality.java
+++
@@ -1,9 +0,0 @@
-package io.openex.management.contract;
-
-import com.google.gson.annotations.SerializedName;
-
-@SuppressWarnings("PackageAccessibility")
-public enum ContractCardinality {
-	@SerializedName("1") One,
-	@SerializedName("n") Multiple
-}
