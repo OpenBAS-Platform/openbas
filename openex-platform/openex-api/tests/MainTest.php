ab2939609206aa490418ffdb63b1ddd221ebf3a3
Samuel Hassine <samuel@hassine.fr>
2020-11-12 00:53:37 +0100

    [api] Introduce integration tests

---
+++ openex-platform/openex-api/tests/MainTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class MainTest extends TestCase
+{
+    public function test42()
+    {
+        $this->assertEquals(42, 42);
+    }
+}
