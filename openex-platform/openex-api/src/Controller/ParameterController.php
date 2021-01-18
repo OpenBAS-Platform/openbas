d30207438e4addc1394da46f2a619265ba7ee2cf
Samuel Hassine <samuel@hassine.fr>
2021-01-19 00:47:59 +0100

    [frontend] Delete components

--- openex-platform/openex-api/src/Controller/ParameterController.php
+++ openex-platform/openex-api/src/Controller/ParameterController.php
@@ -3,11 +3,9 @@
 namespace App\Controller;
 
 use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\View\View;
 use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class ParameterController extends AbstractController
 {
@@ -21,20 +19,6 @@ class ParameterController extends AbstractController
      */
     public function getParametersAction(Request $request)
     {
-    }
-
-    /**
-     * @OA\Property(description="Update a parameter")
-     *
-     * @Rest\View(serializerGroups={"parameter"})
-     * @Rest\Put("/parameters/{parameter_id}")
-     */
-    public function updateExerciseAction(Request $request)
-    {
-    }
-
-    private function parameterNotFound()
-    {
-        return View::create(['message' => 'Parameter not found'], Response::HTTP_NOT_FOUND);
+        return ["test" => "test"];
     }
 }
