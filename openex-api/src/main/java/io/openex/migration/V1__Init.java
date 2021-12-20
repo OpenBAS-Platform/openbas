892f68551cce216032dd02960f69cea58936babc
Julien Richard <richard.julien@gmail.com>
2021-12-20 21:30:09 +0100

    [api] Improve new platform boostrap

--- openex-api/src/main/java/io/openex/migration/V1__Init.java
+++ openex-api/src/main/java/io/openex/migration/V1__Init.java
@@ -8,21 +8,19 @@ import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.UUID;
 
 
 @Component
 public class V1__Init extends BaseJavaMigration {
 
+    private static final String ADMIN_EMAIL = "admin@openex.io";
     private static final String ADMIN_UUID = "89206193-dbfb-4513-a186-d72c037dda4c";
     private static final String ADMIN_TOKEN_UUID = "0d17ce9a-f3a8-4c6d-9721-c98dc3dc023f";
     private static final String ADMIN_PASSWORD = "admin";
 
-    private boolean createAdminUser(Connection connection) throws Exception {
+    private void createAdminUser(Connection connection) throws Exception {
         Argon2PasswordEncoder passwordEncoder = new Argon2PasswordEncoder();
         String password = passwordEncoder.encode(ADMIN_PASSWORD);
         PreparedStatement statement = connection.prepareStatement(
@@ -31,15 +29,15 @@ public class V1__Init extends BaseJavaMigration {
                         "user_password, user_status, user_admin, user_planificateur) " +
                         "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?) ON CONFLICT DO NOTHING ");
         statement.setString(1, ADMIN_UUID);
-        statement.setString(2, "admin@openex.io");
-        statement.setString(3, "admin@openex.io");
+        statement.setString(2, ADMIN_EMAIL);
+        statement.setString(3,ADMIN_EMAIL);
         statement.setString(4, "admin");
         statement.setString(5, "openex");
         statement.setString(6, password);
         statement.setInt(7, 1);
         statement.setBoolean(8, true);
         statement.setBoolean(9, true);
-        return statement.execute();
+        statement.execute();
     }
 
     private void createAdminToken(Connection connection) throws Exception {
@@ -62,9 +60,13 @@ public class V1__Init extends BaseJavaMigration {
         InputStream inputStream = classPathResource.getInputStream();
         String schemaQuery = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
         schemaStatement.execute(schemaQuery);
-        // Create the default values (admin user, ...)
-        boolean isUserCreated = createAdminUser(connection);
-        if (isUserCreated) {
+        // Looking for an already existing administrator
+        String selectAdmin = "SELECT user_id FROM users WHERE user_email = '" + ADMIN_EMAIL + "'";
+        ResultSet rs = connection.createStatement().executeQuery(selectAdmin);
+        // If admin doesn't exist, create one
+        if (!rs.next()) {
+            // Create the default values (admin user, ...)
+            createAdminUser(connection);
             createAdminToken(connection);
         }
     }
