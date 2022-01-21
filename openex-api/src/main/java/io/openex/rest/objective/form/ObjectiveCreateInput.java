d03ba37e65970d08c75920e68137eb0c3fb02d9d
Julien Richard <richard.julien@gmail.com>
2022-01-22 00:12:45 +0100

    [api] Fix objective create input

--- openex-api/src/main/java/io/openex/rest/objective/form/ObjectiveCreateInput.java
+++ openex-api/src/main/java/io/openex/rest/objective/form/ObjectiveCreateInput.java
@@ -4,14 +4,36 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ObjectiveCreateInput {
 
-    @JsonProperty("evaluation_score")
-    private Long score;
+    @JsonProperty("objective_title")
+    private String title;
 
-    public Long getScore() {
-        return score;
+    @JsonProperty("objective_description")
+    private String description;
+
+    @JsonProperty("objective_priority")
+    private Short priority;
+
+    public String getTitle() {
+        return title;
     }
 
-    public void setScore(Long score) {
-        this.score = score;
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Short getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Short priority) {
+        this.priority = priority;
     }
 }
