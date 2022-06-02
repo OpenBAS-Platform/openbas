7e2afc03ebf8f60250ba6d2d4ed630d854f51ba4
Julien Richard <julien.richard@filigran.io>
2022-06-02 10:09:45 +0200

    [api] Rename injector from API to HTTP (#132)

---
+++ openex-framework/src/main/java/io/openex/model/PairModel.java
@@ -0,0 +1,39 @@
+package io.openex.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PairModel {
+
+    @JsonProperty("key")
+    private String key;
+
+    @JsonProperty("value")
+    private String value;
+
+    public PairModel() {
+        // Default constructor
+    }
+
+    public PairModel(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
