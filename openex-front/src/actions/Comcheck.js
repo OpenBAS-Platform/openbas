6b00c065473e4f322eea45680e1a957a6fdc119a
Julien Richard <julien.richard@filigran.io>
2022-07-11 17:09:07 +0200

    [front] Fix linting

--- openex-front/src/actions/Comcheck.js
+++ openex-front/src/actions/Comcheck.js
@@ -1,10 +1,5 @@
 import * as schema from './Schema';
-import {
-  getReferential,
-  postReferential,
-  delReferential,
-} from '../utils/Action';
-import {mediaReader} from "./Schema";
+import { getReferential, postReferential, delReferential } from '../utils/Action';
 
 export const fetchComchecks = (exerciseId) => (dispatch) => {
   const uri = `/api/exercises/${exerciseId}/comchecks`;
