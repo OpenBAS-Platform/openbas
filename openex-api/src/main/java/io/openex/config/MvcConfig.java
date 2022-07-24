65bd4db26a11d1db69220f2de21573bc73d8ba44
Julien Richard <julien.richard@filigran.io>
2022-07-24 15:48:33 +0200

    [api] Replace SpringFox with SpringDoc (#163)

--- openex-api/src/main/java/io/openex/config/MvcConfig.java
+++ openex-api/src/main/java/io/openex/config/MvcConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -55,6 +56,7 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> messageConverters) {
+        messageConverters.add(new StringHttpMessageConverter());
         messageConverters.add(customJackson2HttpMessageConverter());
     }
 
