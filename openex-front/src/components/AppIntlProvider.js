fe492e7ddd487920ecd284096ec6698dd1e4da40
Samuel Hassine <samuel.hassine@filigran.io>
2022-09-17 21:42:38 +0200

    [frontend] Bump all libraries

--- openex-front/src/components/AppIntlProvider.js
+++ openex-front/src/components/AppIntlProvider.js
@@ -2,17 +2,21 @@ import React, { useEffect } from 'react';
 import * as PropTypes from 'prop-types';
 import { IntlProvider } from 'react-intl';
 import moment from 'moment';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers';
 import frLocale from 'date-fns/locale/fr';
 import enLocale from 'date-fns/locale/en-US';
+import esLocale from 'date-fns/locale/es';
+import cnLocale from 'date-fns/locale/zh-CN';
 import locale, { DEFAULT_LANG } from '../utils/BrowserLanguage';
 import i18n from '../utils/Localization';
 import { useHelper } from '../store';
 
 const localeMap = {
-  en: enLocale,
-  fr: frLocale,
+  'en-us': enLocale,
+  'fr-fr': frLocale,
+  'es-es': esLocale,
+  'zg-cn': cnLocale,
 };
 
 const AppIntlProvider = (props) => {
@@ -29,7 +33,7 @@ const AppIntlProvider = (props) => {
   });
   const baseMessages = i18n.messages[lang] || i18n.messages[DEFAULT_LANG];
   if (lang === 'fr') {
-    moment.locale('fr');
+    moment.locale('fr-fr');
   } else {
     moment.locale('en-us');
   }
@@ -50,7 +54,7 @@ const AppIntlProvider = (props) => {
     >
       <LocalizationProvider
         dateAdapter={AdapterDateFns}
-        locale={localeMap[locale]}
+        adapterLocale={localeMap[lang]}
       >
         {children}
       </LocalizationProvider>
--- openex-front/src/components/DateTimePicker.js
+++ openex-front/src/components/DateTimePicker.js
@@ -1,37 +1,49 @@
 import React from 'react';
+import { useIntl } from 'react-intl';
 import TextField from '@mui/material/TextField';
-import KeyboardDateTimePicker from '@mui/lab/DateTimePicker';
+import { DateTimePicker as MuiDateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { Field } from 'react-final-form';
+
+const dateFormatsMap = {
+  'en-us': 'yyyy-MM-dd',
+  'fr-fr': 'dd/MM/yyyy',
+  'es-es': 'dd/MM/yyyy',
+  'zg-cn': 'yyyy-MM-dd',
+};
 
 const renderDateTimePicker = ({
   input: { onBlur, value, ...inputProps },
   meta: { submitting, error, touched },
   textFieldProps,
   ...others
-}) => (
-  <KeyboardDateTimePicker
-    {...inputProps}
-    {...others}
-    ampm={false}
-    format="yyyy-MM-dd HH:mm:ss"
-    value={value ? new Date(value) : null}
-    disabled={submitting}
-    onBlur={() => onBlur(value ? new Date(value).toISOString() : null)}
-    error={error && touched}
-    onChange={(date) => (Date.parse(date)
-      ? inputProps.onChange(date.toISOString())
-      : inputProps.onChange(null))
-    }
-    renderInput={(props) => (
-      <TextField
-        {...props}
-        {...textFieldProps}
-        error={Boolean(touched && error)}
-        helperText={touched && error}
-      />
-    )}
-  />
-);
+}) => {
+  const intl = useIntl();
+  return (
+    <MuiDateTimePicker
+      {...inputProps}
+      {...others}
+      ampm={false}
+      format="yyyy-MM-dd HH:mm:ss"
+      value={value ? new Date(value) : null}
+      disabled={submitting}
+      onBlur={() => onBlur(value ? new Date(value).toISOString() : null)}
+      inputFormat={dateFormatsMap[intl.locale] || 'yyyy-MM-dd'}
+      error={error && touched}
+      onChange={(date) => (Date.parse(date)
+        ? inputProps.onChange(date.toISOString())
+        : inputProps.onChange(null))
+      }
+      renderInput={(props) => (
+        <TextField
+          {...props}
+          {...textFieldProps}
+          error={Boolean(touched && error)}
+          helperText={touched && error}
+        />
+      )}
+    />
+  );
+};
 
 export const DateTimePicker = (props) => (
   <Field name={props.name} component={renderDateTimePicker} {...props} />
