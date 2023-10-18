5b07daef108dca2e112f33a857299ccb055994e1
Guillaume Paris <guillaume.paris@filigran.io>
2023-10-18 15:50:18 +0200

    [backend] Add phone in the user execution context (#320)

--- openex-framework/src/main/java/io/openex/execution/ExecutionContext.java
+++ openex-framework/src/main/java/io/openex/execution/ExecutionContext.java
@@ -10,57 +10,58 @@ import java.util.List;
 
 public class ExecutionContext extends HashMap<String, Object> {
 
-    public static final String USER = "user";
-    public static final String EXERCISE = "exercise";
-    public static final String AUDIENCES = "audiences";
-    public static final String COMCHECK = "comcheck";
-    public static final String PLAYER_URI = "player_uri";
-    public static final String CHALLENGES_URI = "challenges_uri";
-    public static final String SCOREBOARD_URI = "scoreboard_uri";
-    public static final String LESSONS_URI = "lessons_uri";
+  public static final String USER = "user";
+  public static final String EXERCISE = "exercise";
+  public static final String AUDIENCES = "audiences";
+  public static final String COMCHECK = "comcheck";
+  public static final String PLAYER_URI = "player_uri";
+  public static final String CHALLENGES_URI = "challenges_uri";
+  public static final String SCOREBOARD_URI = "scoreboard_uri";
+  public static final String LESSONS_URI = "lessons_uri";
 
-    private ExecutionContext(User user, Exercise exercise, List<String> audiences) {
-        User protectUser = new User();
-        protectUser.setId(user.getId());
-        protectUser.setEmail(user.getEmail());
-        protectUser.setFirstname(user.getFirstname());
-        protectUser.setLastname(user.getLastname());
-        protectUser.setLang(user.getLang());
-        this.put(USER, protectUser);
-        this.put(EXERCISE, exercise);
-        this.put(AUDIENCES, audiences);
-    }
+  private ExecutionContext(User user, Exercise exercise, List<String> audiences) {
+    User protectUser = new User();
+    protectUser.setId(user.getId());
+    protectUser.setEmail(user.getEmail());
+    protectUser.setFirstname(user.getFirstname());
+    protectUser.setLastname(user.getLastname());
+    protectUser.setLang(user.getLang());
+    protectUser.setPhone(user.getPhone());
+    this.put(USER, protectUser);
+    this.put(EXERCISE, exercise);
+    this.put(AUDIENCES, audiences);
+  }
 
-    public ExecutionContext(OpenExConfig config, User user, Injection injection, List<String> audiences) {
-        this(user, injection.getExercise(), audiences);
-        if (injection.getExercise() != null) {
-            String exerciseId = injection.getExercise().getId();
-            String queryParams = "?user=" + user.getId() + "&inject=" + injection.getId();
-            this.put(PLAYER_URI, config.getBaseUrl() + "/private/" + exerciseId + queryParams);
-            this.put(CHALLENGES_URI, config.getBaseUrl() + "/challenges/" + exerciseId + queryParams);
-            this.put(SCOREBOARD_URI, config.getBaseUrl() + "/scoreboard/" + exerciseId + queryParams);
-            this.put(LESSONS_URI, config.getBaseUrl() + "/lessons/" + exerciseId + queryParams);
-        }
+  public ExecutionContext(OpenExConfig config, User user, Injection injection, List<String> audiences) {
+    this(user, injection.getExercise(), audiences);
+    if (injection.getExercise() != null) {
+      String exerciseId = injection.getExercise().getId();
+      String queryParams = "?user=" + user.getId() + "&inject=" + injection.getId();
+      this.put(PLAYER_URI, config.getBaseUrl() + "/private/" + exerciseId + queryParams);
+      this.put(CHALLENGES_URI, config.getBaseUrl() + "/challenges/" + exerciseId + queryParams);
+      this.put(SCOREBOARD_URI, config.getBaseUrl() + "/scoreboard/" + exerciseId + queryParams);
+      this.put(LESSONS_URI, config.getBaseUrl() + "/lessons/" + exerciseId + queryParams);
     }
+  }
 
-    public ExecutionContext(User user, Exercise exercise, String audience) {
-        this(user, exercise, List.of(audience));
-    }
+  public ExecutionContext(User user, Exercise exercise, String audience) {
+    this(user, exercise, List.of(audience));
+  }
 
-    public ExecutionContext(OpenExConfig baseUri, User user, Injection injection, String audience) {
-        this(baseUri, user, injection, List.of(audience));
-    }
+  public ExecutionContext(OpenExConfig baseUri, User user, Injection injection, String audience) {
+    this(baseUri, user, injection, List.of(audience));
+  }
 
-    public User getUser() {
-        return (User) this.get(USER);
-    }
+  public User getUser() {
+    return (User) this.get(USER);
+  }
 
-    public List<String> getAudiences() {
-        //noinspection unchecked
-        return (List<String>) this.get(AUDIENCES);
-    }
+  public List<String> getAudiences() {
+    //noinspection unchecked
+    return (List<String>) this.get(AUDIENCES);
+  }
 
-    public Exercise getExercise() {
-        return (Exercise) this.get(EXERCISE);
-    }
+  public Exercise getExercise() {
+    return (Exercise) this.get(EXERCISE);
+  }
 }
