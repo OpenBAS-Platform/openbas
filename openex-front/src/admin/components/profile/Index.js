b73f2a59e0887b96b642db38e44ad19d61c63ee6
Romuald Lemesle <romuald.lemesle@filigran.io>
2023-10-20 09:44:15 +0200

    [front] Profile page : align style

--- openex-front/src/admin/components/profile/Index.js
+++ openex-front/src/admin/components/profile/Index.js
@@ -18,29 +18,8 @@ import { countryOption } from '../../../utils/Countries';
 const useStyles = makeStyles((theme) => ({
   paper: {
     padding: 20,
-    marginBottom: 40,
-  },
-  appBar: {
-    width: '100%',
-    zIndex: theme.zIndex.drawer + 1,
-  },
-  container: {
-    padding: 20,
-  },
-  logo: {
-    width: '40px',
-    cursor: 'pointer',
-  },
-  title: {
-    fontSize: 25,
-    marginLeft: 20,
-  },
-  toolbar: theme.mixins.toolbar,
-  documents: {
-    color: '#ffffff',
-    position: 'absolute',
-    top: 8,
-    right: 70,
+    marginBottom: 30,
+    borderRadius: 6,
   },
 }));
 
@@ -105,89 +84,84 @@ const Index = () => {
   )(user);
   const userToken = tokens.length > 0 ? R.head(tokens) : undefined;
   return (
-    <div className={classes.container}>
-      <div style={{ width: 800, margin: '0 auto' }}>
+    <div style={{ width: 800, margin: '0 auto' }}>
+      <Paper variant="outlined" className={classes.paper}>
+        <Typography variant="h1" style={{ marginBottom: 20 }}>
+          {t('Profile')}
+        </Typography>
+        <UserForm
+          organizations={R.values(organizationsMap)}
+          onSubmit={onUpdate}
+          initialValues={initialValues}
+        />
+      </Paper>
+      <Paper variant="outlined" className={classes.paper}>
+        <Typography variant="h1" style={{ marginBottom: 20 }}>
+          {t('Information')}
+        </Typography>
+        <ProfileForm
+          onSubmit={onUpdateInformation}
+          initialValues={initialValues}
+        />
+      </Paper>
+      {!initialValues.user_is_external
+        &&
         <Paper variant="outlined" className={classes.paper}>
-          <Typography variant="h5" style={{ marginBottom: 20 }}>
-            {t('Profile')}
+          <Typography variant="h1" style={{ marginBottom: 20 }}>
+            {t('Password')}
           </Typography>
-          <UserForm
-            organizations={R.values(organizationsMap)}
-            onSubmit={onUpdate}
-            initialValues={initialValues}
-          />
+          <PasswordForm onSubmit={onUpdatePassword} />
         </Paper>
-        <Paper variant="outlined" className={classes.paper}>
-          <Typography variant="h5" style={{ marginBottom: 20 }}>
-            {t('Information')}
+      }
+      <Paper variant="outlined" className={classes.paper}>
+        <Typography variant="h1" style={{ marginBottom: 20 }}>
+          {t('API access')}
+        </Typography>
+        <Typography variant="body1">
+          {t(
+            'The OpenEx API relies on the REST standard. The token must be passed into the HTTP header',
+          )}{' '}
+          <strong>Authorization</strong>.
+          <Typography
+            variant="h4"
+            gutterBottom={true}
+            style={{ marginTop: 20 }}
+          >
+            {t('Token key')}
           </Typography>
-          <ProfileForm
-            onSubmit={onUpdateInformation}
-            initialValues={initialValues}
-          />
-        </Paper>
-        {!initialValues.user_is_external
-          &&
-          <Paper variant="outlined" className={classes.paper}>
-            <Typography variant="h5" style={{ marginBottom: 20 }}>
-              {t('Password')}
-            </Typography>
-            <PasswordForm onSubmit={onUpdatePassword} />
-          </Paper>
-        }
-        <Paper variant="outlined" className={classes.paper}>
-          <Typography variant="h5" style={{ marginBottom: 20 }}>
-            {t('API access')}
-          </Typography>
-          <Typography variant="body1">
-            {t(
-              'The OpenEx API relies on the REST standard. The token must be passed into the HTTP header',
-            )}{' '}
-            <strong>Authorization</strong>.
-            <Typography
-              variant="h6"
-              gutterBottom={true}
-              style={{ marginTop: 20 }}
-            >
-              {t('Token key')}
-            </Typography>
-            <pre>{userToken?.token_value}</pre>
-            <Button
-              variant="contained"
-              color="primary"
-              component="a"
-              onClick={() => onRenew(userToken?.token_id)}
-            >
-              {t('RENEW')}
-            </Button>
-            <Typography
-              style={{ marginTop: 20 }}
-              variant="h6"
-              gutterBottom={true}
-            >
-              {t('Example')}
-            </Typography>
-            <pre>
-              GET /api/exercises
-              <br />
-              Content-Type: application/json
-              <br />
-              Authorization: Bearer {userToken?.token_value}
-            </pre>
-          </Typography>
-          <Typography variant="h6" gutterBottom={true}>
-            {t('Documentation')}
-          </Typography>
+          <pre>{userToken?.token_value}</pre>
           <Button
             variant="contained"
             color="primary"
             component="a"
-            href="/swagger-ui/index.html"
+            onClick={() => onRenew(userToken?.token_id)}
           >
-            {t('API specifications')}
+            {t('RENEW')}
           </Button>
-        </Paper>
-      </div>
+          <Typography
+            variant="h4"
+            gutterBottom={true}
+            style={{ marginTop: 20 }}
+          >
+            {t('Example')}
+          </Typography>
+          <pre>
+            GET /api/exercises
+            <br />
+            Content-Type: application/json
+            <br />
+            Authorization: Bearer {userToken?.token_value}
+          </pre>
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          component="a"
+          href="/swagger-ui/index.html"
+        >
+          {t('API specifications')}
+        </Button>
+      </Paper>
     </div>
   );
 };
