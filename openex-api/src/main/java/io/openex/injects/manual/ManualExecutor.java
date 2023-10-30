0805de6e93b79ceb5a68b24b1fdf69ca7db00411
Romuald Lemesle <romuald.lemesle@filigran.io>
2023-10-30 18:14:54 +0100

    [api] Add Jacoco

--- openex-api/src/main/java/io/openex/injects/manual/ManualExecutor.java
+++ openex-api/src/main/java/io/openex/injects/manual/ManualExecutor.java
@@ -1,9 +1,9 @@
 package io.openex.injects.manual;
 
 import io.openex.contract.Contract;
-import io.openex.execution.Injector;
-import io.openex.execution.ExecutableInject;
 import io.openex.database.model.Execution;
+import io.openex.execution.ExecutableInject;
+import io.openex.execution.Injector;
 import io.openex.model.Expectation;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +12,8 @@ import java.util.List;
 @Component(ManualContract.TYPE)
 public class ManualExecutor extends Injector {
 
-    @Override
-    public List<Expectation> process(Execution execution, ExecutableInject injection, Contract contract) {
-        throw new UnsupportedOperationException("Manual inject cannot be executed");
-    }
+  @Override
+  public List<Expectation> process(Execution execution, ExecutableInject injection, Contract contract) {
+    throw new UnsupportedOperationException("Manual inject cannot be executed");
+  }
 }
