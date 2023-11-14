04615aee826b7e77fab2698c018f1ef79aa25375
Guillaume Paris <guillaume.paris@filigran.io>
2023-11-14 16:07:07 +0100

    [frontend] fix useAppDispatch to make ts work with thunk

--- openex-front/src/store.d.ts
+++
@@ -1,5 +0,0 @@
-import { useDispatch } from 'react-redux';
-
-// TODO upgrade redux to make it correctly work https://redux.js.org/usage/usage-with-typescript
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const useAppDispatch: any = useDispatch;
