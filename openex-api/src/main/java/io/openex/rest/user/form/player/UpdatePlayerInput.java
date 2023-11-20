7673f935232077b84384d90dbb941099d6ca7583
Romuald Lemesle <romuald.lemesle@filigran.io>
2023-11-20 17:53:48 +0100

    [backend] Reorganize class

--- openex-api/src/main/java/io/openex/rest/user/form/player/UpdatePlayerInput.java
+++ openex-api/src/main/java/io/openex/rest/user/form/player/UpdatePlayerInput.java
@@ -4,33 +4,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import java.util.ArrayList;
-import java.util.List;
-
-import static io.openex.config.AppConfig.EMAIL_FORMAT;
-
 @Getter
 @Setter
-public class UpdatePlayerInput {
-
-  @Email(message = EMAIL_FORMAT)
-  @NotBlank
-  @JsonProperty("user_email")
-  private String email;
-
-  @JsonProperty("user_firstname")
-  private String firstname;
-
-  @JsonProperty("user_lastname")
-  private String lastname;
-
-  @JsonProperty("user_organization")
-  private String organizationId;
-
-  @JsonProperty("user_country")
-  private String country;
+public class UpdatePlayerInput extends CreatePlayerInput {
 
   @JsonProperty("user_phone")
   private String phone;
@@ -40,8 +16,5 @@ public class UpdatePlayerInput {
 
   @JsonProperty("user_pgp_key")
   private String pgpKey;
-
-  @JsonProperty("user_tags")
-  private List<String> tagIds = new ArrayList<>();
 
 }
