bcfb51df3dd8444789e945c03eeb5a9900a36f00
Guillaume Paris <guillaume.paris@filigran.io>
2023-12-04 10:00:10 +0100

    [devex] tranform builder files to module

--- openex-front/builder/dev/dev.js
+++ openex-front/builder/dev/dev.js
@@ -1,12 +1,19 @@
-const express = require("express");
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const { readFileSync } = require("node:fs");
-const path = require("node:path");
-const esbuild = require("esbuild");
-const chokidar = require("chokidar");
-const compression = require("compression");
+import express, { static as expressStatic } from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
+import { context } from 'esbuild';
+import { watch } from 'chokidar';
+import compression from 'compression';
 
-const basePath = "";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const basePath = '';
 const clients = [];
 const debounce = (func, timeout = 500) => {
   let timer;
@@ -19,41 +26,39 @@ const debounce = (func, timeout = 500) => {
 };
 
 // Start with an initial build
-esbuild
-  .context({
-    logLevel: "info",
-    entryPoints: ["src/index.tsx"],
-    bundle: true,
-    banner: {
-      js: ' (() => new EventSource("http://localhost:3000/dev").onmessage = () => location.reload())();',
-    },
-    loader: {
-      ".js": "jsx",
-      ".svg": "file",
-      ".png": "file",
-      ".woff": "dataurl",
-      ".woff2": "dataurl",
-      ".ttf": "dataurl",
-      ".eot": "dataurl",
-    },
-    assetNames: "media/[name]-[hash]",
-    target: ["chrome58"],
-    minify: false,
-    keepNames: true,
-    sourcemap: true,
-    sourceRoot: "src",
-    outdir: "builder/dev/build",
-  })
+context({
+  logLevel: 'info',
+  entryPoints: ['src/index.tsx'],
+  bundle: true,
+  banner: {
+    js: ' (() => new EventSource("http://localhost:3000/dev").onmessage = () => location.reload())();',
+  },
+  loader: {
+    '.js': 'jsx',
+    '.svg': 'file',
+    '.png': 'file',
+    '.woff': 'dataurl',
+    '.woff2': 'dataurl',
+    '.ttf': 'dataurl',
+    '.eot': 'dataurl',
+  },
+  assetNames: 'media/[name]-[hash]',
+  target: ['chrome58'],
+  minify: false,
+  keepNames: true,
+  sourcemap: true,
+  sourceRoot: 'src',
+  outdir: 'builder/dev/build',
+})
   .then(async (builder) => {
     await builder.rebuild();
     // Listen change for hot recompile
-    chokidar
-      .watch("src/**/*.{js,jsx,ts,tsx}", {
-        awaitWriteFinish: true,
-        ignoreInitial: true,
-      })
+    watch('src/**/*.{js,jsx,ts,tsx}', {
+      awaitWriteFinish: true,
+      ignoreInitial: true,
+    })
       .on(
-        "all",
+        'all',
         debounce(() => {
           const start = new Date().getTime();
           console.log(`[HOT RELOAD] Update of front detected`);
@@ -62,88 +67,88 @@ esbuild
             .then(() => {
               const time = new Date().getTime() - start;
               console.log(
-                `[HOT RELOAD] Rebuild done in ${time} ms, updating frontend`
+                `[HOT RELOAD] Rebuild done in ${time} ms, updating frontend`,
               );
-              clients.forEach((res) => res.write("data: update\n\n"));
+              clients.forEach((res) => res.write('data: update\n\n'));
               clients.length = 0;
             })
             .catch((error) => {
               console.error(error);
             });
-        })
+        }),
       );
     // Start a dev web server
     const app = express();
-    app.set("trust proxy", 1);
-    app.get("/dev", (req, res) => {
+    app.set('trust proxy', 1);
+    app.get('/dev', (req, res) => {
       return clients.push(
         res.writeHead(200, {
-          "Content-Type": "text/event-stream",
-          "Cache-Control": "no-cache",
-          "Access-Control-Allow-Origin": "*",
-          Connection: "keep-alive",
-        })
+          'Content-Type': 'text/event-stream',
+          'Cache-Control': 'no-cache',
+          'Access-Control-Allow-Origin': '*',
+          Connection: 'keep-alive',
+        }),
       );
     });
     app.use(
-      createProxyMiddleware("/api", {
-        target: "http://localhost:8080",
+      createProxyMiddleware('/api', {
+        target: 'http://localhost:8080',
         changeOrigin: true,
         ws: true,
-      })
+      }),
     );
     app.use(
-      createProxyMiddleware("/login", {
-        target: "http://localhost:8080",
+      createProxyMiddleware('/login', {
+        target: 'http://localhost:8080',
         changeOrigin: true,
         ws: true,
-      })
+      }),
     );
     app.use(
-      createProxyMiddleware("/login", {
-        target: "http://localhost:8080",
+      createProxyMiddleware('/login', {
+        target: 'http://localhost:8080',
         changeOrigin: true,
         ws: true,
-      })
+      }),
     );
     app.use(
-      createProxyMiddleware("/logout", {
-        target: "http://localhost:8080",
+      createProxyMiddleware('/logout', {
+        target: 'http://localhost:8080',
         changeOrigin: true,
         ws: true,
-      })
+      }),
     );
     app.use(
-      createProxyMiddleware("/oauth2", {
-        target: "http://localhost:8080",
+      createProxyMiddleware('/oauth2', {
+        target: 'http://localhost:8080',
         changeOrigin: true,
         ws: true,
-      })
+      }),
     );
     app.use(
-      createProxyMiddleware("/saml2", {
-        target: "http://localhost:8080",
+      createProxyMiddleware('/saml2', {
+        target: 'http://localhost:8080',
         changeOrigin: true,
         ws: true,
-      })
+      }),
     );
     app.use(compression({}));
-    app.use(`/css`, express.static(path.join(__dirname, "./build")));
-    app.use(`/js`, express.static(path.join(__dirname, "./build")));
-    app.use(`/media`, express.static(path.join(__dirname, "./build/media")));
+    app.use(`/css`, expressStatic(join(__dirname, './build')));
+    app.use(`/js`, expressStatic(join(__dirname, './build')));
+    app.use(`/media`, expressStatic(join(__dirname, './build/media')));
     app.use(
       `/static`,
-      express.static(path.join(__dirname, "../public/static"))
+      expressStatic(join(__dirname, '../public/static')),
     );
-    app.get("*", (req, res) => {
-      const data = readFileSync(`${__dirname}/index.html`, "utf8");
+    app.get('*', (req, res) => {
+      const data = readFileSync(`${__dirname}/index.html`, 'utf8');
       const withOptionValued = data.replace(/%BASE_PATH%/g, basePath);
       res.header(
-        "Cache-Control",
-        "admin, no-cache, no-store, must-revalidate"
+        'Cache-Control',
+        'admin, no-cache, no-store, must-revalidate',
       );
-      res.header("Expires", "-1");
-      res.header("Pragma", "no-cache");
+      res.header('Expires', '-1');
+      res.header('Pragma', 'no-cache');
       return res.send(withOptionValued);
     });
     app.listen(3000);
