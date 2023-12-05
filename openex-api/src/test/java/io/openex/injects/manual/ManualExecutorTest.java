38465854cc3f6bc5f3f886f208d525339054bd87
Romuald Lemesle <romuald.lemesle@filigran.io>
2023-12-05 09:34:52 +0100

    [CI] Add codecov (#342)

---
+++ openex-api/src/test/java/io/openex/injects/manual/ManualExecutorTest.java
@@ -0,0 +1,27 @@
+package io.openex.injects.manual;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ManualExecutorTest {
+
+  @Autowired
+  private ManualExecutor manualExecutor;
+
+
+  @Test
+  void process() {
+    UnsupportedOperationException error = null;
+    try {
+      this.manualExecutor.process(null, null, null);
+    } catch (UnsupportedOperationException e) {
+      error = e;
+    }
+    Assertions.assertNotNull(error);
+    Assertions.assertEquals("Manual inject cannot be executed", error.getMessage());
+  }
+
+}
