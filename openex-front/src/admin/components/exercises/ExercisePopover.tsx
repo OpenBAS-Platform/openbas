45c7a44b4f4aeaff2cd18c0830421db470623afb
Romuald Lemesle <romu.lemesle@gmail.com>
2023-12-05 21:09:30 +0100

    Update ExercisePopover.tsx

--- openex-front/src/admin/components/exercises/ExercisePopover.tsx
+++ openex-front/src/admin/components/exercises/ExercisePopover.tsx
@@ -226,7 +226,7 @@ const ExercisePopover: FunctionComponent<ExercisePopoverProps> = ({
                   </TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell>{t('Variables with values')}</TableCell>
+                  <TableCell>{t('Variable values')}</TableCell>
                   <TableCell style={{ textAlign: 'center' }}>
                     <Checkbox
                       checked={exportVariableValues}
