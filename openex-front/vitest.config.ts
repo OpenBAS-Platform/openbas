fdfe8cefd817b69a3cc9a6441e20001746672331
Samuel Hassine <samuel.hassine@filigran.io>
2023-12-07 16:37:08 +0100

    [frontend] Remove relay vitest

--- openex-front/vitest.config.ts
+++ openex-front/vitest.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
-    setupFiles: 'src/__tests__/setup-relay-for-vitest.ts',
     include: ['src/__tests__/**/**/*.test.{ts,tsx}']
   },
 })
