58aa4cb8a82ebb30abf6d1bf4ecf1c75fd907625
Guillaume Paris <guillaume.paris@filigran.io>
2023-12-08 15:10:37 +0100

    [front] add jsdom for vitest to work

--- openex-front/vitest.config.ts
+++ openex-front/vitest.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
+// import jsdom to not let tools report them as unused
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import jsdom from 'jsdom';
+
 export default defineConfig({
   plugins: [react()],
   test: {
