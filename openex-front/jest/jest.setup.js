6ab1fbe052d2bff85dba2b53f46568f4b9837127
Guillaume <guillaume.paris@filigran.io>
2023-12-08 15:14:42 +0100

    [front] fix vite & delete useless jest files
    Co-authored-by: Romuald Lemesle <romuald.lemesle@filigran.io>

--- openex-front/jest/jest.setup.js
+++
@@ -1,8 +0,0 @@
-// Default timeout
-jest.setTimeout(100000);
-jest.mock('../src/actions/Application.js', () => {
-  return {
-    fetchMe: () => jest.fn(),
-    fetchParameters: () => jest.fn(),
-  };
-});
