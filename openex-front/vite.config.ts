a13dcec4549f18e219a7ab30da15e89ef75e1fca
Guillaume Paris <guillaume.paris@filigran.io>
2023-12-08 13:53:26 +0100

    [front] fix vite & delte useless jest files

--- openex-front/vite.config.ts
+++ openex-front/vite.config.ts
@@ -1,37 +1,29 @@
-import { defineConfig, transformWithEsbuild } from 'vite';
+import { createLogger, defineConfig, transformWithEsbuild } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+
+const logger = createLogger()
+const loggerError = logger.error
+
+logger.error = (msg, options) => {
+  // Ignore jsx syntax error as it taken into account in a custom plugin
+  if (msg.includes('The JSX syntax extension is not currently enabled')) return
+  loggerError(msg, options)
+}
+
+const basePath = "";
+
+const backProxy = {
+  target: 'http://localhost:8080',
+  changeOrigin: true,
+  ws: true,
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    {
-      name: 'treat-js-files-as-jsx',
-      async transform(code, id) {
-        if (!id.match(/src\/.*\.js$/)) return null;
+  build: {
+    target: ['chrome58'],
+  },
 
-        // Use the exposed transform from vite, instead of directly
-        // transforming with esbuild
-        return transformWithEsbuild(code, id, {
-          loader: 'tsx',
-          jsx: 'automatic',
-        });
-      },
-    },
-    {
-      name: 'asset-base-url',
-      enforce: 'post',
-      transform: (code) => {
-        code = code.replace(/(?<!local)(\/src|~?@|\/@fs\/@)\/(.*?)\.(svg|png)/g, 'src/$2.$3');
-        return {
-          code,
-          map: null,
-        };
-      },
-    },
-    react(),
-  ],
-
-  publicDir: 'builder/public',
   resolve: {
     extensions: ['.js', '.tsx', '.ts', '.jsx', '.json'],
   },
@@ -41,6 +33,36 @@ export default defineConfig({
       'ckeditor5-custom-build/build/ckeditor',
     ],
   },
+
+  customLogger: logger,
+
+  plugins: [
+    {
+    name: 'html-transform',
+    enforce: "pre",
+    apply: 'serve',
+    transformIndexHtml(html) {
+      return html.replace(/%BASE_PATH%/g, basePath)
+      .replace(/%APP_TITLE%/g, "OpenEx Dev")
+      .replace(/%APP_DESCRIPTION%/g, "OpenEx Development platform")
+      .replace(/%APP_FAVICON%/g, `${basePath}/static/ext/favicon.png`)
+      .replace(/%APP_MANIFEST%/g, `${basePath}/static/ext/manifest.json`)
+    }
+    },
+    {
+      name: 'treat-js-files-as-jsx',
+      async transform(code, id) {
+        if (!id.match(/src\/.*\.js$/)) return null;
+        // Use the exposed transform from vite, instead of directly
+        // transforming with esbuild
+        return transformWithEsbuild(code, id, {
+          loader: 'tsx',
+          jsx: 'automatic',
+        });
+      },
+    },
+    react(),
+  ],
 
   server: {
     port: 3000,
@@ -54,31 +76,11 @@ export default defineConfig({
       ],
     },
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        ws: true,
-      },
-      '/login': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        ws: true,
-      },
-      '/logout': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        ws: true,
-      },
-      '/oauth2': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        ws: true,
-      },
-      '/saml2': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        ws: true,
-      },
+      '/api': backProxy,
+      '/login':  backProxy,
+      '/logout':  backProxy,
+      '/oauth2':  backProxy,
+      '/saml2': backProxy,
     },
   },
 });
