14701be03d0395edf44e4e8429f1f367350db922
Romuald Lemesle <romuald.lemesle@filigran.io>
2024-01-03 14:12:06 +0100

    [backend] Rename migration

--- openex-api/src/main/java/io/openex/migration/V2_65__Assets_Asset_Groups.java
+++ openex-api/src/main/java/io/openex/migration/V2_65__Assets_Asset_Groups.java
@@ -8,7 +8,7 @@ import java.sql.Connection;
 import java.sql.Statement;
 
 @Component
-public class V2_64__Assets_Asset_Groups extends BaseJavaMigration {
+public class V2_65__Assets_Asset_Groups extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
