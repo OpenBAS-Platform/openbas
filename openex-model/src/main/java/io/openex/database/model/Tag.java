c97590589efbb69bde41991d3df8f13887b5d2c0
Romuald Lemesle <romuald.lemesle@filigran.io>
2024-01-08 15:49:21 +0100

    [backend] Use lombok

--- openex-model/src/main/java/io/openex/database/model/Tag.java
+++ openex-model/src/main/java/io/openex/database/model/Tag.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import io.openex.database.audit.ModelBaseListener;
 import io.openex.helper.MultiIdDeserializer;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.UuidGenerator;
 
 import jakarta.persistence.*;
@@ -16,78 +18,68 @@ import java.util.Objects;
 @Table(name = "tags")
 @EntityListeners(ModelBaseListener.class)
 public class Tag implements Base {
-    @Id
-    @Column(name = "tag_id")
-    @GeneratedValue(generator = "UUID")
+
+  @Setter
+  @Id
+  @Column(name = "tag_id")
+  @GeneratedValue(generator = "UUID")
     @UuidGenerator
-    @JsonProperty("tag_id")
-    private String id;
+  @JsonProperty("tag_id")
+  private String id;
 
-    @Column(name = "tag_name")
-    @JsonProperty("tag_name")
-    private String name;
+  @Getter
+  @Column(name = "tag_name")
+  @JsonProperty("tag_name")
+  private String name;
 
-    @Column(name = "tag_color")
-    @JsonProperty("tag_color")
-    private String color;
+  @Getter
+  @Column(name = "tag_color")
+  @JsonProperty("tag_color")
+  private String color;
 
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "documents_tags",
-            joinColumns = @JoinColumn(name = "tag_id"),
-            inverseJoinColumns = @JoinColumn(name = "document_id"))
-    @JsonSerialize(using = MultiIdDeserializer.class)
-    @JsonProperty("tags_documents")
-    private List<Document> documents = new ArrayList<>();
+  @Setter
+  @Getter
+  @ManyToMany(fetch = FetchType.LAZY)
+  @JoinTable(name = "documents_tags",
+      joinColumns = @JoinColumn(name = "tag_id"),
+      inverseJoinColumns = @JoinColumn(name = "document_id"))
+  @JsonSerialize(using = MultiIdDeserializer.class)
+  @JsonProperty("tags_documents")
+  private List<Document> documents = new ArrayList<>();
 
-    @JsonIgnore
-    @Override
-    public boolean isUserHasAccess(User user) {
-        return true;
+  @JsonIgnore
+  @Override
+  public boolean isUserHasAccess(User user) {
+    return true;
+  }
+
+  @Override
+  public String getId() {
+    return id;
+  }
+
+  public void setName(String name) {
+    this.name = name.toLowerCase();
+  }
+
+  public void setColor(String color) {
+    this.color = color.toLowerCase();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
-
-    @Override
-    public String getId() {
-        return id;
+    if (o == null || !Base.class.isAssignableFrom(o.getClass())) {
+      return false;
     }
+    Base base = (Base) o;
+    return id.equals(base.getId());
+  }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name.toLowerCase();
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color.toLowerCase();
-    }
-
-    public List<Document> getDocuments() {
-        return documents;
-    }
-
-    public void setDocuments(List<Document> documents) {
-        this.documents = documents;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || !Base.class.isAssignableFrom(o.getClass())) return false;
-        Base base = (Base) o;
-        return id.equals(base.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
 }
