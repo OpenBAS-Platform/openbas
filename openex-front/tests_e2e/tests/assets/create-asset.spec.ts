3c78924806c3f3dcc0c2471c447b949a02e8aa3c
Romuald Lemesle <romuald.lemesle@filigran.io>
2024-02-27 14:14:43 +0100

    [e2e] Lint

--- openex-front/tests_e2e/tests/assets/create-asset.spec.ts
+++ openex-front/tests_e2e/tests/assets/create-asset.spec.ts
@@ -1,27 +1,27 @@
+import { expect } from '@playwright/test';
 import { test } from '../../fixtures/baseFixtures';
 import appUrl from '../../utils/url';
 import LeftMenuPage from '../../model/left-menu.page';
 import AssetsPage from '../../model/assets/assets.page';
 import AssetFormPage from '../../model/assets/asset-form.page';
-import { expect } from '@playwright/test';
 import fillLinuxAssetBase from '../../fixtures/assetFixtures';
 
 test('Create an asset', async ({ page }) => {
   // -- PREPARE --
   await page.goto(appUrl());
 
-  let leftMenuPage = new LeftMenuPage(page);
+  const leftMenuPage = new LeftMenuPage(page);
   await leftMenuPage.goToAssets();
 
   // -- EXECUTE --
-  let assetsPage = new AssetsPage(page);
+  const assetsPage = new AssetsPage(page);
   await assetsPage.getAddButton().click();
 
-  let assetFormPage = new AssetFormPage(page);
+  const assetFormPage = new AssetFormPage(page);
   await fillLinuxAssetBase(page, 'My endpoint name', '192.168.255.255');
   await assetFormPage.getCreateButton().click();
 
   // -- ASSERT --
-  expect(!await assetFormPage.getCreateButton().isVisible());
-  expect(await page.getByText('My endpoint name', { exact: true }).isVisible());
+  await expect(assetFormPage.getCreateButton()).toBeHidden();
+  await expect(page.getByText('My endpoint name', { exact: true })).toBeVisible();
 });
