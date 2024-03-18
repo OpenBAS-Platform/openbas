a5c2f4664b481e413db9aadd4a6e38bfb6838f4c
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-03-18 23:50:23 +0100

    [frontend] Refact

--- openbas-front/src/admin/components/exercises/ExerciseParametersForm.tsx
+++ openbas-front/src/admin/components/exercises/ExerciseParametersForm.tsx
@@ -1,15 +1,15 @@
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button, TextField as MuiTextField } from '@mui/material';
-import React from 'react';
+import { Autocomplete, Button, Chip, TextField, TextField as MuiTextField } from '@mui/material';
+import React, { useState } from 'react';
 import { z } from 'zod';
 import { useFormatter } from '../../../components/i18n';
-import type { ExerciseParametersInput } from '../../../utils/api-types';
-import EmailListField from '../../../components/EmailListField';
+import { zodImplement } from '../../../utils/Zod';
+import { ExerciseUpdateInput } from '../../../utils/api-types';
 
 interface Props {
-  onSubmit: SubmitHandler<ExerciseParametersInput>;
-  initialValues?: ExerciseParametersInput;
+  onSubmit: SubmitHandler<ExerciseUpdateInput>;
+  initialValues?: ExerciseUpdateInput;
   disabled?: boolean;
 }
 
@@ -24,20 +24,27 @@ const ExerciseParametersForm: React.FC<Props> = ({
 }) => {
   // Standard hooks
   const { t } = useFormatter();
+  const [inputValue, setInputValue] = useState('');
 
   const {
     register,
     handleSubmit,
-    setValue,
-    formState: { isDirty, isSubmitting },
-  } = useForm<ExerciseParametersInput>({
+    control,
+    formState: { errors, isDirty, isSubmitting },
+  } = useForm<ExerciseUpdateInput>({
     mode: 'onTouched',
     resolver: zodResolver(
-      z.object({
-        exercise_mail_from: z.string().email(),
-        exercise_mails_reply_to: z.array(z.string()).optional(),
-        exercise_message_header: z.string().optional(),
-      }).strict(),
+      zodImplement<ExerciseUpdateInput>().with(
+        {
+          exercise_description: z.string().optional(),
+          exercise_message_footer: z.string().optional(),
+          exercise_name: z.string(),
+          exercise_subtitle: z.string().optional(),
+          exercise_mail_from: z.string().email().optional(),
+          exercise_mails_reply_to: z.array(z.string().email()).optional(),
+          exercise_message_header: z.string().optional(),
+        },
+      ),
     ),
     defaultValues: initialValues,
   });
@@ -48,14 +55,63 @@ const ExerciseParametersForm: React.FC<Props> = ({
         variant="standard"
         fullWidth
         label={t('Sender email address')}
+        error={!!errors.exercise_mail_from}
+        helperText={
+                    errors.exercise_mail_from && errors.exercise_mail_from?.message
+                }
         inputProps={register('exercise_mail_from')}
         disabled={disabled}
       />
-      <EmailListField
+      <Controller
+        control={control}
         name="exercise_mails_reply_to"
-        savedEmails={initialValues.exercise_mails_reply_to || []}
-        label={t('Reply to')}
-        setFieldValue={(value) => setValue('exercise_mails_reply_to', [value])}
+        render={({ field, fieldState }) => {
+          return (
+            <Autocomplete
+              multiple
+              id="email-input"
+              freeSolo
+              open={false}
+              options={[]}
+              value={field.value}
+              onChange={() => {
+                if (undefined !== field.value && inputValue !== '' && !field.value.includes(inputValue)) {
+                  field.onChange([...(field.value || []), inputValue]);
+                }
+              }}
+              onBlur={field.onBlur}
+              inputValue={inputValue}
+              onInputChange={(_event, newInputValue) => {
+                setInputValue(newInputValue);
+              }}
+              disableClearable={true}
+              renderTags={(tags: string[], getTagProps) => tags.map((email: string, index: number) => (
+                <Chip
+                  variant="outlined"
+                  label={email}
+                  {...getTagProps({ index })}
+                  key={email}
+                  onDelete={() => {
+                    const newValue = [...(field.value || [])];
+                    newValue.splice(index, 1);
+                    field.onChange(newValue);
+                  }
+                                    }
+                />
+              ))}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  variant="standard"
+                  label={t('Reply to')}
+                  style={{ marginTop: 20 }}
+                  error={!!fieldState.error}
+                  helperText={fieldState.error?.message}
+                />
+              )}
+            />
+          );
+        }}
       />
       <MuiTextField
         variant="standard"
@@ -64,6 +120,10 @@ const ExerciseParametersForm: React.FC<Props> = ({
         rows={1}
         label={t('Messages header')}
         style={{ marginTop: 20 }}
+        error={!!errors.exercise_message_header}
+        helperText={
+                    errors.exercise_message_header && errors.exercise_message_header?.message
+                }
         inputProps={register('exercise_message_header')}
         disabled={disabled}
       />
@@ -73,7 +133,7 @@ const ExerciseParametersForm: React.FC<Props> = ({
           color="primary"
           type="submit"
           disabled={!isDirty || isSubmitting}
-        >{ t('Update')}
+        >{t('Update')}
         </Button>
       </div>
     </form>
