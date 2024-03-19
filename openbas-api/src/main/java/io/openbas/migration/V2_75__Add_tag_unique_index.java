912f67f2bcf8ae08e1652cdec6f8682d7c09d4d7
Romuald Lemesle <romuald.lemesle@filigran.io>
2024-03-19 18:11:46 +0100

    Fix

--- openbas-api/src/main/java/io/openbas/migration/V2_75__Add_tag_unique_index.java
+++ openbas-api/src/main/java/io/openbas/migration/V2_75__Add_tag_unique_index.java
@@ -23,23 +23,36 @@ public class V2_75__Add_tag_unique_index extends BaseJavaMigration {
           """);
 
       // Deduplicate tag in other tables
-      deduplicateInTable(statement, "asset_groups_tags", "asset_group_id");
-      deduplicateInTable(statement, "assets_tags", "asset_id");
-      deduplicateInTable(statement, "challenges_tags", "challenge_id");
-      deduplicateInTable(statement, "documents_tags", "document_id");
-      deduplicateInTable(statement, "exercises_tags", "exercise_id");
-      deduplicateInTable(statement, "injects_tags", "inject_id");
-      deduplicateInTable(statement, "logs_tags", "log_id");
-      deduplicateInTable(statement, "organizations_tags", "organization_id");
-      deduplicateInTable(statement, "scenarios_tags", "scenario_id");
-      deduplicateInTable(statement, "teams_tags", "team_id");
-      deduplicateInTable(statement, "users_tags", "user_id");
+      deduplicateInTable(statement, "asset_groups_tags", "asset_groups_tags_pkey");
+      deduplicateInTable(statement, "assets_tags", "assets_tags_pkey");
+      deduplicateInTable(statement, "challenges_tags", "challenges_tags_pkey");
+      deduplicateInTable(statement, "documents_tags", "documents_tags_pkey");
+      deduplicateInTable(statement, "exercises_tags", "exercises_tags_pkey");
+      deduplicateInTable(statement, "injects_tags", "injects_tags_pkey");
+      deduplicateInTable(statement, "logs_tags", "logs_tags_pkey");
+      deduplicateInTable(statement, "organizations_tags", "organizations_tags_pkey");
+      deduplicateInTable(statement, "scenarios_tags", "scenarios_tags_pkey");
+      deduplicateInTable(statement, "teams_tags", "teams_tags_pkey");
+      deduplicateInTable(statement, "users_tags", "users_tags_pkey");
 
       // Remove duplicate tags in tags table
       statement.executeUpdate("""
             DELETE FROM tags
             WHERE tag_id NOT IN (SELECT uniq_id FROM uniq_tags);
           """);
+
+      // Recreate primary constraint
+      recreatePrimaryConstraint(statement, "asset_groups_tags", "asset_groups_tags_pkey", "asset_group_id");
+      recreatePrimaryConstraint(statement, "assets_tags", "assets_tags_pkey", "asset_id");
+      recreatePrimaryConstraint(statement, "challenges_tags", "challenges_tags_pkey", "challenge_id");
+      recreatePrimaryConstraint(statement, "documents_tags", "documents_tags_pkey", "document_id");
+      recreatePrimaryConstraint(statement, "exercises_tags", "exercises_tags_pkey", "exercise_id");
+      recreatePrimaryConstraint(statement, "injects_tags", "injects_tags_pkey", "inject_id");
+      recreatePrimaryConstraint(statement, "logs_tags", "logs_tags_pkey", "log_id");
+      recreatePrimaryConstraint(statement, "organizations_tags", "organizations_tags_pkey", "organization_id");
+      recreatePrimaryConstraint(statement, "scenarios_tags", "scenarios_tags_pkey", "scenario_id");
+      recreatePrimaryConstraint(statement, "teams_tags", "teams_tags_pkey", "team_id");
+      recreatePrimaryConstraint(statement, "users_tags", "users_tags_pkey", "user_id");
 
       // Add unicity
       statement.executeUpdate("""
@@ -53,7 +66,12 @@ public class V2_75__Add_tag_unique_index extends BaseJavaMigration {
     }
   }
 
-  private static void deduplicateInTable(Statement statement, String table, String keyId) throws SQLException {
+  private static void deduplicateInTable(Statement statement, String table, String constraintKey) throws SQLException {
+    // Remove primary key
+    statement.executeUpdate("""
+        ALTER TABLE """ + " ".concat(table).concat(" ") + """
+            DROP CONSTRAINT  """ + " ".concat(constraintKey).concat(" ") + """
+        """);
     // Set uniq tag
     statement.executeUpdate("""
         UPDATE """ + " ".concat(table).concat(" ") + """
@@ -61,16 +79,34 @@ public class V2_75__Add_tag_unique_index extends BaseJavaMigration {
         SET tag_id = uniq_tag.uniq_id
         FROM uniq_tags uniq_tag
         JOIN tags t ON uniq_tag.tag_name = t.tag_name
-        WHERE tableName.tag_id = t.tag_id
-          AND t.tag_id NOT IN (SELECT uniq_id FROM uniq_tags)
-          AND NOT EXISTS (
-            SELECT 1
-            FROM """ + " ".concat(table).concat(" ") + """
-            tableName2
-            WHERE tableName2.tag_id = uniq_tag.uniq_id
-              AND tableName2.""" + " ".concat(keyId).concat(" ") + """
-                = tableName.""" + " ".concat(keyId).concat(" ") + """
-          );
+        """);
+    // Remove duplicate line by create a new table with distinct
+    statement.executeUpdate("""
+        CREATE TABLE """ + " ".concat(table).concat("_temp") + """
+            AS
+          SELECT DISTINCT * FROM """ + " ".concat(table).concat(" ") + """
+        """);
+    statement.executeUpdate("""
+        DROP TABLE """ + " ".concat(table).concat(" ") + """
+        """);
+    statement.executeUpdate("""
+        ALTER TABLE """ + " ".concat(table).concat("_temp") + """
+          RENAME
+        TO """ + " ".concat(table).concat(" ") + """
         """);
   }
+
+  private static void recreatePrimaryConstraint(Statement statement, String table, String constraintKey, String key)
+      throws SQLException {
+    // Add primary key
+    statement.executeUpdate("""
+        ALTER TABLE """ + " ".concat(table).concat(" ") + """
+          ADD CONSTRAINT """ + " ".concat(constraintKey).concat(" ") + """
+          PRIMARY KEY (
+            """ + " ".concat(key).concat(" ") + """
+            , tag_id
+            );
+        """);
+  }
+
 }
