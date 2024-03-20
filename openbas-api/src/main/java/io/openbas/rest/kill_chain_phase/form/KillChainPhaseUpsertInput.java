69fbb9a851a9b1d2b9d2075d67380eaebd7b6d57
Julien Richard <julien.richard@filigran.io>
2024-03-20 01:45:27 +0100

    [api] Adapt api for python client

---
+++ openbas-api/src/main/java/io/openbas/rest/kill_chain_phase/form/KillChainPhaseUpsertInput.java
@@ -0,0 +1,20 @@
+package io.openbas.rest.kill_chain_phase.form;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KillChainPhaseUpsertInput {
+
+    @JsonProperty("kill_chain_phases")
+    private List<KillChainPhaseCreateInput> killChainPhases = new ArrayList<>();
+
+    public List<KillChainPhaseCreateInput> getKillChainPhases() {
+        return killChainPhases;
+    }
+
+    public void setKillChainPhases(List<KillChainPhaseCreateInput> killChainPhases) {
+        this.killChainPhases = killChainPhases;
+    }
+}
