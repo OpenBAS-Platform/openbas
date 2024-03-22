3a628e13cd207807b2e69ba0c077c8934a928cc9
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-03-22 17:17:55 +0100

    [frontend] Add atomic testings

--- openbas-front/src/admin/Index.tsx
+++ openbas-front/src/admin/Index.tsx
@@ -17,6 +17,7 @@ const Dashboard = lazy(() => import('./components/Dashboard'));
 const IndexProfile = lazy(() => import('./components/profile/Index'));
 const FullTextSearch = lazy(() => import('./components/search/FullTextSearch'));
 const Exercises = lazy(() => import('./components/exercises/Exercises'));
+const Atomic = lazy(() => import('./components/atomic-testing/AtomicTestings'));
 const IndexExercise = lazy(() => import('./components/exercises/Index'));
 const Scenarios = lazy(() => import('./components/scenarios/Scenarios'));
 const IndexScenario = lazy(() => import('./components/scenarios/scenario/IndexScenario'));
@@ -70,6 +71,7 @@ const Index = () => {
               <Route path="fulltextsearch" element={errorWrapper(FullTextSearch)()} />
               <Route path="exercises" element={errorWrapper(Exercises)()} />
               <Route path="exercises/:exerciseId/*" element={errorWrapper(IndexExercise)()} />
+              <Route path="atomic" element={errorWrapper(Atomic)()} />
               <Route path="scenarios" element={errorWrapper(Scenarios)()} />
               <Route path="scenarios/:scenarioId/*" element={errorWrapper(IndexScenario)()} />
               <Route path="assets/*" element={errorWrapper(Assets)()} />
