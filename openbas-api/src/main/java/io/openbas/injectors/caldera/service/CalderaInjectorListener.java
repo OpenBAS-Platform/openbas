03bd097b930208e0769891d1fc8015d35e2783e7
Romuald Lemesle <romuald.lemesle@filigran.io>
2024-04-30 21:23:43 +0200

    Change expectation validation strategy

--- openbas-api/src/main/java/io/openbas/injectors/caldera/service/CalderaInjectorListener.java
+++ openbas-api/src/main/java/io/openbas/injectors/caldera/service/CalderaInjectorListener.java
@@ -1,15 +1,13 @@
 package io.openbas.injectors.caldera.service;
 
 import io.openbas.asset.AssetGroupService;
-import io.openbas.asset.EndpointService;
-import io.openbas.database.model.*;
+import io.openbas.database.model.ExecutionStatus;
+import io.openbas.database.model.Inject;
+import io.openbas.database.model.InjectStatus;
 import io.openbas.database.repository.InjectRepository;
 import io.openbas.database.repository.InjectStatusRepository;
-import io.openbas.inject_expectation.InjectExpectationService;
 import io.openbas.injectors.caldera.CalderaContract;
-import io.openbas.injectors.caldera.config.CalderaInjectorConfig;
 import io.openbas.injectors.caldera.model.ResultStatus;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -19,12 +17,10 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static io.openbas.database.model.InjectStatusExecution.traceError;
 import static io.openbas.database.model.InjectStatusExecution.traceInfo;
-import static io.openbas.inject_expectation.InjectExpectationUtils.resultsBySourceId;
 
 @Service
 @RequiredArgsConstructor
@@ -32,10 +28,7 @@ public class CalderaInjectorListener {
 
   private final InjectRepository injectRepository;
   private final InjectStatusRepository injectStatusRepository;
-  private final InjectExpectationService injectExpectationService;
   private final CalderaInjectorService calderaService;
-  private final CalderaInjectorConfig calderaInjectorConfig;
-  private final EndpointService endpointService;
   private final AssetGroupService assetGroupService;
 
   @Scheduled(fixedDelay = 60000, initialDelay = 0)
@@ -45,46 +38,40 @@ public class CalderaInjectorListener {
     List<InjectStatus> injectStatuses = this.injectStatusRepository.pendingForInjectType(CalderaContract.TYPE);
     // For each one ask for traces and status
     injectStatuses.forEach((injectStatus -> {
-      Inject inject = injectStatus.getInject();
       // Add traces and close inject if needed.
       Instant finalExecutionTime = injectStatus.getTrackingSentDate();
-      List<Asset> assets = injectStatus.getInject().getAssets();
-      List<AssetGroup> assetGroups = injectStatus.getInject().getAssetGroups();
+
       List<String> linkIds = injectStatus.statusIdentifiers();
       List<ResultStatus> completedActions = new ArrayList<>();
       for (String linkId : linkIds) {
         try {
           ResultStatus resultStatus = this.calderaService.results(linkId);
-          if( resultStatus.getPaw() == null ) {
-            injectStatus.getTraces().add(traceInfo("Results are not yet available (still on-going)"));
-          } else {
-            Endpoint currentEndpoint = this.endpointService.findByExternalReference(resultStatus.getPaw()).orElseThrow();
-            String currentAssetId = currentEndpoint.getId();
-            if (resultStatus.isComplete()) {
-              completedActions.add(resultStatus);
-              computeExpectationForAsset(inject, currentAssetId, resultStatus.isFail(), resultStatus.getContent());
-              injectStatus.setTrackingTotalSuccess(injectStatus.getTrackingTotalSuccess() + 1);
-              // Compute biggest execution time
-              if (resultStatus.getFinish().isAfter(finalExecutionTime)) {
-                finalExecutionTime = resultStatus.getFinish();
-              }
-            } else if (injectStatus.getTrackingSentDate().isBefore(Instant.now().minus(5L, ChronoUnit.MINUTES))) {
-              resultStatus.setFail(true);
-              completedActions.add(resultStatus);
-              computeExpectationForAsset(inject, currentAssetId, resultStatus.isFail(), "Time out");
-              injectStatus.setTrackingTotalError(injectStatus.getTrackingTotalSuccess() + 1);
+
+          if (resultStatus.isComplete()) {
+            completedActions.add(resultStatus);
+
+            injectStatus.setTrackingTotalSuccess(injectStatus.getTrackingTotalSuccess() + 1);
+
+            // Compute biggest execution time
+            if (resultStatus.getFinish().isAfter(finalExecutionTime)) {
+              finalExecutionTime = resultStatus.getFinish();
             }
+            // TimeOut
+          } else if (injectStatus.getTrackingSentDate().isBefore(Instant.now().minus(5L, ChronoUnit.MINUTES))) {
+            resultStatus.setFail(true);
+            completedActions.add(resultStatus);
+
+            injectStatus.setTrackingTotalError(injectStatus.getTrackingTotalSuccess() + 1);
           }
         } catch (Exception e) {
-           injectStatus.getTraces().add(
-               traceError("Caldera failed to get result of the executed ability")
-           );
+          injectStatus.getTraces().add(
+              traceError("Caldera error to execute ability")
+          );
         }
       }
 
       // Compute status only if all actions are completed
       if (completedActions.size() == linkIds.size()) {
-        assetGroups.forEach((assetGroup -> computeExpectationForAssetGroup(inject, assetGroup)));
         int failedActions = (int) completedActions.stream().filter(ResultStatus::isFail).count();
         computeInjectStatus(injectStatus, finalExecutionTime, completedActions.size(), failedActions);
         // Update related inject
@@ -93,79 +80,29 @@ public class CalderaInjectorListener {
     }));
   }
 
-  // -- EXPECTATION --
-
-  private void computeExpectationForAsset(
-      @NotNull final Inject inject,
-      @NotBlank final String assetId,
-      @NotNull final boolean fail, // Is action failed, success for expectation
-      @NotBlank final String result) {
-    InjectExpectation expectation = this.injectExpectationService
-        .preventionExpectationForAsset(inject, assetId);
-    if (expectation != null) {
-      // Not already handle
-      List<InjectExpectationResult> results = resultsBySourceId(expectation, this.calderaInjectorConfig.getId());
-      if (results.isEmpty()) {
-        this.injectExpectationService.computeExpectation(
-            expectation,
-            this.calderaInjectorConfig.getId(),
-            CalderaInjectorConfig.PRODUCT_NAME,
-            result,
-            fail);
-      }
-    }
-  }
-
-  private void computeExpectationForAssetGroup(
-      @NotNull final Inject inject,
-      @NotBlank final AssetGroup assetGroup) {
-    InjectExpectation expectationAssetGroup = this.injectExpectationService
-        .preventionExpectationForAssetGroup(inject, assetGroup);
-    if (expectationAssetGroup != null) {
-      List<InjectExpectation> expectationAssets = this.injectExpectationService
-          .preventionExpectationForAssets(inject, assetGroup);
-      // Not already handle
-      List<InjectExpectationResult> results = resultsBySourceId(
-          expectationAssetGroup,
-          this.calderaInjectorConfig.getId()
-      );
-      if (results.isEmpty()) {
-        this.injectExpectationService.computeExpectationGroup(
-            expectationAssetGroup,
-            expectationAssets,
-            this.calderaInjectorConfig.getId(),
-            CalderaInjectorConfig.PRODUCT_NAME
-        );
-      }
-    }
-  }
-
   // -- INJECT STATUS --
 
-  @Transactional
-  public void computeInjectStatus(
-          @NotNull final InjectStatus injectStatus,
-          @NotNull final Instant finalExecutionTime,
-          final int completedActions,
-          final int failedActions) {
-     boolean hasError = injectStatus.getTraces().stream()
-         .anyMatch(trace -> trace.getStatus().equals(ExecutionStatus.ERROR));
-     injectStatus.setName(hasError ? ExecutionStatus.ERROR : ExecutionStatus.SUCCESS);
-     injectStatus.getTraces().add(
-         traceInfo("caldera",
-             "Caldera success to execute ability on " + (completedActions - failedActions)
-                 + "/" + completedActions + " asset(s)")
-     );
+  private void computeInjectStatus(
+      @NotNull final InjectStatus injectStatus,
+      @NotNull final Instant finalExecutionTime,
+      final int completedActions,
+      final int failedActions) {
+    boolean hasError = injectStatus.getTraces().stream()
+        .anyMatch(trace -> trace.getStatus().equals(ExecutionStatus.ERROR));
+    injectStatus.setName(hasError ? ExecutionStatus.ERROR : ExecutionStatus.SUCCESS);
+    injectStatus.getTraces().add(
+        traceInfo("caldera",
+            "Caldera success to execute ability on " + (completedActions - failedActions)
+                + "/" + completedActions + " asset(s)")
+    );
     long executionTime = (finalExecutionTime.toEpochMilli() - injectStatus.getTrackingSentDate().toEpochMilli());
     injectStatus.setTrackingTotalExecutionTime(executionTime);
-    injectStatus.setTrackingEndDate(Instant.now());
     this.injectStatusRepository.save(injectStatus);
   }
 
   // -- INJECT --
 
-  @Transactional
-  public void computeInject(@NotNull final InjectStatus injectStatus) {
+  private void computeInject(@NotNull final InjectStatus injectStatus) {
     Inject relatedInject = injectStatus.getInject();
     relatedInject.setUpdatedAt(Instant.now());
     this.injectRepository.save(relatedInject);
--- openbas-api/src/main/java/io/openbas/injectors/caldera/service/CalderaInjectorService.java
+++ openbas-api/src/main/java/io/openbas/injectors/caldera/service/CalderaInjectorService.java
@@ -1,99 +1,92 @@
 package io.openbas.injectors.caldera.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import io.openbas.injectors.caldera.client.CalderaInjectorClient;
 import io.openbas.injectors.caldera.client.model.*;
 import io.openbas.injectors.caldera.model.Obfuscator;
 import io.openbas.injectors.caldera.model.ResultStatus;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
-import org.apache.hc.client5.http.ClientProtocolException;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
-import java.util.*;
-import java.util.logging.Level;
+import java.util.Base64;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.springframework.util.StringUtils.hasText;
 
 @Service
 @RequiredArgsConstructor
-@Log
 public class CalderaInjectorService {
 
-    private final CalderaInjectorClient client;
+  private final CalderaInjectorClient client;
 
-    // -- ABILITIES --
+  // -- ABILITIES --
 
-    public List<Ability> abilities() {
-        return this.client.abilities();
+  public List<Ability> abilities() {
+    return this.client.abilities();
+  }
+
+  public void exploit(
+      @NotBlank final String obfuscator,
+      @NotBlank final String paw,
+      @NotBlank final String abilityId) {
+    this.client.exploit(obfuscator, paw, abilityId);
+  }
+
+  public List<Obfuscator> obfuscators() {
+    return this.client.obfuscators()
+        .stream()
+        .filter((o) -> !"base64noPadding".equals(o.getName())) // Not work on Caldera Side
+        .collect(Collectors.toList());
+  }
+
+  // -- LINK --
+
+  public ExploitResult exploitResult(
+      @NotBlank final String paw,
+      @NotBlank final String abilityId) throws RuntimeException {
+    Agent agent = this.client.agent(paw, "links");
+    // Take the last created
+    Link agentLink = agent.getLinks()
+        .stream()
+        .filter((l) -> l.getAbility().getAbility_id().equals(abilityId))
+        .max(Comparator.comparing(l -> Instant.parse(l.getDecide())))
+        .orElseThrow(() -> new RuntimeException("Caldera fail to execute ability " + abilityId + " on paw " + paw));
+    assert paw.equals(agentLink.getPaw());
+    ExploitResult exploitResult = new ExploitResult();
+    exploitResult.setLinkId(agentLink.getId());
+    byte[] decodedBytes = Base64.getDecoder().decode(agentLink.getCommand());
+    exploitResult.setCommand(new String(decodedBytes, StandardCharsets.UTF_8));
+    return exploitResult;
+  }
+
+  public ResultStatus results(@NotBlank final String linkId) {
+    ResultStatus resultStatus = new ResultStatus();
+    Result result = this.client.results(linkId);
+    // No result or not finish -> in progress #see caldera code
+    if (Optional.ofNullable(result).map(Result::getLink).map(Link::getFinish).isEmpty()) {
+      resultStatus.setComplete(false);
+    } else {
+      resultStatus.setComplete(true);
+      Link resultLink = result.getLink();
+      resultStatus.setPaw(resultLink.getPaw());
+      resultStatus.setFinish(Instant.parse(resultLink.getFinish()));
+      // Status == 0 -> success || Status > 0 -> failed #see caldera code
+      resultStatus.setFail(resultLink.getStatus() > 0);
+
+      // Result output can be : #see caldera code
+      //    - empty if ability execution return nothing
+      //    - json object with stdout & stderr if ability execution return something
+      String resultOutput = result.getOutput();
+      byte[] decodedBytes = Base64.getDecoder().decode(resultOutput);
+      String decodedString = new String(decodedBytes, StandardCharsets.UTF_8);
+      resultStatus.setContent(hasText(decodedString) ? decodedString : "no output to show");
     }
-
-    public void exploit(
-            @NotBlank final String obfuscator,
-            @NotBlank final String paw,
-            @NotBlank final String abilityId) {
-        this.client.exploit(obfuscator, paw, abilityId);
-    }
-
-    public List<Obfuscator> obfuscators() {
-        return this.client.obfuscators();
-    }
-
-    public List<Agent> agents() {
-        try {
-            return this.client.agents().stream().toList();
-        } catch (ClientProtocolException | JsonProcessingException e) {
-            log.log(Level.SEVERE, "Error getting the list of Caldera agents", e);
-            return new ArrayList<>();
-        }
-    }
-
-    // -- LINK --
-
-    public ExploitResult exploitResult(
-            @NotBlank final String paw,
-            @NotBlank final String abilityId) throws RuntimeException {
-        Agent agent = this.client.agent(paw, "links");
-        // Take the last created
-        Link agentLink = agent.getLinks()
-                .stream()
-                .filter((l) -> l.getAbility().getAbility_id().equals(abilityId))
-                .max(Comparator.comparing(l -> Instant.parse(l.getDecide())))
-                .orElseThrow(() -> new RuntimeException("Caldera fail to execute ability " + abilityId + " on paw " + paw));
-        assert paw.equals(agentLink.getPaw());
-        ExploitResult exploitResult = new ExploitResult();
-        exploitResult.setLinkId(agentLink.getId());
-        byte[] decodedBytes = Base64.getDecoder().decode(agentLink.getCommand());
-        exploitResult.setCommand(new String(decodedBytes, StandardCharsets.UTF_8));
-        return exploitResult;
-    }
-
-    public ResultStatus results(@NotBlank final String linkId) {
-        ResultStatus resultStatus = new ResultStatus();
-        Result result = this.client.results(linkId);
-        // No result or not finish -> in progress #see caldera code
-        if (Optional.ofNullable(result).map(Result::getLink).map(Link::getFinish).isEmpty()) {
-            resultStatus.setComplete(false);
-        } else {
-            resultStatus.setComplete(true);
-            Link resultLink = result.getLink();
-            resultStatus.setPaw(resultLink.getPaw());
-            resultStatus.setFinish(Instant.parse(resultLink.getFinish()));
-            // Status == 0 -> success || Status > 0 -> failed #see caldera code
-            resultStatus.setFail(resultLink.getStatus() > 0);
-
-            // Result output can be : #see caldera code
-            //    - empty if ability execution return nothing
-            //    - json object with stdout & stderr if ability execution return something
-            String resultOutput = result.getOutput();
-            byte[] decodedBytes = Base64.getDecoder().decode(resultOutput);
-            String decodedString = new String(decodedBytes, StandardCharsets.UTF_8);
-            resultStatus.setContent(hasText(decodedString) ? decodedString : "no output to show");
-        }
-        return resultStatus;
-    }
+    return resultStatus;
+  }
 
 }
