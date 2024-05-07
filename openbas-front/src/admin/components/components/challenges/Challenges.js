11b5f78549614d578f2d014fa46d31acf43adef5
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-05-07 19:58:41 +0200

    [frontend] clean

--- openbas-front/src/admin/components/components/challenges/Challenges.js
+++ openbas-front/src/admin/components/components/challenges/Challenges.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@mui/styles';
 import { List, ListItem, ListItemIcon, ListItemText, ListItemSecondaryAction, Tooltip, Chip } from '@mui/material';
 import { useDispatch } from 'react-redux';
-import { EmojiEventsOutlined, RowingOutlined } from '@mui/icons-material';
+import { RowingOutlined } from '@mui/icons-material';
 import * as R from 'ramda';
 import { Link } from 'react-router-dom';
 import SearchFilter from '../../../../components/SearchFilter';
