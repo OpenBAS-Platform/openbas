d8fb72f35746bac53959af98e7cd5842cbce2436
Samuel Hassine <samuel.hassine@filigran.io>
2024-05-17 20:42:07 +0200

    [all] Fix HTTP clients

--- openbas-api/src/main/java/io/openbas/injectors/opencti/service/OpenCTIService.java
+++ openbas-api/src/main/java/io/openbas/injectors/opencti/service/OpenCTIService.java
@@ -3,15 +3,17 @@ package io.openbas.injectors.opencti.service;
 import io.openbas.database.model.DataAttachment;
 import io.openbas.database.model.Execution;
 import io.openbas.injectors.opencti.config.OpenCTIConfig;
-import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.ClientProtocolException;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.http.*;
+import org.apache.hc.core5.http.HttpStatus;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.time.Instant;
 import java.util.List;
 
@@ -28,54 +30,60 @@ public class OpenCTIService {
     }
 
     public void createCase(Execution execution, String name, String description, List<DataAttachment> attachments) throws Exception {
-        HttpClient httpclient = HttpClients.createDefault();
-        // Prepare the query
-        HttpPost httpPost = new HttpPost(config.getUrl() + "/graphql");
-        httpPost.addHeader("Authorization", "Bearer " + config.getToken());
-        httpPost.addHeader("Content-Type","application/json; charset=utf-8");
-        httpPost.addHeader("Accept", "application/json");
-        // TODO support attachement
-        // if( attachments.size() > 0 ) {
-        //    DataAttachment attachment = attachments.get(0);
-        // }
-        String caseBody = String.format("{\"query\": \"mutation { caseIncidentAdd(input: { name: \\\"%s\\\", description: \\\"%s\\\" }) { id } }\"}", name, description);
-        StringEntity httpBody = new StringEntity(caseBody);
-        httpPost.setEntity(httpBody);
-        httpclient.execute(httpPost, classicHttpResponse -> {
-            if (classicHttpResponse.getCode() == HttpStatus.SC_OK) {
-                String body = EntityUtils.toString(classicHttpResponse.getEntity());
-                execution.addTrace(traceSuccess("Case created (" + body + ")"));
-                return true;
-            } else {
-                execution.addTrace(traceError("Fail to POST"));
-                return false;
-            }
-        });
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            // Prepare the query
+            HttpPost httpPost = new HttpPost(config.getUrl() + "/graphql");
+            httpPost.addHeader("Authorization", "Bearer " + config.getToken());
+            httpPost.addHeader("Content-Type", "application/json; charset=utf-8");
+            httpPost.addHeader("Accept", "application/json");
+            // TODO support attachement
+            // if( attachments.size() > 0 ) {
+            //    DataAttachment attachment = attachments.get(0);
+            // }
+            String caseBody = String.format("{\"query\": \"mutation { caseIncidentAdd(input: { name: \\\"%s\\\", description: \\\"%s\\\" }) { id } }\"}", name, description);
+            StringEntity httpBody = new StringEntity(caseBody);
+            httpPost.setEntity(httpBody);
+            httpClient.execute(httpPost, classicHttpResponse -> {
+                if (classicHttpResponse.getCode() == HttpStatus.SC_OK) {
+                    String body = EntityUtils.toString(classicHttpResponse.getEntity());
+                    execution.addTrace(traceSuccess("Case created (" + body + ")"));
+                    return true;
+                } else {
+                    execution.addTrace(traceError("Fail to POST"));
+                    return false;
+                }
+            });
+        } catch (IOException e) {
+            throw new ClientProtocolException("Unexpected response for request on: " + config.getUrl() + "/graphql");
+        }
     }
 
     public void createReport(Execution execution, String name, String description, List<DataAttachment> attachments) throws Exception {
-        HttpClient httpclient = HttpClients.createDefault();
-        // Prepare the query
-        HttpPost httpPost = new HttpPost(config.getUrl() + "/graphql");
-        httpPost.addHeader("Authorization", "Bearer " + config.getToken());
-        httpPost.addHeader("Content-Type","application/json; charset=utf-8");
-        httpPost.addHeader("Accept", "application/json");
-        // TODO support attachement
-        // if( attachments.size() > 0 ) {
-        //    DataAttachment attachment = attachments.get(0);
-        // }
-        String caseBody = String.format("{\"query\": \"mutation { reportAdd(input: { name: \\\"%s\\\", description: \\\"%s\\\", published: \\\"%s\\\" }) { id } }\"}", name, description, Instant.now().toString());
-        StringEntity httpBody = new StringEntity(caseBody);
-        httpPost.setEntity(httpBody);
-        httpclient.execute(httpPost, classicHttpResponse -> {
-            if (classicHttpResponse.getCode() == HttpStatus.SC_OK) {
-                String body = EntityUtils.toString(classicHttpResponse.getEntity());
-                execution.addTrace(traceSuccess("Report created (" + body + ")"));
-                return true;
-            } else {
-                execution.addTrace(traceError("Fail to POST"));
-                return false;
-            }
-        });
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            // Prepare the query
+            HttpPost httpPost = new HttpPost(config.getUrl() + "/graphql");
+            httpPost.addHeader("Authorization", "Bearer " + config.getToken());
+            httpPost.addHeader("Content-Type", "application/json; charset=utf-8");
+            httpPost.addHeader("Accept", "application/json");
+            // TODO support attachement
+            // if( attachments.size() > 0 ) {
+            //    DataAttachment attachment = attachments.get(0);
+            // }
+            String caseBody = String.format("{\"query\": \"mutation { reportAdd(input: { name: \\\"%s\\\", description: \\\"%s\\\", published: \\\"%s\\\" }) { id } }\"}", name, description, Instant.now().toString());
+            StringEntity httpBody = new StringEntity(caseBody);
+            httpPost.setEntity(httpBody);
+            httpClient.execute(httpPost, classicHttpResponse -> {
+                if (classicHttpResponse.getCode() == HttpStatus.SC_OK) {
+                    String body = EntityUtils.toString(classicHttpResponse.getEntity());
+                    execution.addTrace(traceSuccess("Report created (" + body + ")"));
+                    return true;
+                } else {
+                    execution.addTrace(traceError("Fail to POST"));
+                    return false;
+                }
+            });
+        } catch (IOException e) {
+            throw new ClientProtocolException("Unexpected response for request on: " + config.getUrl() + "/graphql");
+        }
     }
 }
