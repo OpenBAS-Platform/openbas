25f54832691ea87ee01cff456f66ca98d5ec85e2
Gael Leblan <gael.leblan@filigran.io>
2024-05-23 16:01:01 +0200

    [backend] Switching get statistics to raw native query (#898)

---
+++ openbas-model/src/main/java/io/openbas/database/raw/RawGlobalInjectExpectation.java
@@ -0,0 +1,13 @@
+package io.openbas.database.raw;
+
+public interface RawGlobalInjectExpectation {
+
+    String getInject_expectation_type();
+
+    Integer getInject_expectation_score();
+
+    String getInject_title();
+
+    String getAttack_pattern_id();
+
+}
