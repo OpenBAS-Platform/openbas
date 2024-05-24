bab3657c3517dce9325147fa8d50766cd6afa788
ChristianDongmo <christian.dongmo@filigran.io>
2024-05-24 13:58:13 +0200

    [backend] add query to fecth documents

---
+++ openbas-model/src/main/java/io/openbas/database/raw/RawDocument.java
@@ -0,0 +1,15 @@
+package io.openbas.database.raw;
+
+import java.util.List;
+
+public interface RawDocument {
+
+    String getDocument_Id();
+    String getDocument_name();
+    String getDocument_description();
+    String getDocument_type();
+    String getDocument_target();
+    List<String> getDocument_tags();
+    List<String> getDocument_exercises();
+    List<String> getDocument_scenarios();
+}
