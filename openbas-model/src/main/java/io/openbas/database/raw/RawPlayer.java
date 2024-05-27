06ec419ce8f64c0c50ceed3f5ccda424fe134bcf
Christian DONGMO <christian.dongmo@filigran.io>
2024-05-27 18:20:05 +0200

    Issue/players (#982)
    * [backend] add query to fecth players
    
    * [backend] add query to fecth players (feedback on PR : using lowercase in queries)

---
+++ openbas-model/src/main/java/io/openbas/database/raw/RawPlayer.java
@@ -0,0 +1,19 @@
+package io.openbas.database.raw;
+
+import io.openbas.helper.UserHelper;
+
+import java.util.List;
+
+public interface RawPlayer {
+
+    default String getUser_gravatar() {
+        return UserHelper.getGravatar(getUser_email());
+    }
+
+    String getUser_id();
+    String getUser_firstname();
+    String getUser_lastname();
+    String getUser_email();
+    String getUser_organization();
+    List<String> getUser_tags();
+}
