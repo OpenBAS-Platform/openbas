d3d5ec4e0f5947cacd27481d05f3c2c9a86e3419
Gael Leblan <gael.leblan@filigran.io>
2024-05-27 21:06:33 +0200

    [backend] Feedback on PR -> using lombok on SimpleRawInjectExpectation.java

--- openbas-model/src/main/java/io/openbas/database/raw/impl/SimpleRawInjectExpectation.java
+++ openbas-model/src/main/java/io/openbas/database/raw/impl/SimpleRawInjectExpectation.java
@@ -1,7 +1,11 @@
 package io.openbas.database.raw.impl;
 
 import io.openbas.database.raw.RawInjectExpectation;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class SimpleRawInjectExpectation implements RawInjectExpectation {
 
     private String inject_expectation_id;
@@ -10,58 +14,4 @@ public class SimpleRawInjectExpectation implements RawInjectExpectation {
     private String team_id;
     private String asset_id;
     private String asset_group_id;
-
-    @Override
-    public String getInject_expectation_type() {
-        return inject_expectation_type;
-    }
-
-    @Override
-    public Integer getInject_expectation_score() {
-        return inject_expectation_score;
-    }
-
-    @Override
-    public String getTeam_id() {
-        return team_id;
-    }
-
-    @Override
-    public String getAsset_id() {
-        return asset_id;
-    }
-
-    @Override
-    public String getAsset_group_id() {
-        return asset_group_id;
-    }
-
-    @Override
-    public String getInject_expectation_id() {
-        return inject_expectation_id;
-    }
-
-    public void setInject_expectation_type(String inject_expectation_type) {
-        this.inject_expectation_type = inject_expectation_type;
-    }
-
-    public void setInject_expectation_score(Integer inject_expectation_score) {
-        this.inject_expectation_score = inject_expectation_score;
-    }
-
-    public void setTeam_id(String team_id) {
-        this.team_id = team_id;
-    }
-
-    public void setAsset_id(String asset_id) {
-        this.asset_id = asset_id;
-    }
-
-    public void setAsset_group_id(String asset_group_id) {
-        this.asset_group_id = asset_group_id;
-    }
-
-    public void setInject_expectation_id(String inject_expectation_id) {
-        this.inject_expectation_id = inject_expectation_id;
-    }
 }
