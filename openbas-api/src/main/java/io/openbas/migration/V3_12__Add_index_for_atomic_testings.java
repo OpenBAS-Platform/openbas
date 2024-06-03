a89576066971b5be4de63c6ada08610e62d6a8e3
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-06-03 12:26:12 +0200

    [backend] delete duplicated indexes

--- openbas-api/src/main/java/io/openbas/migration/V3_12__Add_index_for_atomic_testings.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_12__Add_index_for_atomic_testings.java
@@ -18,12 +18,6 @@ public class V3_12__Add_index_for_atomic_testings extends BaseJavaMigration {
     Connection connection = context.getConnection();
     Statement select = connection.createStatement();
     select.execute(""
-        + "CREATE INDEX idx_fk_inject_expectation ON injects_expectations (inject_id);"
-        + "CREATE INDEX idx_fk_inject_tag ON injects_tags (inject_id);"
-        + "CREATE INDEX idx_fk_inject_assets ON injects_assets (inject_id);"
-        + "CREATE INDEX idx_fk_inject_asset_groups ON injects_asset_groups (inject_id);"
-        + "CREATE INDEX idx_fk_inject_teams ON injects_teams (inject_id);"
-        + "CREATE INDEX idx_fk_inject_injects_documents ON injects_documents (inject_id);"
         + "CREATE INDEX idx_null_exercise_and_scenario ON injects (inject_id) WHERE inject_scenario IS NULL AND inject_exercise IS NULL;"
         + "");
   }
