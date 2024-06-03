7299c1037b5520fd8ba86969ffab41037bfa43e7
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-06-03 17:55:42 +0200

    [backend] rebase

--- openbas-api/src/main/java/io/openbas/migration/V3_19__Add_index_for_atomic_testings.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_19__Add_index_for_atomic_testings.java
@@ -1,7 +1,6 @@
 package io.openbas.migration;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.sql.Statement;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
@@ -12,11 +11,8 @@ public class V3_19__Add_index_for_atomic_testings extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
-    try (Connection connection = context.getConnection();
-        Statement createIndex = connection.createStatement()) {
-      createIndex.execute("CREATE INDEX idx_null_exercise_and_scenario ON injects (inject_id) WHERE inject_scenario IS NULL AND inject_exercise IS NULL;");
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
+    Connection connection = context.getConnection();
+    Statement createIndex = connection.createStatement();
+    createIndex.execute("CREATE INDEX idx_null_exercise_and_scenario ON injects (inject_id) WHERE inject_scenario IS NULL AND inject_exercise IS NULL;");
   }
 }
