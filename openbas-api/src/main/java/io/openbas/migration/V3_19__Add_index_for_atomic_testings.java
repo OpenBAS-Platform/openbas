047c1565bfe33447907de023c04d406625428cf3
Stephanya Casanova <savacano28@users.noreply.github.com>
2024-06-05 11:56:37 +0200

    [backend] Add index to atomic testing (#1001)

---
+++ openbas-api/src/main/java/io/openbas/migration/V3_19__Add_index_for_atomic_testings.java
@@ -0,0 +1,18 @@
+package io.openbas.migration;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+import org.springframework.stereotype.Component;
+
+@Component
+public class V3_19__Add_index_for_atomic_testings extends BaseJavaMigration {
+
+  @Override
+  public void migrate(Context context) throws Exception {
+    Connection connection = context.getConnection();
+    Statement createIndex = connection.createStatement();
+    createIndex.execute("CREATE INDEX idx_null_exercise_and_scenario ON injects (inject_id) WHERE inject_scenario IS NULL AND inject_exercise IS NULL;");
+  }
+}
