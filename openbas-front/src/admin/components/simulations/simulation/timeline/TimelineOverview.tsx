c7039b576c2bba1b26453293c955608928e1132a
Stephanya Casanova <savacano28@users.noreply.github.com>
2024-06-11 16:10:59 +0200

    [frontend] Refact timeline composant (#1051)

---
+++ openbas-front/src/admin/components/simulations/simulation/timeline/TimelineOverview.tsx
@@ -0,0 +1,289 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@mui/styles';
+import { Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemSecondaryAction, ListItemText, Paper, Typography } from '@mui/material';
+import { Link, useParams } from 'react-router-dom';
+import { PreviewOutlined } from '@mui/icons-material';
+import * as R from 'ramda';
+import { useFormatter } from '../../../../../components/i18n';
+import { useHelper } from '../../../../../store';
+import Empty from '../../../../../components/Empty';
+import InjectIcon from '../../../common/injects/InjectIcon';
+import InjectPopover from '../../../common/injects/InjectPopover';
+import ProgressBarCountdown from '../../../../../components/ProgressBarCountdown';
+import AnimationMenu from '../AnimationMenu';
+import InjectOverTimeArea from './InjectOverTimeArea';
+import InjectOverTimeLine from './InjectOverTimeLine';
+import UpdateInject from '../../../common/injects/UpdateInject';
+import ItemStatus from '../../../../../components/ItemStatus';
+import type { InjectHelper } from '../../../../../actions/injects/inject-helper';
+import type { ExercisesHelper } from '../../../../../actions/exercises/exercise-helper';
+import type { Exercise, Inject } from '../../../../../utils/api-types';
+import type { TagHelper } from '../../../../../actions/helper';
+import type { InjectStore } from '../../../../../actions/injects/Inject';
+import { fetchExerciseInjects, updateInjectForExercise } from '../../../../../actions/Inject';
+import useDataLoader from '../../../../../utils/hooks/useDataLoader';
+import { fetchExerciseTeams } from '../../../../../actions/Exercise';
+import { useAppDispatch } from '../../../../../utils/hooks';
+import Timeline from '../../../../../components/Timeline';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    width: '100%',
+    margin: '-12px 0 50px 0',
+  },
+  paperChart: {
+    height: '100%',
+    minHeight: '100%',
+    margin: '10px 0 0 0',
+    padding: 15,
+    borderRadius: 4,
+  },
+  paper: {
+    position: 'relative',
+    padding: 0,
+    overflow: 'hidden',
+    height: '100%',
+  },
+  item: {
+    height: 50,
+    minHeight: 50,
+    maxHeight: 50,
+    paddingRight: 0,
+  },
+  bodyItems: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  bodyItem: {
+    height: '100%',
+    fontSize: 14,
+    float: 'left',
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    paddingRight: 10,
+  },
+}));
+
+const TimelineOverview = () => {
+  const classes = useStyles();
+  const dispatch = useAppDispatch();
+  const { exerciseId } = useParams() as { exerciseId: Exercise['exercise_id'] };
+  const { t, fndt } = useFormatter();
+  const [selectedInjectId, setSelectedInjectId] = useState<string | null>(null);
+
+  const {
+    exercise,
+    injects,
+    teams,
+    tagsMap,
+    selectedInject,
+  } = useHelper((helper: InjectHelper & ExercisesHelper & TagHelper) => {
+    const exerciseTeams = helper.getExerciseTeams(exerciseId);
+    return {
+      exercise: helper.getExercise(exerciseId),
+      injects: helper.getExerciseInjects(exerciseId),
+      teams: exerciseTeams,
+      tagsMap: helper.getTagsMap(),
+      selectedInject: selectedInjectId && helper.getInject(selectedInjectId),
+    };
+  });
+
+  // Fetching Data
+  useDataLoader(() => {
+    dispatch(fetchExerciseTeams(exerciseId));
+    dispatch(fetchExerciseInjects(exerciseId));
+  });
+
+  const pendingInjects = R.sortWith(
+    [R.ascend(R.prop('inject_depends_duration'))],
+    injects.filter((i: InjectStore) => i.inject_status === null),
+  );
+  const processedInjects = R.sortWith(
+    [R.descend(R.prop('inject_depends_duration'))],
+    injects.filter((i: InjectStore) => i.inject_status !== null),
+  );
+
+  const onUpdateInject = async (inject: Inject) => {
+    if (selectedInjectId) {
+      dispatch(updateInjectForExercise(exerciseId, selectedInjectId, inject));
+    }
+  };
+
+  return (
+    <div className={classes.root}>
+      <AnimationMenu exerciseId={exerciseId}/>
+      <div className="clearfix"/>
+      <Timeline exerciseId={exerciseId} injects={injects} teams={teams}></Timeline>
+      <div className="clearfix"/>
+      <Grid container spacing={3} style={{ marginTop: 50, paddingBottom: 24 }}>
+        <Grid container item spacing={3}>
+          <Grid item xs={6} sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Typography variant="h4">{t('Pending injects')}</Typography>
+            <Paper variant="outlined" classes={{ root: classes.paper }}>
+              {pendingInjects.length > 0 ? (
+                <List style={{ paddingTop: 0 }}>
+                  {pendingInjects.map((inject: InjectStore) => {
+                    const isDisabled = !inject.inject_injector_contract.injector_contract_content_parsed?.config.expose;
+                    return (
+                      <ListItem
+                        key={inject.inject_id}
+                        dense={true}
+                        classes={{ root: classes.item }}
+                        divider={true}
+                        button={true}
+                        disabled={isDisabled || !inject.inject_enabled}
+                        onClick={() => setSelectedInjectId(inject.inject_id)}
+                      >
+                        <ListItemIcon>
+                          <InjectIcon
+                            type={inject.inject_type}
+                            variant="inline"
+                          />
+                        </ListItemIcon>
+                        <ListItemText
+                          primary={
+                            <div className={classes.bodyItems}>
+                              <div
+                                className={classes.bodyItem}
+                                style={{ width: '50%' }}
+                              >
+                                {inject.inject_title}
+                              </div>
+                              <div
+                                className={classes.bodyItem}
+                                style={{ width: '20%' }}
+                              >
+                                <ProgressBarCountdown
+                                  date={inject.inject_date}
+                                  paused={
+                                              exercise?.exercise_status === 'PAUSED'
+                                              || exercise?.exercise_status === 'CANCELED'
+                                          }
+                                />
+                              </div>
+                              <div
+                                className={classes.bodyItem}
+                                style={{
+                                  fontFamily: 'Consolas, monaco, monospace',
+                                  fontSize: 12,
+                                }}
+                              >
+                                {fndt(inject.inject_date)}
+                              </div>
+                            </div>
+                                }
+                        />
+                        <ListItemSecondaryAction>
+                          <InjectPopover
+                            inject={inject}
+                            tagsMap={tagsMap}
+                            setSelectedInjectId={setSelectedInjectId}
+                            isDisabled={isDisabled}
+                          />
+                        </ListItemSecondaryAction>
+                      </ListItem>
+                    );
+                  })}
+                </List>
+              ) : (
+                <Empty message={t('No pending injects in this simulation.')}/>
+              )}
+            </Paper>
+          </Grid>
+          <Grid item xs={6} sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Typography variant="h4">{t('Processed injects')}</Typography>
+            <Paper variant="outlined" classes={{ root: classes.paper }}>
+              {processedInjects.length > 0 ? (
+                <List style={{ paddingTop: 0 }}>
+                  {processedInjects.map((inject: InjectStore) => (
+                    <ListItemButton
+                      key={inject.inject_id}
+                      dense
+                      classes={{ root: classes.item }}
+                      divider
+                      component={Link}
+                      to={`/admin/exercises/${exerciseId}/injects/${inject.inject_id}?backlabel=Animation&backuri=/admin/exercises/${exerciseId}/animation/timeline`}
+                    >
+                      <ListItemIcon>
+                        <InjectIcon type={inject.inject_type} variant="inline"/>
+                      </ListItemIcon>
+                      <ListItemText
+                        primary={
+                          <div className={classes.bodyItems}>
+                            <div
+                              className={classes.bodyItem}
+                              style={{ width: '40%' }}
+                            >
+                              {inject.inject_title}
+                            </div>
+                            <div
+                              className={classes.bodyItem}
+                              style={{ width: '20%' }}
+                            >
+                              <ItemStatus
+                                variant="inList"
+                                label={t(inject.inject_status?.status_name)}
+                                status={inject.inject_status?.status_name}
+                              />
+                            </div>
+                            <div
+                              className={classes.bodyItem}
+                              style={{
+                                fontFamily: 'Consolas, monaco, monospace',
+                                fontSize: 12,
+                              }}
+                            >
+                              {fndt(inject.inject_status?.tracking_sent_date)} (
+                              {inject.inject_status && inject.inject_status.tracking_total_execution_time && (inject.inject_status.tracking_total_execution_time / 1000).toFixed(2)}
+                              {t('s')})
+                            </div>
+                          </div>
+                                }
+                      />
+                      <ListItemSecondaryAction>
+                        <PreviewOutlined/>
+                      </ListItemSecondaryAction>
+                    </ListItemButton>
+                  ))}
+                </List>
+              ) : (
+                <Empty message={t('No processed injects in this simulation.')}/>
+              )}
+            </Paper>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid container={true} spacing={3}>
+        <Grid item xs={6}>
+          <Typography variant="h4">
+            {t('Sent injects over time')}
+          </Typography>
+          <Paper variant="outlined" classes={{ root: classes.paperChart }}>
+            <InjectOverTimeArea exerciseId={exerciseId}/>
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="h4">
+            {t('Sent injects over time')}
+          </Typography>
+          <Paper variant="outlined" classes={{ root: classes.paperChart }}>
+            <InjectOverTimeLine exerciseId={exerciseId}/>
+          </Paper>
+        </Grid>
+      </Grid>
+      {selectedInject && (
+        <UpdateInject
+          open={selectedInjectId !== null}
+          handleClose={() => setSelectedInjectId(null)}
+          onUpdateInject={onUpdateInject}
+          injectorContract={selectedInject.inject_injector_contract.injector_contract_content_parsed}
+          inject={selectedInject}
+          teamsFromExerciseOrScenario={teams}
+          isAtomic={false}
+        />
+      )}
+    </div>
+  );
+};
+export default TimelineOverview;
---
+++ openbas-front/src/components/Timeline.tsx
@@ -0,0 +1,344 @@
+import React, { FunctionComponent } from 'react';
+import { makeStyles, useTheme } from '@mui/styles';
+import { CastForEducationOutlined, CastOutlined } from '@mui/icons-material';
+import * as R from 'ramda';
+import type { Inject, Team } from '../utils/api-types';
+import type { InjectStore } from '../actions/injects/Inject';
+import InjectIcon from '../admin/components/common/injects/InjectIcon';
+import { splitDuration } from '../utils/Time';
+import type { Theme } from './Theme';
+import { useFormatter } from './i18n';
+import SearchFilter from './SearchFilter';
+import TagsFilter from '../admin/components/common/filters/TagsFilter';
+import useSearchAnFilter from '../utils/SortingFiltering';
+import { useHelper } from '../store';
+import type { InjectHelper } from '../actions/injects/inject-helper';
+import { truncate } from '../utils/String';
+
+const useStyles = makeStyles(() => ({
+  container: {
+    marginTop: 60,
+    paddingRight: 40,
+  },
+  names: {
+    float: 'left',
+    width: '10%',
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
+  lineName: {
+    width: '100%',
+    height: 50,
+    lineHeight: '50px',
+  },
+  name: {
+    fontSize: 14,
+    fontWeight: 400,
+    display: 'flex',
+    alignItems: 'center',
+  },
+  timeline: {
+    float: 'left',
+    width: '90%',
+    position: 'relative',
+  },
+  line: {
+    position: 'relative',
+    width: '100%',
+    height: 50,
+    lineHeight: '50px',
+    padding: '0 20px 0 20px',
+    borderBottom: '1px solid rgba(255, 255, 255, 0.15)',
+    verticalAlign: 'middle',
+  },
+  scale: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    top: 0,
+    left: 0,
+  },
+  tick: {
+    position: 'absolute',
+    width: 1,
+  },
+  tickLabelTop: {
+    position: 'absolute',
+    left: -28,
+    top: -20,
+    width: 100,
+    fontSize: 10,
+  },
+  tickLabelBottom: {
+    position: 'absolute',
+    left: -28,
+    bottom: -20,
+    width: 100,
+    fontSize: 10,
+  },
+  injectGroup: {
+    position: 'absolute',
+    padding: '6px 5px 0 5px',
+    zIndex: 1000,
+    display: 'grid',
+    gridAutoFlow: 'column',
+    gridTemplateRows: 'repeat(2, 20px)',
+  },
+}));
+
+interface Props {
+  exerciseId: string,
+  injects: Inject[],
+  teams: Team[],
+}
+
+const Timeline: FunctionComponent<Props> = ({ exerciseId, injects, teams }) => {
+  // Standard hooks
+  const classes = useStyles();
+  const theme = useTheme<Theme>();
+  const { t } = useFormatter();
+
+  // Filter and sort hook
+  const searchColumns = ['title', 'description', 'content'];
+  const filtering = useSearchAnFilter(
+    'inject',
+    'depends_duration',
+    searchColumns,
+  );
+
+  // Retrieve data
+  const {
+    injectsPerTeam,
+    technicalInjectsPerType,
+  } = useHelper((helper: InjectHelper) => {
+    const techicalInjectsWithNoTeam = helper.getExerciseTechnicalInjectsWithNoTeam(exerciseId);
+    return {
+      injectsPerTeam: R.mergeAll(
+        teams.map((a) => ({
+          [a.team_id]: helper.getTeamExerciseInjects(a.team_id),
+        })),
+      ),
+      technicalInjectsPerType: R.groupBy(R.prop('inject_type'))(techicalInjectsWithNoTeam),
+    };
+  });
+
+  const injectsMap = { ...injectsPerTeam, ...technicalInjectsPerType };
+
+  // SortedTeams
+  const technicalTeams: Team[] = R.pipe(
+    R.groupBy(R.prop('inject_type')),
+    R.toPairs,
+    R.filter(
+      (n: [string, InjectStore[]]) => !(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        n[1][0].inject_injector_contract?.injector_contract_content_parsed?.fields?.filter((f: any) => f.key === 'teams').length > 0
+      ),
+    ),
+    R.map(
+      (n: [string, InjectStore[]]) => ({
+        team_id: n[0],
+        team_name: n[0],
+      }),
+    ),
+  )(injects as Inject[]);
+
+  const sortedNativeTeams = R.sortWith(
+    [R.ascend(R.prop('team_name'))],
+    teams,
+  );
+  const sortedTeams = [...technicalTeams, ...sortedNativeTeams];
+
+  // Timeline
+  const lastInject = R.pipe(
+    R.sortWith([R.descend(R.prop('inject_depends_duration'))]),
+    R.head,
+  )(injects);
+  const totalDuration = lastInject
+    ? lastInject.inject_depends_duration + 3600
+    : 60;
+  const tickDuration = Math.round(totalDuration / 20);
+  const ticks = [...Array(21)].map((_, i) => tickDuration * i);
+  // eslint-disable-next-line consistent-return
+  const byTick = R.groupBy((inject: InjectStore) => {
+    const duration = inject.inject_depends_duration;
+    for (const tick of ticks) {
+      if (duration < tick) {
+        return tick - tickDuration;
+      }
+    }
+  });
+
+  const grid0 = theme.palette.mode === 'light' ? 'rgba(0,0,0,0)' : 'rgba(255,255,255,0)';
+  const grid5 = theme.palette.mode === 'light'
+    ? 'rgba(0,0,0,0.05)'
+    : 'rgba(255,255,255,0.05)';
+  const grid25 = theme.palette.mode === 'light'
+    ? '1px solid rgba(0, 0, 0, 0.25)'
+    : '1px solid rgba(255, 255, 255, 0.25)';
+  const grid15 = theme.palette.mode === 'light'
+    ? '1px dashed rgba(0, 0, 0, 0.15)'
+    : '1px dashed rgba(255, 255, 255, 0.15)';
+
+  return (
+    <>
+      <div style={{ float: 'left', marginRight: 10 }}>
+        <SearchFilter
+          variant="small"
+          onChange={filtering.handleSearch}
+          keyword={filtering.keyword}
+        />
+      </div>
+      <div style={{ float: 'left', marginRight: 10 }}>
+        <TagsFilter
+          onAddTag={filtering.handleAddTag}
+          onRemoveTag={filtering.handleRemoveTag}
+          currentTags={filtering.tags}
+        />
+      </div>
+      <div className="clearfix"/>
+      {sortedTeams.length > 0 ? (
+        <div className={classes.container}>
+          <div className={classes.names}>
+            {sortedTeams.map((team) => (
+              <div key={team.team_id} className={classes.lineName}>
+                <div className={classes.name}>
+                  {team.team_name.startsWith('openbas_') ? (
+                    <CastOutlined fontSize="small"/>
+                  ) : (
+                    <CastForEducationOutlined fontSize="small"/>
+                  )}
+                        &nbsp;&nbsp;
+                  {team.team_name.startsWith('openbas_')
+                    ? t(team.team_name)
+                    : truncate(team.team_name, 20)}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className={classes.timeline}>
+            {sortedTeams.map((team, index) => {
+              const injectsGroupedByTick = byTick(
+                filtering.filterAndSort(injectsMap[team.team_id]),
+              );
+              return (
+                <div
+                  key={team.team_id}
+                  className={classes.line}
+                  style={{ backgroundColor: index % 2 === 0 ? grid0 : grid5 }}
+                >
+                  {Object.keys(injectsGroupedByTick).map((key, i) => {
+                    const injectGroupPosition = (parseFloat(key) * 100) / totalDuration;
+                    return (
+                      <div
+                        key={i}
+                        className={classes.injectGroup}
+                        style={{ left: `${injectGroupPosition}%` }}
+                      >
+                        {injectsGroupedByTick[key].map((inject: InjectStore) => (
+                          <InjectIcon
+                            key={inject.inject_id}
+                            type={inject.inject_type}
+                            tooltip={inject.inject_title}
+                            done={inject.inject_status !== null}
+                            disabled={!inject.inject_enabled}
+                            size="small"
+                          />
+                        ))}
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+            <div className={classes.scale}>
+              {ticks.map((tick, index) => {
+                const duration = splitDuration(tick);
+                return (
+                  <div
+                    key={tick}
+                    className={classes.tick}
+                    style={{
+                      left: `${index * 5}%`,
+                      height: index % 5 === 0 ? 'calc(100% + 30px)' : '100%',
+                      top: index % 5 === 0 ? -15 : 0,
+                      borderRight: index % 5 === 0 ? grid25 : grid15,
+                    }}
+                  >
+                    <div className={classes.tickLabelTop}>
+                      {index % 5 === 0
+                        ? `${duration.days}
+                              ${t('d')}, ${duration.hours}
+                              ${t('h')}, ${duration.minutes}
+                              ${t('m')}`
+                        : ''}
+                    </div>
+                    <div className={classes.tickLabelBottom}>
+                      {index % 5 === 0
+                        ? `${duration.days}
+                              ${t('d')}, ${duration.hours}
+                              ${t('h')}, ${duration.minutes}
+                              ${t('m')}`
+                        : ''}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className={classes.container}>
+          <div className={classes.names}>
+            <div className={classes.lineName}>
+              <div className={classes.name}>
+                <CastForEducationOutlined fontSize="small"/>
+                    &nbsp;&nbsp;
+                {t('No team')}
+              </div>
+            </div>
+          </div>
+          <div className={classes.timeline}>
+            <div className={classes.line}> &nbsp; </div>
+            <div className={classes.scale}>
+              {ticks.map((tick, index) => {
+                const duration = splitDuration(tick);
+                return (
+                  <div
+                    key={tick}
+                    className={classes.tick}
+                    style={{
+                      left: `${index * 5}%`,
+                      height: index % 5 === 0 ? '110%' : '100%',
+                      top: index % 5 === 0 ? '-5%' : 0,
+                      borderRight: index % 5 === 0 ? grid25 : grid15,
+                    }}
+                  >
+                    <div className={classes.tickLabelTop}>
+                      {index % 5 === 0
+                        ? `${duration.days}
+                              ${t('d')}, ${duration.hours}
+                              ${t('h')}, ${duration.minutes}
+                              ${t('m')}`
+                        : ''}
+                    </div>
+                    <div className={classes.tickLabelBottom}>
+                      {index % 5 === 0
+                        ? `${duration.days}
+                              ${t('d')}, ${duration.hours}
+                              ${t('h')}, ${duration.minutes}
+                              ${t('m')}`
+                        : ''}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Timeline;
