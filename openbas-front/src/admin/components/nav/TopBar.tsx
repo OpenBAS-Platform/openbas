1c524fd2eb4274bf08f64c52bb6f93160145eedd
Gael Leblan <gael.leblan@filigran.io>
2024-06-12 17:08:43 +0200

    [frontend/backend] Fixing full text search wrong indicators

--- openbas-front/src/admin/components/nav/TopBar.tsx
+++ openbas-front/src/admin/components/nav/TopBar.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { AppBar, Badge, Box, Grid, IconButton, Menu, MenuItem, Popover, Toolbar, Tooltip } from '@mui/material';
-import {Link, useLocation, useNavigate, useSearchParams } from 'react-router-dom';
+import { Link, useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import { AccountCircleOutlined, AppsOutlined, ImportantDevicesOutlined, NotificationsOutlined } from '@mui/icons-material';
 import { makeStyles, useTheme } from '@mui/styles';
 import { logout } from '../../../actions/Application';
