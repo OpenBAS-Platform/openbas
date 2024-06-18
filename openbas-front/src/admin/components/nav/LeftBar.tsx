b408288f76a285ab8530d0dd96f12cc0eea5ff5f
Johanah LEKEU <johanah.lekeu@filigran.io>
2024-06-18 11:18:49 +0200

    [Frontend|Backend]Mappers list

--- openbas-front/src/admin/components/nav/LeftBar.tsx
+++ openbas-front/src/admin/components/nav/LeftBar.tsx
@@ -511,10 +511,10 @@ const LeftBar = () => {
                   <DescriptionOutlined />
                 </ListItemIcon>
                 {navOpen && (
-                <ListItemText
-                  classes={{ primary: classes.menuItemText }}
-                  primary={t('Reports')}
-                />
+                  <ListItemText
+                    classes={{ primary: classes.menuItemText }}
+                    primary={t('Reports')}
+                  />
                 )}
               </MenuItem>
             </span>
@@ -640,6 +640,7 @@ const LeftBar = () => {
               { link: '/admin/settings', label: 'Parameters', exact: true },
               { link: '/admin/settings/security', label: 'Security' },
               { link: '/admin/settings/taxonomies', label: 'Taxonomies' },
+              { link: '/admin/settings/data_ingestion', label: 'Data ingestion' },
             ],
           )}
         </MenuList>
