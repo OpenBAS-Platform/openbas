03ba3d8f6293468b7c06715b4cf0a47a17d24de4
Christian DONGMO <christian.dongmo@filigran.io>
2024-06-24 17:22:59 +0200

    [Frontend] 1058 - Creating an Scenario, simulation or atomic testing should redirect you to the page of the element created (#1124)
    Redirection to the detail page is done

--- openbas-front/src/admin/components/simulations/simulation/ExerciseCreation.tsx
+++ openbas-front/src/admin/components/simulations/simulation/ExerciseCreation.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import ButtonCreate from '../../../../components/common/ButtonCreate';
 import Drawer from '../../../../components/common/Drawer';
 import { useFormatter } from '../../../../components/i18n';
@@ -6,15 +7,19 @@ import { useAppDispatch } from '../../../../utils/hooks';
 import type { ExerciseCreateInput } from '../../../../utils/api-types';
 import { addExercise } from '../../../../actions/Exercise';
 import ExerciseCreationForm from './ExerciseCreationForm';
+import type { ExerciseStore } from '../../../../actions/exercises/Exercise';
 
 const ExerciseCreation = () => {
   // Standard hooks
   const [open, setOpen] = useState(false);
   const { t } = useFormatter();
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const onSubmit = (data: ExerciseCreateInput) => {
-    dispatch(addExercise(data));
-    setOpen(false);
+    dispatch(addExercise(data)).then((result: { result: string, entities: { scenarios: Record<string, ExerciseStore> } }) => {
+      setOpen(false);
+      navigate(`/admin/exercises/${result.result}`);
+    });
   };
   return (
     <>
