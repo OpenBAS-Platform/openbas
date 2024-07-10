3e035abec8e4c73ea365633bc388739be208cf84
Christian-DONGMO <christian.dongmo@filigran.io>
2024-07-10 17:59:06 +0200

    [Backend] 1057 - Duplicate an exercise Correction from demo

--- openbas-api/src/main/java/io/openbas/utils/Constants.java
+++ openbas-api/src/main/java/io/openbas/utils/Constants.java
@@ -2,6 +2,8 @@ package io.openbas.utils;
 
 public class Constants {
 
+    private Constants () {}
+
     public static final int MAX_SIZE_OF_STRING = 255;
     public static final String ARTICLES = "articles";
 }
