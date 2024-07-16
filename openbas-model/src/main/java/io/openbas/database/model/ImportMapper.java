270a6e5842c2eaa531bb94088b744594eec8137b
Gael Leblan <gael.leblan@filigran.io>
2024-07-16 14:25:15 +0200

    [backend] Adding XLS importer (#1092)
    Co-authored-by: Johanah LEKEU <49673066+johanah29@users.noreply.github.com>

---
+++ openbas-model/src/main/java/io/openbas/database/model/ImportMapper.java
@@ -0,0 +1,80 @@
+package io.openbas.database.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.openbas.database.audit.ModelBaseListener;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "import_mappers")
+@EntityListeners(ModelBaseListener.class)
+public class ImportMapper implements Base {
+
+    @Id
+    @Column(name = "mapper_id")
+    @JsonProperty("import_mapper_id")
+    @GeneratedValue
+    @UuidGenerator
+    @NotNull
+    private UUID id;
+
+    @Column(name = "mapper_name")
+    @JsonProperty("import_mapper_name")
+    @NotBlank
+    private String name;
+
+    @Column(name = "mapper_inject_type_column")
+    @JsonProperty("import_mapper_inject_type_column")
+    @NotNull
+    private String injectTypeColumn;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    @JoinColumn(name="importer_mapper_id", nullable = false)
+    @JsonProperty("inject_importers")
+    private List<InjectImporter> injectImporters = new ArrayList<>();
+
+    @CreationTimestamp
+    @Column(name="mapper_created_at")
+    @JsonProperty("import_mapper_created_at")
+    private Instant creationDate;
+
+    @UpdateTimestamp
+    @Column(name= "mapper_updated_at")
+    @JsonProperty("import_mapper_updated_at")
+    private Instant updateDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || !Base.class.isAssignableFrom(o.getClass())) return false;
+        Base base = (Base) o;
+        return id.equals(base.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String getId() {
+        return this.id != null ? this.id.toString(): "";
+    }
+
+    @Override
+    public void setId(String id) {
+        this.id = UUID.fromString(id);
+    }
+}
---
+++ openbas-model/src/main/java/io/openbas/database/repository/ImportMapperRepository.java
@@ -0,0 +1,18 @@
+package io.openbas.database.repository;
+
+import io.openbas.database.model.ImportMapper;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ImportMapperRepository extends CrudRepository<ImportMapper, UUID> {
+
+    @NotNull
+    Page<ImportMapper> findAll(@NotNull Specification<ImportMapper> spec, @NotNull Pageable pageable);
+}
