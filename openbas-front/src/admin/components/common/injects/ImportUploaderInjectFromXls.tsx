23a33b0e99baa34cd225c84c5289d30a31c5024a
Gael Leblan <gael.leblan@filigran.io>
2024-07-22 17:55:07 +0200

    [backend] Fixing a few errors

--- openbas-front/src/admin/components/common/injects/ImportUploaderInjectFromXls.tsx
+++ openbas-front/src/admin/components/common/injects/ImportUploaderInjectFromXls.tsx
@@ -8,7 +8,7 @@ import ImportUploaderInjectFromXlsFile from './ImportUploaderInjectFromXlsFile';
 import ImportUploaderInjectFromXlsInjects from './ImportUploaderInjectFromXlsInjects';
 import { InjectContext } from '../Context';
 import { storeXlsFile } from '../../../../actions/mapper/mapper-actions';
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 const ImportUploaderInjectFromXls = () => {
   // Standard hooks
--- openbas-front/src/admin/components/settings/data_ingestion/xls_formatter/RulesContractContent.tsx
+++ openbas-front/src/admin/components/settings/data_ingestion/xls_formatter/RulesContractContent.tsx
@@ -269,17 +269,17 @@ const RulesContractContent: React.FC<Props> = ({
                             || methods.getValues(`mapper_inject_importers.${index}.inject_importer_rule_attributes.${rulesIndex}.rule_attribute_default_value`)?.length === 0)
                             && (!methods.getValues(`mapper_inject_importers.${index}.inject_importer_rule_attributes.${rulesIndex}.rule_attribute_additional_config.timePattern`)
                             || methods.getValues(`mapper_inject_importers.${index}.inject_importer_rule_attributes.${rulesIndex}.rule_attribute_additional_config`)?.timePattern?.length === 0)}
-                          >
+                        >
                           <CogOutline />
                         </Badge>
                       ) : (
-                      <Badge
+                        <Badge
                           color="secondary" variant="dot"
                           invisible={!methods.getValues(`mapper_inject_importers.${index}.inject_importer_rule_attributes.${rulesIndex}.rule_attribute_default_value`)
                               || methods.getValues(`mapper_inject_importers.${index}.inject_importer_rule_attributes.${rulesIndex}.rule_attribute_default_value`)?.length === 0}
-                      >
-                        <CogOutline />
-                      </Badge>)
+                        >
+                          <CogOutline />
+                        </Badge>)
                     }
                   </IconButton>
                 </div>
