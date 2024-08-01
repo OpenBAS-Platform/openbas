cadd9f8a70aca034ce0b640ab0ce9504c54bf2a7
Johanah LEKEU <johanah.lekeu@filigran.io>
2024-08-01 15:21:00 +0200

    Resolve comment

--- openbas-front/src/actions/injects/Inject.d.ts
+++ openbas-front/src/actions/injects/Inject.d.ts
@@ -9,8 +9,9 @@ export type InjectInput = {
   inject_depends_duration_seconds: number;
 };
 
-export type InjectStore = Omit<Inject, 'inject_tags' | 'inject_content' | 'inject_injector_contract'> & {
+export type InjectStore = Omit<Inject, 'inject_tags' | 'inject_content' | 'inject_injector_contract' | 'inject_teams'> & {
   inject_tags: string[] | undefined;
+  inject_teams: string[] | undefined;
   inject_content: { expectationScore: number, challenges: string[] | undefined }
   inject_injector_contract: {
     // as we don't know the type of the content of a contract we need to put any here
