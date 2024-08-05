6faae22d36aadb182eb878dc366e1e5d96cacf8f
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-08-05 13:02:22 +0200

    [backend/frontend] refact

--- openbas-model/src/main/java/io/openbas/database/repository/InjectExpectationRepository.java
+++ openbas-model/src/main/java/io/openbas/database/repository/InjectExpectationRepository.java
@@ -102,7 +102,7 @@ public interface InjectExpectationRepository extends CrudRepository<InjectExpect
 
     @Query(value = "SELECT "
             + "team_id, asset_id, asset_group_id, inject_expectation_type, "
-            + "inject_expectation_score, inject_expectation_expected_score, inject_expectation_id, exercise_id "
+            + "inject_expectation_score, inject_expectation_group, inject_expectation_expected_score, inject_expectation_id, exercise_id "
             + "FROM injects_expectations i "
             + "where i.inject_expectation_id IN :ids",
             nativeQuery = true)
