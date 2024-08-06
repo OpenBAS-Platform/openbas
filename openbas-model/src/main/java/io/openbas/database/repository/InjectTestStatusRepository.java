fc30ab3167dbb91af342efc14ea1d240d4b9e6a9
Johanah LEKEU <johanah.lekeu@filigran.io>
2024-08-06 13:27:36 +0200

    Finalize test service and db persistence

---
+++ openbas-model/src/main/java/io/openbas/database/repository/InjectTestStatusRepository.java
@@ -0,0 +1,18 @@
+package io.openbas.database.repository;
+
+import io.openbas.database.model.Inject;
+import io.openbas.database.model.InjectTestStatus;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface InjectTestStatusRepository extends CrudRepository<InjectTestStatus, String>,
+    JpaSpecificationExecutor<InjectTestStatus> {
+
+  @NotNull
+  Optional<InjectTestStatus> findById(@NotNull String id);
+
+  Optional<InjectTestStatus> findByInject(@NotNull Inject inject);
+}
