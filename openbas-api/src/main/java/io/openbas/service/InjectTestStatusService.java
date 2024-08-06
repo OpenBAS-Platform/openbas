af09d5da5557f4810bcccef0585d3bf2541a809e
Johanah LEKEU <johanah.lekeu@filigran.io>
2024-08-06 13:27:36 +0200

    Finalize test service and db persistence

---
+++ openbas-api/src/main/java/io/openbas/service/InjectTestStatusService.java
@@ -0,0 +1,60 @@
+package io.openbas.service;
+
+import io.openbas.database.model.*;
+import io.openbas.database.repository.InjectRepository;
+import io.openbas.database.repository.InjectTestStatusRepository;
+import io.openbas.database.repository.UserRepository;
+import io.openbas.execution.ExecutableInject;
+import io.openbas.execution.ExecutionContext;
+import io.openbas.execution.ExecutionContextService;
+import io.openbas.execution.Injector;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static io.openbas.config.SessionHelper.currentUser;
+
+@Service
+@Log
+@RequiredArgsConstructor
+public class InjectTestStatusService {
+
+  private ApplicationContext context;
+  private final UserRepository userRepository;
+  private final InjectRepository injectRepository;
+  private final ExecutionContextService executionContextService;
+  private final InjectTestStatusRepository injectTestStatusRepository;
+
+  @Autowired
+  public void setContext(ApplicationContext context) {
+    this.context = context;
+  }
+
+  @Transactional
+  public InjectTestStatus testInject(String injectId) {
+    Inject inject = injectRepository.findById(injectId).orElseThrow();
+    User user = this.userRepository.findById(currentUser().getId()).orElseThrow();
+    List<ExecutionContext> userInjectContexts = List.of(
+        this.executionContextService.executionContext(user, inject, "Direct test")
+    );
+    Injector executor = context.getBean(
+        inject.getInjectorContract().map(injectorContract -> injectorContract.getInjector().getType()).orElseThrow(),
+        io.openbas.execution.Injector.class);
+    ExecutableInject injection = new ExecutableInject(false, true, inject, List.of(), inject.getAssets(),
+        inject.getAssetGroups(), userInjectContexts);
+    Execution execution = executor.executeInjection(injection);
+
+    //Save inject test status
+    InjectTestStatus injectTestStatus = InjectTestStatus.fromExecutionTest(execution);
+    injectTestStatus.setInject(inject);
+    this.injectTestStatusRepository.save(injectTestStatus);
+
+    return injectTestStatus;
+  }
+
+}
