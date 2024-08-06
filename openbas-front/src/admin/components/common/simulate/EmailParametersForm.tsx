a5628a1e01b40f20339d200a385b2d08495548a8
Gael Leblan <gael.leblan@filigran.io>
2024-08-06 14:56:54 +0200

    [frontend] Fix on email adresses

--- openbas-front/src/admin/components/common/simulate/EmailParametersForm.tsx
+++ openbas-front/src/admin/components/common/simulate/EmailParametersForm.tsx
@@ -24,7 +24,7 @@ const useStyles = makeStyles(() => ({
 
 export interface SettingUpdateInput {
   setting_mail_from?: string;
-  setting_mails_reply_to?: string[];
+  setting_mails_reply_to?: { message: string }[];
   setting_message_header?: string;
 }
 
@@ -103,7 +103,7 @@ const EmailParametersForm: React.FC<Props> = ({
                 value={field.value}
                 onChange={() => {
                   if (undefined !== field.value && inputValue !== '' && !field.value.includes(inputValue)) {
-                    field.onChange([...(field.value || []), inputValue]);
+                    field.onChange([...(field.value || []), inputValue.trim()]);
                   }
                 }}
                 onBlur={field.onBlur}
@@ -133,7 +133,7 @@ const EmailParametersForm: React.FC<Props> = ({
                     label={t('Reply to')}
                     style={{ marginTop: 20 }}
                     error={!!fieldState.error}
-                    helperText={fieldState.error?.message}
+                    helperText={errors.setting_mails_reply_to?.find((value) => value != null).message ?? ''}
                   />
                 )}
               />
