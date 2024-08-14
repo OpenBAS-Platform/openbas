fba3eb49307b621e42b779fe88473275de0c21d7
Johanah LEKEU <johanah.lekeu@filigran.io>
2024-08-14 16:48:57 +0200

    Resolve drone issues

--- openbas-api/src/main/java/io/openbas/migration/V3_31__Add_Injects_tests_statuses.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_31__Add_Injects_tests_statuses.java
@@ -2,10 +2,12 @@ package io.openbas.migration;
 
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
+import org.springframework.stereotype.Component;
 
 import java.sql.Connection;
 import java.sql.Statement;
 
+@Component
 public class V3_31__Add_Injects_tests_statuses extends BaseJavaMigration {
 
   @Override
