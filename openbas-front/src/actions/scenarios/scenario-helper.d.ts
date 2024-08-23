da332e9ee859479cea625a54eaac9b116d96cc25
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-08-23 10:46:30 +0200

    [frontend] refact

--- openbas-front/src/actions/scenarios/scenario-helper.d.ts
+++ openbas-front/src/actions/scenarios/scenario-helper.d.ts
@@ -1,8 +1,13 @@
 import type { ScenarioStore } from './Scenario';
 import type { TeamStore } from '../teams/Team';
+import type { LessonsAnswer, LessonsCategory, LessonsQuestion, Objective } from '../../utils/api-types';
 
 export interface ScenariosHelper {
   getScenario: (scenarioId: string) => ScenarioStore;
   getScenarios: () => ScenarioStore[];
   getScenarioTeams: (scenarioId: string) => TeamStore[];
+  getScenarioObjectives: (scenarioId: string) => Objective[];
+  getScenarioLessonsCategories: (scenarioId: string) => LessonsCategory[];
+  getScenarioLessonsQuestions: (scenarioId: string) => LessonsQuestion[];
+  getScenarioLessonsAnswers: (scenarioId: string) => LessonsAnswer[];
 }
