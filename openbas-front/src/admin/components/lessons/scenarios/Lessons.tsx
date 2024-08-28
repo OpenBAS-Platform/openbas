25d67c879aa873e4595c5d8b6537404f88a1633e
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-08-28 18:37:08 +0200

    [frontend] close anonymization  modal

--- openbas-front/src/admin/components/lessons/scenarios/Lessons.tsx
+++ openbas-front/src/admin/components/lessons/scenarios/Lessons.tsx
@@ -323,7 +323,7 @@ const Lessons: React.FC<Props> = ({
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => setOpenEmptyLessons(false)}>
+          <Button onClick={() => setOpenAnonymize(false)}>
             {t('Cancel')}
           </Button>
           <Button color="secondary" onClick={toggleAnonymize}>
