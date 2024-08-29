8e30aece41ce0aec73ae64ac7be3b6e94970a24a
Thibaut R <thibaut.rollos@filigran.io>
2024-08-29 11:01:41 +0200

    added hello world to confirm redeployment

--- openbas-front/src/public/components/login/LoginForm.js
+++ openbas-front/src/public/components/login/LoginForm.js
@@ -19,6 +19,7 @@ const LoginForm = (props) => {
   };
   return (
     <div style={{ padding: 15 }}>
+      <h1>Hello world !</h1>
       <Form onSubmit={onSubmit} validate={validate}>
         {({ handleSubmit, submitting, pristine }) => (
           <form onSubmit={handleSubmit}>
