5825c3cd0e1d8fee79eaff085dcc8683ac895fec
Gael Leblan <gael.leblan@filigran.io>
2024-09-06 11:22:32 +0200

    [backend/frontend] Chaining injects logically

--- openbas-front/src/admin/components/common/injects/InjectChainsForm.js
+++ openbas-front/src/admin/components/common/injects/InjectChainsForm.js
@@ -2,178 +2,117 @@ import React, { Component } from 'react';
 import * as PropTypes from 'prop-types';
 import * as R from 'ramda';
 import { withStyles } from '@mui/styles';
-import { Accordion, AccordionDetails, AccordionSummary, FormControl, IconButton, InputLabel, MenuItem, Select, Tooltip, Typography } from '@mui/material';
-import { Add, DeleteOutlined, ExpandMore } from '@mui/icons-material';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import OldTextField from '../../../../components/fields/OldTextField';
 import inject18n from '../../../../components/i18n';
 
-const styles = () => ({
-  container: {
-    display: 'inline-flex',
-    alignItems: 'center',
-  },
-  red: {
-    borderColor: 'rgb(244, 67, 54)',
-  },
-  importerStyle: {
-    display: 'flex',
-    alignItems: 'center',
-    marginTop: 20,
-  },
+const styles = (theme) => ({
+    duration: {
+        marginTop: 20,
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        border: `1px solid ${theme.palette.primary.main}`,
+        borderRadius: 4,
+        padding: 15,
+    },
+    durationDisabled: {
+        marginTop: 20,
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        border: `1px solid ${theme.palette.action.disabled}`,
+        borderRadius: 4,
+        padding: 15,
+    },
+    trigger: {
+        fontFamily: 'Consolas, monaco, monospace',
+        fontSize: 12,
+        paddingTop: 15,
+        color: theme.palette.primary.main,
+    },
+    triggerDisabled: {
+        fontFamily: 'Consolas, monaco, monospace',
+        fontSize: 12,
+        paddingTop: 15,
+        color: theme.palette.action.disabled,
+    },
+    icon: {
+        paddingTop: 4,
+        display: 'inline-block',
+    },
+    text: {
+        display: 'inline-block',
+        flexGrow: 1,
+        marginLeft: 10,
+    },
+    autoCompleteIndicator: {
+        display: 'none',
+    },
 });
 
 class InjectForm extends Component {
-  render() {
-    const {
-      t,
-      values,
-      form,
-      classes,
-      injects,
-    } = this.props;
+    render() {
+        const {
+            t,
+            values,
+            form,
+            injects,
+        } = this.props;
 
-    const handleChange = (event) => {
-      form.mutators.setValue('inject_depends_to', event.target.value);
-    };
+        const handleChange = (event) => {
+            form.mutators.setValue('inject_depends_to', event.target.value);
+        };
 
-    const parents = injects.filter((currentInject) => currentInject.inject_id === values.inject_depends_on);
-    const childrens = injects.filter((currentInject) => currentInject.inject_depends_on === values.inject_id);
-
-    return (
-      <>
-        <div className={classes.importerStyle}>
-          <Typography variant="h2" sx={{ m: 0 }}>
-            {t('Parent')}
-          </Typography>
-          <IconButton
-            color="secondary"
-            aria-label="Add"
-            size="large"
-            disabled={parents.length > 0}
-          >
-            <Add fontSize="small"/>
-          </IconButton>
-        </div>
-
-        {parents.map((parent, index) => {
-          return (
-            <Accordion
-              key={parent.inject_id}
-              variant="outlined"
-              style={{ width: '100%', marginBottom: '10px' }}
-            >
-              <AccordionSummary
-                expandIcon={<ExpandMore/>}
-              >
-                <div className={classes.container}>
-                  <Typography>
-                    #{index + 1} {parent.inject_title}
-                  </Typography>
-                  <Tooltip title={t('Delete')}>
-                    <IconButton color="error">
-                      <DeleteOutlined fontSize="small"/>
-                    </IconButton>
-                  </Tooltip>
-                </div>
-              </AccordionSummary>
-              <AccordionDetails>
-                <FormControl style={{ width: '100%' }}>
-                  <InputLabel id="inject_id">{t('Inject')}</InputLabel>
-                  <Select
-                    labelId="condition"
+        return (
+            <>
+                <OldTextField
+                    variant="standard"
+                    name="inject_title"
                     fullWidth={true}
-                    value={parent.inject_id}
-                  >
-                    {injects.map((currentInject, selectIndex) => {
-                      return (<MenuItem key={`select-${index}-inject-${selectIndex}`} value={currentInject.inject_id}>{currentInject.inject_title}</MenuItem>);
-                    })}
-                  </Select>
-                </FormControl>
-                <FormControl style={{ width: '100%' }}>
-                  <InputLabel id="condition">{t('Condition')}</InputLabel>
-                  <Select
-                    labelId="condition"
-                    value={'Success'}
-                    fullWidth={true}
+                    label={t('From')}
                     disabled
-                  >
-                    <MenuItem value="Success">{t('Success')}</MenuItem>
-                  </Select>
-                </FormControl>
-              </AccordionDetails>
-            </Accordion>
-          );
-        })}
+                />
 
-        <div className={classes.importerStyle}>
-          <Typography variant="h2" sx={{ m: 0 }}>
-            {t('Childrens')}
-          </Typography>
-          <IconButton
-            color="secondary"
-            aria-label="Add"
-            size="large"
-          >
-            <Add fontSize="small"/>
-          </IconButton>
-        </div>
-        {childrens.map((parent, index) => {
-          return (
-            <Accordion
-              key={parent.inject_id}
-              variant="outlined"
-              style={{ width: '100%', marginBottom: '10px' }}
-            >
-              <AccordionSummary
-                expandIcon={<ExpandMore/>}
-              >
-                <div className={classes.container}>
-                  <Typography>
-                    #{index + 1} {parent.inject_title}
-                  </Typography>
-                  <Tooltip title={t('Delete')}>
-                    <IconButton color="error">
-                      <DeleteOutlined fontSize="small"/>
-                    </IconButton>
-                  </Tooltip>
-                </div>
-              </AccordionSummary>
-              <AccordionDetails>
-                <FormControl style={{ width: '100%' }}>
-                  <InputLabel id="inject_id">{t('Inject')}</InputLabel>
-                  <Select
-                    labelId="condition"
-                    fullWidth={true}
-                  >
-                    {injects.map((currentInject, selectIndex) => {
-                      return (<MenuItem key={`select-${index}-inject-${selectIndex}`} value={currentInject.inject_id}>{currentInject.inject_title}</MenuItem>);
-                    })}
-                  </Select>
+                <FormControl style={{ width: '100%', paddingTop: '20px' }}>
+                    <InputLabel id="condition" style={{ paddingTop: '30px' }}>{t('Condition')}</InputLabel>
+                    <Select
+                        labelId="condition"
+                        value={'Success'}
+                        fullWidth={true}
+                        disabled
+                    >
+                        <MenuItem value="Success">{t('Success')}</MenuItem>
+                    </Select>
                 </FormControl>
-                <FormControl style={{ width: '100%' }}>
-                  <InputLabel id="condition">{t('Condition')}</InputLabel>
-                  <Select
-                    labelId="condition"
-                    value={'Success'}
-                    fullWidth={true}
-                    disabled
-                  >
-                    <MenuItem value="Success">{t('Success')}</MenuItem>
-                  </Select>
+                <FormControl style={{ width: '100%', paddingTop: '20px' }}>
+                    <InputLabel id="to" style={{ paddingTop: '30px' }}>{t('To')}</InputLabel>
+                    <Select
+                        variant="standard"
+                        labelId="to"
+                        fullWidth={true}
+                        value={values.inject_depends_to}
+                        onChange={handleChange}
+                        multiple
+                    >
+                        <MenuItem key={'null'} value={null}>{t('-')}</MenuItem>
+                        {
+                            injects.map((inject) => {
+                                return <MenuItem key={inject.inject_id} value={inject.inject_id}>{inject.inject_title}</MenuItem>;
+                            })
+                        }
+                    </Select>
                 </FormControl>
-              </AccordionDetails>
-            </Accordion>
-          );
-        })}
-      </>
-    );
-  }
+            </>
+        );
+    }
 }
 
 InjectForm.propTypes = {
-  classes: PropTypes.object,
-  t: PropTypes.func,
-  tPick: PropTypes.func,
-  injects: PropTypes.object,
+    classes: PropTypes.object,
+    t: PropTypes.func,
+    tPick: PropTypes.func,
+    injects: PropTypes.object,
 };
 
 export default R.compose(inject18n, withStyles(styles))(InjectForm);
