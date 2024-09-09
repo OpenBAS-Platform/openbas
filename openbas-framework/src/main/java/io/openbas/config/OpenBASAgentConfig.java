e5e891ee555c3f183bad4898f2f6bdbd0ac95102
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-09-09 09:22:50 +0200

    [backend] Add elevation required attribute

--- openbas-framework/src/main/java/io/openbas/config/OpenBASAgentConfig.java
+++ openbas-framework/src/main/java/io/openbas/config/OpenBASAgentConfig.java
@@ -6,35 +6,14 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "openbas.rabbitmq")
+@ConfigurationProperties(prefix = "openbas.agent")
 @Data
-public class RabbitmqConfig {
+public class OpenBASAgentConfig {
 
-  @JsonProperty("rabbitmq_prefix")
-  private String prefix;
+    @JsonProperty("non_system_user")
+    private String nonSystemUser;
 
-  @JsonProperty("rabbitmq_hostname")
-  private String hostname;
-
-  @JsonProperty("rabbitmq_vhost")
-  private String vhost;
-
-  @JsonProperty("rabbitmq_ssl")
-  private boolean ssl;
-
-  @JsonProperty("rabbitmq_port")
-  private int port;
-
-  @JsonProperty("rabbitmq_management-port")
-  private int managementPort;
-
-  @JsonProperty("rabbitmq_user")
-  private String user;
-
-  @JsonProperty("rabbitmq_pass")
-  private String pass;
-
-  @JsonProperty("rabbitmq_queue-type")
-  private String queueType;
+    @JsonProperty("non_system_pwd")
+    private String nonSystemPwd;
 
 }
