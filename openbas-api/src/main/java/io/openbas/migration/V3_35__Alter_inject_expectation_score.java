fefc201d96084d0eb4a0d157619d614aed4e6603
Johanah LEKEU <johanah.lekeu@filigran.io>
2024-09-13 16:51:05 +0200

    Resolve drone errors

--- openbas-api/src/main/java/io/openbas/migration/V3_35__Alter_inject_expectation_score.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_35__Alter_inject_expectation_score.java
@@ -8,13 +8,14 @@ import java.sql.Connection;
 import java.sql.Statement;
 
 @Component
-public class V3_34__Alter_inject_expectation_score extends BaseJavaMigration {
+public class V3_35__Alter_inject_expectation_score extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
     Connection connection = context.getConnection();
     Statement select = connection.createStatement();
     select.execute(
-        "ALTER TABLE injects_expectations ALTER COLUMN inject_expectation_expected_score set not null default 100;");
+        "ALTER TABLE injects_expectations ALTER COLUMN inject_expectation_expected_score SET NOT NULL,\n"
+            + "ALTER COLUMN inject_expectation_expected_score SET DEFAULT 100;");
   }
 }
