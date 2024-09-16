c1c1668db524644f157211cb72a99e4ad13699f9
Damien Goujard <damien.goujard@filigran.io>
2024-09-16 10:45:35 +0200

    [backend] Manage migration after rebase

--- openbas-api/src/main/java/io/openbas/migration/V3_35__Add_column_status_commands_lines.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_35__Add_column_status_commands_lines.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 import java.sql.Statement;
 
 @Component
-public class V3_34__Add_column_status_commands_lines extends BaseJavaMigration {
+public class V3_35__Add_column_status_commands_lines extends BaseJavaMigration {
 
     @Override
     public void migrate(Context context) throws Exception {
