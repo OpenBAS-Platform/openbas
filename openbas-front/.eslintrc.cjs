b57c798a0a2a8d983d1c4ff21bc339c3a85a7666
Guillaume Paris <guillaume.paris@filigran.io>
2024-09-16 11:57:29 +0200

    [frontend] fix linter/ts

--- openbas-front/.eslintrc.cjs
+++ openbas-front/.eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  "root": true,
+  root: true,
   extends: [
     'airbnb-base',
     'airbnb-typescript/base',
@@ -7,26 +7,26 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-    'plugin:i18next/recommended'
+    'plugin:i18next/recommended',
   ],
   settings: {
-    "react": {
-      "version": "detect"
-    }
+    react: {
+      version: 'detect',
+    },
   },
   parserOptions: {
     ecmaVersion: 2020,
-    project: './tsconfig.json',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
     tsconfigRootDir: __dirname,
-    parser: '@typescript-eslint/parser'
+    parser: '@typescript-eslint/parser',
   },
   env: {
     browser: true,
   },
   overrides: [
     {
-      "files": ["*.jsx", "*.js", "*.ts", "*.tsx"]
-    }
+      files: ['*.jsx', '*.js', '*.ts', '*.tsx'],
+    },
   ],
   ignorePatterns: [
     '**/builder/**',
@@ -37,7 +37,7 @@ module.exports = {
     '**/__generated__/**',
     '**/src/static/ext/**',
   ],
-  plugins: ["import-newlines", "i18next", "custom-rules"],
+  plugins: ['import-newlines', 'i18next', 'custom-rules'],
   rules: {
     'custom-rules/classes-rule': 1,
     'no-restricted-syntax': 0,
@@ -48,61 +48,61 @@ module.exports = {
     'arrow-body-style': 'off',
     'max-len': [
       'error', 180, 2, {
-        'ignoreUrls': true,
-        'ignoreComments': false,
-        'ignoreRegExpLiterals': true,
-        'ignoreStrings': true,
-        'ignoreTemplateLiterals': true
-      }
+        ignoreUrls: true,
+        ignoreComments: false,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
     ],
     'no-restricted-imports': [
       'error', {
         patterns: [
           {
             group: ['@mui/material/*', '!@mui/material/locale', '!@mui/material/styles', '!@mui/material/colors', '!@mui/material/transitions'],
-            message: "Please use named import from @mui/material instead."
+            message: 'Please use named import from @mui/material instead.',
           },
           {
             group: ['@mui/styles/*'],
-            message: "Please use named import from @mui/styles instead."
-          }
-        ]
-      }
+            message: 'Please use named import from @mui/styles instead.',
+          },
+        ],
+      },
     ],
     '@typescript-eslint/naming-convention': ['error', {
-      'selector': 'variable',
-      'format': ['camelCase', 'UPPER_CASE'],
-      'leadingUnderscore': 'allow',
-      'trailingUnderscore': 'allow',
-      'filter': {
-        'regex': '/([^_]*)/',
-        'match': true
-      }
+      selector: 'variable',
+      format: ['camelCase', 'UPPER_CASE'],
+      leadingUnderscore: 'allow',
+      trailingUnderscore: 'allow',
+      filter: {
+        regex: '/([^_]*)/',
+        match: true,
+      },
     }],
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': [
       'error',
       {
-        'argsIgnorePattern': '^_',
-        'varsIgnorePattern': '^_',
-        'caughtErrorsIgnorePattern': '^_'
-      }
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
     ],
-    'import-newlines/enforce': ['error', {items: 20, 'max-len': 180}],
+    'import-newlines/enforce': ['error', { items: 20, 'max-len': 180 }],
     'react/jsx-indent': [2, 2],
     'react/jsx-indent-props': [2, 2],
     'react/jsx-closing-bracket-location': 'error',
-    "import/no-extraneous-dependencies": [
-      "error",
+    'import/no-extraneous-dependencies': [
+      'error',
       {
-        "devDependencies": [
-          "**/*.test.tsx",
-          "**/*.test.ts",
-          "vite.config.ts",
-          "vitest.config.ts",
-          "playwright.config.ts",
-        ]
-      }
-    ]
-  }
-}
+        devDependencies: [
+          '**/*.test.tsx',
+          '**/*.test.ts',
+          'vite.config.ts',
+          'vitest.config.ts',
+          'playwright.config.ts',
+        ],
+      },
+    ],
+  },
+};
