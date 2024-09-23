c2f74f8abdd10bcb6bd700379a736bb1956141a0
Marine LM <marine.lemezo@filigran.io>
2024-09-23 09:08:48 +0200

    [frontend/backend] remove updateAttributes and isListened from api-types file
    Signed-off-by: Marine LM <marine.lemezo@filigran.io>

--- openbas-model/src/main/java/io/openbas/database/model/Base.java
+++ openbas-model/src/main/java/io/openbas/database/model/Base.java
@@ -1,5 +1,6 @@
 package io.openbas.database.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Transient;
 import org.springframework.beans.BeanUtils;
 
@@ -16,11 +17,13 @@ public interface Base {
         return this.isUserHasAccess(user.isAdmin());
     }
 
+    @JsonIgnore
     @Transient
     default void setUpdateAttributes(Object input) {
         BeanUtils.copyProperties(input, this);
     }
 
+    @JsonIgnore
     default boolean isListened() {
         return true;
     }
