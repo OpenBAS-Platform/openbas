134620fafc78a407305aa651a8bee39886ecd364
Johanah LEKEU <johanah.lekeu@filigran.io>
2024-09-26 10:49:11 +0200

    Resolve drone errors

--- openbas-api/src/main/java/io/openbas/migration/V3_41__Add_column_expiration_time_expectations.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_41__Add_column_expiration_time_expectations.java
@@ -2,10 +2,12 @@ package io.openbas.migration;
 
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
+import org.springframework.stereotype.Component;
 
 import java.sql.Connection;
 import java.sql.Statement;
 
+@Component
 public class V3_41__Add_column_expiration_time_expectations extends BaseJavaMigration {
 
   @Override
