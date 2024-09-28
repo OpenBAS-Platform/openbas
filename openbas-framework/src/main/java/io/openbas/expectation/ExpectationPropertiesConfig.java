8559acbcb6225f342aafd147f49ac32317ae6acb
Romuald Lemesle <romuald.lemesle@filigran.io>
2024-09-28 16:36:32 +0200

    [backend/frontend] Add default values

--- openbas-framework/src/main/java/io/openbas/expectation/ExpectationPropertiesConfig.java
+++ openbas-framework/src/main/java/io/openbas/expectation/ExpectationPropertiesConfig.java
@@ -13,20 +13,20 @@ public class ExpectationPropertiesConfig {
   public static long DEFAULT_TECHNICAL_EXPECTATION_EXPIRATION_TIME = 21600L;
   public static long DEFAULT_HUMAN_EXPECTATION_EXPIRATION_TIME = 3600L;
 
-  @Value("${openbas.expectation.technical.expiration-time}")
+  @Value("${openbas.expectation.technical.expiration-time:#{null}}")
   private Long technicalExpirationTime;
-  @Value("${openbas.expectation.detection.expiration-time}")
+  @Value("${openbas.expectation.detection.expiration-time:#{null}}")
   private Long detectionExpirationTime;
-  @Value("${openbas.expectation.prevention.expiration-time}")
+  @Value("${openbas.expectation.prevention.expiration-time:#{null}}")
   private Long preventionExpirationTime;
 
-  @Value("${openbas.expectation.human.expiration-time}")
+  @Value("${openbas.expectation.human.expiration-time:#{null}}")
   private Long humanExpirationTime;
-  @Value("${openbas.expectation.challenge.expiration-time}")
+  @Value("${openbas.expectation.challenge.expiration-time:#{null}}")
   private Long challengeExpirationTime;
-  @Value("${openbas.expectation.article.expiration-time}")
+  @Value("${openbas.expectation.article.expiration-time:#{null}}")
   private Long articleExpirationTime;
-  @Value("${openbas.expectation.manual.expiration-time}")
+  @Value("${openbas.expectation.manual.expiration-time:#{null}}")
   private Long manualExpirationTime;
 
   public long getDetectionExpirationTime() {
