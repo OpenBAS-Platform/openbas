67224995e2d1196884b84bb8d1a932068ca7022d
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-10-03 16:45:46 +0200

    [backend] refact

--- openbas-model/src/main/java/io/openbas/database/raw/RawInjectExpectationForCompute.java
+++ openbas-model/src/main/java/io/openbas/database/raw/RawInjectExpectationForCompute.java
@@ -2,21 +2,26 @@ package io.openbas.database.raw;
 
 public interface RawInjectExpectationForCompute {
 
+  String getInject_expectation_id();
   String getInject_expectation_type();
 
   Double getInject_expectation_score();
 
   Double getInject_expectation_expected_score();
 
-  RawTeam getTeam();
+  String getTeam_id();
+  String getTeam_name();
 
-  RawAsset getAsset();
+  String getAsset_id();
+  String getAsset_name();
+  String getAsset_type();
 
-  RawAssetGroup getAsset_group();
+  String getAsset_group_id();
+  String getAsset_group_name();
+  String getAsset_group_asset_ids();
 
-  RawUser getUser();
-
-  String getInject_expectation_id();
+  String getUser_id();
+  String getUser_name();
 
   Boolean getInject_expectation_group();
 }
