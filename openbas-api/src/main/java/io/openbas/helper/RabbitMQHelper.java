d69aa0f2a686d7211ad2ed4767a91a036945b6e0
Gael Leblan <gael.leblan@filigran.io>
2024-10-07 10:31:49 +0200

    [backend] Fix call to the mgmt plugin not working when using ssl (#1618)
    (cherry picked from commit f8e15250d7c61ac8690cd5aef1d608561b94c214)

--- openbas-api/src/main/java/io/openbas/helper/RabbitMQHelper.java
+++ openbas-api/src/main/java/io/openbas/helper/RabbitMQHelper.java
@@ -1,12 +1,34 @@
 package io.openbas.helper;
 
 import io.openbas.config.RabbitmqConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.BasicHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
+import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.http.config.Registry;
+import org.apache.hc.core5.http.config.RegistryBuilder;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
+import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.hc.core5.ssl.TrustStrategy;
 import org.springframework.boot.json.BasicJsonParser;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.logging.Level;
@@ -26,14 +48,18 @@ public class RabbitMQHelper {
     public static String getRabbitMQVersion(RabbitmqConfig rabbitmqConfig) {
         // If we already have the version, we don't need to get it again
         if (rabbitMQVersion == null) {
-            RestTemplate restTemplate = new RestTemplateBuilder()
-               .setConnectTimeout(Duration.ofSeconds(2))
-               .setReadTimeout(Duration.ofSeconds(2))
-               .build();
             // Init the rabbit MQ management api overview url
-            String uri = rabbitmqConfig.isSsl() ? "https://" : "http://"
+            String uri = "http://"
                     + rabbitmqConfig.getHostname() + ":" + rabbitmqConfig.getManagementPort()
                     + "/api/overview";
+
+            RestTemplate restTemplate;
+            try {
+                restTemplate = restTemplate(rabbitmqConfig);
+            } catch (KeyStoreException | NoSuchAlgorithmException | KeyManagementException | CertificateException | IOException e) {
+                LOGGER.severe(e.getMessage());
+                return null;
+            }
 
             // Init the headers
             HttpHeaders headers = new HttpHeaders();
@@ -42,7 +68,7 @@ public class RabbitMQHelper {
             HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
 
             // Make the call
-            ResponseEntity<?> result = null;
+            ResponseEntity<?> result;
             try {
                 result = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
             } catch (RestClientException e) {
@@ -56,5 +82,47 @@ public class RabbitMQHelper {
         }
 
         return rabbitMQVersion;
+    }
+
+    private static RestTemplate restTemplate(RabbitmqConfig rabbitmqConfig)
+            throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException, IOException, CertificateException {
+        RestTemplate restTemplate = new RestTemplateBuilder()
+                .setConnectTimeout(Duration.ofSeconds(2))
+                .setReadTimeout(Duration.ofSeconds(2))
+                .build();
+
+        if(rabbitmqConfig.isSsl() && rabbitmqConfig.isManagementInsecure()) {
+            HttpComponentsClientHttpRequestFactory requestFactoryHttp = new HttpComponentsClientHttpRequestFactory();
+
+            TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
+            SSLContext sslContext = SSLContexts.custom()
+                    .loadTrustMaterial(null, acceptingTrustStrategy)
+                    .build();
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+            Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory> create()
+                    .register("https", sslsf)
+                    .register("http", new PlainConnectionSocketFactory())
+                    .build();
+
+            BasicHttpClientConnectionManager connectionManager =
+                    new BasicHttpClientConnectionManager(socketFactoryRegistry);
+            CloseableHttpClient httpClient = HttpClients.custom()
+                    .setConnectionManager(connectionManager)
+                    .build();
+            requestFactoryHttp.setHttpClient(httpClient);
+            restTemplate = new RestTemplate(requestFactoryHttp);
+        } else if (rabbitmqConfig.isSsl()) {
+            SSLContext sslContext = new SSLContextBuilder()
+                    .loadTrustMaterial(rabbitmqConfig.getTrustStore().getURL(), rabbitmqConfig.getTrustStorePassword().toCharArray()).build();
+            SSLConnectionSocketFactory sslConFactory = new SSLConnectionSocketFactory(sslContext);
+            HttpClientConnectionManager cm = PoolingHttpClientConnectionManagerBuilder.create()
+                    .setSSLSocketFactory(sslConFactory)
+                    .build();
+            CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(cm).build();
+            ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+            restTemplate = new RestTemplate(requestFactory);
+        }
+
+        return restTemplate;
     }
 }
