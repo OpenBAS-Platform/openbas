0c907eb9e683370727419225a0b2f91c0d10aeb8
Johanah LEKEU <johanah.lekeu@filigran.io>
2024-10-17 15:24:15 +0200

    [backend]Add inject execution test

--- openbas-api/src/test/java/io/openbas/utils/fixtures/InjectFixture.java
+++ openbas-api/src/test/java/io/openbas/utils/fixtures/InjectFixture.java
@@ -8,6 +8,7 @@ import static io.openbas.injectors.email.EmailContract.TYPE;
 public class InjectFixture {
 
   public static final String INJECT_EMAIL_NAME = "Test email inject";
+  public static final String INJECT_SMS_NAME = "Test sms inject";
 
     /*
      public static final String OVH_DEFAULT = "e9e902bc-b03d-4223-89e1-fca093ac79dd";
@@ -19,6 +20,15 @@ public class InjectFixture {
   public static Inject getInjectForEmailContract(InjectorContract injectorContract) {
     Inject inject = new Inject();
     inject.setTitle(INJECT_EMAIL_NAME);
+    inject.setInjectorContract(injectorContract);
+    inject.setEnabled(true);
+    inject.setDependsDuration(0L);
+    return inject;
+  }
+
+  public static Inject getInjectForSmsContract(InjectorContract injectorContract) {
+    Inject inject = new Inject();
+    inject.setTitle(INJECT_SMS_NAME);
     inject.setInjectorContract(injectorContract);
     inject.setEnabled(true);
     inject.setDependsDuration(0L);
