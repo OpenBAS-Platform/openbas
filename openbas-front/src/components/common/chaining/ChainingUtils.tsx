b65049bbf51b2e9543647b118f908c991500a264
Gael Leblan <gael.leblan@filigran.io>
2024-10-22 02:31:15 +0200

    [frontend/backend] Chaining injects conditionally (#1385)

--- openbas-front/src/components/common/chaining/ChainingUtils.tsx
+++ openbas-front/src/components/common/chaining/ChainingUtils.tsx
@@ -1,26 +1,6 @@
-import type { InjectDependency } from '../../../utils/api-types';
-import type { InjectStore } from '../../../actions/injects/Inject';
-
 const breakpointAndOr = /(&&|\|\|)/gm;
 const breakpointValue = /==/gm;
 const typeFromName = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}-(.*)-Success/mg;
-
-const fromInjectDependencyToInputDependency = (dependencies: InjectDependency[]) => {
-  const result : Record<string, string> = {};
-  if (dependencies === null) {
-    return null;
-  }
-  for (let i = 0; i < dependencies.length; i += 1) {
-    if (dependencies[i].dependency_relationship?.inject_parent_id !== undefined
-        && dependencies[i].dependency_condition !== undefined) {
-      const key = dependencies[i].dependency_relationship!.inject_parent_id;
-      if (key !== undefined) {
-        result[key as unknown as string] = dependencies[i].dependency_condition!;
-      }
-    }
-  }
-  return dependencies.length > 0 ? result : null;
-};
 
 const fromInjectDependencyToLabel = (dependency: string) => {
   let label = '';
@@ -40,19 +20,4 @@ const fromInjectDependencyToLabel = (dependency: string) => {
   return label;
 };
 
-const convertInjectStore = (injectStore: InjectStore) => {
-  const dependingOn : Record<string, string> = {};
-  injectStore.inject_depends_on?.forEach((value) => {
-    if (value.dependency_condition != null && value.dependency_relationship?.inject_parent_id !== undefined) {
-      dependingOn[value.dependency_relationship?.inject_parent_id as unknown as string] = value.dependency_condition;
-    }
-  });
-  const newResult = {
-    ...injectStore,
-    inject_depends_on: Object.keys(dependingOn).length === 0 ? null : dependingOn,
-  };
-
-  return newResult;
-};
-
-export default { fromInjectDependencyToInputDependency, fromInjectDependencyToLabel, convertInjectStore };
+export default { fromInjectDependencyToLabel };
