ca7d52eeddc636d5c116486ece68b98a0d577170
Guillaume <guillaume.paris@filigran.io>
2024-10-23 09:02:38 +0200

    [backend] Fix raw teams request (#1704)
    Co-authored-by: Stephanya Casanova <stephanya.casanova@filigran.io>

--- openbas-api/src/main/java/io/openbas/rest/helper/TeamHelper.java
+++ openbas-api/src/main/java/io/openbas/rest/helper/TeamHelper.java
@@ -17,124 +17,136 @@ import java.util.stream.Collectors;
 
 public class TeamHelper {
 
-    public static List<TeamSimple> rawTeamToSimplerTeam(List<RawTeam> teams,
-                                                        InjectExpectationRepository injectExpectationRepository,
-                                                        InjectRepository injectRepository,
-                                                        CommunicationRepository communicationRepository,
-                                                        ExerciseTeamUserRepository exerciseTeamUserRepository,
-                                                        ScenarioRepository scenarioRepository) {
-        // Getting a map of inject expectations
-        Map<String, RawInjectExpectation> mapInjectExpectation = injectExpectationRepository.rawByIds(
-                        teams.stream().flatMap(rawTeam -> rawTeam.getTeam_expectations().stream()).toList()
-                )
-                .stream().collect(Collectors.toMap(RawInjectExpectation::getInject_expectation_id, Function.identity()));
+  public static List<TeamSimple> rawTeamToSimplerTeam(List<RawTeam> teams,
+      InjectExpectationRepository injectExpectationRepository,
+      InjectRepository injectRepository,
+      CommunicationRepository communicationRepository,
+      ExerciseTeamUserRepository exerciseTeamUserRepository,
+      ScenarioRepository scenarioRepository) {
+    // Getting a map of inject expectations
+    Map<String, RawInjectExpectation> mapInjectExpectation = injectExpectationRepository.rawByIds(
+            teams.stream().flatMap(rawTeam -> rawTeam.getTeam_expectations().stream()).toList()
+        )
+        .stream().collect(Collectors.toMap(RawInjectExpectation::getInject_expectation_id, Function.identity()));
 
-        // Getting a map of communications
-        Map<String, RawCommunication> mapCommunication = communicationRepository.rawByIds(
-                        teams.stream().flatMap(rawTeam -> rawTeam.getTeam_communications().stream()).toList()
-                )
-                .stream().collect(Collectors.toMap(RawCommunication::getCommunication_id, Function.identity()));
+    // Getting a map of communications
+    Map<String, RawCommunication> mapCommunication = communicationRepository.rawByIds(
+            teams.stream().flatMap(rawTeam -> rawTeam.getTeam_communications().stream()).toList()
+        )
+        .stream().collect(Collectors.toMap(RawCommunication::getCommunication_id, Function.identity()));
 
-        // Getting a map of exercises team users by team id
-        Map<String, List<RawExerciseTeamUser>> mapExerciseTeamUser = exerciseTeamUserRepository.rawByTeamIds(
-                        teams.stream().map(RawTeam::getTeam_id).toList()
-                )
-                .stream().collect(Collectors.groupingBy(RawExerciseTeamUser::getTeam_id));
+    // Getting a map of exercises team users by team id
+    Map<String, List<RawExerciseTeamUser>> mapExerciseTeamUser = exerciseTeamUserRepository.rawByTeamIds(
+            teams.stream().map(RawTeam::getTeam_id).toList()
+        )
+        .stream().collect(Collectors.groupingBy(RawExerciseTeamUser::getTeam_id));
 
-        // Getting a map of Injects by scenarios ids
-        Map<String, Set<String>> mapInjectsByScenarioIds = scenarioRepository.rawInjectsFromScenarios(
-                teams.stream().flatMap(rawTeam -> rawTeam.getTeam_scenarios().stream()).toList()
-        ).stream().collect(Collectors.toMap(RawScenario::getScenario_id, RawScenario::getScenario_injects));
+    // Getting a map of Injects by scenarios ids
+    Map<String, Set<String>> mapInjectsByScenarioIds = scenarioRepository.rawInjectsFromScenarios(
+        teams.stream().flatMap(rawTeam -> rawTeam.getTeam_scenarios().stream()).toList()
+    ).stream().collect(Collectors.toMap(RawScenario::getScenario_id, RawScenario::getScenario_injects));
 
-        // Then, for all the raw teams, we will create a simpler team object and then send it back to the front
-        return teams.stream().map(rawTeam -> {
-            // We create the simpler team object using the raw one
-            TeamSimple teamSimple = new TeamSimple(rawTeam);
+    // Then, for all the raw teams, we will create a simpler team object and then send it back to the front
+    return teams.stream().map(rawTeam -> {
+      // We create the simpler team object using the raw one
+      TeamSimple teamSimple = new TeamSimple(rawTeam);
 
-            // We set the inject expectations
-            teamSimple.setInjectExpectations(
-                    rawTeam.getTeam_expectations().stream().map(
-                            expectation -> {
-                                // We set the inject expectation using the map we generated earlier
-                                InjectExpectation injectExpectation = new InjectExpectation();
-                                Optional<RawInjectExpectation> raw = Optional.ofNullable(mapInjectExpectation.get(expectation));
-                                raw.ifPresent(toProcess -> {
-                                    injectExpectation.setScore(toProcess.getInject_expectation_score());
-                                    injectExpectation.setExpectedScore(toProcess.getInject_expectation_expected_score());
-                                    injectExpectation.setId(toProcess.getInject_expectation_id());
-                                    injectExpectation.setExpectedScore(toProcess.getInject_expectation_expected_score());
-                                    if (toProcess.getExercise_id() != null) {
-                                        injectExpectation.setExercise(new Exercise());
-                                        injectExpectation.getExercise().setId(toProcess.getExercise_id());
-                                    }
-                                    injectExpectation.setTeam(new Team());
-                                    injectExpectation.getTeam().setId(rawTeam.getTeam_id());
-                                    injectExpectation.setType(InjectExpectation.EXPECTATION_TYPE.valueOf(toProcess.getInject_expectation_type()));
-                                });
-                                return injectExpectation;
-                            }
-                    ).toList()
-            );
+      // We set the inject expectations
+      teamSimple.setInjectExpectations(
+          rawTeam.getTeam_expectations().stream().map(
+              expectation -> {
+                // We set the inject expectation using the map we generated earlier
+                InjectExpectation injectExpectation = new InjectExpectation();
+                Optional<RawInjectExpectation> raw = Optional.ofNullable(mapInjectExpectation.get(expectation));
+                raw.ifPresent(toProcess -> {
+                  injectExpectation.setScore(toProcess.getInject_expectation_score());
+                  injectExpectation.setExpectedScore(toProcess.getInject_expectation_expected_score());
+                  injectExpectation.setId(toProcess.getInject_expectation_id());
+                  injectExpectation.setExpectedScore(toProcess.getInject_expectation_expected_score());
+                  if (toProcess.getExercise_id() != null) {
+                    injectExpectation.setExercise(new Exercise());
+                    injectExpectation.getExercise().setId(toProcess.getExercise_id());
+                  }
+                  injectExpectation.setTeam(new Team());
+                  injectExpectation.getTeam().setId(rawTeam.getTeam_id());
+                  injectExpectation.setType(
+                      InjectExpectation.EXPECTATION_TYPE.valueOf(toProcess.getInject_expectation_type()));
+                });
+                return injectExpectation;
+              }
+          ).toList()
+      );
 
-            // We set the communications using the map we generated earlier
-            // This object has content, content_html and attachments ignored because WE DON'T WANT THE FULL EXTENT
-            teamSimple.setCommunications(
-                    rawTeam.getTeam_communications().stream().map(communicationId -> {
-                        RawCommunication raw = mapCommunication.get(communicationId);
-                        Communication communication = new Communication();
-                        communication.setAck(raw.getCommunication_ack());
-                        communication.setId(raw.getCommunication_id());
-                        communication.setIdentifier(raw.getCommunication_message_id());
-                        communication.setReceivedAt(raw.getCommunication_received_at());
-                        communication.setSentAt(raw.getCommunication_sent_at());
-                        communication.setSubject(raw.getCommunication_subject());
-                        Inject inject = new Inject();
-                        inject.setId(raw.getCommunication_inject());
-                        Exercise exercise = new Exercise();
-                        exercise.setId(raw.getCommunication_exercise());
-                        inject.setExercise(exercise);
-                        communication.setInject(inject);
-                        communication.setUsers(raw.getCommunication_users().stream().map(id -> {
-                            User user = new User();
-                            user.setId(id);
-                            return user;
-                        }).toList());
-                        communication.setAnimation(raw.getCommunication_animation());
-                        communication.setFrom(raw.getCommunication_from());
-                        communication.setTo(raw.getCommunication_to());
-                        return communication;
-                    }).toList()
-            );
+      // We set the communications using the map we generated earlier
+      // This object has content, content_html and attachments ignored because WE DON'T WANT THE FULL EXTENT
+      teamSimple.setCommunications(
+          rawTeam.getTeam_communications().stream().map(communicationId -> {
+            RawCommunication raw = mapCommunication.get(communicationId);
+            Communication communication = new Communication();
+            communication.setAck(raw.getCommunication_ack());
+            communication.setId(raw.getCommunication_id());
+            communication.setIdentifier(raw.getCommunication_message_id());
+            communication.setReceivedAt(raw.getCommunication_received_at());
+            communication.setSentAt(raw.getCommunication_sent_at());
+            communication.setSubject(raw.getCommunication_subject());
+            Inject inject = new Inject();
+            inject.setId(raw.getCommunication_inject());
+            Exercise exercise = new Exercise();
+            exercise.setId(raw.getCommunication_exercise());
+            inject.setExercise(exercise);
+            communication.setInject(inject);
+            communication.setUsers(raw.getCommunication_users().stream().map(id -> {
+              User user = new User();
+              user.setId(id);
+              return user;
+            }).toList());
+            communication.setAnimation(raw.getCommunication_animation());
+            communication.setFrom(raw.getCommunication_from());
+            communication.setTo(raw.getCommunication_to());
+            return communication;
+          }).toList()
+      );
 
-            // We set the tuple of exercise/user/team
-            List<RawExerciseTeamUser> exerciseTeamUsers = mapExerciseTeamUser.get(rawTeam);
-            if(exerciseTeamUsers != null) {
-                teamSimple.setExerciseTeamUsers(exerciseTeamUsers.stream().map(
-                        ExerciseTeamUser::fromRawExerciseTeamUser
-                ).collect(Collectors.toSet()));
-            }
+      // We set the tuple of exercise/user/team
+      List<RawExerciseTeamUser> exerciseTeamUsers = mapExerciseTeamUser.get(rawTeam);
+      if (exerciseTeamUsers != null) {
+        teamSimple.setExerciseTeamUsers(exerciseTeamUsers.stream().map(
+            ExerciseTeamUser::fromRawExerciseTeamUser
+        ).collect(Collectors.toSet()));
+      }
 
-            // We set the injects linked to the scenarios
-            teamSimple.setScenariosInjects(
-                getInjectTeamsIds(teamSimple.getId(),
-                rawTeam.getTeam_scenarios().stream().flatMap(
-                    scenario -> mapInjectsByScenarioIds.get(scenario).stream()
-                ).collect(Collectors.toSet()),
-                injectRepository)
-            );
+      // We set the injects linked to the scenarios
+      teamSimple.setScenariosInjects(
+          getInjectTeamsIds(teamSimple.getId(),
+              rawTeam.getTeam_scenarios().stream().flatMap(
+                  scenario -> mapInjectsByScenarioIds.get(scenario).stream()
+              ).collect(Collectors.toSet()),
+              injectRepository)
+      );
 
-            // We set the injects linked to the exercises
-            teamSimple.setExercisesInjects(
-                getInjectTeamsIds(teamSimple.getId(),
-                rawTeam.getTeam_exercise_injects(),
-                injectRepository)
-            );
+      // We set the injects linked to the exercises
+      teamSimple.setExercisesInjects(
+          getInjectTeamsIds(teamSimple.getId(),
+              rawTeam.getTeam_exercise_injects(),
+              injectRepository)
+      );
 
-          return teamSimple;
-        }).collect(Collectors.toList());
-    }
+      return teamSimple;
+    }).collect(Collectors.toList());
+  }
 
-  private static Set<String> getInjectTeamsIds(final String teamId, Set<String> injectIds, final InjectRepository injectRepository) {
+  public static List<TeamSimple> rawAllTeamToSimplerAllTeam(List<RawTeam> teams) {
+    // Then, for all the raw teams, we will create a simpler team object and then send it back to the front
+    return teams.stream().map(rawTeam -> {
+      // We create the simpler team object using the raw one
+      TeamSimple teamSimple = new TeamSimple(rawTeam);
+
+      return teamSimple;
+    }).collect(Collectors.toList());
+  }
+
+  private static Set<String> getInjectTeamsIds(final String teamId, Set<String> injectIds,
+      final InjectRepository injectRepository) {
     Set<RawInject> rawInjectTeams = injectRepository.findRawInjectTeams(injectIds, teamId);
     return rawInjectTeams.stream()
         .map(RawInject::getInject_id)
--- openbas-model/src/main/java/io/openbas/database/model/TeamSimple.java
+++ openbas-model/src/main/java/io/openbas/database/model/TeamSimple.java
@@ -18,142 +18,149 @@ import static java.time.Instant.now;
 @Setter
 @Getter
 public class TeamSimple {
-    @JsonProperty("team_id")
-    @NotBlank
-    private String id;
 
-    @NotBlank
-    @JsonProperty("team_name")
-    @Queryable(searchable = true)
-    private String name;
+  @JsonProperty("team_id")
+  @NotBlank
+  private String id;
 
-    @JsonProperty("team_description")
-    private String description;
+  @NotBlank
+  @JsonProperty("team_name")
+  @Queryable(searchable = true)
+  private String name;
 
-    @JsonProperty("team_created_at")
-    private Instant createdAt = now();
+  @JsonProperty("team_description")
+  private String description;
 
-    @JsonProperty("team_updated_at")
-    private Instant updatedAt = now();
+  @JsonProperty("team_created_at")
+  private Instant createdAt = now();
 
-    @JsonProperty("team_tags")
-    private Set<String> tags = new HashSet<>();
+  @JsonProperty("team_updated_at")
+  private Instant updatedAt = now();
 
-    @JsonProperty("team_organization")
-    private String organization;
+  @JsonProperty("team_tags")
+  private Set<String> tags = new HashSet<>();
 
-    @JsonProperty("team_users")
-    private Set<String> users = new HashSet<>();
+  @JsonProperty("team_organization")
+  private String organization;
 
-    @JsonProperty("team_exercises")
-    private Set<String> exercises = new HashSet<>();
+  @JsonProperty("team_users")
+  private Set<String> users = new HashSet<>();
 
-    @JsonProperty("team_scenarios")
-    private Set<String> scenarios = new HashSet<>();
+  @JsonProperty("team_exercises")
+  private Set<String> exercises = new HashSet<>();
 
-    @JsonProperty("team_contextual")
-    private Boolean contextual = false;
+  @JsonProperty("team_scenarios")
+  private Set<String> scenarios = new HashSet<>();
 
-    @JsonProperty("team_exercises_users")
-    private Set<ExerciseTeamUser> exerciseTeamUsers = new HashSet<>();
+  @JsonProperty("team_contextual")
+  private Boolean contextual = false;
 
-    @JsonProperty("team_users_number")
-    public long getUsersNumber() {
-        return this.users.size();
+  @JsonProperty("team_exercises_users")
+  private Set<ExerciseTeamUser> exerciseTeamUsers = new HashSet<>();
+
+  @JsonProperty("team_users_number")
+  public long getUsersNumber() {
+    return this.users.size();
+  }
+
+  // region transient
+  @JsonProperty("team_exercise_injects")
+  private Set<String> exercisesInjects = new HashSet<>();
+
+  @JsonProperty("team_exercise_injects_number")
+  public long getExercisesInjectsNumber() {
+    return this.exercisesInjects.size();
+  }
+
+  @JsonProperty("team_scenario_injects")
+  Set<String> scenariosInjects = new HashSet<>();
+
+  @JsonProperty("team_scenario_injects_number")
+  public long getScenariosInjectsNumber() {
+    return this.scenariosInjects.size();
+  }
+
+  @JsonIgnore
+  private List<InjectExpectation> injectExpectations = new ArrayList<>();
+
+  @JsonProperty("team_inject_expectations")
+  private Set<String> getInjectExpectationsAsStringList() {
+    return getInjectExpectations().stream().map(InjectExpectation::getId).collect(Collectors.toSet());
+  }
+
+  @JsonProperty("team_injects_expectations_number")
+  public long getInjectExceptationsNumber() {
+    return getInjectExpectations().size();
+  }
+
+  @JsonProperty("team_injects_expectations_total_score")
+  @NotNull
+  public double getInjectExceptationsTotalScore() {
+    return getInjectExpectations().stream()
+        .filter((inject) -> inject.getScore() != null)
+        .mapToDouble(InjectExpectation::getScore).sum();
+  }
+
+  @JsonProperty("team_injects_expectations_total_score_by_exercise")
+  @NotNull
+  public Map<String, Double> getInjectExceptationsTotalScoreByExercise() {
+    return getInjectExpectations().stream()
+        .filter(expectation -> Objects.nonNull(expectation.getExercise()) && Objects.nonNull(expectation.getScore()))
+        .collect(Collectors.groupingBy(expectation -> expectation.getExercise().getId(),
+            Collectors.summingDouble(InjectExpectation::getScore)));
+  }
+
+  @JsonProperty("team_injects_expectations_total_expected_score")
+  @NotNull
+  public double getInjectExceptationsTotalExpectedScore() {
+    return getInjectExpectations().stream().filter(expectation -> Objects.nonNull(expectation.getExpectedScore()))
+        .mapToDouble(InjectExpectation::getExpectedScore).sum();
+  }
+
+  @JsonProperty("team_injects_expectations_total_expected_score_by_exercise")
+  @NotNull
+  public Map<String, Double> getInjectExpectationsTotalExpectedScoreByExercise() {
+    Map<String, Double> result = getInjectExpectations().stream()
+        .filter(expectation -> Objects.nonNull(expectation.getExercise()))
+        .collect(Collectors.groupingBy(expectation -> expectation.getExercise().getId(),
+            Collectors.summingDouble(InjectExpectation::getExpectedScore)));
+    return result;
+  }
+  // endregion
+
+  @JsonProperty("team_communications")
+  List<Communication> communications = new ArrayList<>();
+
+  public TeamSimple(RawTeam raw) {
+    super();
+    this.id = raw.getTeam_id();
+    this.scenarios = Optional.ofNullable(raw.getTeam_scenarios()).orElse(new HashSet<>());
+    this.exercisesInjects = Optional.ofNullable(raw.getTeam_exercise_injects()).orElse(new HashSet<>());
+    this.contextual = raw.getTeam_contextual();
+    this.exercises = Optional.ofNullable(raw.getTeam_exercises()).orElse(new HashSet<>());
+    this.createdAt = raw.getTeam_created_at();
+    this.description = raw.getTeam_description();
+    this.updatedAt = raw.getTeam_updated_at();
+    this.name = raw.getTeam_name();
+    this.organization = raw.getTeam_organization();
+    this.users = raw.getTeam_users();
+    this.tags = raw.getTeam_tags();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
-
-    // region transient
-    @JsonProperty("team_exercise_injects")
-    private Set<String> exercisesInjects = new HashSet<>();
-
-    @JsonProperty("team_exercise_injects_number")
-    public long getExercisesInjectsNumber() {
-        return this.exercisesInjects.size();
+    if (o == null || !Base.class.isAssignableFrom(o.getClass())) {
+      return false;
     }
+    Base base = (Base) o;
+    return id.equals(base.getId());
+  }
 
-    @JsonProperty("team_scenario_injects")
-    Set<String> scenariosInjects = new HashSet<>();
-
-    @JsonProperty("team_scenario_injects_number")
-    public long getScenariosInjectsNumber() {
-        return this.scenariosInjects.size();
-    }
-
-    @JsonIgnore
-    private List<InjectExpectation> injectExpectations = new ArrayList<>();
-
-    @JsonProperty("team_inject_expectations")
-    private Set<String> getInjectExpectationsAsStringList() {
-        return getInjectExpectations().stream().map(InjectExpectation::getId).collect(Collectors.toSet());
-    }
-
-    @JsonProperty("team_injects_expectations_number")
-    public long getInjectExceptationsNumber() {
-        return getInjectExpectations().size();
-    }
-
-    @JsonProperty("team_injects_expectations_total_score")
-    @NotNull
-    public double getInjectExceptationsTotalScore() {
-        return getInjectExpectations().stream()
-            .filter((inject) -> inject.getScore() != null)
-            .mapToDouble(InjectExpectation::getScore).sum();
-    }
-    @JsonProperty("team_injects_expectations_total_score_by_exercise")
-    @NotNull
-    public Map<String, Double> getInjectExceptationsTotalScoreByExercise() {
-        return getInjectExpectations().stream()
-            .filter(expectation -> Objects.nonNull(expectation.getExercise()) && Objects.nonNull(expectation.getScore()))
-            .collect(Collectors.groupingBy(expectation -> expectation.getExercise().getId(),
-                Collectors.summingDouble(InjectExpectation::getScore)));
-    }
-
-    @JsonProperty("team_injects_expectations_total_expected_score")
-    @NotNull
-    public double getInjectExceptationsTotalExpectedScore() {
-        return getInjectExpectations().stream() .filter(expectation -> Objects.nonNull(expectation.getExpectedScore())).mapToDouble(InjectExpectation::getExpectedScore).sum();
-    }
-
-    @JsonProperty("team_injects_expectations_total_expected_score_by_exercise")
-    @NotNull
-    public Map<String, Double> getInjectExpectationsTotalExpectedScoreByExercise() {
-        Map<String, Double> result = getInjectExpectations().stream()
-                .filter(expectation -> Objects.nonNull(expectation.getExercise()))
-                .collect(Collectors.groupingBy(expectation -> expectation.getExercise().getId(),
-                        Collectors.summingDouble(InjectExpectation::getExpectedScore)));
-        return result;
-    }
-    // endregion
-
-    @JsonProperty("team_communications")
-    List<Communication> communications = new ArrayList<>();
-
-    public TeamSimple(RawTeam raw) {
-        super();
-        this.id = raw.getTeam_id();
-        this.scenarios = raw.getTeam_scenarios();
-        this.exercisesInjects = raw.getTeam_exercise_injects();
-        this.contextual = raw.getTeam_contextual();
-        this.exercises = raw.getTeam_exercises();
-        this.createdAt = raw.getTeam_created_at();
-        this.description = raw.getTeam_description();
-        this.updatedAt = raw.getTeam_updated_at();
-        this.name = raw.getTeam_name();
-        this.organization = raw.getTeam_organization();
-        this.users = raw.getTeam_users();
-        this.tags = raw.getTeam_tags();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || !Base.class.isAssignableFrom(o.getClass())) return false;
-        Base base = (Base) o;
-        return id.equals(base.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
 }
