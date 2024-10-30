dd3899b71e786b3cea67394fbee07e2ed48cad3e
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-10-30 17:32:42 +0100

    [backend] spotless

--- openbas-api/src/main/java/io/openbas/rest/atomic_testing/form/AttackPatternDTO.java
+++ openbas-api/src/main/java/io/openbas/rest/atomic_testing/form/AttackPatternDTO.java
@@ -1,7 +1,25 @@
 package io.openbas.rest.atomic_testing.form;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
 public class AttackPatternDTO {
-//  attack_pattern_name
-//  attack_pattern_id
-//  attack_pattern_external_id
+
+  @JsonProperty("attack_pattern_id")
+  @NotBlank
+  private String id;
+
+  @JsonProperty("attack_pattern_name")
+  @NotBlank
+  private String name;
+
+  @JsonProperty("attack_pattern_external_id")
+  @NotBlank
+  private String externalId;
 }
