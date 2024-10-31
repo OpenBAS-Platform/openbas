7ce381c8179cd0851117283c217dadf27ea2f645
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-10-31 16:41:01 +0100

    [backend] Add tests

---
+++ openbas-api/src/test/java/io/openbas/rest/MigrationApiTest.java
@@ -0,0 +1,9 @@
+package io.openbas.rest;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class MigrationApiTest {}
