680a99868bf25eecf26c0a2e987cb0403636f1d5
Gael Leblan <gael.leblan@filigran.io>
2024-11-07 16:16:57 +0100

    [backend] Allow for use of AWS IAM Role (#1453)
    * [backend] Allow for use of AWS IAM Role

--- openbas-model/src/main/java/io/openbas/config/MinioConfig.java
+++ openbas-model/src/main/java/io/openbas/config/MinioConfig.java
@@ -1,5 +1,7 @@
 package io.openbas.config;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -7,6 +9,7 @@ import jakarta.validation.constraints.NotNull;
 
 @Component
 @ConfigurationProperties(prefix = "minio")
+@Data
 public class MinioConfig {
 
     @NotNull
@@ -21,52 +24,4 @@ public class MinioConfig {
     private int port = 9000;
     private String bucket = "openbas";
     private boolean secure = false;
-
-    public String getAccessKey() {
-        return accessKey;
-    }
-
-    public void setAccessKey(String accessKey) {
-        this.accessKey = accessKey;
-    }
-
-    public String getAccessSecret() {
-        return accessSecret;
-    }
-
-    public void setAccessSecret(String accessSecret) {
-        this.accessSecret = accessSecret;
-    }
-
-    public String getEndpoint() {
-        return endpoint;
-    }
-
-    public void setEndpoint(String endpoint) {
-        this.endpoint = endpoint;
-    }
-
-    public String getBucket() {
-        return bucket;
-    }
-
-    public void setBucket(String bucket) {
-        this.bucket = bucket;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-    public boolean isSecure() {
-        return secure;
-    }
-
-    public void setSecure(boolean secure) {
-        this.secure = secure;
-    }
 }
