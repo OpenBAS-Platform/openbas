c3ae43762af6aab3781fdb03bd8d41744a78f440
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-11-11 15:34:37 +0100

    [backend] wip

--- openbas-front/src/components/ItemTargets.tsx
+++ openbas-front/src/components/ItemTargets.tsx
@@ -36,7 +36,6 @@ const ItemTargets: FunctionComponent<Props> = ({
     truncateLimit = 6;
   }
 
-  console.log(targets);
   // Extract the first two targets as visible chips
   const visibleTargets = getVisibleItems(targets, 2);
   const tooltipLabel = getLabelOfRemainingItems(targets, 2, 'target_name');
