d0b3e5995c10172d511c5f3672644bcc619cf797
Johanah LEKEU <johanah.lekeu@filigran.io>
2024-11-12 09:21:58 +0100

    apply spotless

--- openbas-model/src/main/java/io/openbas/database/model/Exercise.java
+++ openbas-model/src/main/java/io/openbas/database/model/Exercise.java
@@ -20,11 +20,9 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-
 import java.time.Instant;
 import java.util.*;
 import java.util.stream.Collectors;
-
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.UuidGenerator;
