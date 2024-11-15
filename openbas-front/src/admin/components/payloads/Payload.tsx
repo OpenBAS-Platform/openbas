926435b14b106038f0c8227912dae5eab6c8a614
Johanah LEKEU <johanah.lekeu@filigran.io>
2024-11-15 16:24:00 +0100

    add demo feedbacks

--- openbas-front/src/admin/components/payloads/Payload.tsx
+++ openbas-front/src/admin/components/payloads/Payload.tsx
@@ -120,11 +120,7 @@ const Payload: FunctionComponent<Props> = ({
         >
           {t('External ID')}
         </Typography>
-        {selectedPayload?.payload_external_id && selectedPayload?.payload_external_id.length > 0 ? (
-          <pre>
-            <ItemCopy content={selectedPayload?.payload_external_id} />
-          </pre>
-        ) : '-'}
+        {emptyFilled(selectedPayload?.payload_external_id)}
       </Grid>
       <Grid item xs={12} style={{ paddingTop: 10 }}>
         <Typography
@@ -140,7 +136,7 @@ const Payload: FunctionComponent<Props> = ({
           gutterBottom
           style={{ marginTop: 20 }}
         >
-          {t('Attack commands')}
+          {t('Attack command')}
         </Typography>
         <pre>
           <ItemCopy content={
@@ -181,7 +177,9 @@ const Payload: FunctionComponent<Props> = ({
                                 {argument.key}
                               </TableCell>
                               <TableCell>
-                                {argument.default_value}
+                                <pre>
+                                  <ItemCopy content={argument.default_value} />
+                                </pre>
                               </TableCell>
                             </TableRow>
                           </>
@@ -204,7 +202,7 @@ const Payload: FunctionComponent<Props> = ({
           selectedPayload?.payload_prerequisites && selectedPayload?.payload_prerequisites.length === 0 ? '-'
             : (
                 <TableContainer component={Paper}>
-                  <Table sx={{ minWidth: 650 }}>
+                  <Table sx={{ minWidth: 650, justifyContent: 'center' }}>
                     <TableHead>
                       <TableRow sx={{ textTransform: 'uppercase', fontWeight: 'bold' }}>
                         <TableCell width="30%">{t('Command executor')}</TableCell>
@@ -226,7 +224,13 @@ const Payload: FunctionComponent<Props> = ({
                                 {prerequisite.get_command}
                               </TableCell>
                               <TableCell>
-                                {prerequisite.check_command}
+                                {prerequisite.check_command !== undefined
+                                  ? (
+                                      <pre>
+                                        <ItemCopy content={prerequisite.check_command} />
+                                      </pre>
+                                    ) : '-'}
+
                               </TableCell>
                             </TableRow>
                           </>
@@ -250,7 +254,7 @@ const Payload: FunctionComponent<Props> = ({
           gutterBottom
           style={{ marginTop: 20 }}
         >
-          {t('Cleanup commands')}
+          {t('Cleanup command')}
         </Typography>
         {selectedPayload?.payload_cleanup_command && selectedPayload?.payload_cleanup_command.length > 0
           ? <pre><ItemCopy content={selectedPayload?.payload_cleanup_command} /></pre> : '-'}
