7199bb83d94199d727a4047a4b78e3954a471076
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-11-21 17:53:54 +0100

    [backend] Add architecture to command payload

--- openbas-api/src/test/java/io/openbas/utils/fixtures/PayloadFixture.java
+++ openbas-api/src/test/java/io/openbas/utils/fixtures/PayloadFixture.java
@@ -15,7 +15,6 @@ public class PayloadFixture {
     command.setContent("cd ..");
     command.setExecutor("PowerShell");
     command.setPlatforms(new Endpoint.PLATFORM_TYPE[] {Endpoint.PLATFORM_TYPE.Windows});
-    command.setExecutableArch(PlatformArchitecture.x86_64);
     command.setSource(MANUAL);
     command.setStatus(VERIFIED);
     command.setAttackPatterns(Collections.emptyList());
