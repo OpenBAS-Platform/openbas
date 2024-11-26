2a326babb6120bb5ac5df1396a9439d5eb489236
Guillaume Paris <guillaume.paris@filigran.io>
2024-11-26 10:14:18 +0100

    [frontend] pass to v8 e2e test & coverage

--- openbas-front/playwright.config.ts
+++ openbas-front/playwright.config.ts
@@ -20,16 +20,36 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['monocart-reporter', {
+      name: `OpenBAS Report`,
+      outputFile: './test-results/report.html',
+      // global coverage report options
+      coverage: {
+        entryFilter: () => true,
+        sourceFilter: (sourcePath: string) => sourcePath.startsWith('src'),
+      },
+      /*
+      onEnd: async (reportData) => {
+        // teams integration with webhook
+        await teamsWebhook(reportData);
+      } */
+    }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://localhost:3001',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    ignoreHTTPSErrors: true,
   },
-
+  expect: { timeout: 60000 },
+  timeout: 200000,
   /* Configure projects for major browsers */
   projects: [
     { name: 'setup', testMatch: /.*\.setup\.ts/ },
@@ -38,6 +58,10 @@ export default defineConfig({
       use: {
         ...devices['Desktop Chrome'],
         storageState: 'tests_e2e/.auth/user.json',
+        viewport: {
+          width: 1920,
+          height: 1080,
+        },
       },
       dependencies: ['setup'],
     },
---
+++ openbas-front/tests_e2e/conf/mcr.config.ts
@@ -0,0 +1,27 @@
+import { CoverageReportOptions } from 'monocart-coverage-reports';
+
+// https://github.com/cenfun/monocart-coverage-reports
+const coverageOptions: CoverageReportOptions = {
+
+  name: 'OpenBAS Playwright Coverage Report',
+
+  reports: [
+    'console-details',
+    'v8',
+    'lcovonly',
+  ],
+
+  entryFilter: {
+    '**/node_modules/**': false,
+    '**/**': true,
+  },
+
+  sourceFilter: {
+    '**/node_modules/**': false,
+    '**/**': true,
+  },
+
+  outputDir: './test-results/coverage',
+};
+
+export default coverageOptions;
