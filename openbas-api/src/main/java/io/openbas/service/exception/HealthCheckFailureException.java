fec17566716bc5319e6b4c5e9970fa51b3551905
Hedi Tarchouni <hedi.tarchouni@filigran.io>
2024-11-28 17:35:51 +0100

    formatted with spotless

--- openbas-api/src/main/java/io/openbas/service/exception/HealthCheckFailureException.java
+++ openbas-api/src/main/java/io/openbas/service/exception/HealthCheckFailureException.java
@@ -1,11 +1,11 @@
 package io.openbas.service.exception;
 
 public class HealthCheckFailureException extends Exception {
-    public HealthCheckFailureException(String message) {
-        super(message);
+  public HealthCheckFailureException(String message) {
+    super(message);
+  }
 
-    }
-    public HealthCheckFailureException(String message, Throwable cause) {
-      super(message, cause);
-    }
+  public HealthCheckFailureException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
