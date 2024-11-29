3a1b7f7212af1e409a907747e3c4b9a42440723a
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-11-29 19:55:03 +0100

    [backend/frontend] wip

--- openbas-api/src/main/java/io/openbas/rest/collector_frontend_error/LogApi.java
+++ openbas-api/src/main/java/io/openbas/rest/collector_frontend_error/LogApi.java
@@ -4,10 +4,8 @@ import io.openbas.rest.collector_frontend_error.form.ErrorDetailsInput;
 import io.openbas.rest.helper.RestBehavior;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
