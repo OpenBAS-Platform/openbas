f951d5c9da0adf7d724571fb89e87e8ea68f7e1d
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-11-29 16:24:47 +0100

    [backend/frontend] wip

---
+++ openbas-api/src/main/java/io/openbas/rest/collector_frontend_error/LogController.java
@@ -0,0 +1,23 @@
+package io.openbas.rest.collector_frontend_error;
+
+import io.openbas.rest.collector_frontend_error.form.ErrorDetailsInput;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/logs")
+public class LogController {
+
+  public static final Logger logger = LoggerFactory.getLogger(LogController.class);
+
+  @PostMapping("/frontend-error")
+  public ResponseEntity<Void> logError(@RequestBody ErrorDetailsInput errorDetails) {
+    logger.error("Error received: " + errorDetails);
+    return ResponseEntity.ok().build();
+  }
+}
