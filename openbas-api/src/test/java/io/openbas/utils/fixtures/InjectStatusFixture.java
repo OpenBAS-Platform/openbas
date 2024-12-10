b0fe61a33acbe5803c1d4094259f131c3a23af73
Johanah LEKEU <johanah.lekeu@filigran.io>
2024-12-10 12:21:48 +0100

    Resolve drone errors

--- openbas-api/src/test/java/io/openbas/utils/fixtures/InjectStatusFixture.java
+++ openbas-api/src/test/java/io/openbas/utils/fixtures/InjectStatusFixture.java
@@ -2,17 +2,20 @@ package io.openbas.utils.fixtures;
 
 import io.openbas.database.model.ExecutionStatus;
 import io.openbas.database.model.InjectStatus;
-import io.openbas.database.model.InjectStatusCommandLine;
+import io.openbas.database.model.PayloadCommandBlock;
+import io.openbas.database.model.StatusPayload;
+
 import java.time.Instant;
 import java.util.List;
 
 public class InjectStatusFixture {
+
   public static InjectStatus createDefaultInjectStatus() {
     InjectStatus injectStatus = new InjectStatus();
     injectStatus.setTrackingSentDate(Instant.now());
     injectStatus.setName(ExecutionStatus.SUCCESS);
-    injectStatus.setCommandsLines(
-        new InjectStatusCommandLine(List.of("cmd"), List.of("clean cmd"), "id1234567"));
+    injectStatus.setPayloadOutput(new StatusPayload(null, null, null, null, null, null, null, null, null, null, null,
+        List.of(new PayloadCommandBlock("cmd", "content", List.of("clean cmd"))), "cmd"));
     return injectStatus;
   }
 }
