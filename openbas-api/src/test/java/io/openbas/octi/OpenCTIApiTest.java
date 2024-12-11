fdec0a0f7e782073d5a745cedc8c8edfdca0f047
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-12-11 08:49:02 +0100

    [frontend/backend] wip

--- openbas-api/src/test/java/io/openbas/octi/OpenCTIApiTest.java
+++ openbas-api/src/test/java/io/openbas/octi/OpenCTIApiTest.java
@@ -2,25 +2,42 @@ package io.openbas.octi;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import io.openbas.opencti.OpenCTIApi;
 import io.openbas.rest.attack_pattern.AttackPatternApi;
-import io.openbas.rest.exercise.form.ExerciseSimple;
 import io.openbas.rest.inject.InjectApi;
 import io.openbas.rest.injector_contract.InjectorContractApi;
 import io.openbas.rest.kill_chain_phase.KillChainPhaseApi;
 import io.openbas.rest.scenario.ScenarioApi;
-import java.util.Collections;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+/**
+ * Feature: OCTI OBAS - GENERATION SCENARIOS
+ *
+ * Currently, OCTI uses the following endpoints to simulate scenarios from different types of
+ * entities such as (case incidents, groupings, reports, malwares, incidents, campaigns, intrusion
+ * sets, threat actor groups, threat actors individuals):
+ *
+ * - KillChainPhasesApi -> killChainPhases [GET: /api/kill-chain-phases/]
+ * - AttackPatternApi -> attackPatterns [GET: /api/attack_patterns/]
+ * - AttackPatternApi -> injectorContracts [GET: /api/attack_patterns/{attackPatternId}/injector_contracts]
+ * - InjectorContractApi -> injectorContracts [POST: /api/injector_contracts/search]
+ * - ScenarioApi -> creationScenario [POST: /api/scenarios/]
+ * - InjectApi -> createInjectForScenario [POST: /api/injects/{scenarioId}/injects]
+ * - OpenCTIApi -> latestExerciseByExternalId [GET: /api/opencti/v1/exercises/latest/{externalReferenceId}]
+ *
+ * These non-regression tests will help us maintain control over any modifications to these
+ * endpoints.
+ */
 @ExtendWith(MockitoExtension.class)
 public class OpenCTIApiTest {
 
@@ -33,32 +50,26 @@ public class OpenCTIApiTest {
 
   private MockMvc mockMvc;
 
-  /**
-   * Feature: OCTI OBAS - GENERATION SCENARIOS
-   *
-   * Currently, OCTI uses the following endpoints to simulate scenarios from different types of
-   * entities such as (case incidents, groupings, reports, malwares, incidents, campaigns, intrusion
-   * sets, threat actor groups, threat actors individuals):
-   *
-   * - KillChainPhasesApi -> killChainPhases [GET: /api/kill-chain-phases/]
-   * - AttackPatternApi -> attackPatterns [GET: /api/attack_patterns/]
-   * - AttackPatternApi -> injectorContracts [GET: /api/attack_patterns/{attackPatternId}/injector_contracts]
-   * - InjectorContractApi -> injectorContracts [POST: /api/injector_contracts/search]
-   * - ScenarioApi -> creationScenario [POST: /api/scenarios/]
-   * - InjectApi -> createInjectForScenario [POST: /api/injects/{scenarioId}/injects]
-   * - OpenCTIApi -> latestExerciseByExternalId [GET: /api/opencti/v1/exercises/latest/{externalReferenceId}]
-   *
-   * These non-regression tests will help us maintain control over any modifications to these
-   * endpoints.
-   */
+  @BeforeEach
+  public void setUp() {
+    // Initialize mocks before each test
+    MockitoAnnotations.openMocks(this);
+
+    // Set up MockMvc with controllers
+    mockMvc =
+        MockMvcBuilders.standaloneSetup(
+                killChainPhaseApi,
+                attackPatternApi,
+                injectorContractApi,
+                scenarioApi,
+                injectApi,
+                openCTIApi)
+            .build();
+  }
 
   // -- KILL CHAIN PHASES --
-
   @Test
   public void testGetKillChainPhases_Sucess() throws Exception {
-    // -- PREPARE --
-    mockMvc = MockMvcBuilders.standaloneSetup(killChainPhaseApi).build();
-
     // -- EXECUTE --
     mockMvc
         .perform(get("/api/kill_chain_phases").contentType(MediaType.APPLICATION_JSON))
@@ -68,8 +79,6 @@ public class OpenCTIApiTest {
   // -- ATTACK PATTERNS  --
   @Test
   public void testGetAttackPatterns_Sucess() throws Exception {
-
-    mockMvc = MockMvcBuilders.standaloneSetup(attackPatternApi).build();
     // -- EXECUTE --
     mockMvc
         .perform(get("/api/attack_patterns").contentType(MediaType.APPLICATION_JSON))
@@ -79,11 +88,8 @@ public class OpenCTIApiTest {
   // -- INJECTOR CONTRACTS --
   @Test
   public void testGetInjectorContracts_Success() throws Exception {
-
-    mockMvc = MockMvcBuilders.standaloneSetup(injectorContractApi).build();
     // -- PREPARE --
     String attackPatternId = "attackPatternId";
-
     // -- EXECUTE --
     mockMvc
         .perform(
@@ -94,8 +100,6 @@ public class OpenCTIApiTest {
 
   @Test
   public void testSearchInjectorContracts_ValidInput() throws Exception {
-
-    mockMvc = MockMvcBuilders.standaloneSetup(injectorContractApi).build();
     // -- PREPARE --
     String jsonInput =
         "{"
@@ -112,7 +116,6 @@ public class OpenCTIApiTest {
             + "\"textSearch\": \"searchText\", "
             + "\"sorts\": [{\"property\": \"label\", \"direction\": \"asc\"}]"
             + "}";
-
     // -- EXECUTE --
     mockMvc
         .perform(
@@ -123,11 +126,8 @@ public class OpenCTIApiTest {
   }
 
   // -- SCENARIO --
-
   @Test
   public void testCreateScenario_ValidInput() throws Exception {
-
-    mockMvc = MockMvcBuilders.standaloneSetup(scenarioApi).build();
     // -- PREPARE --
     String jsonInput =
         "{"
@@ -148,42 +148,18 @@ public class OpenCTIApiTest {
         .andExpect(status().isOk());
   }
 
-  // -- CREATE INJECTS --
-
+  // -- INJECTS --
   @Test
   public void testCreateInjectForScenario_ValidInput() throws Exception {
-
-    mockMvc = MockMvcBuilders.standaloneSetup(injectApi).build();
     // -- PREPARE --
     String jsonInput =
         "{"
             + "\"inject_title\": \"Valid Title\", "
-            + "\"inject_description\": \"Valid Description\", "
+            + "\"inject_type\": \"openbas_email\", "
             + "\"inject_injector_contract\": \"contract-id\", "
             + "\"inject_content\": null, "
             + "\"inject_depends_duration\": 100, "
-            + "\"inject_teams\": [\"Team1\"], "
-            + "\"inject_tags\": [\"Tag1\"]"
-            + "}";
-
-    // -- EXECUTE --
-    mockMvc
-        .perform(
-            post("/api/scenarios/{scenarioId}/injects", "scenario-id")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonInput))
-        .andExpect(status().isOk());
-  }
-
-  @Test
-  public void testCreateInjectForScenario_InputWithAdditionalParam() throws Exception {
-
-    mockMvc = MockMvcBuilders.standaloneSetup(injectApi).build();
-    // -- PREPARE --
-    String jsonInput =
-        "{"
-            + "\"inject_title\": null, "
-            + "\"inject_description\": \"Test for updated InjectInput with param architectureRequest for obas >=v1.10 and octi >=v6.4\", "
+            + "\"inject_tags\": [\"Tag1\"], "
             + "\"inject_additional_param\": \"additional_param\""
             + "}";
 
@@ -197,23 +173,14 @@ public class OpenCTIApiTest {
   }
 
   // -- LAST EXERCISE BY EXTERNAL ID --
-
   @Test
   public void testGetLatestExerciseByExternalReference_Success() throws Exception {
-
-    mockMvc = MockMvcBuilders.standaloneSetup(openCTIApi).build();
-    // -- PREPARE --
     String externalReferenceId = "valid-id";
-    ExerciseSimple exercise = new ExerciseSimple();
-    exercise.setId("exercise-id");
-    exercise.setExpectationResultByTypes(Collections.emptyList());
 
-    // -- EXECUTE --
     mockMvc
         .perform(
-            get("/opencti/v1/exercises/latest/{externalReferenceId}", externalReferenceId)
+            get("/api/opencti/v1/exercises/latest/{externalReferenceId}", externalReferenceId)
                 .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk())
-        .andExpect(header().string("version", "v1"));
+        .andExpect(status().isOk());
   }
 }
