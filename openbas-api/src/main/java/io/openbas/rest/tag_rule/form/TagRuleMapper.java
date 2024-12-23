a610f46a1e2512100fc93037f67f19c2dfdf047c
Hedi Tarchouni <hedi.tarchouni@filigran.io>
2024-12-23 15:03:10 +0100

    add tagrule mapper

---
+++ openbas-api/src/main/java/io/openbas/rest/tag_rule/form/TagRuleMapper.java
@@ -0,0 +1,16 @@
+package io.openbas.rest.tag_rule.form;
+
+import io.openbas.database.model.Asset;
+import io.openbas.database.model.TagRule;
+import java.util.stream.Collectors;
+
+public class TagRuleMapper {
+  public static TagRuleOutput toTagRuleOutput(final TagRule tagRule) {
+    return TagRuleOutput.builder()
+        .id(tagRule.getId())
+        .tagName(tagRule.getTag().getName())
+        .assets(
+            tagRule.getAssets().stream().collect(Collectors.toMap(Asset::getId, Asset::getName)))
+        .build();
+  }
+}
