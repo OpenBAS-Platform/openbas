099c08c91ff4cbf73501326ff614f7de8cf8d233
Hedi Tarchouni <hedi.tarchouni@filigran.io>
2024-12-24 10:52:37 +0100

    spotless

--- openbas-api/src/test/java/io/openbas/rest/tag_rule/TagRuleApiTest.java
+++ openbas-api/src/test/java/io/openbas/rest/tag_rule/TagRuleApiTest.java
@@ -2,7 +2,6 @@ package io.openbas.rest.tag_rule;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
--- openbas-api/src/test/java/io/openbas/service/TagRuleServiceTest.java
+++ openbas-api/src/test/java/io/openbas/service/TagRuleServiceTest.java
@@ -2,7 +2,6 @@ package io.openbas.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
