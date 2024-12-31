b25d4fd83a31f247252e9e813342ad5cda6e958b
Damien Goujard <damien.goujard@filigran.io>
2024-12-31 17:25:20 +0100

    [frontend] Delete unused file

--- openbas-front/src/admin/components/assets/security_platforms/SecurityPlatform.d.ts
+++
@@ -1,5 +0,0 @@
-import type { SecurityPlatform } from '../../../../utils/api-types';
-
-export type SecurityPlatformStore = Omit<SecurityPlatform, 'asset_tags'> & {
-  asset_tags: string[] | undefined;
-};
