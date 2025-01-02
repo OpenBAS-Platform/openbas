409c0f656568e61a33e5739d7d695f1bdb938581
Johanah LEKEU <johanah.lekeu@filigran.io>
2025-01-02 11:54:59 +0100

    [Backend]Test teams creation/update

--- openbas-api/src/test/java/io/openbas/utils/fixtures/TeamFixture.java
+++ openbas-api/src/test/java/io/openbas/utils/fixtures/TeamFixture.java
@@ -2,11 +2,38 @@ package io.openbas.utils.fixtures;
 
 import io.openbas.database.model.Team;
 import io.openbas.database.model.User;
+import io.openbas.rest.team.form.TeamCreateInput;
 import java.util.ArrayList;
+import java.util.List;
 
 public class TeamFixture {
 
   public static final String TEAM_NAME = "My team";
+
+  public static TeamCreateInput createTeam() {
+    TeamCreateInput teamCreateInput = new TeamCreateInput();
+    teamCreateInput.setName("Test team");
+    teamCreateInput.setDescription("Team description");
+    return teamCreateInput;
+  }
+
+  public static TeamCreateInput createContextualExerciseTeam(List<String> exerciseIds) {
+    TeamCreateInput teamCreateInput = new TeamCreateInput();
+    teamCreateInput.setName("Exercise team");
+    teamCreateInput.setDescription("Team description");
+    teamCreateInput.setContextual(true);
+    teamCreateInput.setExerciseIds(exerciseIds);
+    return teamCreateInput;
+  }
+
+  public static TeamCreateInput createContextualScenarioTeam(List<String> scenarioIds) {
+    TeamCreateInput teamCreateInput = new TeamCreateInput();
+    teamCreateInput.setName("Scenario team");
+    teamCreateInput.setDescription("Team description");
+    teamCreateInput.setContextual(true);
+    teamCreateInput.setScenarioIds(scenarioIds);
+    return teamCreateInput;
+  }
 
   public static Team getTeam(final User user) {
     return getTeam(user, TEAM_NAME, false); // Call the other method with default value
