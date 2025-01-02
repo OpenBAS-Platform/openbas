f74b141b3bb3241bceb5b535885326bb4bf0b212
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-01-02 16:55:36 +0100

    [backend/frontend] Add endpoint detail and refact list endpoint

--- openbas-front/src/admin/components/assets/endpoints/endpoint/Index.tsx
+++ openbas-front/src/admin/components/assets/endpoints/endpoint/Index.tsx
@@ -1,5 +1,7 @@
 import { makeStyles } from '@mui/styles';
 import { lazy } from 'react';
+import Breadcrumbs from '../../../../../components/Breadcrumbs';
+import { useFormatter } from '../../../../../components/i18n';
 
 const useStyles = makeStyles(() => ({
   item: {
@@ -16,8 +18,20 @@ const useStyles = makeStyles(() => ({
 const Endpoint = lazy(() => import('./Endpoint'));
 
 const Index = () => {
+  const classes = useStyles();
+  const { t } = useFormatter();
+
   // Fetching data
-  return (<></>);
+
+  return (
+    <Breadcrumbs
+    variant="object"
+    elements={[
+      { label: t('Assets'), link: '/admin/assets' },
+      { label: t('Endpoints'), link: '/admin/assets/endpoints' },
+    ]}
+  />
+  );
 };
 
 export default Index;
