c2042c1bd71a0d5abfb67ff899b9e1a643f8fed1
Hedi Tarchouni <hedi.tarchouni@filigran.io>
2025-01-03 17:32:34 +0100

    spotless

--- openbas-model/src/main/java/io/openbas/database/repository/TagRuleRepository.java
+++ openbas-model/src/main/java/io/openbas/database/repository/TagRuleRepository.java
@@ -1,13 +1,12 @@
 package io.openbas.database.repository;
 
 import io.openbas.database.model.TagRule;
+import java.util.List;
 import java.util.Optional;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
