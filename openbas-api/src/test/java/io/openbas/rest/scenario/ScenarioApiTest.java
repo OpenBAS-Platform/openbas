36981b360c73cdfc971da9a624fe0774ba2d2be9
Hedi Tarchouni <hedi.tarchouni@filigran.io>
2025-01-03 16:21:12 +0100

    removed unit tests

--- openbas-api/src/test/java/io/openbas/rest/scenario/ScenarioApiTest.java
+++ openbas-api/src/test/java/io/openbas/rest/scenario/ScenarioApiTest.java
@@ -1,40 +1,21 @@
 package io.openbas.rest.scenario;
 
-import static io.openbas.injectors.email.EmailContract.EMAIL_DEFAULT;
 import static io.openbas.rest.scenario.ScenarioApi.SCENARIO_URI;
 import static io.openbas.utils.JsonUtils.asJsonString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.jayway.jsonpath.JsonPath;
-import io.openbas.database.model.Asset;
-import io.openbas.database.model.Inject;
-import io.openbas.database.model.Scenario;
-import io.openbas.database.model.Tag;
-import io.openbas.database.repository.*;
+import io.openbas.database.repository.ScenarioRepository;
 import io.openbas.rest.scenario.form.ScenarioInformationInput;
 import io.openbas.rest.scenario.form.ScenarioInput;
-import io.openbas.rest.scenario.form.ScenarioUpdateTagsInput;
-import io.openbas.rest.scenario.form.UpdateScenarioInput;
-import io.openbas.service.ScenarioService;
-import io.openbas.utils.fixtures.InjectFixture;
-import io.openbas.utils.fixtures.InjectorContractFixture;
-import io.openbas.utils.fixtures.ScenarioFixture;
-import io.openbas.utils.fixtures.TagFixture;
 import io.openbas.utils.mockUser.WithMockObserverUser;
 import io.openbas.utils.mockUser.WithMockPlannerUser;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import org.junit.jupiter.api.*;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -44,25 +25,17 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(PER_CLASS)
 public class ScenarioApiTest {
-  public static final String EMAIL_DEFAULT = "138ad8f8-32f8-4a22-8114-aaa12322bd09";
 
   @Autowired private MockMvc mvc;
 
   @Autowired private ScenarioRepository scenarioRepository;
-  @Autowired private TagRepository tagRepository;
-  @Autowired private AssetRepository assetRepository;
-  @Autowired private InjectorContractRepository injectorContractRepository;
-
-  @Mock TagRepository tagRepositoryMock;
-  @Mock ScenarioService scenarioServiceMock;
-  @InjectMocks private ScenarioApi scenarioApi;
 
   static String SCENARIO_ID;
-    @Autowired
-    private InjectRepository injectRepository;
 
-  void cleanup() {
+  @AfterAll
+  void afterAll() {
     this.scenarioRepository.deleteById(SCENARIO_ID);
   }
 
@@ -76,12 +49,12 @@ public class ScenarioApiTest {
 
     // -- EXECUTE & ASSERT --
     this.mvc
-        .perform(
-            post(SCENARIO_URI)
-                .content(asJsonString(scenarioInput))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
-        .andExpect(status().is4xxClientError());
+            .perform(
+                    post(SCENARIO_URI)
+                            .content(asJsonString(scenarioInput))
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().is4xxClientError());
 
     // -- PREPARE --
     String name = "My scenario";
@@ -91,22 +64,21 @@ public class ScenarioApiTest {
 
     // -- EXECUTE --
     String response =
-        this.mvc
-            .perform(
-                post(SCENARIO_URI)
-                    .content(asJsonString(scenarioInput))
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().is2xxSuccessful())
-            .andExpect(jsonPath("$.scenario_name").value(name))
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
+            this.mvc
+                    .perform(
+                            post(SCENARIO_URI)
+                                    .content(asJsonString(scenarioInput))
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().is2xxSuccessful())
+                    .andExpect(jsonPath("$.scenario_name").value(name))
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
 
     // -- ASSERT --
     assertNotNull(response);
     SCENARIO_ID = JsonPath.read(response, "$.scenario_id");
-    cleanup();
   }
 
   @DisplayName("Retrieve scenarios")
@@ -116,16 +88,15 @@ public class ScenarioApiTest {
   void retrieveScenariosTest() throws Exception {
     // -- EXECUTE --
     String response =
-        this.mvc
-            .perform(get(SCENARIO_URI).accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().is2xxSuccessful())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
+            this.mvc
+                    .perform(get(SCENARIO_URI).accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().is2xxSuccessful())
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
 
     // -- ASSERT --
     assertNotNull(response);
-    cleanup();
   }
 
   @DisplayName("Retrieve scenario")
@@ -135,16 +106,15 @@ public class ScenarioApiTest {
   void retrieveScenarioTest() throws Exception {
     // -- EXECUTE --
     String response =
-        this.mvc
-            .perform(get(SCENARIO_URI + "/" + SCENARIO_ID).accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().is2xxSuccessful())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
+            this.mvc
+                    .perform(get(SCENARIO_URI + "/" + SCENARIO_ID).accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().is2xxSuccessful())
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
 
     // -- ASSERT --
     assertNotNull(response);
-    cleanup();
   }
 
   @DisplayName("Update scenario")
@@ -154,12 +124,12 @@ public class ScenarioApiTest {
   void updateScenarioTest() throws Exception {
     // -- PREPARE --
     String response =
-        this.mvc
-            .perform(get(SCENARIO_URI + "/" + SCENARIO_ID).accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().is2xxSuccessful())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
+            this.mvc
+                    .perform(get(SCENARIO_URI + "/" + SCENARIO_ID).accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().is2xxSuccessful())
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
 
     ScenarioInput scenarioInput = new ScenarioInput();
     String subtitle = "A subtitle";
@@ -169,21 +139,20 @@ public class ScenarioApiTest {
 
     // -- EXECUTE --
     response =
-        this.mvc
-            .perform(
-                put(SCENARIO_URI + "/" + SCENARIO_ID)
-                    .content(asJsonString(scenarioInput))
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().is2xxSuccessful())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
+            this.mvc
+                    .perform(
+                            put(SCENARIO_URI + "/" + SCENARIO_ID)
+                                    .content(asJsonString(scenarioInput))
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().is2xxSuccessful())
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
 
     // -- ASSERT --
     assertNotNull(response);
     assertEquals(subtitle, JsonPath.read(response, "$.scenario_subtitle"));
-    cleanup();
   }
 
   @DisplayName("Update scenario information")
@@ -199,21 +168,20 @@ public class ScenarioApiTest {
 
     // -- EXECUTE --
     String response =
-        this.mvc
-            .perform(
-                put(SCENARIO_URI + "/" + SCENARIO_ID + "/information")
-                    .content(asJsonString(scenarioInformationInput))
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().is2xxSuccessful())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
+            this.mvc
+                    .perform(
+                            put(SCENARIO_URI + "/" + SCENARIO_ID + "/information")
+                                    .content(asJsonString(scenarioInformationInput))
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().is2xxSuccessful())
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
 
     // -- ASSERT --
     assertNotNull(response);
     assertEquals(header, JsonPath.read(response, "$.scenario_message_header"));
-    cleanup();
   }
 
   @DisplayName("Delete scenario")
@@ -223,68 +191,7 @@ public class ScenarioApiTest {
   void deleteScenarioTest() throws Exception {
     // -- EXECUTE 1 ASSERT --
     this.mvc
-        .perform(delete(SCENARIO_URI + "/" + SCENARIO_ID))
-        .andExpect(status().is2xxSuccessful());
-    cleanup();
-  }
-
-
-  @DisplayName("Update with apply rule")
-  @Test
-  public void testUpdateScenario_WITH_apply_rule_false() throws Exception {
-    String tag1 = "tag1";
-    String tag2 = "tag2";
-    Tag tag3 = createTag("tag3");
-    Scenario scenario = createScenario("testScenario", List.of(tag1, tag2));
-
-    UpdateScenarioInput updateScenarioInput = new UpdateScenarioInput();
-    updateScenarioInput.setApplyTagRule(false);
-    updateScenarioInput.setTagIds(List.of(tag3.getId()));
-
-
-    this.mvc
-            .perform(
-                    put(SCENARIO_URI + "/" + scenario.getId())
-                            .content(asJsonString(updateScenarioInput))
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().is2xxSuccessful())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
-
-
-    Scenario updatedScenario = scenarioRepository.findById(scenario.getId()).orElseThrow();
-    assertEquals(updatedScenario.getTags().size(), 1);
-    assertEquals(new HashSet<>(updatedScenario.getInjects().getFirst().getAssets()),
-            new HashSet<>(scenario.getInjects().getFirst().getAssets()));
-  }
-
-
-  private Scenario createScenario(String scenarioName, List<String> tagNames) {
-    Scenario scenario = new Scenario();
-    tagNames.forEach(tagName -> scenario.getTags().add(createTag(tagName)));
-    scenarioRepository.save(scenario);
-    createInject(scenario.getId());
-    return scenarioRepository.findById(scenario.getId()).orElseThrow();
-  }
-
-  private Inject createInject(String scenarioId) {
-    Inject inject =  InjectFixture.getInjectForEmailContract(injectorContractRepository.findById(EMAIL_DEFAULT).orElseThrow());
-    inject.setScenario(scenarioRepository.findById(scenarioId).orElseThrow());
-    return injectRepository.save(inject);
-  }
-
-  private Tag createTag(String tagName) {
-    Tag tag = new Tag();
-    tag.setName(tagName + System.currentTimeMillis());
-    tag.setColor("#0000");
-    return tagRepository.save(tag);
-  }
-
-  private Asset createAsset(String assetName) {
-    Asset asset = new Asset();
-    asset.setName(assetName);
-    return assetRepository.save(asset);
+            .perform(delete(SCENARIO_URI + "/" + SCENARIO_ID))
+            .andExpect(status().is2xxSuccessful());
   }
 }
