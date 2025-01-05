cceacdbb763290be3bbb65d3c6db7fb67daf6de6
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-01-05 17:36:43 +0100

    [backend/frontend] Fix find all query for endpoints

--- openbas-model/src/main/java/io/openbas/database/specification/EndpointSpecification.java
+++ openbas-model/src/main/java/io/openbas/database/specification/EndpointSpecification.java
@@ -11,17 +11,23 @@ public class EndpointSpecification {
   private EndpointSpecification() {}
 
   public static Specification<Endpoint> findEndpointsForInjection() {
-    return (root, query, criteriaBuilder) ->
-        criteriaBuilder.and(
-            criteriaBuilder.isNull(root.get("agents").get("parent")),
-            criteriaBuilder.isNull(root.get("agents").get("inject")));
+    return (root, query, criteriaBuilder) -> {
+      query.groupBy(root.get("id"));
+      return criteriaBuilder.and(
+          criteriaBuilder.isNull(root.get("agents").get("parent")),
+          criteriaBuilder.isNull(root.get("agents").get("inject"))
+      );
+    };
   }
 
   public static Specification<Endpoint> findEndpointsForExecution() {
-    return (root, query, criteriaBuilder) ->
-        criteriaBuilder.or(
-            criteriaBuilder.isNotNull(root.get("agents").get("parent")),
-            criteriaBuilder.isNotNull(root.get("agents").get("inject")));
+    return (root, query, criteriaBuilder) -> {
+      query.groupBy(root.get("id"));
+      return criteriaBuilder.or(
+          criteriaBuilder.isNotNull(root.get("agents").get("parent")),
+          criteriaBuilder.isNotNull(root.get("agents").get("inject"))
+      );
+    };
   }
 
   public static Specification<Endpoint> findEndpointsForInjectionByHostname(
@@ -29,13 +35,6 @@ public class EndpointSpecification {
     Specification<Endpoint> hostnameSpec =
         (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("hostname"), hostname);
     return findEndpointsForInjection().and(hostnameSpec);
-  }
-
-  public static Specification<Endpoint> findEndpointsForExecutionByHostname(
-      @NotBlank final String hostname) {
-    Specification<Endpoint> hostnameSpec =
-        (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("hostname"), hostname);
-    return findEndpointsForExecution().and(hostnameSpec);
   }
 
   public static Specification<Endpoint> fromIds(@NotNull final List<String> ids) {
