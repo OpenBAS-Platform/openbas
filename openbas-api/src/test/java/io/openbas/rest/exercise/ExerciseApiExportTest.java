4639c22654aa9db08b6aa79e1487f2d249302a35
Antoine MAZEAS <antoine.mazeas@filigran.io>
2025-01-06 10:25:13 +0100

    working test
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- openbas-api/src/test/java/io/openbas/rest/exercise/ExerciseApiExportTest.java
+++ openbas-api/src/test/java/io/openbas/rest/exercise/ExerciseApiExportTest.java
@@ -5,17 +5,23 @@ import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.openbas.IntegrationTest;
 import io.openbas.database.model.Exercise;
+import io.openbas.rest.exercise.exports.ExerciseFileExport;
+import io.openbas.service.ChallengeService;
+import io.openbas.service.VariableService;
 import io.openbas.utils.fixtures.*;
 import io.openbas.utils.fixtures.composers.*;
 import io.openbas.utils.mockUser.WithMockAdminUser;
 import jakarta.annotation.Resource;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import org.junit.jupiter.api.*;
+import org.opensaml.saml.saml1.core.Assertion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -38,6 +44,8 @@ public class ExerciseApiExportTest extends IntegrationTest {
   @Autowired private ObjectiveComposer objectiveComposer;
   @Autowired private DocumentComposer documentComposer;
   @Autowired private TagComposer tagComposer;
+  @Autowired private VariableService variableService;
+  @Autowired private ChallengeService challengeService;
   @Resource protected ObjectMapper mapper;
 
   static Exercise EXERCISE;
@@ -90,16 +98,36 @@ public class ExerciseApiExportTest extends IntegrationTest {
             .getResponse()
             .getContentAsByteArray();
 
-    try (ZipInputStream zis = new ZipInputStream(new ByteArrayInputStream(response))) {
+    String jsonExport = getZipEntryAsStringFromByteArrayByName(response, "%s.json".formatted(EXERCISE.getName()));
+    ObjectMapper exportMapper = mapper.copy();
+    JsonNode expectedJson = exportMapper.readTree(
+            exportMapper.writeValueAsBytes(
+                    ExerciseFileExport.fromExercise(EXERCISE, exportMapper, variableService, challengeService)
+                            .withOptions(0)));
+    JsonNode actualJson = exportMapper.readTree(jsonExport);
+
+    Assertions.assertEquals(expectedJson, actualJson);
+  }
+
+  // looks for a specific entry in a zip buffer by name, and returns a string representation of it
+  // ensure you are using this for text files within zips, not binary files
+  private String getZipEntryAsStringFromByteArrayByName(byte[] byteArray, String entryName) throws IOException {
+    try (ZipInputStream zis = new ZipInputStream(new ByteArrayInputStream(byteArray))) {
       ZipEntry entry;
       while ((entry = zis.getNextEntry()) != null) {
-        if (entry.getName().equals("%s.json".formatted(EXERCISE.getName()))) {
+        if (entry.getName().equals(entryName)) {
+          StringBuilder sb = new StringBuilder();
+          byte[] buffer = new byte[1024];
+          int read = 0;
+          while ((read = zis.read(buffer, 0, 1024)) >= 0) {
+            sb.append(new String(buffer, 0, read));
+          }
           // force exit of test since we have found the correct entry
-          return;
+          return sb.toString();
         }
       }
       // no zip entry corresponding to expected json
-      Assertions.fail();
+      throw new IOException("Zip entry '%s' not found".formatted(entryName));
     }
   }
 }
