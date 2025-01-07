10dbb6cd15e3311b87f8bc636633f420f5572ccd
Damien Goujard <damien.goujard@filigran.io>
2025-01-07 11:34:27 +0100

    [backend] Fix migration

--- openbas-api/src/main/java/io/openbas/migration/V3_57__Update_table_asset_agent_jobs.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_57__Update_table_asset_agent_jobs.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V3_55__Update_table_asset_agent_jobs extends BaseJavaMigration {
+public class V3_57__Update_table_asset_agent_jobs extends BaseJavaMigration {
   @Override
   public void migrate(Context context) throws Exception {
     Statement select = context.getConnection().createStatement();
