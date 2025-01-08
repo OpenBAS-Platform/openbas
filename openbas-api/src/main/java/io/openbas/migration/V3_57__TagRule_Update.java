cc8fd77ea07e38b4b265b5da8fc6363ca1e39c87
Hedi Tarchouni <hedi.tarchouni@filigran.io>
2025-01-08 14:44:06 +0100

    delete tag_rule_assets table

---
+++ openbas-api/src/main/java/io/openbas/migration/V3_57__TagRule_Update.java
@@ -0,0 +1,35 @@
+package io.openbas.migration;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+import org.springframework.stereotype.Component;
+
+@Component
+public class V3_57__TagRule_Update extends BaseJavaMigration {
+
+  @Override
+  public void migrate(Context context) throws Exception {
+    Connection connection = context.getConnection();
+    Statement select = connection.createStatement();
+    select.execute(
+        """
+                   CREATE TABLE tag_rule_asset_groups (
+                       tag_rule_id varchar(255) not null
+                            constraint tag_rule_id_fk
+                               references tag_rules,
+                       asset_group_id varchar(255) not null
+                           constraint asset_group_id_fk
+                               references asset_groups
+                                 on delete cascade,
+                       primary key (tag_rule_id, asset_group_id)
+                   );
+                """);
+
+    select.execute(
+        """
+                   DROP TABLE IF EXISTS tag_rule_assets
+               """);
+  }
+}
