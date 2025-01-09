ca7f5178271dbfc2e1d80ad5c26fb860ae35947c
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-01-09 17:58:32 +0100

    [backend/frontend] delete legacy dryinjects and dryruns

--- openbas-api/src/main/java/io/openbas/migration/V3_59__Delete_all_dryInject_dryrun.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_59__Delete_all_dryInject_dryrun.java
@@ -13,9 +13,17 @@ public class V3_59__Delete_all_dryInject_dryrun extends BaseJavaMigration {
   public void migrate(Context context) throws Exception {
     Connection connection = context.getConnection();
     Statement select = connection.createStatement();
-    select.execute("DROP TABLE dryinjects;");
-    select.execute("DROP TABLE dryinjects_statuses;");
-    select.execute("DROP TABLE dryruns;");
-    select.execute("DROP TABLE dryruns_users;");
+    select.execute(
+        """
+                                        DROP TABLE dryinjects_statuses """);
+    select.execute(
+        """
+                                         DROP TABLE dryruns_users """);
+    select.execute(
+        """
+                                         DROP TABLE dryinjects """);
+    select.execute(
+        """
+                                         DROP TABLE dryruns """);
   }
 }
