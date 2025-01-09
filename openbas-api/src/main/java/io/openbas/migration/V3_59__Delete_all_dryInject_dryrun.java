e34b7da02d4c897a575a3bbf9581fcca17c5b701
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-01-09 17:15:34 +0100

    [backend/frontend] delete legacy dryinjects and dryruns

--- openbas-api/src/main/java/io/openbas/migration/V3_59__Delete_all_dryInject_dryrun.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_59__Delete_all_dryInject_dryrun.java
@@ -1,11 +1,10 @@
 package io.openbas.migration;
 
+import java.sql.Connection;
+import java.sql.Statement;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
-
-import java.sql.Connection;
-import java.sql.Statement;
 
 @Component
 public class V3_59__Delete_all_dryInject_dryrun extends BaseJavaMigration {
