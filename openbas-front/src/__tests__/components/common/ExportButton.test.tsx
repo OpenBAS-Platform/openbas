0574be6b0a46ce9adc8cfabf771c48754db60cbd
Antoine MAZEAS <antoine.mazeas@filigran.io>
2025-01-15 15:53:04 +0100

    maybe almost working ??
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- openbas-front/src/__tests__/components/common/ExportButton.test.tsx
+++ openbas-front/src/__tests__/components/common/ExportButton.test.tsx
@@ -1,8 +1,16 @@
 import ExportButton from '../../../components/common/ExportButton';
 import { act, render } from '@testing-library/react'; // @testing-library/dom is needed as well as it is a peer dependency of @testing-library/react
 import { describe, expect, it, vi } from 'vitest';
-import { getDefaultTags } from "../../fixtures/api-types.fixtures";
+import {createDefaultTags, createTagMap} from "../../fixtures/api-types.fixtures";
 import { faker } from '@faker-js/faker';
+import TestRootComponent from "../../fixtures/TestRootComponent";
+import type { TagHelper as TagHelperType } from '../../../actions/helper';
+import {fetchTags } from '../../../actions/Tag'
+import React from "react";
+import {useAppDispatch} from "../../../utils/hooks";
+import useDataLoader from "../../../utils/hooks/useDataLoader";
+import {fetchPlatformParameters} from "../../../actions/Application";
+import { store } from '../../../store'
 
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 type testobj = { [key: string]: any };
@@ -15,28 +23,49 @@ function createObjWithDefaultKeys(objtype: string): testobj {
 }
 
 describe('Generic export button', () => {
-    const numberOfElements : number = 10;
-    const numberOfTags : number = 5;
     const exportType: string = "testobj"
+    const exportData: testobj[] = [
+        createObjWithDefaultKeys(exportType),
+        createObjWithDefaultKeys(exportType),
+        createObjWithDefaultKeys(exportType)
+    ]
+    const numberOfElements : number = exportData.length;
     const exportKeys = [
         `${exportType}_name`,
+        `${exportType}_tags`,
     ];
+    const tags = createDefaultTags(5);
+    const tagMap = createTagMap(tags);
+    for(let obj of exportData) {
+        obj[`${exportType}_tags`] = tags.map(t => t.tag_id);
+    }
     vi.mock(
-        '../../../../../../src/actions/Tag',
-        () => ({ fetchTags: () => getDefaultTags(numberOfTags)})
+        '../../../actions/Tag',
+        () => ({fetchTags: () => tagMap})
     );
 
-    it("does something", () => {
-        const { getByDisplayValue } = render(
-          <ExportButton totalElements={numberOfElements} exportProps={{
-              exportType: exportType,
-              exportKeys: exportKeys,
-              exportData: [createObjWithDefaultKeys(exportType)],
-              exportFileName: "export"
-              }} />,
+    useAppDispatch()(fetchTags())
+
+    it("does something", async () => {
+        const { getByRole } = render(
+            <TestRootComponent>
+                <ExportButton totalElements={numberOfElements} exportProps={{
+                    exportType: exportType,
+                    exportKeys: exportKeys,
+                    exportData: [createObjWithDefaultKeys(exportType)],
+                    exportFileName: "export.csv"
+                }} />
+            </TestRootComponent>,
         );
-        act(() => {
-            const firstname = getByDisplayValue("Export this list");
+        await act(async () => {
+            const firstname = getByRole("link");
+            if (firstname.onclick) {
+                const toto = await firstname.onclick(new MouseEvent("click"))
+                console.log("here it is");
+                console.log(firstname.href)
+                console.log(firstname);
+            }
+
             expect(firstname).toBeDefined();
         });
     });
