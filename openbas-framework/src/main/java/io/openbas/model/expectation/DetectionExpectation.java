b026f67eab59714f08cbf0d2677dc41385c44f41
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-01-16 19:26:02 +0100

    [backend/frontend] Add prevention, detection and manual Expectations for agents

--- openbas-framework/src/main/java/io/openbas/model/expectation/DetectionExpectation.java
+++ openbas-framework/src/main/java/io/openbas/model/expectation/DetectionExpectation.java
@@ -2,10 +2,7 @@ package io.openbas.model.expectation;
 
 import static io.openbas.database.model.InjectExpectation.EXPECTATION_TYPE.DETECTION;
 
-import io.openbas.database.model.Asset;
-import io.openbas.database.model.AssetGroup;
-import io.openbas.database.model.InjectExpectation;
-import io.openbas.database.model.InjectExpectationSignature;
+import io.openbas.database.model.*;
 import io.openbas.model.Expectation;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -22,6 +19,7 @@ public class DetectionExpectation implements Expectation {
   private Double score;
   private String name;
   private String description;
+  private Agent agent;
   private Asset asset;
   private AssetGroup assetGroup;
   private boolean expectationGroup;
@@ -33,6 +31,25 @@ public class DetectionExpectation implements Expectation {
   @Override
   public InjectExpectation.EXPECTATION_TYPE type() {
     return DETECTION;
+  }
+
+  public static DetectionExpectation detectionExpectationForAgent(
+      @Nullable final Double score,
+      @NotBlank final String name,
+      final String description,
+      @NotNull final Agent agent,
+      final boolean expectationGroup,
+      final Long expirationTime,
+      final List<InjectExpectationSignature> expectationSignatures) {
+    DetectionExpectation detectionExpectation = new DetectionExpectation();
+    detectionExpectation.setScore(Objects.requireNonNullElse(score, 100.0));
+    detectionExpectation.setName(name);
+    detectionExpectation.setDescription(description);
+    detectionExpectation.setAgent(agent);
+    detectionExpectation.setExpectationGroup(expectationGroup);
+    detectionExpectation.setExpirationTime(expirationTime);
+    detectionExpectation.setInjectExpectationSignatures(expectationSignatures);
+    return detectionExpectation;
   }
 
   public static DetectionExpectation detectionExpectationForAsset(
