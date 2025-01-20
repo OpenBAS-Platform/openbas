ca4bf55c74d4a3b7a472b8c70b2dd26523735efe
Gael Leblan <gael.leblan@filigran.io>
2025-01-20 11:57:35 +0100

    [backend] Adding swagger documentation on User Api endpoints

--- openbas-api/src/main/java/io/openbas/rest/user/form/user/CreateUserInput.java
+++ openbas-api/src/main/java/io/openbas/rest/user/form/user/CreateUserInput.java
@@ -3,6 +3,7 @@ package io.openbas.rest.user.form.user;
 import static io.openbas.config.AppConfig.EMAIL_FORMAT;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import java.util.ArrayList;
@@ -17,23 +18,30 @@ public class CreateUserInput {
   @Email(message = EMAIL_FORMAT)
   @NotBlank
   @JsonProperty("user_email")
+  @Schema(description = "The email of the user")
   private String email;
 
   @JsonProperty("user_admin")
+  @Schema(description = "True if the user is admin")
   private boolean admin;
 
   @JsonProperty("user_firstname")
+  @Schema(description = "First name of the user")
   private String firstname;
 
   @JsonProperty("user_lastname")
+  @Schema(description = "Last name of the user")
   private String lastname;
 
   @JsonProperty("user_organization")
+  @Schema(description = "Organization of the user")
   private String organizationId;
 
   @JsonProperty("user_plain_password")
+  @Schema(description = "Password of the user as plain text")
   private String password;
 
   @JsonProperty("user_tags")
+  @Schema(description = "Tags of the user")
   private List<String> tagIds = new ArrayList<>();
 }
--- openbas-api/src/main/java/io/openbas/rest/user/form/user/UserOutput.java
+++ openbas-api/src/main/java/io/openbas/rest/user/form/user/UserOutput.java
@@ -1,6 +1,7 @@
 package io.openbas.rest.user.form.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import java.util.Set;
 import lombok.Builder;
@@ -12,24 +13,31 @@ public class UserOutput {
 
   @JsonProperty("user_id")
   @NotBlank
+  @Schema(description = "User ID")
   private String id;
 
   @JsonProperty("user_firstname")
+  @Schema(description = "First name of the user")
   private String firstname;
 
   @JsonProperty("user_lastname")
+  @Schema(description = "Last name of the user")
   private String lastname;
 
   @JsonProperty("user_email")
   @NotBlank
+  @Schema(description = "Email of the user")
   private String email;
 
   @JsonProperty("user_admin")
+  @Schema(description = "True if the user is admin")
   private boolean admin;
 
   @JsonProperty("user_organization_name")
+  @Schema(description = "Organization of the user")
   private String organizationName;
 
   @JsonProperty("user_tags")
+  @Schema(description = "Tags of the user")
   private Set<String> tags;
 }
