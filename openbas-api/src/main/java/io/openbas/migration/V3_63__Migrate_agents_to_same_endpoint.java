45a5a8fd70f2176e010a54f45fa04160c45f633e
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-01-30 11:42:27 +0100

    [backend] Clean Caldera

--- openbas-api/src/main/java/io/openbas/migration/V3_63__Migrate_agents_to_same_endpoint.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_63__Migrate_agents_to_same_endpoint.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V3_62__Migrate_agents_to_same_endpoint extends BaseJavaMigration {
+public class V3_63__Migrate_agents_to_same_endpoint extends BaseJavaMigration {
   @Override
   public void migrate(Context context) throws Exception {
     Statement select = context.getConnection().createStatement();
