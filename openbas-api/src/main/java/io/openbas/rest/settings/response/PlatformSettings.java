Luatix/OpenEx

[backend] Adding swagger documentation on Settings Api endpoints

openbas-api/src/main/java/io/openbas/rest/settings/response/PlatformSettings.java
@@ -5,6 +5,7 @@ import static lombok.AccessLevel.NONE;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.openbas.rest.settings.form.PolicyInput;
 import io.openbas.rest.settings.form.ThemeInput;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,106 +23,140 @@ import lombok.Setter;
 public class PlatformSettings {
 
   @JsonProperty("platform_name")
+  @Schema(description = "Name of the platform")
   private String platformName;
 
   @JsonProperty("platform_base_url")
+  @Schema(description = "Base URL of the platform")
   private String platformBaseUrl;
 
   @JsonProperty("platform_agent_url")
+  @Schema(description = "Agent URL of the platform")
   private String platformAgentUrl;
 
   @JsonProperty("platform_theme")
+  @Schema(description = "Theme of the platform")
   private String platformTheme;
 
   @JsonProperty("platform_lang")
+  @Schema(description = "Language of the platform")
   private String platformLang;
 
   @JsonProperty("platform_enterprise_edition")
+  @Schema(description = "'true' if the platform has enterprise edition activated")
   private String platformEnterpriseEdition;
 
   @JsonProperty("platform_whitemark")
+  @Schema(description = "'true' if the platform has the whitemark activated")
   private String platformWhitemark;
 
   @JsonProperty("platform_openid_providers")
+  @Schema(description = "List of OpenID providers")
   private List<OAuthProvider> platformOpenIdProviders;
 
   @JsonProperty("platform_saml2_providers")
+  @Schema(description = "List of Saml2 providers")
   private List<OAuthProvider> platformSaml2Providers;
 
   @JsonProperty("auth_openid_enable")
+  @Schema(description = "True if OpenID is enabled")
   private Boolean authOpenidEnable;
 
   @JsonProperty("auth_saml2_enable")
+  @Schema(description = "True if Saml2 is enabled")
   private Boolean authSaml2Enable;
 
   @JsonProperty("auth_local_enable")
+  @Schema(description = "True if local authentication is enabled")
   private Boolean authLocalEnable;
 
   @JsonProperty("map_tile_server_light")
+  @Schema(description = "Url to the server containing the map tile with light theme")
   private String mapTileServerLight;
 
   @JsonProperty("map_tile_server_dark")
+  @Schema(description = "Url to the server containing the map tile with dark theme")
   private String mapTileServerDark;
 
   @JsonProperty("xtm_opencti_enable")
+  @Schema(description = "True if connection with OpenCTI is enabled")
   private Boolean xtmOpenctiEnable;
 
   @JsonProperty("xtm_opencti_url")
+  @Schema(description = "Url of OpenCTI")
   private String xtmOpenctiUrl;
 
   @JsonProperty("platform_version")
+  @Schema(description = "Current version of the platform")
   private String platformVersion;
 
   @JsonProperty("postgre_version")
+  @Schema(description = "Current version of the PGSQL")
   private String postgreVersion;
 
   @JsonProperty("java_version")
+  @Schema(description = "Current version of the Java")
   private String javaVersion;
 
   @JsonProperty("rabbitmq_version")
+  @Schema(description = "Current version of the RabbitMQ")
   private String rabbitMQVersion;
 
   @JsonProperty("platform_ai_enabled")
+  @Schema(description = "True if AI is enabled for the platform")
   private Boolean aiEnabled;
 
   @JsonProperty("platform_ai_has_token")
+  @Schema(description = "True if we have an AI token")
   private Boolean aiHasToken;
 
   @JsonProperty("platform_ai_type")
+  @Schema(description = "Type of AI")
   private String aiType;
 
   @JsonProperty("platform_ai_model")
+  @Schema(description = "Model chosen of AI")
   private String aiModel;
 
   @JsonProperty("executor_caldera_enable")
+  @Schema(description = "True if the Caldera Executor is enabled")
   private Boolean executorCalderaEnable;
 
   @JsonProperty("executor_caldera_public_url")
+  @Schema(description = "Url of the Caldera Executor")
   private String executorCalderaPublicUrl;
 
   @JsonProperty("executor_tanium_enable")
+  @Schema(description = "True if the Tanium Executor is enabled")
   private Boolean executorTaniumEnable;
 
   // THEME
 
   @JsonProperty("platform_light_theme")
+  @Schema(description = "Description of the light theme")
   private ThemeInput themeLight;
 
   @JsonProperty("platform_dark_theme")
+  @Schema(description = "Description of the dark theme")
   private ThemeInput themeDark;
 
   // POLICIES
 
   @JsonProperty("platform_policies")
+  @Schema(description = "Policies of the platform")
   private PolicyInput policies;
 
   // FEATURE FLAG
   @JsonProperty("disabled_dev_features")
+  @Schema(description = "List of disabled dev features")
   private List<String> disabledDevFeatures = new ArrayList<>();
 
   // PLATFORM MESSAGE
   @JsonProperty("platform_banner_by_level")
   @Getter(NONE)
+  @Schema(
+      description =
+          "Map of the messages to display on the screen by their level (the level available are DEBUG, INFO, WARN, ERROR, FATAL)")
   private Map<String, List<String>> platformBannerByLevel;
 
   public Map<String, List<String>> getPlatformBannerByLevel() {
@@ -137,32 +172,40 @@ public class PlatformSettings {
   // EXPECTATION
   @NotNull
   @JsonProperty("expectation_detection_expiration_time")
+  @Schema(description = "Time to wait before detection time has expired")
   private long detectionExpirationTime;
 
   @NotNull
   @JsonProperty("expectation_prevention_expiration_time")
+  @Schema(description = "Time to wait before prevention time has expired")
   private long preventionExpirationTime;
 
   @NotNull
   @JsonProperty("expectation_challenge_expiration_time")
+  @Schema(description = "Time to wait before challenge time has expired")
   private long challengeExpirationTime;
 
   @NotNull
   @JsonProperty("expectation_article_expiration_time")
+  @Schema(description = "Time to wait before article time has expired")
   private long articleExpirationTime;
 
   @NotNull
   @JsonProperty("expectation_manual_expiration_time")
+  @Schema(description = "Time to wait before manual expectation time has expired")
   private long manualExpirationTime;
 
   @NotNull
   @JsonProperty("expectation_manual_default_score_value")
+  @Schema(description = "Default score for manuel expectation")
   private int expectationDefaultScoreValue;
 
   // EMAIL CONFIG
   @JsonProperty("default_mailer")
+  @Schema(description = "Mail to use by default")
   private String defaultMailer;
 
   @JsonProperty("default_reply_to")
+  @Schema(description = "Reply to to use by default")
   private String defaultReplyTo;
 }

OpenBAS-Platform/openbas

openbas-api/src/main/java/io/openbas/rest/settings/response/PlatformSettings.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.openbas.rest.settings.PreviewFeature;
 import io.openbas.rest.settings.form.PolicyInput;
 import io.openbas.rest.settings.form.ThemeInput;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,106 +24,140 @@ import lombok.Setter;
 public class PlatformSettings {
 
   @JsonProperty("platform_name")
+  @Schema(description = "Name of the platform")
   private String platformName;
 
   @JsonProperty("platform_base_url")
+  @Schema(description = "Base URL of the platform")
   private String platformBaseUrl;
 
   @JsonProperty("platform_agent_url")
+  @Schema(description = "Agent URL of the platform")
   private String platformAgentUrl;
 
   @JsonProperty("platform_theme")
+  @Schema(description = "Theme of the platform")
   private String platformTheme;
 
   @JsonProperty("platform_lang")
+  @Schema(description = "Language of the platform")
   private String platformLang;
 
   @JsonProperty("platform_enterprise_edition")
+  @Schema(description = "'true' if the platform has enterprise edition activated")
   private String platformEnterpriseEdition;
 
   @JsonProperty("platform_whitemark")
+  @Schema(description = "'true' if the platform has the whitemark activated")
   private String platformWhitemark;
 
   @JsonProperty("platform_openid_providers")
+  @Schema(description = "List of OpenID providers")
   private List<OAuthProvider> platformOpenIdProviders;
 
   @JsonProperty("platform_saml2_providers")
+  @Schema(description = "List of Saml2 providers")
   private List<OAuthProvider> platformSaml2Providers;
 
   @JsonProperty("auth_openid_enable")
+  @Schema(description = "True if OpenID is enabled")
   private Boolean authOpenidEnable;
 
   @JsonProperty("auth_saml2_enable")
+  @Schema(description = "True if Saml2 is enabled")
   private Boolean authSaml2Enable;
 
   @JsonProperty("auth_local_enable")
+  @Schema(description = "True if local authentication is enabled")
   private Boolean authLocalEnable;
 
   @JsonProperty("map_tile_server_light")
+  @Schema(description = "Url to the server containing the map tile with light theme")
   private String mapTileServerLight;
 
   @JsonProperty("map_tile_server_dark")
+  @Schema(description = "Url to the server containing the map tile with dark theme")
   private String mapTileServerDark;
 
   @JsonProperty("xtm_opencti_enable")
+  @Schema(description = "True if connection with OpenCTI is enabled")
   private Boolean xtmOpenctiEnable;
 
   @JsonProperty("xtm_opencti_url")
+  @Schema(description = "Url of OpenCTI")
   private String xtmOpenctiUrl;
 
   @JsonProperty("platform_version")
+  @Schema(description = "Current version of the platform")
   private String platformVersion;
 
   @JsonProperty("postgre_version")
+  @Schema(description = "Current version of the PGSQL")
   private String postgreVersion;
 
   @JsonProperty("java_version")
+  @Schema(description = "Current version of the Java")
   private String javaVersion;
 
   @JsonProperty("rabbitmq_version")
+  @Schema(description = "Current version of the RabbitMQ")
   private String rabbitMQVersion;
 
   @JsonProperty("platform_ai_enabled")
+  @Schema(description = "True if AI is enabled for the platform")
   private Boolean aiEnabled;
 
   @JsonProperty("platform_ai_has_token")
+  @Schema(description = "True if we have an AI token")
   private Boolean aiHasToken;
 
   @JsonProperty("platform_ai_type")
+  @Schema(description = "Type of AI")
   private String aiType;
 
   @JsonProperty("platform_ai_model")
+  @Schema(description = "Model chosen of AI")
   private String aiModel;
 
   @JsonProperty("executor_caldera_enable")
+  @Schema(description = "True if the Caldera Executor is enabled")
   private Boolean executorCalderaEnable;
 
   @JsonProperty("executor_caldera_public_url")
+  @Schema(description = "Url of the Caldera Executor")
   private String executorCalderaPublicUrl;
 
   @JsonProperty("executor_tanium_enable")
+  @Schema(description = "True if the Tanium Executor is enabled")
   private Boolean executorTaniumEnable;
 
   // THEME
 
   @JsonProperty("platform_light_theme")
+  @Schema(description = "Description of the light theme")
   private ThemeInput themeLight;
 
   @JsonProperty("platform_dark_theme")
+  @Schema(description = "Description of the dark theme")
   private ThemeInput themeDark;
 
   // POLICIES
 
   @JsonProperty("platform_policies")
+  @Schema(description = "Policies of the platform")
   private PolicyInput policies;
 
   // FEATURE FLAG
   @JsonProperty("enabled_dev_features")
+  @Schema(description = "List of enabled dev features")
   private List<PreviewFeature> enabledDevFeatures = new ArrayList<>();
 
   // PLATFORM MESSAGE
   @JsonProperty("platform_banner_by_level")
   @Getter(NONE)
+  @Schema(
+      description =
+          "Map of the messages to display on the screen by their level (the level available are DEBUG, INFO, WARN, ERROR, FATAL)")
   private Map<String, List<String>> platformBannerByLevel;
 
   public Map<String, List<String>> getPlatformBannerByLevel() {
@@ -138,32 +173,40 @@ public class PlatformSettings {
   // EXPECTATION
   @NotNull
   @JsonProperty("expectation_detection_expiration_time")
+  @Schema(description = "Time to wait before detection time has expired")
   private long detectionExpirationTime;
 
   @NotNull
   @JsonProperty("expectation_prevention_expiration_time")
+  @Schema(description = "Time to wait before prevention time has expired")
   private long preventionExpirationTime;
 
   @NotNull
   @JsonProperty("expectation_challenge_expiration_time")
+  @Schema(description = "Time to wait before challenge time has expired")
   private long challengeExpirationTime;
 
   @NotNull
   @JsonProperty("expectation_article_expiration_time")
+  @Schema(description = "Time to wait before article time has expired")
   private long articleExpirationTime;
 
   @NotNull
   @JsonProperty("expectation_manual_expiration_time")
+  @Schema(description = "Time to wait before manual expectation time has expired")
   private long manualExpirationTime;
 
   @NotNull
   @JsonProperty("expectation_manual_default_score_value")
+  @Schema(description = "Default score for manuel expectation")
   private int expectationDefaultScoreValue;
 
   // EMAIL CONFIG
   @JsonProperty("default_mailer")
+  @Schema(description = "Mail to use by default")
   private String defaultMailer;
 
   @JsonProperty("default_reply_to")
+  @Schema(description = "Reply to to use by default")
   private String defaultReplyTo;
 }