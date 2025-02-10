30b5b574dd49fc189d9afa6789625db22da9c297
Gael Leblan <gael.leblan@filigran.io>
2025-02-10 09:40:34 +0100

    [backend] Adding swagger documentation on Settings Api endpoints (#2378)

--- openbas-api/src/main/java/io/openbas/rest/settings/form/SettingsPlatformWhitemarkUpdateInput.java
+++ openbas-api/src/main/java/io/openbas/rest/settings/form/SettingsPlatformWhitemarkUpdateInput.java
@@ -3,6 +3,7 @@ package io.openbas.rest.settings.form;
 import static io.openbas.config.AppConfig.MANDATORY_MESSAGE;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,5 +13,6 @@ import lombok.Setter;
 public class SettingsPlatformWhitemarkUpdateInput {
   @NotBlank(message = MANDATORY_MESSAGE)
   @JsonProperty("platform_whitemark")
+  @Schema(description = "The whitemark of the platform")
   private String platformWhitemark;
 }
