07612db031e123b58d2667ae0be9f786aec34050
Damien Goujard <damien.goujard@filigran.io>
2025-02-10 17:29:57 +0100

    [backend] Delete agent test

--- openbas-model/src/main/java/io/openbas/database/repository/AgentRepository.java
+++ openbas-model/src/main/java/io/openbas/database/repository/AgentRepository.java
@@ -3,15 +3,15 @@ package io.openbas.database.repository;
 import io.openbas.database.model.Agent;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AgentRepository
-    extends JpaRepository<Agent, String>, JpaSpecificationExecutor<Agent> {
+    extends CrudRepository<Agent, String>, JpaSpecificationExecutor<Agent> {
 
   /**
    * Returns the agents having the injectId passed in parameter
