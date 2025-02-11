b9146fd4601daab80135bb5c21c98d0d0b5c6e18
Johanah LEKEU <johanah.lekeu@filigran.io>
2025-02-11 10:15:42 +0100

    Resolve comments

--- openbas-front/src/components/Autocomplete.js
+++ openbas-front/src/components/Autocomplete.js
@@ -56,7 +56,7 @@ const renderAutocomplete = ({
                   {
                     typeof openCreate === 'function' && (
                       <IconButton
-                        style={{ position: 'absolute', right: '10px' }}
+                        style={{ position: 'absolute', right: '35px' }}
                         onClick={() => openCreate()}
                       >
                         <AddOutlined />
