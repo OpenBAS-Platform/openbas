ed3f6632764c2eda6f7c2fea7b1d804cb448a1ef
Antoine MAZEAS <antoine.mazeas@filigran.io>
2025-02-11 16:28:32 +0100

    lint
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- openbas-front/src/admin/components/assets/endpoints/EndpointsList.tsx
+++ openbas-front/src/admin/components/assets/endpoints/EndpointsList.tsx
@@ -1,11 +1,9 @@
 import { DevicesOtherOutlined } from '@mui/icons-material';
 import { Chip, List, ListItem, ListItemIcon, ListItemText, Tooltip } from '@mui/material';
 import * as React from 'react';
-import { CSSProperties, FunctionComponent, useEffect, useState } from 'react';
+import { CSSProperties, FunctionComponent } from 'react';
 import { makeStyles } from 'tss-react/mui';
 
-import { findEndpoints } from '../../../../actions/assets/endpoint-actions';
-import ListLoader from '../../../../components/common/loader/ListLoader';
 import ItemTags from '../../../../components/ItemTags';
 import PlatformIcon from '../../../../components/PlatformIcon';
 import { EndpointOutput, EndpointOverviewOutput } from '../../../../utils/api-types';
@@ -68,59 +66,59 @@ const EndpointsList: FunctionComponent<Props> = ({
   return (
     <>
       <List>
-                {endpoints?.map((endpoint) => {
-                  return (
-                    <ListItem
-                      key={endpoint.asset_id}
-                      classes={{ root: classes.item }}
-                      divider={true}
-                      secondaryAction={component(endpoint)}
+        {endpoints?.map((endpoint) => {
+          return (
+            <ListItem
+              key={endpoint.asset_id}
+              classes={{ root: classes.item }}
+              divider={true}
+              secondaryAction={component(endpoint)}
+            >
+              <ListItemIcon>
+                <DevicesOtherOutlined color="primary" />
+              </ListItemIcon>
+              <ListItemText
+                primary={(
+                  <>
+                    <div
+                      className={classes.bodyItem}
+                      style={inlineStyles.asset_name}
                     >
-                      <ListItemIcon>
-                        <DevicesOtherOutlined color="primary" />
-                      </ListItemIcon>
-                      <ListItemText
-                        primary={(
-                          <>
-                            <div
-                              className={classes.bodyItem}
-                              style={inlineStyles.asset_name}
-                            >
-                              {endpoint.asset_name}
-                            </div>
-                            <div
-                              className={classes.bodyItem}
-                              style={inlineStyles.asset_platform}
-                            >
-                              <PlatformIcon platform={endpoint.endpoint_platform} width={20} marginRight={10} />
-                              {' '}
-                              {endpoint.endpoint_platform}
-                            </div>
-                            <div
-                              className={classes.bodyItem}
-                              style={inlineStyles.asset_tags}
-                            >
-                              <ItemTags variant="reduced-view" tags={endpoint.asset_tags} />
-                            </div>
-                            <div
-                              className={classes.bodyItem}
-                              style={inlineStyles.asset_type}
-                            >
-                              <Tooltip title={endpoint.asset_type}>
-                                <Chip
-                                  variant="outlined"
-                                  className={classes.typeChip}
-                                  label={endpoint.asset_type}
-                                />
-                              </Tooltip>
-                            </div>
-                          </>
-                        )}
-                      />
-                    </ListItem>
-                  );
-                })}
-              </List>
+                      {endpoint.asset_name}
+                    </div>
+                    <div
+                      className={classes.bodyItem}
+                      style={inlineStyles.asset_platform}
+                    >
+                      <PlatformIcon platform={endpoint.endpoint_platform} width={20} marginRight={10} />
+                      {' '}
+                      {endpoint.endpoint_platform}
+                    </div>
+                    <div
+                      className={classes.bodyItem}
+                      style={inlineStyles.asset_tags}
+                    >
+                      <ItemTags variant="reduced-view" tags={endpoint.asset_tags} />
+                    </div>
+                    <div
+                      className={classes.bodyItem}
+                      style={inlineStyles.asset_type}
+                    >
+                      <Tooltip title={endpoint.asset_type}>
+                        <Chip
+                          variant="outlined"
+                          className={classes.typeChip}
+                          label={endpoint.asset_type}
+                        />
+                      </Tooltip>
+                    </div>
+                  </>
+                )}
+              />
+            </ListItem>
+          );
+        })}
+      </List>
 
     </>
   );
