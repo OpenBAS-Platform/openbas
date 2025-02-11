26241d48769078827fddf4beb7ca110ed0d659ed
Damien Goujard <damien.goujard@filigran.io>
2025-02-11 11:09:18 +0100

    [backend] Manage asset group for traces results

--- openbas-api/src/main/java/io/openbas/rest/inject/service/InjectService.java
+++ openbas-api/src/main/java/io/openbas/rest/inject/service/InjectService.java
@@ -92,6 +92,7 @@ public class InjectService {
   public Map<Asset, Boolean> resolveAllAssetsToExecute(@NotNull final Inject inject) {
     Map<Asset, Boolean> assets =
         inject.getAssets().stream().collect(Collectors.toMap(asset -> asset, asset -> false));
+    List<String> assetIds = new ArrayList<>();
     inject
         .getAssetGroups()
         .forEach(
@@ -99,7 +100,13 @@ public class InjectService {
               List<Asset> assetsFromGroup =
                   this.assetGroupService.assetsFromAssetGroup(assetGroup.getId());
               // Verify asset validity
-              assetsFromGroup.forEach((asset) -> assets.put(asset, true));
+              assetsFromGroup.forEach(
+                  (asset) -> {
+                    if (!assetIds.contains(asset.getId())) {
+                      assets.put(asset, true);
+                      assetIds.add(asset.getId());
+                    }
+                  });
             }));
     return assets;
   }
