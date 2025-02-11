7c39b58b58daeeb136b94c3950311e88fc0b2896
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-02-11 15:00:09 +0100

    [frontend] Remove color

--- openbas-front/src/components/OrganizationField.js
+++ openbas-front/src/components/OrganizationField.js
@@ -87,7 +87,7 @@ class OrganizationField extends Component {
           renderOption={(props, option) => (
             <Box component="li" {...props} key={option.id}>
               <div className={classes.icon}>
-                <DomainOutlined sx={{ color: '#3880b7' }} />
+                <DomainOutlined />
               </div>
               <div className={classes.text}>{option.label}</div>
             </Box>
