96db0caf5ab9b150d34c43d0672d0d50d49660b0
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-02-12 12:29:12 +0100

    [backend] clean

--- openbas-api/src/main/java/io/openbas/utils/EndpointMapper.java
+++ openbas-api/src/main/java/io/openbas/utils/EndpointMapper.java
@@ -24,7 +24,10 @@ public class EndpointMapper {
         .id(endpoint.getId())
         .name(endpoint.getName())
         .type(endpoint.getType())
-        .agents(toAgentOutputs(endpoint.getAgents()))
+        .agents(toAgentOutputs(
+            endpoint.getAgents().stream()
+                .filter(agent -> agent.getParent() == null && agent.getInject() == null)
+                .collect(Collectors.toList())))
         .platform(endpoint.getPlatform())
         .arch(endpoint.getArch())
         .tags(endpoint.getTags().stream().map(Tag::getId).collect(Collectors.toSet()))
