9ec1de6b3c83b95f76ddbb9c5586a813fff27ca5
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-02-19 13:28:28 +0100

    [backend/frontend] WIP

--- openbas-front/src/admin/components/agents/Agents.tsx
+++ openbas-front/src/admin/components/agents/Agents.tsx
@@ -250,7 +250,7 @@ SHA512: ca07dc1d0a5297e29327e483f4f35dadb254d96a16a5c33da5ad048e6965a3863d621518
           current: true,
         }]}
       />
-      <Alert variant="outlined" severity="info" style={{ marginBottom: 30 }}>
+      <Alert variant="outlined" severity="info" style={{ marginBottom: theme.spacing(2) }}>
         {t('Here, you can download and install simulation agents available in your executors. Depending on the integrations you have enabled, some of them may be unavailable.')}
         <br />
         <br />
