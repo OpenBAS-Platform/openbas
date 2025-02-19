bd39bdfdbca7cfe07fc90fdd9f3a0ad0afe891c0
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-02-19 09:10:49 +0100

    [backend/frontend] WIP

---
+++ openbas-front/src/components/ExecutorBanner.tsx
@@ -0,0 +1,68 @@
+import { type FunctionComponent } from 'react';
+
+import calderaLogo from '../static/images/executors/logo_caldera.png';
+import crowdstrikeLogo from '../static/images/executors/logo_crowdstrike.png';
+import openBasLogo from '../static/images/executors/logo_openbas.png';
+import taniumLogo from '../static/images/executors/logo_tanium.png';
+import unknownDark from '../static/images/platforms/unknown-dark.png';
+
+interface ExecutorBannerProps {
+  executor: string;
+  label: string;
+  height?: number;
+}
+
+const executorBanners: Record<string, {
+  img: string;
+  backgroundColor: string;
+}> = {
+  openbas_tanium: {
+    img: openBasLogo,
+    backgroundColor: '#001BDB',
+  },
+  openbas_caldera: {
+    img: crowdstrikeLogo,
+    backgroundColor: '#E12E37',
+  },
+  openbas_agent: {
+    img: taniumLogo,
+    backgroundColor: '#E03E41',
+  },
+  openbas_crowdstrike: {
+    img: calderaLogo,
+    backgroundColor: '#8B1316',
+  },
+  Unknown: {
+    img: unknownDark,
+    backgroundColor: '#b6b7b7',
+  },
+};
+
+const renderExecutorBanner = (executor: string, label: string, height: number | undefined = 150) => {
+  const executorData = executorBanners[executor] || executorBanners.Unknown;
+  return (
+    <div
+      style={{
+        backgroundColor: executorData.backgroundColor,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: height,
+        overflow: 'hidden',
+        position: 'relative',
+        padding: 0,
+      }}
+    >
+      <img
+        src={executorData.img}
+        alt={label}
+        style={{ objectFit: 'cover' }}
+      />
+    </div>
+  );
+};
+const ExecutorBanner: FunctionComponent<ExecutorBannerProps> = ({ executor, label, height }) => {
+  return renderExecutorBanner(executor, label, height);
+};
+
+export default ExecutorBanner;
