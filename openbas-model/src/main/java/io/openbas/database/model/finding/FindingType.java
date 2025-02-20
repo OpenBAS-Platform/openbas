fa7d00ea645871bf7cba9d29174bb0637907ded9
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-02-21 00:11:42 +0100

    [backend] Add findings

--- openbas-model/src/main/java/io/openbas/database/model/finding/FindingType.java
+++ openbas-model/src/main/java/io/openbas/database/model/finding/FindingType.java
@@ -6,14 +6,14 @@ import org.jetbrains.annotations.NotNull;
 
 public class FindingType {
 
-  public static final String STRING_VALUE = "String";
+  public static final String TEXT_VALUE = "Text";
   public static final String IPV6_VALUE = "IPV6";
   public static final String CREDENTIALS_VALUE = "Credentials";
 
   @Getter
   public enum ValueType {
-    @JsonProperty(STRING_VALUE)
-    STRING(STRING_VALUE),
+    @JsonProperty(TEXT_VALUE)
+    TEXT(TEXT_VALUE),
     @JsonProperty(IPV6_VALUE)
     IPV6(IPV6_VALUE),
     @JsonProperty(CREDENTIALS_VALUE)
