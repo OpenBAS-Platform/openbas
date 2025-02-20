bb6ed5ab83079708d1e497a50d1fc6b4a047c55f
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-02-20 22:55:57 +0100

    [backend] Add findings

---
+++ openbas-api/src/main/java/io/openbas/migration/V3_68__Add_Findings.java
@@ -0,0 +1,31 @@
+package io.openbas.migration;
+
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+import org.springframework.stereotype.Component;
+
+import java.sql.Statement;
+
+@Component
+public class V3_68__Add_Findings extends BaseJavaMigration {
+
+  @Override
+  public void migrate(Context context) throws Exception {
+    try (Statement statement = context.getConnection().createStatement()) {
+      statement.execute(
+          """
+              CREATE TABLE findings (
+                  finding_id varchar(255) NOT NULL CONSTRAINT findings_pkey PRIMARY KEY,
+                  finding_field VARCHAR(255) NOT NULL,
+                  finding_type VARCHAR(255) NOT NULL,
+                  finding_value TEXT NOT NULL,
+                  finding_labels TEXT[],
+                  finding_inject_id VARCHAR(255) NOT NULL CONSTRAINT finding_inject_id_fk REFERENCES injects ON DELETE CASCADE,
+                  finding_created_at TIMESTAMP DEFAULT now(),
+                  finding_updated_at TIMESTAMP DEFAULT now()
+              );
+              """
+      );
+    }
+  }
+}
---
+++ openbas-api/src/main/java/io/openbas/rest/finding/FindingService.java
@@ -0,0 +1,62 @@
+package io.openbas.rest.finding;
+
+import io.openbas.database.model.Inject;
+import io.openbas.database.model.finding.Finding;
+import io.openbas.database.repository.FindingRepository;
+import io.openbas.rest.inject.service.InjectService;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.constraints.NotBlank;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static io.openbas.helper.StreamHelper.fromIterable;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class FindingService {
+
+  private final FindingRepository findingRepository;
+  private final InjectService injectService;
+
+  // -- CRUD --
+
+  public List<Finding> findings() {
+    return fromIterable(this.findingRepository.findAll());
+  }
+
+  public Finding finding(@NotNull final String id) {
+    return this.findingRepository.findById(id)
+        .orElseThrow(() -> new EntityNotFoundException("Finding not found with id: " + id));
+  }
+
+  public Finding findingByField(@NotNull final String field) {
+    return this.findingRepository.findByField(field)
+        .orElseThrow(() -> new EntityNotFoundException("Finding not found with field: " + field));
+  }
+
+  public Finding createFinding(@NotNull final Finding finding, @NotBlank final String injectId) {
+    Inject inject = this.injectService.inject(injectId);
+    finding.setInject(inject);
+    return this.findingRepository.save(finding);
+  }
+
+  public Finding updateFinding(@NotNull final Finding finding, @NotNull final String injectId) {
+    if (!finding.getInject().getId().equals(injectId)) {
+      throw new IllegalArgumentException("Inject id cannot be changed: " + injectId);
+    }
+    return this.findingRepository.save(finding);
+  }
+
+  public void deleteFinding(@NotNull final String id) {
+    if (!this.findingRepository.existsById(id)) {
+      throw new EntityNotFoundException("Finding not found with id: " + id);
+    }
+    this.findingRepository.deleteById(id);
+  }
+
+}
