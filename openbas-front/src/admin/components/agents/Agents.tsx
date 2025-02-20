e453dce535f5de83a93245ec2dac96806a768028
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-02-20 10:16:14 +0100

    [backend/frontend] WIP

--- openbas-front/src/admin/components/agents/Agents.tsx
+++ openbas-front/src/admin/components/agents/Agents.tsx
@@ -398,7 +398,7 @@ SHA512: ca07dc1d0a5297e29327e483f4f35dadb254d96a16a5c33da5ad048e6965a3863d621518
         TransitionComponent={Transition}
         onClose={closeInstall}
         PaperProps={{ elevation: 1 }}
-        fullWidth={true}
+        fullWidth
         maxWidth="md"
       >
         <DialogTitle><Typography variant="h6" style={{ marginTop: 10 }}>{t(` Installation ${selectedExecutor?.executor_name} - ${platform}`)}</Typography></DialogTitle>
@@ -416,7 +416,7 @@ SHA512: ca07dc1d0a5297e29327e483f4f35dadb254d96a16a5c33da5ad048e6965a3863d621518
                         labelId="arch"
                         value={arch}
                         onChange={event => setArch(event.target.value ?? 'x86_64')}
-                        fullWidth={true}
+                        fullWidth
                       >
                         <MenuItem value="x86_64">{t('x86_64')}</MenuItem>
                         <MenuItem value="arm64">{t('arm64')}</MenuItem>
