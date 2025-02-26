ea45dee272c214360426254fb646aca1f7a89f45
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-02-26 15:08:33 +0100

    [backend/frontend] Clean

--- openbas-api/src/test/java/io/openbas/utils/ExpectationUtilsTest.java
+++ openbas-api/src/test/java/io/openbas/utils/ExpectationUtilsTest.java
@@ -1,15 +1,20 @@
 package io.openbas.utils;
 
+import static io.openbas.utils.ExpectationUtils.getPreventionExpectationList;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.emptyMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import io.openbas.database.model.Asset;
 import io.openbas.database.model.Endpoint;
+import io.openbas.database.model.Inject;
+import io.openbas.database.model.InjectorContract;
 import io.openbas.database.raw.RawAsset;
 import io.openbas.database.raw.RawAssetGroup;
+import io.openbas.model.expectation.PreventionExpectation;
 import io.openbas.rest.atomic_testing.form.InjectTargetWithResult;
-import io.openbas.utils.fixtures.RawAssetFixture;
-import io.openbas.utils.fixtures.RawAssetGroupFixture;
+import io.openbas.utils.fixtures.*;
+
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
@@ -18,71 +23,22 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-class AtomicTestingUtilsTest {
+class ExpectationUtilsTest {
 
   @Test
-  @DisplayName("Fetch target results for every target linked to inject")
-  void shouldFetchTargetsWithResultsLinkedToInject() {
+  @DisplayName("Build expectations with the signature parent process name for obas implant")
+  void shouldBuildExpectationsWithSignatureParentProcessNameForObasImplant_Prevention() {
     // -- PREPARE --
-    String asset1Id = "7e3fee31-70ac-4bcc-befb-8c37397f1e25";
-    String asset2Id = "8f4rgg31-70ac-4bcc-befb-8f4rgg311e25";
-    String assetGroupId = "9p4rgg31-70ac-4bcc-befb-8f4rgg311e25";
+    Asset asset= AssetFixture.createDefaultAsset("Asset toto");
+    InjectorContract injectorContract = InjectorContractFixture.createDefaultInjectorContract();
+    Inject inject= InjectFixture.createTechnicalInject(injectorContract, "Inject 0", asset);
 
-    List<String> injectAssets = List.of(asset1Id, asset2Id);
-
-    Map<String, RawAsset> assets =
-        Map.of(
-            asset1Id,
-                RawAssetFixture.createDefaultRawAsset(
-                    asset1Id, "raw asset one", Endpoint.PLATFORM_TYPE.Linux),
-            asset2Id,
-                RawAssetFixture.createDefaultRawAsset(
-                    asset2Id, "raw asset two", Endpoint.PLATFORM_TYPE.Windows));
-
-    Map<String, RawAssetGroup> assetGroups =
-        Map.of(
-            assetGroupId,
-            RawAssetGroupFixture.createDefaultRawAssetGroup(
-                assetGroupId, "raw asset group one", List.of(asset1Id)));
+    PreventionExpectation preventionExpectation = ExpectationFixture.createTechnicalPreventionExpectationForAsset(asset, 60L);
 
     // -- EXECUTE --
-    List<InjectTargetWithResult> result =
-        AtomicTestingUtils.getTargetsWithResultsFromRaw(
-            emptyList(),
-            injectAssets,
-            emptyMap(),
-            emptyMap(),
-            emptyMap(),
-            assets,
-            emptyMap(),
-            assetGroups);
+    List<PreventionExpectation> preventionExpectations = getPreventionExpectationList(asset, inject, preventionExpectation);
 
     // -- ASSERT --
-    assertEquals(3, result.size(), "The result should contain three targets");
 
-    assertEquals(
-        injectAssets,
-        result.stream()
-            .filter(target -> target.getTargetType() == TargetType.ASSETS)
-            .map(InjectTargetWithResult::getId)
-            .toList(),
-        "Expected assets to be in the result");
-
-    assertEquals(
-        List.of(assetGroupId),
-        result.stream()
-            .filter(target -> target.getTargetType() == TargetType.ASSETS_GROUPS)
-            .map(InjectTargetWithResult::getId)
-            .toList(),
-        "Expected asset group to be in the result");
-
-    assertEquals(
-        1,
-        result.stream()
-            .filter(target -> target.getTargetType() == TargetType.ASSETS_GROUPS)
-            .map(InjectTargetWithResult::getChildren)
-            .flatMap(List::stream)
-            .count(),
-        "Expected asset group to have 1 child");
   }
 }
