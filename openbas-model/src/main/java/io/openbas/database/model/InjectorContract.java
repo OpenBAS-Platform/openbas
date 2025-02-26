710a5d433ad5ab552157a9b943bcad6035e21e9b
Guillaume Paris <guillaume.paris@filigran.io>
2025-02-26 16:20:38 +0100

    [frontend/backend] fix contract based features using key instead of type

--- openbas-model/src/main/java/io/openbas/database/model/InjectorContract.java
+++ openbas-model/src/main/java/io/openbas/database/model/InjectorContract.java
@@ -174,13 +174,16 @@ public class InjectorContract implements Base {
 
   public static final String CONTACT_CONTENT_FIELDS = "fields";
   public static final String CONTACT_ELEMENT_CONTENT_KEY = "key";
+  public static final String CONTACT_ELEMENT_CONTENT_TYPE = "type";
   public static final String CONTACT_ELEMENT_CONTENT_MANDATORY = "mandatory";
   public static final String CONTACT_ELEMENT_CONTENT_MANDATORY_GROUPS = "mandatoryGroups";
   public static final String CONTACT_ELEMENT_CONTENT_MANDATORY_CONDITIONAL =
       "mandatoryConditionField";
 
   public static final String CONTACT_ELEMENT_CONTENT_KEY_ASSETS = "assets";
-  public static final String CONTACT_ELEMENT_CONTENT_KEY_TEAMS = "teams";
+
+  public static final String CONTACT_ELEMENT_CONTENT_TYPE_ASSET = "asset";
+  public static final String CONTACT_ELEMENT_CONTENT_TYPE_TEAM = "team";
 
   public static final String DEFAULT_VALUE_FIELD = "defaultValue";
 }
