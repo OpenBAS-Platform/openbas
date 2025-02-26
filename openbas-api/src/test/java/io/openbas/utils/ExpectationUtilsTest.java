c6419d07e52e5a63e9630309dccf33466ed01b16
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-02-26 17:26:20 +0100

    [backend/frontend] Add tests

--- openbas-api/src/test/java/io/openbas/utils/ExpectationUtilsTest.java
+++ openbas-api/src/test/java/io/openbas/utils/ExpectationUtilsTest.java
@@ -1,22 +1,14 @@
 package io.openbas.utils;
 
-import static io.openbas.utils.ExpectationUtils.getPreventionExpectationList;
-import static java.util.Collections.emptyList;
-import static java.util.Collections.emptyMap;
+import static io.openbas.database.model.InjectExpectationSignature.EXPECTATION_SIGNATURE_TYPE_PARENT_PROCESS_NAME;
+import static io.openbas.utils.ExpectationUtils.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import io.openbas.database.model.Asset;
-import io.openbas.database.model.Endpoint;
-import io.openbas.database.model.Inject;
-import io.openbas.database.model.InjectorContract;
-import io.openbas.database.raw.RawAsset;
-import io.openbas.database.raw.RawAssetGroup;
+import io.openbas.database.model.*;
+import io.openbas.model.expectation.DetectionExpectation;
 import io.openbas.model.expectation.PreventionExpectation;
-import io.openbas.rest.atomic_testing.form.InjectTargetWithResult;
 import io.openbas.utils.fixtures.*;
-
 import java.util.List;
-import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -29,16 +21,105 @@ class ExpectationUtilsTest {
   @DisplayName("Build expectations with the signature parent process name for obas implant")
   void shouldBuildExpectationsWithSignatureParentProcessNameForObasImplant_Prevention() {
     // -- PREPARE --
-    Asset asset= AssetFixture.createDefaultAsset("Asset toto");
+    Endpoint endpoint = EndpointFixture.createEndpoint();
+    Agent agent = AgentFixture.createAgent(endpoint, "ext");
+    agent.setId("agentId");
+    endpoint.setAgents(List.of(agent));
     InjectorContract injectorContract = InjectorContractFixture.createDefaultInjectorContract();
-    Inject inject= InjectFixture.createTechnicalInject(injectorContract, "Inject 0", asset);
-
-    PreventionExpectation preventionExpectation = ExpectationFixture.createTechnicalPreventionExpectationForAsset(asset, 60L);
+    Inject inject = InjectFixture.createTechnicalInject(injectorContract, "Inject", endpoint);
+    inject.setId("injectId");
+    PreventionExpectation preventionExpectation =
+        ExpectationFixture.createTechnicalPreventionExpectationForAsset(endpoint, 60L);
+    DetectionExpectation detectionExpectation =
+        ExpectationFixture.createTechnicalDetectionExpectationForAsset(endpoint, 60L);
 
     // -- EXECUTE --
-    List<PreventionExpectation> preventionExpectations = getPreventionExpectationList(asset, inject, preventionExpectation);
+    List<PreventionExpectation> preventionExpectations =
+        getPreventionExpectationList(endpoint, inject, preventionExpectation);
+
+    List<DetectionExpectation> detectionExpectations =
+        getDetectionExpectationList(endpoint, inject, detectionExpectation);
 
     // -- ASSERT --
+    InjectExpectationSignature signature =
+        InjectExpectationSignature.builder()
+            .type(EXPECTATION_SIGNATURE_TYPE_PARENT_PROCESS_NAME)
+            .value("obas-implant-injectId-agent-agentId")
+            .build();
 
+    assertEquals(1, preventionExpectations.size());
+    assertEquals(1, detectionExpectations.size());
+
+    assertEquals(PreventionExpectation.class, preventionExpectations.getFirst().getClass());
+    assertEquals(DetectionExpectation.class, detectionExpectations.getFirst().getClass());
+
+    assertEquals(
+        signature,
+        preventionExpectations.stream()
+            .flatMap(prev -> prev.getInjectExpectationSignatures().stream())
+            .toList()
+            .getFirst());
+
+    assertEquals(
+        signature,
+        detectionExpectations.stream()
+            .flatMap(det -> det.getInjectExpectationSignatures().stream())
+            .toList()
+            .getFirst());
+  }
+
+  @Test
+  @DisplayName("Build expectations with the signature parent process name for caldera implant")
+  void shouldBuildExpectationsWithSignatureParentProcessNameForCalderaImplant() {
+    // -- PREPARE --
+    Endpoint endpoint = EndpointFixture.createEndpoint();
+    InjectorContract injectorContract = InjectorContractFixture.createDefaultInjectorContract();
+    Inject inject = InjectFixture.createTechnicalInject(injectorContract, "Inject", endpoint);
+    inject.setId("injectId");
+    Agent agentParent = AgentFixture.createAgent(endpoint, "ext-parent");
+    agentParent.setId("agentParentId");
+    Agent agent = AgentFixture.createAgent(endpoint, "ext");
+    agent.setId("agentId");
+    agent.setInject(inject);
+    agent.setParent(agentParent);
+
+    PreventionExpectation preventionExpectation =
+        ExpectationFixture.createTechnicalPreventionExpectationForAsset(endpoint, 60L);
+    DetectionExpectation detectionExpectation =
+        ExpectationFixture.createTechnicalDetectionExpectationForAsset(endpoint, 60L);
+
+    // -- EXECUTE --
+    List<PreventionExpectation> preventionExpectations =
+        getPreventionExpectationListForCaldera(endpoint, List.of(agent), preventionExpectation);
+
+    List<DetectionExpectation> detectionExpectations =
+        getDetectionExpectationListForCaldera(endpoint, List.of(agent), detectionExpectation);
+
+    // -- ASSERT --
+    InjectExpectationSignature signature =
+        InjectExpectationSignature.builder()
+            .type(EXPECTATION_SIGNATURE_TYPE_PARENT_PROCESS_NAME)
+            .value("obas-implant-caldera-injectId-agent-agentParentId")
+            .build();
+
+    assertEquals(1, preventionExpectations.size());
+    assertEquals(1, detectionExpectations.size());
+
+    assertEquals(PreventionExpectation.class, preventionExpectations.getFirst().getClass());
+    assertEquals(DetectionExpectation.class, detectionExpectations.getFirst().getClass());
+
+    assertEquals(
+        signature,
+        preventionExpectations.stream()
+            .flatMap(prev -> prev.getInjectExpectationSignatures().stream())
+            .toList()
+            .getFirst());
+
+    assertEquals(
+        signature,
+        detectionExpectations.stream()
+            .flatMap(det -> det.getInjectExpectationSignatures().stream())
+            .toList()
+            .getFirst());
   }
 }
