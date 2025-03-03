8e639d510b575171ceab2b7b68cc32f518f3ab67
Marine LM <marine.lemezo@filigran.io>
2025-03-03 17:22:02 +0100

    [backend] add openbas.xtm.opencti.api_url
    Signed-off-by: Marine LM <marine.lemezo@filigran.io>

--- openbas-api/src/main/java/io/openbas/injectors/opencti/config/OpenCTIConfig.java
+++ openbas-api/src/main/java/io/openbas/injectors/opencti/config/OpenCTIConfig.java
@@ -1,20 +1,23 @@
 package io.openbas.injectors.opencti.config;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "openbas.xtm.opencti")
-@Getter
-@Setter
+@Data
 public class OpenCTIConfig {
 
   @NotBlank private Boolean enable;
 
   @NotBlank private String url;
+  private String apiUrl;
 
   @NotBlank private String token;
+
+  public String getUrl() {
+    return (apiUrl != null && !apiUrl.isBlank()) ? apiUrl : url + "/graphql";
+  }
 }
