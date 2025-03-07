60e09ec17fc43dd7dcdefec5c86da2f01cdfd71e
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-03-07 10:32:20 +0100

    [frontend] clean

--- openbas-front/src/actions/Executor.js
+++ openbas-front/src/actions/Executor.js
@@ -5,3 +5,5 @@ export const fetchExecutors = () => (dispatch) => {
   const uri = '/api/executors';
   return getReferential(schema.arrayOfExecutors, uri)(dispatch);
 };
+
+export default fetchExecutors();
