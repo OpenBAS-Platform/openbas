f967843e8d32fd9997ee91ae8ece4818bbd207d5
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-03-19 20:15:39 +0100

    [backend/frontend] wip

--- openbas-api/src/main/java/io/openbas/migration/V3_74__Add_Output_parser.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_74__Add_Output_parser.java
@@ -1,6 +1,5 @@
 package io.openbas.migration;
 
-import java.sql.Connection;
 import java.sql.Statement;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
@@ -11,8 +10,39 @@ public class V3_74__Add_Output_parser extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
-    Connection connection = context.getConnection();
-    Statement select = connection.createStatement();
-    select.execute("ALTER TABLE payloads ADD column payload_output_parsers json;");
+    try (Statement statement = context.getConnection().createStatement()) {
+      statement.execute(
+          """
+
+              CREATE TABLE output_parsers (
+                    output_parser_id VARCHAR(255) NOT NULL PRIMARY KEY,
+                    output_parser_mode VARCHAR(50) NOT NULL,
+                    output_parser_type VARCHAR(50) NOT NULL,
+                    output_parser_rule TEXT NOT NULL,
+                    output_parser_payload_id VARCHAR(255) NOT NULL,
+                    output_parser_created_at TIMESTAMP DEFAULT now(),
+                    output_parser_updated_at TIMESTAMP DEFAULT now(),
+                    CONSTRAINT output_parser_payload_id_fk FOREIGN KEY (output_parser_payload_id)
+                        REFERENCES payloads ON DELETE CASCADE
+                );
+              """);
+
+      statement.execute(
+          """
+
+              CREATE TABLE contract_output_elements (
+                    contract_output_element_id VARCHAR(255) NOT NULL PRIMARY KEY,
+                    contract_output_element_group int NOT NULL,
+                    contract_output_element_name VARCHAR(50) NOT NULL,
+                    contract_output_element_key VARCHAR(50) NOT NULL,
+                    contract_output_element_type VARCHAR(50) NOT NULL,
+                    contract_output_element_output_parser_id VARCHAR(255) NOT NULL,
+                    contract_output_element_created_at TIMESTAMP DEFAULT now(),
+                    contract_output_element_updated_at TIMESTAMP DEFAULT now(),
+                    CONSTRAINT contract_output_element_output_parser_id_fk FOREIGN KEY (contract_output_element_output_parser_id)
+                        REFERENCES output_parsers ON DELETE CASCADE
+                );
+              """);
+    }
   }
 }
--- openbas-model/src/main/java/io/openbas/database/model/ContractOutputElement.java
+++ openbas-model/src/main/java/io/openbas/database/model/ContractOutputElement.java
@@ -1,26 +1,47 @@
 package io.openbas.database.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import io.openbas.helper.MonoIdDeserializer;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.annotations.UuidGenerator;
 
 @Data
-public class ContractOutputElement {
+@Entity
+@Table(name = "contract_output_elements")
+public class ContractOutputElement implements Base {
 
-  @NotNull
-  @JsonProperty("group")
+  @Id
+  @Column(name = "contract_output_element_id")
+  @GeneratedValue(generator = "UUID")
+  @UuidGenerator
+  @JsonProperty("contract_output_element_id")
+  @NotBlank
+  private String id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JsonSerialize(using = MonoIdDeserializer.class)
+  @JoinColumn(name = "contract_output_element_output_parser")
+  @JsonProperty("contract_output_element_output_parser")
+  @Schema(type = "string")
+  private OutputParser outputParser;
+
+  @Column(name = "contract_output_element_group")
+  @JsonProperty("contract_output_element_group")
   private int group;
 
-  @NotBlank
-  @JsonProperty("label")
-  private String label;
+  @Column(name = "contract_output_element_name")
+  @JsonProperty("contract_output_element_name")
+  private String name;
 
-  @NotBlank
-  @JsonProperty("key")
+  @Column(name = "contract_output_element_key")
+  @JsonProperty("contract_output_element_key")
   private String key;
 
-  @NotBlank
-  @JsonProperty("type")
+  @Column(name = "contract_output_element_type")
+  @JsonProperty("contract_output_element_type")
   private ContractOutputType type;
 }
