1e5ce47146aca7ecf466195ef44349cfda897fb7
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-03-19 17:31:20 +0100

    [backend/frontend] wip

--- openbas-model/src/main/java/io/openbas/database/model/OutputContractElement.java
+++ openbas-model/src/main/java/io/openbas/database/model/OutputContractElement.java
@@ -1,16 +1,37 @@
 package io.openbas.database.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.hibernate.annotations.UuidGenerator;
 
 @Data
-public class OutputContractElement {
+@Entity
+@Table(name = "output_contract_element")
+public class OutputContractElement implements Base {
 
-  /**
-   *   -> ParserOutPutContract : group, name, key, contractoutputType
-   *
-   * */
-  int group;
-  String name;
-  String key;
-  ContractOutputType type;
+  @Id
+  @Column(name = "output_contract_element_id")
+  @GeneratedValue(generator = "UUID")
+  @UuidGenerator
+  @JsonProperty("output_contract_element_id")
+  @NotBlank
+  private String id;
+
+  @Column(name = "output_contract_element_group")
+  @JsonProperty("output_contract_element_group")
+  private int group;
+
+  @Column(name = "output_contract_element_label")
+  @JsonProperty("output_contract_element_label")
+  private String name;
+
+  @Column(name = "output_contract_element_key")
+  @JsonProperty("output_contract_element_key")
+  private String key;
+
+  @Column(name = "output_contract_element_type")
+  @JsonProperty("output_contract_element_type")
+  private ContractOutputType type;
 }
