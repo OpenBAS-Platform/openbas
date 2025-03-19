46939dc1f6ac3e335eb17aa60d6b4c9932215545
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-03-19 19:20:43 +0100

    [backend/frontend] wip

--- openbas-api/src/main/java/io/openbas/migration/V3_74__Add_Output_parser.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_74__Add_Output_parser.java
@@ -13,6 +13,6 @@ public class V3_74__Add_Output_parser extends BaseJavaMigration {
   public void migrate(Context context) throws Exception {
     Connection connection = context.getConnection();
     Statement select = connection.createStatement();
-    select.execute("ALTER TABLE payloads ADD column payload_output_parser varchar(100);");
+    select.execute("ALTER TABLE payloads ADD column payload_output_parser json;");
   }
 }
