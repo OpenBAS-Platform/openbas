30a0fbdad31b5193e81f6992b991018dd6bd8d30
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-03-19 19:16:13 +0100

    [backend/frontend] wip

--- openbas-api/src/main/java/io/openbas/migration/V3_74__Add_Output_parser.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_74__Add_Output_parser.java
@@ -1,5 +1,6 @@
 package io.openbas.migration;
 
+import java.sql.Connection;
 import java.sql.Statement;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
@@ -10,21 +11,8 @@ public class V3_74__Add_Output_parser extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
-    try (Statement statement = context.getConnection().createStatement()) {
-      statement.execute(
-          """
-              CREATE TABLE output_parsers (
-                    output_parser_id VARCHAR(255) NOT NULL PRIMARY KEY,
-                    output_parser_mode VARCHAR(50) NOT NULL,
-                    output_parser_type VARCHAR(50) NOT NULL,
-                    output_parser_rule TEXT NOT NULL,
-                    output_parser_payload_id VARCHAR(255) NOT NULL,
-                    output_parser_created_at TIMESTAMP DEFAULT now(),
-                    output_parser_updated_at TIMESTAMP DEFAULT now(),
-                    CONSTRAINT output_parser_payload_id_fk FOREIGN KEY (output_parser_payload_id)
-                        REFERENCES payloads ON DELETE CASCADE
-                );
-              """);
-    }
+    Connection connection = context.getConnection();
+    Statement select = connection.createStatement();
+    select.execute("ALTER TABLE payloads ADD column payload_output_parser varchar(100);");
   }
 }
