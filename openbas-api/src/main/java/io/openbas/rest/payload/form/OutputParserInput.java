145b8c05c6aeebb05bf9c955a9c041ca4015e18c
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-03-20 09:51:32 +0100

    [backend/frontend] wip

--- openbas-api/src/main/java/io/openbas/rest/payload/form/OutputParserInput.java
+++ openbas-api/src/main/java/io/openbas/rest/payload/form/OutputParserInput.java
@@ -3,6 +3,7 @@ package io.openbas.rest.payload.form;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.openbas.database.model.ParserMode;
 import io.openbas.database.model.ParserType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,14 +13,18 @@ import lombok.Setter;
 public class OutputParserInput {
 
   @JsonProperty("output_parser_mode")
+  @Schema(description = "Paser Mode: STDOUT, STDERR, READ_FILE")
   private ParserMode mode;
 
   @JsonProperty("output_parser_type")
+  @Schema(description = "Parser Type: REGEX")
   private ParserType type;
 
   @JsonProperty("output_parser_rule")
+  @Schema(description = "Parser Rule")
   private String rule;
 
   @JsonProperty("output_parser_contract_output_elements")
+  @Schema(description = "List of Contract output elements")
   private List<ContractOutputElementInput> contractOutputElements;
 }
