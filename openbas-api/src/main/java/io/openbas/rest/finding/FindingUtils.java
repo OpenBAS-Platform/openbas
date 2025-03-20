844cc9678e810929331fe5f7ca7467425ab81874
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-03-20 16:46:06 +0100

    [backend/frontend] wip

---
+++ openbas-api/src/main/java/io/openbas/rest/finding/FindingUtils.java
@@ -0,0 +1,29 @@
+package io.openbas.rest.finding;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.openbas.database.model.ParserMode;
+
+public class FindingUtils {
+
+  public static String extractRawOutputByMode(final String rawOutput, final ParserMode mode) {
+    if (rawOutput == null || rawOutput.isEmpty()) {
+      return "";
+    }
+
+    try {
+      ObjectMapper objectMapper = new ObjectMapper();
+      JsonNode rootNode = objectMapper.readTree(rawOutput);
+
+      if (mode == ParserMode.STDOUT && rootNode.has("stdout")) {
+        return rootNode.get("stdout").asText();
+      } else if (mode == ParserMode.STDERR && rootNode.has("stderr")) {
+        return rootNode.get("stderr").asText();
+      }
+    } catch (Exception e) {
+      return "ERROR: Invalid JSON format";
+    }
+
+    return "";
+  }
+}
