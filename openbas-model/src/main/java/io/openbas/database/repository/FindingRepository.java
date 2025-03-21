45936845eea7288e72411ae7bbbaab8bacdebfeb
Julien Richard <julien.richard@filigran.io>
2025-03-21 18:59:37 +0100

    [backend] Working on histogram query capability

--- openbas-model/src/main/java/io/openbas/database/repository/FindingRepository.java
+++ openbas-model/src/main/java/io/openbas/database/repository/FindingRepository.java
@@ -1,12 +1,9 @@
 package io.openbas.database.repository;
 
 import io.openbas.database.model.Finding;
-
-import java.time.Instant;
-import java.util.Date;
-import java.util.List;
-
 import io.openbas.database.raw.RawFinding;
+import java.time.Instant;
+import java.util.List;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -21,12 +18,12 @@ public interface FindingRepository
   List<Finding> findAllByInjectId(@NotNull final String injectId);
 
   @Query(
-          value =
-                  "SELECT f.finding_id, f.finding_value, f.finding_type, f.finding_field," +
-                          " f.finding_inject_id, i.inject_scenario, f.finding_created_at, f.finding_updated_at "
-                          + "FROM findings f "
-                          + "LEFT JOIN injects i ON i.inject_id = f.finding_inject_id "
-                          + "WHERE f.finding_updated_at > :from ORDER BY f.finding_updated_at LIMIT 500;",
-          nativeQuery = true)
+      value =
+          "SELECT f.finding_id, f.finding_value, f.finding_type, f.finding_field,"
+              + " f.finding_inject_id, i.inject_scenario, f.finding_created_at, f.finding_updated_at "
+              + "FROM findings f "
+              + "LEFT JOIN injects i ON i.inject_id = f.finding_inject_id "
+              + "WHERE f.finding_updated_at > :from ORDER BY f.finding_updated_at LIMIT 500;",
+      nativeQuery = true)
   List<RawFinding> findForIndexing(@Param("from") Instant from);
 }
--- openbas-model/src/main/java/io/openbas/engine/handler/Handler.java
+++ openbas-model/src/main/java/io/openbas/engine/handler/Handler.java
@@ -1,11 +1,10 @@
 package io.openbas.engine.handler;
 
 import io.openbas.engine.model.EsBase;
-
 import java.time.Instant;
 import java.util.List;
 
 public interface Handler<T extends EsBase> {
 
-    List<T> fetch(Instant from);
+  List<T> fetch(Instant from);
 }
