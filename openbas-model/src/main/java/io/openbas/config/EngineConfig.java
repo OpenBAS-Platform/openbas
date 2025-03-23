27933b0e5ef56b60177470fba10d633d8092a53e
Julien Richard <julien.richard@filigran.io>
2025-03-23 15:35:28 +0100

    [backend] Introduce resource configuration

---
+++ openbas-model/src/main/java/io/openbas/config/EngineConfig.java
@@ -0,0 +1,38 @@
+package io.openbas.config;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "engine")
+@Data
+public class EngineConfig {
+
+  private String engineSelector = "elk";
+
+  private String indexPrefix = "openbas";
+
+  private String indexSuffix = "-000001";
+
+  private String numberOfShards = "1";
+
+  private String numberOfReplicas = "1";
+
+  private int maxResultWindow = 100000;
+
+  private long maxPrimaryShardDocs = 75000000;
+
+  private String maxPrimaryShardsSize = "50Gb";
+
+  private String maxFieldsSize = "4096";
+
+  @NotNull private String url;
+
+  private String username;
+
+  private String password;
+
+  private boolean secure = false;
+}
