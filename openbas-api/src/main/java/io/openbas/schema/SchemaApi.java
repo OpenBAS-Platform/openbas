7770fbd5d4bc587ddad24ca999adbdd12b6a6ade
Julien Richard <julien.richard@filigran.io>
2025-03-24 21:23:54 +0100

    [backend] Introduce schema api with Indexable annotation

--- openbas-api/src/main/java/io/openbas/schema/SchemaApi.java
+++ openbas-api/src/main/java/io/openbas/schema/SchemaApi.java
@@ -2,19 +2,31 @@ package io.openbas.schema;
 
 import static io.openbas.utils.schema.SchemaUtils.isValidClassName;
 
+import io.openbas.engine.EsEngine;
 import io.openbas.rest.helper.RestBehavior;
 import io.openbas.schema.model.PropertySchemaDTO;
+import io.openbas.utils.schema.PropertySchema;
 import io.openbas.utils.schema.SchemaUtils;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping
 public class SchemaApi extends RestBehavior {
+
+  private EsEngine esEngine;
+
+  @Autowired
+  public void setEsEngine(EsEngine esEngine) {
+    this.esEngine = esEngine;
+  }
 
   @PostMapping("/api/schemas/{className}")
   public List<PropertySchemaDTO> schemas(
@@ -37,5 +49,21 @@ public class SchemaApi extends RestBehavior {
         .filter(p -> filterNames.isEmpty() || filterNames.contains(p.getJsonName()))
         .map(PropertySchemaDTO::new)
         .toList();
+  }
+
+  @GetMapping("/api/engine/schemas")
+  public Set<PropertySchemaDTO> engineSchemas() {
+    return esEngine.getModels().stream()
+        .flatMap(
+            model -> {
+              try {
+                return SchemaUtils.schemaWithSubtypes(model.getModel()).stream();
+              } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+              }
+            })
+        .filter(PropertySchema::isFilterable)
+        .map(PropertySchemaDTO::new)
+        .collect(Collectors.toSet());
   }
 }
