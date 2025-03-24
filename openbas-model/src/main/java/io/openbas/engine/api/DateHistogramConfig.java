b330657b349577c7e3627b0848a4bca2c88f18bb
Julien Richard <julien.richard@filigran.io>
2025-03-24 14:27:36 +0100

    [backend] Introduce multi timeseries api

--- openbas-model/src/main/java/io/openbas/engine/api/DateHistogramConfig.java
+++ openbas-model/src/main/java/io/openbas/engine/api/DateHistogramConfig.java
@@ -9,23 +9,26 @@ import lombok.Setter;
 @Getter
 @Setter
 public class DateHistogramConfig {
+  private String name;
   private Instant start;
   private Instant end;
   private Filters.FilterGroup filter;
   private String field = "base_created_at";
   private HistogramInterval interval = HistogramInterval.day;
 
-  public DateHistogramConfig() {
+  public DateHistogramConfig(String name) {
+    this.name = name;
     this.end = Instant.now();
     this.start = this.end.minus(30, ChronoUnit.DAYS);
   }
 
-  public DateHistogramConfig(Filters.FilterGroup filter) {
-    this();
+  public DateHistogramConfig(String name, Filters.FilterGroup filter) {
+    this(name);
     this.filter = filter;
   }
 
-  public DateHistogramConfig(Instant start, Instant end) {
+  public DateHistogramConfig(String name, Instant start, Instant end) {
+    this(name);
     this.start = start;
     this.end = end;
   }
--- openbas-model/src/main/java/io/openbas/engine/model/EsTimeseries.java
+++ openbas-model/src/main/java/io/openbas/engine/model/EsTimeseries.java
@@ -1,17 +1,23 @@
 package io.openbas.engine.model;
 
-import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class EsTimeseries {
-  private Instant date;
-  private long value;
+  private String label;
+  private String color;
+  private List<EsTimeseriesData> data = new ArrayList<>();
 
-  public EsTimeseries(Instant date, long value) {
-    this.date = date;
-    this.value = value;
+  public EsTimeseries(String label) {
+    this.label = label;
+  }
+
+  public EsTimeseries(String label, List<EsTimeseriesData> data) {
+    this.label = label;
+    this.data = data;
   }
 }
