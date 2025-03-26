f683cd4310751d073edc5dc224a43e134fa995e2
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-03-26 16:58:30 +0100

    [backend/frontend] Spotless

--- openbas-front/src/actions/custom_dashboards/customdashboardwidget-action.ts
+++ openbas-front/src/actions/custom_dashboards/customdashboardwidget-action.ts
@@ -2,6 +2,7 @@ import { simplePostCall } from '../../utils/Action';
 import { type WidgetInput } from '../../utils/api-types';
 import { CUSTOM_DASHBOARD_URI } from './customdashboard-action';
 
+// eslint-disable-next-line import/prefer-default-export
 export const createCustomDashboardWidget = (customDashboardId: string, input: WidgetInput) => {
   return simplePostCall(`${CUSTOM_DASHBOARD_URI}/${customDashboardId}/widgets`, input);
 };
