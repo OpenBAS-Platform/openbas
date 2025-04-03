f92ff2b6cc597cd0ff27a94935e6154cd45182af
Guillaume Paris <guillaume.paris@filigran.io>
2025-04-03 22:02:36 +0200

    [frontend] Improve redux selector & use an up to date immutable lib

--- openbas-front/src/store.ts
+++ openbas-front/src/store.ts
@@ -1,9 +1,9 @@
 import { composeWithDevTools } from '@redux-devtools/extension';
+import { fromJS, isImmutable } from 'immutable';
 import * as R from 'ramda';
 import { useSelector } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import { thunk } from 'redux-thunk';
-import Immutable from 'seamless-immutable';
 
 import { storeHelper } from './actions/Schema';
 import { entitiesInitializer } from './reducers/Referential';
@@ -11,12 +11,13 @@ import createRootReducer from './reducers/Root';
 
 // Default application state
 const initialState = {
-  app: Immutable({
+  app: fromJS({
     logged: {},
     worker: { status: 'RUNNING' },
   }),
   referential: entitiesInitializer,
-};
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+} as any;
 
 const initStore = () => {
   if (process.env.NODE_ENV === 'development') {
@@ -35,6 +36,28 @@ const initStore = () => {
 
 // TODO type selector object
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const useHelper = (selector: any) => useSelector(state => selector(storeHelper(state)), R.equals);
+export const useHelper = (selector: any) => (() => {
+  const selected = useSelector(state => selector(storeHelper(state)), R.equals);
+
+  if (selected === null || selected === undefined) {
+    return selected;
+  }
+
+  if (isImmutable(selected)) {
+    return selected.toJS();
+  }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const result: Record<string, any> = {};
+  for (const key in selected) {
+    if (Object.prototype.hasOwnProperty.call(selected, key)) {
+      if (selected[key] && isImmutable(selected[key])) {
+        result[key] = selected[key].toJS();
+      } else {
+        result[key] = selected[key];
+      }
+    }
+  }
+  return result;
+})();
 
 export const store = initStore();
