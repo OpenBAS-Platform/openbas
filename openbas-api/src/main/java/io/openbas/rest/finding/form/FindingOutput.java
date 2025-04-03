a24c8b86219d874e5377d4c7c75a6cd6f0ed02c6
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-04-03 13:07:44 +0200

    [backend] Wip

---
+++ openbas-api/src/main/java/io/openbas/rest/finding/form/FindingOutput.java
@@ -0,0 +1,50 @@
+package io.openbas.rest.finding.form;
+
+import static java.time.Instant.now;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.openbas.database.model.*;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FindingOutput {
+
+  @JsonProperty("finding_id")
+  @NotBlank
+  private String id;
+
+  @JsonProperty("finding_field")
+  @NotBlank
+  private String field;
+
+  @JsonProperty("finding_type")
+  @NotNull
+  protected ContractOutputType type;
+
+  @JsonProperty("finding_value")
+  @NotBlank
+  protected String value;
+
+  @JsonProperty("finding_name")
+  protected String name;
+
+  // -- AUDIT --
+  @JsonProperty("finding_created_at")
+  @NotNull
+  private Instant creationDate = now();
+
+  // Relation
+  @JsonProperty("finding_tags")
+  private Set<String> tagIds = new HashSet<>();
+
+  @JsonProperty("finding_assets")
+  private Set<String> assetIds = new HashSet<>();
+}
