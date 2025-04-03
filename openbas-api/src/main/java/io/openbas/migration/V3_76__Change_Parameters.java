959506fb7f052d0ea5f0c1e44b3234c48db6c7b6
Julien Richard <julien.richard@filigran.io>
2025-04-03 09:30:36 +0200

    [backend] move migration after rebase

--- openbas-api/src/main/java/io/openbas/migration/V3_76__Change_Parameters.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_76__Change_Parameters.java
@@ -7,7 +7,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V3_74__Change_Parameters extends BaseJavaMigration {
+public class V3_76__Change_Parameters extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
