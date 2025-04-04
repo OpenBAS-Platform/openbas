b88c8bb99882f67d1fddd9e9dd22c02680587c96
Johanah LEKEU <johanah.lekeu@filigran.io>
2025-04-04 12:21:27 +0200

    Start search with dynamic assets

--- openbas-api/src/main/java/io/openbas/rest/asset_group/AssetGroupApi.java
+++ openbas-api/src/main/java/io/openbas/rest/asset_group/AssetGroupApi.java
@@ -4,6 +4,7 @@ import static io.openbas.database.model.User.ROLE_USER;
 import static io.openbas.database.specification.AssetGroupSpecification.fromIds;
 import static io.openbas.helper.StreamHelper.fromIterable;
 import static io.openbas.helper.StreamHelper.iterableToSet;
+import static io.openbas.utils.FilterUtilsJpa.computeFilterGroupJpa;
 
 import io.openbas.aop.LogExecutionTime;
 import io.openbas.database.model.AssetGroup;
@@ -25,11 +26,14 @@ import io.openbas.utils.pagination.SearchPaginationInput;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
@@ -77,9 +81,10 @@ public class AssetGroupApi extends RestBehavior {
   public Page<EndpointOutput> endpointsFromAssetGroup(
       @RequestBody @Valid SearchPaginationInput searchPaginationInput,
       @PathVariable @NotBlank final String assetGroupId) {
+
     Page<Endpoint> endpointPage =
-        endpointService.searchEndpoints(
-            EndpointSpecification.findEndpointsForAssetGroup(assetGroupId), searchPaginationInput);
+        endpointService.searchManagedEndpoints(
+            EndpointSpecification.findEndpointsForAssetGroup(assetGroupId), assetGroupId, searchPaginationInput);
     // Convert the Page of Endpoint to a Page of EndpointOutput
     List<EndpointOutput> endpointOutputs =
         endpointPage.getContent().stream().map(endpointMapper::toEndpointOutput).toList();
