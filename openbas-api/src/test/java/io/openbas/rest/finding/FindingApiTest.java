882de25bd2b1094272f0d876e9d8b95510645e61
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-04-04 16:37:16 +0200

    [backend] Wip

---
+++ openbas-api/src/test/java/io/openbas/rest/finding/FindingApiTest.java
@@ -0,0 +1,56 @@
+package io.openbas.rest.finding;
+
+import static io.openbas.utils.JsonUtils.asJsonString;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.openbas.IntegrationTest;
+import io.openbas.utils.fixtures.composers.EndpointComposer;
+import io.openbas.utils.fixtures.composers.ExerciseComposer;
+import io.openbas.utils.fixtures.composers.InjectComposer;
+import io.openbas.utils.fixtures.composers.ScenarioComposer;
+import io.openbas.utils.mockUser.WithMockAdminUser;
+import io.openbas.utils.pagination.SearchPaginationInput;
+import jakarta.annotation.Resource;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@TestInstance(PER_CLASS)
+@Transactional
+class FindingApiTest extends IntegrationTest {
+
+  private static final String FINDING_URI = "/api/findings";
+  @Resource protected ObjectMapper mapper;
+  @Autowired private MockMvc mvc;
+
+  @Autowired private FindingComposer findingComposer;
+  @Autowired private EndpointComposer endpointComposer;
+  @Autowired private InjectComposer injectComposer;
+  @Autowired private ScenarioComposer scenarioComposer;
+  @Autowired private ExerciseComposer simulationComposer;
+
+  @DisplayName("Search global findings")
+  @Test
+  @WithMockAdminUser
+  public void given_a_search_input_should_return_page_of_findings() throws Exception {
+
+    SearchPaginationInput input = new SearchPaginationInput();
+
+    mvc.perform(
+            post(FINDING_URI + "/search/")
+                .content(asJsonString(input))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().is2xxSuccessful())
+        .andReturn()
+        .getResponse()
+        .getContentAsString();
+  }
+}
