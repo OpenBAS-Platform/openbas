b6c2720186a262b03572cc4d558a0155f66daf38
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-04-04 12:34:18 +0200

    [backend] Wip

--- openbas-front/src/actions/findings/finding-actions.ts
+++ openbas-front/src/actions/findings/finding-actions.ts
@@ -15,3 +15,15 @@ export const searchFindingsForInjects = (injectId: string, searchPaginationInput
   const uri = `${FINDING_URI}/injects/${injectId}/search`;
   return simplePostCall(uri, data);
 };
+
+export const searchFindingsForSimulations = (simulationId: string, searchPaginationInput: SearchPaginationInput) => {
+  const data = searchPaginationInput;
+  const uri = `${FINDING_URI}/exercises/${simulationId}/search`;
+  return simplePostCall(uri, data);
+};
+
+export const searchFindingsForScenarios = (scenarioId: string, searchPaginationInput: SearchPaginationInput) => {
+  const data = searchPaginationInput;
+  const uri = `${FINDING_URI}/scenarios/${scenarioId}/search`;
+  return simplePostCall(uri, data);
+};
