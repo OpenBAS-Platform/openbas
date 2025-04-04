5af94fc5b7dd9b4a7759ec56ab024f6c484f7abe
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-04-04 12:31:06 +0200

    [backend] Wip

--- openbas-model/src/main/java/io/openbas/database/specification/FindingSpecification.java
+++ openbas-model/src/main/java/io/openbas/database/specification/FindingSpecification.java
@@ -11,4 +11,14 @@ public class FindingSpecification {
   public static Specification<Finding> findFindingsForInject(@NotNull final String injectId) {
     return (root, query, cb) -> cb.equal(root.get("inject").get("id"), injectId);
   }
+
+  public static Specification<Finding> findFindingsForSimulation(
+      @NotNull final String simulationId) {
+    return (root, query, cb) ->
+        cb.equal(root.get("inject").get("exercise").get("id"), simulationId);
+  }
+
+  public static Specification<Finding> findFindingsForScenario(@NotNull final String scenarioId) {
+    return (root, query, cb) -> cb.equal(root.get("inject").get("scenario").get("id"), scenarioId);
+  }
 }
