b673dda43ed91285f82f023487bd4107bdc3b3bf
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-04-08 14:31:29 +0200

    [backend] Clean

--- openbas-api/src/main/java/io/openbas/rest/inject/output/InjectSimple.java
+++ openbas-api/src/main/java/io/openbas/rest/inject/output/InjectSimple.java
@@ -1,6 +1,10 @@
 package io.openbas.rest.inject.output;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import java.util.*;
 import lombok.Builder;
@@ -10,16 +14,20 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@JsonInclude(NON_NULL)
 public class InjectSimple {
 
+  @Schema(description = "Inject Id")
   @JsonProperty("inject_id")
   @NotBlank
   private String id;
 
+  @Schema(description = "Inject Title")
   @JsonProperty("inject_title")
   @NotBlank
   private String title;
 
+  @Schema(description = "Tags")
   @JsonProperty("inject_tags")
   private Set<String> tags;
 }
--- openbas-api/src/main/java/io/openbas/rest/payload/output/output_parser/ContractOutputElementSimple.java
+++ openbas-api/src/main/java/io/openbas/rest/payload/output/output_parser/ContractOutputElementSimple.java
@@ -3,6 +3,8 @@ package io.openbas.rest.payload.output.output_parser;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.openbas.database.model.ContractOutputType;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Set;
 import lombok.Builder;
@@ -14,29 +16,35 @@ import lombok.Data;
 public class ContractOutputElementSimple {
 
   @JsonProperty("contract_output_element_id")
+  @NotBlank
   private String id;
 
   @JsonProperty("contract_output_element_rule")
   @Schema(description = "The rule to apply for parsing the output, for example, can be a regex.")
+  @NotBlank
   private String rule;
 
   @JsonProperty("contract_output_element_name")
   @Schema(description = "Represents the name of the rule.")
+  @NotBlank
   private String name;
 
   @JsonProperty("contract_output_element_key")
   @Schema(description = "Represents a unique key identifier.")
+  @NotBlank
   private String key;
 
   @JsonProperty("contract_output_element_type")
   @Schema(
       description = "Represents the data type being extracted.",
       example = "text, number, port, portscan, ipv4, ipv6, credentials")
+  @NotNull
   private ContractOutputType type;
 
   @JsonProperty("contract_output_element_tags")
   private List<String> tagIds;
 
   @JsonProperty("contract_output_element_regex_groups")
+  @NotNull
   private Set<RegexGroupSimple> regexGroups;
 }
