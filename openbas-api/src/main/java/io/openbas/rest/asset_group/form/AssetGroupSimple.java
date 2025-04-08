5b76ff03ccc9201b51d98392e8432aec25a6de76
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-04-08 12:55:34 +0200

    [backend] Fix findings for exercises

--- openbas-api/src/main/java/io/openbas/rest/asset_group/form/AssetGroupSimple.java
+++ openbas-api/src/main/java/io/openbas/rest/asset_group/form/AssetGroupSimple.java
@@ -1,27 +1,33 @@
 package io.openbas.rest.asset_group.form;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.openbas.database.model.Filters;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import java.util.Set;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Builder
-@Data
+@JsonInclude(NON_NULL)
 public class AssetGroupSimple {
 
+  @Schema(description = "Asset group Id")
   @JsonProperty("asset_group_id")
   @NotBlank
   private String id;
 
+  @Schema(description = "Asset group Name")
   @JsonProperty("asset_group_name")
   @NotBlank
   private String name;
 
-  @JsonProperty("asset_group_dynamic_filter")
-  private Filters.FilterGroup dynamicFilter;
-
+  @Schema(description = "Tags")
   @JsonProperty("asset_group_tags")
   private Set<String> tags;
 }
--- openbas-api/src/main/java/io/openbas/utils/AssetGroupMapper.java
+++ openbas-api/src/main/java/io/openbas/utils/AssetGroupMapper.java
@@ -1,7 +1,6 @@
 package io.openbas.utils;
 
 import io.openbas.database.model.AssetGroup;
-import io.openbas.rest.asset_group.form.AssetGroupOutput;
 import io.openbas.rest.asset_group.form.AssetGroupSimple;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -11,23 +10,10 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class AssetGroupMapper {
 
-  public AssetGroupOutput toAssetGroupOutput(AssetGroup assetGroup) {
-    return AssetGroupOutput.builder()
-        .id(assetGroup.getId())
-        .name(assetGroup.getName())
-        .description(assetGroup.getDescription())
-        .dynamicFilter(assetGroup.getDynamicFilter())
-        .assets(
-            assetGroup.getAssets().stream().map(asset -> asset.getId()).collect(Collectors.toSet()))
-        .tags(assetGroup.getTags().stream().map(tag -> tag.getId()).collect(Collectors.toSet()))
-        .build();
-  }
-
   public AssetGroupSimple toAssetGroupSimple(AssetGroup assetGroup) {
     return AssetGroupSimple.builder()
         .id(assetGroup.getId())
         .name(assetGroup.getName())
-        .dynamicFilter(assetGroup.getDynamicFilter())
         .tags(assetGroup.getTags().stream().map(tag -> tag.getId()).collect(Collectors.toSet()))
         .build();
   }
