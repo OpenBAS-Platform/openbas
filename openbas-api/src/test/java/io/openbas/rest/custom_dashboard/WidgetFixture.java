beaef040a4af54f16ee74b6e7a6cd4aceb0dcb3f
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-04-08 13:13:53 +0200

    [backend] Add elastic & fix tests

--- openbas-api/src/test/java/io/openbas/rest/custom_dashboard/WidgetFixture.java
+++ openbas-api/src/test/java/io/openbas/rest/custom_dashboard/WidgetFixture.java
@@ -3,6 +3,7 @@ package io.openbas.rest.custom_dashboard;
 import static io.openbas.database.model.Widget.WidgetType.VERTICAL_BAR_CHART;
 
 import io.openbas.database.model.Widget;
+import io.openbas.database.model.WidgetLayout;
 import io.openbas.engine.api.DateHistogramWidget;
 
 public class WidgetFixture {
@@ -15,6 +16,8 @@ public class WidgetFixture {
     DateHistogramWidget widgetConfig = new DateHistogramWidget();
     widgetConfig.setTitle(NAME);
     widget.setHistogramWidget(widgetConfig);
+    WidgetLayout widgetLayout = new WidgetLayout();
+    widget.setLayout(widgetLayout);
     return widget;
   }
 }
