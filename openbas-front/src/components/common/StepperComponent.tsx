5e4d8175afbdc7f64c8508b93bb4c6483bdd5683
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-04-09 21:02:58 +0200

    [frontend] Improv UI

--- openbas-front/src/components/common/StepperComponent.tsx
+++ openbas-front/src/components/common/StepperComponent.tsx
@@ -1,21 +1,35 @@
 import { Step, StepLabel, Stepper } from '@mui/material';
 import type { FunctionComponent } from 'react';
 
+import { getAvailableSteps, type StepType } from '../../admin/components/workspaces/custom_dashboards/widgets/WidgetUtils';
+import type { Widget } from '../../utils/api-types';
+
 const StepperComponent: FunctionComponent<{
-  steps: string[];
+  widgetType: Widget['widget_type'];
+  steps: StepType[];
   activeStep: number;
   handlePrevious: (index: number) => void;
-}> = ({ steps, activeStep, handlePrevious }) => {
+}> = ({ widgetType, steps, activeStep, handlePrevious }) => {
+  const availableSteps = getAvailableSteps(widgetType);
+
   return (
     <Stepper activeStep={activeStep}>
       {steps.map((label, index) => {
+        const isEnabled = availableSteps.includes(label);
         return (
-          <Step key={label}>
+          <Step
+            key={label}
+            disabled={!isEnabled}
+          >
             <StepLabel
-              style={{ cursor: index < activeStep ? 'pointer' : 'default' }}
-              onClick={() => handlePrevious(index)}
+              onClick={isEnabled ? () => handlePrevious(index) : undefined}
+              sx={{
+                cursor: isEnabled && index < activeStep ? 'pointer' : 'default',
+                opacity: isEnabled ? 1 : 0.5,
+              }}
             >
               {label}
+              {isEnabled && index < activeStep}
             </StepLabel>
           </Step>
         );
