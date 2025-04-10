f66dc732f6cc4266d2517c0b1eab274056f1ae3d
Guillaume Paris <guillaume.paris@filigran.io>
2025-04-10 16:31:29 +0200

    wip

--- openbas-front/src/admin/components/workspaces/custom_dashboards/widgets/WidgetStructuralViz.tsx
+++ openbas-front/src/admin/components/workspaces/custom_dashboards/widgets/WidgetStructuralViz.tsx
@@ -1,8 +1,12 @@
+import { useTheme } from '@mui/material/styles';
 import { memo, useEffect, useState } from 'react';
+import Chart from 'react-apexcharts';
 
 import { series } from '../../../../../actions/dashboards/dashboard-action';
+import { useFormatter } from '../../../../../components/i18n';
 import Loader from '../../../../../components/Loader';
 import { type EsSeries, type Widget } from '../../../../../utils/api-types';
+import { lineChartOptions, verticalBarsChartOptions } from '../../../../../utils/Charts';
 import { isEmptyField, isNotEmptyField } from '../../../../../utils/utils';
 import MatrixMitre from './viz/MatrixMitre';
 
@@ -11,6 +15,9 @@ interface WidgetStructuralVizProps { widget: Widget }
 const WidgetStructuralViz = ({ widget }: WidgetStructuralVizProps) => {
   const [structuralVizData, setStructuralVizData] = useState<EsSeries[]>([]);
   const [loading, setLoading] = useState(true);
+  const theme = useTheme();
+  const { t } = useFormatter();
+
   useEffect(() => {
     series(widget.widget_id).then((response) => {
       if (response.data && isNotEmptyField(response.data)) {
@@ -26,21 +33,66 @@ const WidgetStructuralViz = ({ widget }: WidgetStructuralVizProps) => {
     return <Loader variant="inElement" />;
   }
 
-  console.log(structuralVizData);
+  const seriesData
+      = structuralVizData.map(({ label, data, color }) => {
+        if (data) {
+          return ({
+            label,
+            data: data.map(n => ({
+              x: n.label,
+              y: n.value,
+            })),
+          });
+        }
+        return [];
+      });
+
+  console.log(structuralVizData, seriesData);
 
   switch (widget.widget_type) {
     case 'security-coverage':
       return (
         <MatrixMitre data={structuralVizData} />
       );
-    case 'line':
-      return (
-
-        <div>coucou</div>
-      );
     case 'vertical-barchart':
       return (
-        <div>coucou 2</div>
+        <Chart
+          options={verticalBarsChartOptions(
+            theme,
+            null,
+            undefined,
+            false,
+            false,
+            false,
+            true,
+            'dataPoints',
+            true,
+            false,
+            undefined,
+            t('No data to display'),
+          )}
+          series={seriesData}
+          type="bar"
+          width="100%"
+          height="100%"
+        />
+      );
+    case 'line':
+      return (
+        <Chart
+          options={lineChartOptions(
+            theme,
+            false,
+            null,
+            null,
+            undefined,
+            false,
+          )}
+          series={seriesData}
+          type="line"
+          width="100%"
+          height="100%"
+        />
       );
     default:
       return 'Not implemented yet';
