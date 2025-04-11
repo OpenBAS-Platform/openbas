76b48b9adc1c9f553f2cd0568b71c1980394bd69
Marine LM <marine.lemezo@filigran.io>
2025-04-11 10:51:48 +0200

    [frontend] fix EEChip
    Signed-off-by: Marine LM <marine.lemezo@filigran.io>

--- openbas-front/src/admin/components/common/entreprise_edition/EEChip.tsx
+++ openbas-front/src/admin/components/common/entreprise_edition/EEChip.tsx
@@ -1,23 +1,17 @@
-import { useState } from 'react';
+import { Tooltip } from '@mui/material';
 import { makeStyles } from 'tss-react/mui';
 
-import { type UserHelper } from '../../../../actions/helper';
-import { useHelper } from '../../../../store';
 import useEnterpriseEdition from '../../../../utils/hooks/useEnterpriseEdition';
-import EnterpriseEditionAgreement from './EnterpriseEditionAgreement';
 
-// Deprecated - https://mui.com/system/styles/basics/
-// Do not use it for new code.
-const useStyles = makeStyles()(theme => ({
+const useStyles = makeStyles<{ isClickable: boolean }>()((theme, { isClickable }) => ({
   container: {
     fontSize: 'xx-small',
     height: 14,
-    display: 'inline-flex',
+    display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     width: 21,
     margin: 'auto',
-    marginLeft: 6,
     borderRadius: theme.borderRadius,
     border: `1px solid ${theme.palette.ee.main}`,
     color: theme.palette.ee.main,
@@ -41,32 +35,24 @@ const useStyles = makeStyles()(theme => ({
   },
 }));
 
-const EEChip = ({ clickable = true, floating = false }: {
+const EEChip = ({ clickable = true, floating = false, onClick = undefined }: {
   clickable?: boolean;
+  onClick?: (value: boolean) => void;
   floating?: boolean;
 }) => {
-  const { classes } = useStyles();
+  const { classes } = useStyles({ isClickable: clickable });
   const isEnterpriseEdition = useEnterpriseEdition();
-  const [displayDialog, setDisplayDialog] = useState(false);
-  const userAdmin = useHelper((helper: UserHelper) => {
-    const me = helper.getMe();
-    return me?.user_admin ?? false;
-  });
+
   return (!isEnterpriseEdition && (
-    <>
-      <div
-        className={floating ? classes.containerFloating : classes.container}
-        onClick={() => clickable && setDisplayDialog(true)}
-      >
+    <Tooltip
+      title="Enterprise Edition Feature"
+      className={floating ? classes.containerFloating : classes.container}
+      onClick={() => clickable && onClick && onClick(true)}
+    >
+      <span>
         EE
-      </div>
-      {userAdmin && (
-        <EnterpriseEditionAgreement
-          open={displayDialog}
-          onClose={() => setDisplayDialog(false)}
-        />
-      )}
-    </>
+      </span>
+    </Tooltip>
   ));
 };
 
