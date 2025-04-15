1037e7a6466adab3b89a28889e4dc06b4303d70d
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-04-15 16:22:07 +0200

    [backend] Improv filter properties

--- openbas-model/src/main/java/io/openbas/engine/model/inject/EsInject.java
+++ openbas-model/src/main/java/io/openbas/engine/model/inject/EsInject.java
@@ -15,31 +15,31 @@ public class EsInject extends EsBase {
   /* Every attribute must be uniq, so prefixed with the entity type! */
   /* Except relationships, they should have same name on every model! */
 
-  @Queryable(label = "title", filterable = true, sortable = true)
+  @Queryable(label = "inject title")
   private String inject_title;
 
-  @Queryable(label = "status", filterable = true, sortable = true)
+  @Queryable(label = "inject status")
   private String inject_status;
 
   // -- SIDE --
 
-  @Queryable(label = "scenario", filterable = true, sortable = true)
+  @Queryable(label = "scenario")
   @EsQueryable(keyword = true)
   private String base_scenario_side; // Must finish by _side
 
-  @Queryable(label = "simulation", filterable = true, sortable = true)
+  @Queryable(label = "simulation")
   @EsQueryable(keyword = true)
   private String base_simulation_side; // Must finish by _side
 
-  @Queryable(label = "attack patterns", filterable = true, sortable = true)
+  @Queryable(label = "attack patterns")
   @EsQueryable(keyword = true)
   private Set<String> base_attack_patterns_side; // Must finish by _side
 
-  @Queryable(label = "kill chain phases", filterable = true, sortable = true)
+  @Queryable(label = "kill chain phases")
   @EsQueryable(keyword = true)
   private Set<String> base_kill_chain_phases_side; // Must finish by _side
 
-  @Queryable(label = "inject contract", filterable = true, sortable = true)
+  @Queryable(label = "inject contract")
   @EsQueryable(keyword = true)
   private String base_inject_contract_side; // Must finish by _side
 }
