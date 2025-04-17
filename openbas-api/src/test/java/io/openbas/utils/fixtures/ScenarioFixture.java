637dfd9b6a9345518b8cb6c2b6c6771a794e3169
Marine LM <marine.lemezo@filigran.io>
2025-04-17 16:10:05 +0200

    [backend] add TUs
    Signed-off-by: Marine LM <marine.lemezo@filigran.io>

--- openbas-api/src/test/java/io/openbas/utils/fixtures/ScenarioFixture.java
+++ openbas-api/src/test/java/io/openbas/utils/fixtures/ScenarioFixture.java
@@ -8,6 +8,8 @@ import io.openbas.database.model.Team;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Set;
 
