f35e1486dad0c768dc2902a055a5cf25eb9881a7
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-04-18 09:03:26 +0200

    [backend] Align default expectation score

--- openbas-framework/src/main/java/io/openbas/expectation/ExpectationBuilderService.java
+++ openbas-framework/src/main/java/io/openbas/expectation/ExpectationBuilderService.java
@@ -12,14 +12,13 @@ public class ExpectationBuilderService {
 
   private final ExpectationPropertiesConfig expectationPropertiesConfig;
 
-  public static Double DEFAULT_TECHNICAL_EXPECTATION_SCORE = 100.0;
-  public static Double DEFAULT_HUMAN_EXPECTATION_SCORE = 0.0;
+  public static Double DEFAULT_EXPECTATION_SCORE = 100.0;
 
   public Expectation buildPreventionExpectation() {
     Expectation preventionExpectation = new Expectation();
     preventionExpectation.setType(PREVENTION);
     preventionExpectation.setName("Expect inject to be prevented");
-    preventionExpectation.setScore(DEFAULT_TECHNICAL_EXPECTATION_SCORE);
+    preventionExpectation.setScore(DEFAULT_EXPECTATION_SCORE);
     preventionExpectation.setExpirationTime(
         this.expectationPropertiesConfig.getPreventionExpirationTime());
     return preventionExpectation;
@@ -29,7 +28,7 @@ public class ExpectationBuilderService {
     Expectation detectionExpectation = new Expectation();
     detectionExpectation.setType(DETECTION);
     detectionExpectation.setName("Expect inject to be detected");
-    detectionExpectation.setScore(DEFAULT_TECHNICAL_EXPECTATION_SCORE);
+    detectionExpectation.setScore(DEFAULT_EXPECTATION_SCORE);
     detectionExpectation.setExpirationTime(
         this.expectationPropertiesConfig.getDetectionExpirationTime());
     return detectionExpectation;
@@ -39,7 +38,7 @@ public class ExpectationBuilderService {
     Expectation challengeExpectation = new Expectation();
     challengeExpectation.setType(CHALLENGE);
     challengeExpectation.setName("Expect targets to complete the challenge(s)");
-    challengeExpectation.setScore(DEFAULT_HUMAN_EXPECTATION_SCORE);
+    challengeExpectation.setScore(DEFAULT_EXPECTATION_SCORE);
     challengeExpectation.setExpirationTime(
         this.expectationPropertiesConfig.getChallengeExpirationTime());
     return challengeExpectation;
@@ -49,7 +48,7 @@ public class ExpectationBuilderService {
     Expectation articleExpectation = new Expectation();
     articleExpectation.setType(ARTICLE);
     articleExpectation.setName("Expect targets to read the article(s)");
-    articleExpectation.setScore(DEFAULT_HUMAN_EXPECTATION_SCORE);
+    articleExpectation.setScore(DEFAULT_EXPECTATION_SCORE);
     articleExpectation.setExpirationTime(
         this.expectationPropertiesConfig.getArticleExpirationTime());
     return articleExpectation;
