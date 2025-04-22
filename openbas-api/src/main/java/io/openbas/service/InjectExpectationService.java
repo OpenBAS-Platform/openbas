515c799f36abc3c9cd01f373b8ac94d3715b8878
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-04-22 15:00:18 +0200

    [backend/frontend] PR feedbacks

--- openbas-api/src/main/java/io/openbas/service/InjectExpectationService.java
+++ openbas-api/src/main/java/io/openbas/service/InjectExpectationService.java
@@ -3,6 +3,7 @@ package io.openbas.service;
 import static io.openbas.database.model.InjectExpectation.EXPECTATION_TYPE.*;
 import static io.openbas.helper.StreamHelper.fromIterable;
 import static io.openbas.service.InjectExpectationUtils.*;
+import static io.openbas.utils.ExpectationUtils.HUMAN_EXPECTATION;
 import static io.openbas.utils.ExpectationUtils.isAssetGroupExpectation;
 import static java.time.Instant.now;
 
@@ -68,7 +69,7 @@ public class InjectExpectationService {
             .findAny();
 
     String result;
-    if (List.of(MANUAL, CHALLENGE, ARTICLE).contains(injectExpectation.getType())) {
+    if (HUMAN_EXPECTATION.contains(injectExpectation.getType())) {
       result = input.getScore() >= injectExpectation.getExpectedScore() ? SUCCESS : FAILED;
       injectExpectation.getResults().clear();
       exists = Optional.empty();
@@ -110,7 +111,7 @@ public class InjectExpectationService {
       injectExpectation.setScore(input.getScore());
     } else {
       if (input.getScore() > injectExpectation.getScore()
-          || List.of(MANUAL, CHALLENGE, ARTICLE).contains(injectExpectation.getType())) {
+          || HUMAN_EXPECTATION.contains(injectExpectation.getType())) {
         injectExpectation.setScore(input.getScore());
       } else {
         injectExpectation.setScore(
@@ -136,8 +137,7 @@ public class InjectExpectationService {
     }
 
     // If the expectation is type manual, We should update expectations for teams and players
-    if (List.of(MANUAL, CHALLENGE, ARTICLE).contains(injectExpectation.getType())
-        && updated.getTeam() != null) {
+    if (HUMAN_EXPECTATION.contains(injectExpectation.getType()) && updated.getTeam() != null) {
       computeExpectationsForTeamsAndPlayer(updated, result);
     }
     return updated;
