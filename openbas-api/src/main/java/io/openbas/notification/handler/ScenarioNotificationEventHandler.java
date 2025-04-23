de6b6bc1115f0e16ab66f2690367880d7e31b775
Hedi <hedi.tarchouni@filigran.io>
2025-04-23 16:12:08 +0200

    [backend] NotifcationEvent logic Issue/2808  (#2808)
    Co-authored-by: Guillaume Paris <guillaume.paris@filigran.io>

---
+++ openbas-api/src/main/java/io/openbas/notification/handler/ScenarioNotificationEventHandler.java
@@ -0,0 +1,51 @@
+package io.openbas.notification.handler;
+
+import io.openbas.database.model.Exercise;
+import io.openbas.notification.model.NotificationEvent;
+import io.openbas.notification.model.NotificationEventType;
+import io.openbas.rest.exercise.form.ExercisesGlobalScoresInput;
+import io.openbas.rest.exercise.response.ExercisesGlobalScoresOutput;
+import io.openbas.rest.exercise.service.ExerciseService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ScenarioNotificationEventHandler implements NotificationEventHandler {
+
+  private final ExerciseService exerciseService;
+
+  @Override
+  public void handle(NotificationEvent event) {
+    if (NotificationEventType.SIMULATION_COMPLETED.equals(event.getEventType())) {
+      // get the last 2 simulations
+      Exercise lastSimulation =
+          exerciseService.previousFinishedSimulation(event.getResourceId(), event.getTimestamp());
+      if (lastSimulation == null || lastSimulation.getEnd().isEmpty()) {
+        return;
+      }
+      Exercise secondLastSimulation =
+          exerciseService.previousFinishedSimulation(
+              event.getResourceId(), lastSimulation.getEnd().get());
+      if (secondLastSimulation == null) {
+        return;
+      }
+
+      // calculate if there is a score degradation
+      ExercisesGlobalScoresInput exercisesGlobalScoresInput =
+          new ExercisesGlobalScoresInput(
+              List.of(lastSimulation.getId(), secondLastSimulation.getId()));
+      ExercisesGlobalScoresOutput exercisesGlobalScoresOutput =
+          exerciseService.getExercisesGlobalScores(exercisesGlobalScoresInput);
+      if (exerciseService.isThereAScoreDegradation(
+          exercisesGlobalScoresOutput.globalScoresByExerciseIds().get(lastSimulation.getId()),
+          exercisesGlobalScoresOutput
+              .globalScoresByExerciseIds()
+              .get(secondLastSimulation.getId()))) {
+
+        // TODO find and activate notification rule
+      }
+    }
+  }
+}
