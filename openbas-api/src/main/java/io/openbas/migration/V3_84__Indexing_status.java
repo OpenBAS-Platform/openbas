3be5be3bc6d154108707217032122b5476ea073c
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-04-23 13:09:54 +0200

    [backend] Mv migration

--- openbas-api/src/main/java/io/openbas/migration/V3_84__Indexing_status.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_84__Indexing_status.java
@@ -7,7 +7,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V3_83__Indexing_status extends BaseJavaMigration {
+public class V3_84__Indexing_status extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
--- openbas-api/src/main/java/io/openbas/migration/V3_85__Add_Custom_Dashboard.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_85__Add_Custom_Dashboard.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V3_84__Add_Custom_Dashboard extends BaseJavaMigration {
+public class V3_85__Add_Custom_Dashboard extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
