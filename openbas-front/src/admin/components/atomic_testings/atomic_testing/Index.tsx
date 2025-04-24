642921da9f9efe03895e7e756da72a49cf2a4e99
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-04-24 09:25:14 +0200

    [backend/frontend] Clean

--- openbas-front/src/admin/components/atomic_testings/atomic_testing/Index.tsx
+++ openbas-front/src/admin/components/atomic_testings/atomic_testing/Index.tsx
@@ -1,16 +1,20 @@
 import { Alert, AlertTitle } from '@mui/material';
 import { Suspense, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import { interval } from 'rxjs';
 
 import { fetchInjectResultOverviewOutput } from '../../../../actions/atomic_testings/atomic-testing-actions';
 import { useFormatter } from '../../../../components/i18n';
 import Loader from '../../../../components/Loader';
 import { type InjectResultOverviewOutput } from '../../../../utils/api-types';
+import { FIVE_SECONDS } from '../../../../utils/Time';
 import { TeamContext } from '../../common/Context';
 import { InjectResultOverviewOutputContext } from '../InjectResultOverviewOutputContext';
 import teamContextForAtomicTesting from './context/TeamContextForAtomicTesting';
 import IndexHeader from './IndexHeader';
 import IndexRoutes from './IndexRoutes';
+
+const interval$ = interval(FIVE_SECONDS);
 
 const Index = () => {
   const { t } = useFormatter();
@@ -22,15 +26,32 @@ const Index = () => {
 
   useEffect(() => {
     setLoading(true);
-    fetchInjectResultOverviewOutput(injectId)
-      .then((result: { data: InjectResultOverviewOutput }) => {
-        setInjectResultOverviewOutput(result.data);
-      })
-      .finally(() => {
+    fetchInjectResultOverviewOutput(injectId).then((result: { data: InjectResultOverviewOutput }) => {
+      setInjectResultOverviewOutput(result.data);
+    }).finally(() => {
+      setLoading(false);
+      setPristine(false);
+    });
+  }, [injectId]);
+
+  useEffect(() => {
+    const subscription = interval$.subscribe(() => {
+      setLoading(true);
+      fetchInjectResultOverviewOutput(injectId).then((result: { data: InjectResultOverviewOutput }) => {
+        if (result.data.inject_updated_at !== injectResultOverviewOutput?.inject_updated_at) {
+          setInjectResultOverviewOutput(result.data);
+        }
+      }).catch(() => {
+        subscription.unsubscribe();
+      }).finally(() => {
         setLoading(false);
         setPristine(false);
       });
-  }, [injectId]);
+    });
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [injectResultOverviewOutput]);
 
   const updateInjectResultOverviewOutput = () => {
     fetchInjectResultOverviewOutput(injectId).then((result: { data: InjectResultOverviewOutput }) => {
