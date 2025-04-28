166d576a60e502540d006f0b2a4278d45c9d0c81
Hedi Tarchouni <hedi.tarchouni@filigran.io>
2025-04-28 14:36:01 +0200

    removed usage of deprecated new URL()

--- openbas-api/src/main/java/io/openbas/utils/ImageUtils.java
+++ openbas-api/src/main/java/io/openbas/utils/ImageUtils.java
@@ -3,16 +3,19 @@ package io.openbas.utils;
 import jakarta.validation.constraints.NotBlank;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Base64;
 
 public class ImageUtils {
   public static String downloadImageAndEncodeBase64(final @NotBlank String imageUrl) {
-    try (InputStream inputStream = new URL(imageUrl).openStream()) {
+    try (InputStream inputStream = new URI(imageUrl).toURL().openStream()) {
       byte[] imageBytes = inputStream.readAllBytes();
       return Base64.getEncoder().encodeToString(imageBytes);
     } catch (IOException e) {
       throw new RuntimeException("error while downloading image from " + imageUrl, e);
+    } catch (URISyntaxException e) {
+      throw new RuntimeException(e);
     }
   }
 }
