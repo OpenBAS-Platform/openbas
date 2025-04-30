8b142dcba4b434c1348e15f9e36e9df79ebacf62
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-04-30 17:02:56 +0200

    [frontend] Add custom dashboard header

--- openbas-front/src/admin/components/workspaces/custom_dashboards/CustomDashboardHeader.tsx
+++ openbas-front/src/admin/components/workspaces/custom_dashboards/CustomDashboardHeader.tsx
@@ -1,5 +1,5 @@
 import { Tooltip, Typography } from '@mui/material';
-import { type FunctionComponent } from 'react';
+import { type FunctionComponent, useCallback, useState } from 'react';
 import { makeStyles } from 'tss-react/mui';
 
 import { type CustomDashboard } from '../../../../utils/api-types';
@@ -21,15 +21,30 @@ const CustomDashboardHeader: FunctionComponent<Props> = ({ customDashboard }) =>
   // Standard hooks
   const { classes } = useStyles();
 
+  const [currentCustomDashboard, setCustomDashboard] = useState(customDashboard);
+
+  const handleUpdate = useCallback(
+    (customDashboard: CustomDashboard) => {
+      setCustomDashboard({
+        ...currentCustomDashboard,
+        ...customDashboard,
+      });
+    },
+    [],
+  );
+
   return (
     <div className={classes.container}>
-      <Tooltip title={customDashboard.custom_dashboard_name}>
+      <Tooltip title={currentCustomDashboard.custom_dashboard_name}>
         <Typography variant="h1" style={{ margin: 0 }}>
-          {truncate(customDashboard.custom_dashboard_name, 80)}
+          {truncate(currentCustomDashboard.custom_dashboard_name, 80)}
         </Typography>
       </Tooltip>
       <div className={classes.rightAligned}>
-        <CustomDashboardPopover customDashboard={customDashboard} />
+        <CustomDashboardPopover
+          customDashboard={currentCustomDashboard}
+          onUpdate={handleUpdate}
+        />
       </div>
     </div>
   );
