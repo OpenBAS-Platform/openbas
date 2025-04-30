f54a27ae6ceecd408f2fdda195f59cc69e830a2b
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-04-30 18:00:30 +0200

    [frontend] Add composant FindingReferenceLink

--- openbas-front/src/components/common/FindingReferenceLink.tsx
+++ openbas-front/src/components/common/FindingReferenceLink.tsx
@@ -38,7 +38,7 @@ const FindingReferenceLink: FunctionComponent<Props> = ({ finding, type }) => {
       const injectId = finding.finding_inject?.inject_id;
       const simulationId = finding.finding_simulation?.exercise_id;
 
-      if (!title || !injectId) return <Typography color="textSecondary">-</Typography>;
+      if (!title || !injectId) return '-';
 
       const isAtomic = !simulationId;
       const url = isAtomic
@@ -52,7 +52,7 @@ const FindingReferenceLink: FunctionComponent<Props> = ({ finding, type }) => {
       const title = finding.finding_simulation?.exercise_name;
       const id = finding.finding_simulation?.exercise_id;
 
-      if (!title || !id) return <Typography color="textSecondary">-</Typography>;
+      if (!title || !id) return '-';
 
       return renderLink(title, `${SIMULATION_BASE_URL}/${id}`);
     }
@@ -61,7 +61,7 @@ const FindingReferenceLink: FunctionComponent<Props> = ({ finding, type }) => {
       const title = finding.finding_scenario?.scenario_name;
       const id = finding.finding_scenario?.scenario_id;
 
-      if (!title || !id) return <Typography color="textSecondary">-</Typography>;
+      if (!title || !id) return '-';
 
       return renderLink(title, `${SCENARIO_BASE_URL}/${id}`);
     }
