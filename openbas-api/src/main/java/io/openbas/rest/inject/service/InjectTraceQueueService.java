024dce4155b010e076c93d41dbfe3fde1f704071
Gael Leblan <gael.leblan@filigran.io>
2025-05-13 21:02:05 +0200

    [backend] Batching execution traces

--- openbas-api/src/main/java/io/openbas/rest/inject/service/InjectTraceQueueService.java
+++ openbas-api/src/main/java/io/openbas/rest/inject/service/InjectTraceQueueService.java
@@ -2,49 +2,51 @@ package io.openbas.rest.inject.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.*;
+import io.openbas.config.QueueConfig;
 import io.openbas.config.RabbitmqConfig;
-import io.openbas.rest.inject.form.InjectExecutionCallback;
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.Resource;
+import io.openbas.rest.inject.service.queue.QueueExecution;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.logging.Level;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
-import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
-@Log
-@Service
-@RequiredArgsConstructor
-public class InjectTraceQueueService {
+@Slf4j
+public class InjectTraceQueueService<T> {
 
-  private final InjectStatusService injectStatusService;
+  private final Class<T> clazz;
+  private final QueueExecution<T> queueExecution;
 
-  public static final String ROUTING_KEY = "_push_routing_exchange";
-  public static final String EXCHANGE_KEY = "_amqp.traces.exchange";
-  public static final String QUEUE_NAME = "_execution_traces_queue";
+  public static final String ROUTING_KEY = "_push_routing_%s";
+  public static final String EXCHANGE_KEY = "_amqp.%s.exchange";
+  public static final String QUEUE_NAME = "_execution_%s";
 
-  @Resource protected ObjectMapper mapper;
+  protected ObjectMapper mapper;
 
-  @Resource private RabbitmqConfig rabbitmqConfig;
-  private Connection connection;
-  private Channel channel;
+  private final Connection connection;
+  private final Channel publisherChannel;
 
-  private String routingKey;
-  private String exchangeName;
-  private String queueName;
+  private final String routingKey;
+  private final String exchangeName;
+  private final String queueName;
 
-  private BlockingQueue<InjectExecutionCallback> queue;
-  private final Object batchLock = new Object();
-  private ScheduledExecutorService scheduledExecutor;
-  private final AtomicBoolean processingBatch = new AtomicBoolean(false);
+  private final BlockingQueue<T> queue;
 
-  @PostConstruct
-  public void init() throws Exception {
+  private final QueueConfig queueConfig;
+
+  public InjectTraceQueueService(
+      Class<T> clazz,
+      QueueExecution<T> queueExecution,
+      RabbitmqConfig rabbitmqConfig,
+      ObjectMapper mapper,
+      QueueConfig queueConfig)
+      throws IOException, TimeoutException {
+    this.clazz = clazz;
+    this.queueExecution = queueExecution;
+    this.mapper = mapper;
+    this.queueConfig = queueConfig;
+
     ConnectionFactory factory = new ConnectionFactory();
     factory.setHost(rabbitmqConfig.getHostname());
     factory.setPort(rabbitmqConfig.getPort());
@@ -57,97 +59,96 @@ public class InjectTraceQueueService {
     factory.setRequestedHeartbeat(30);
 
     connection = factory.newConnection();
-    channel = connection.createChannel();
-    channel.basicQos(30); // Per consumer limit
-    exchangeName = rabbitmqConfig.getPrefix() + InjectTraceQueueService.EXCHANGE_KEY;
-    routingKey = rabbitmqConfig.getPrefix() + InjectTraceQueueService.ROUTING_KEY;
-    queueName = rabbitmqConfig.getPrefix() + InjectTraceQueueService.QUEUE_NAME;
-    channel.exchangeDeclare(exchangeName, "topic", true);
-    channel.queueDeclare(queueName, true, false, false, null);
-    channel.queueBind(queueName, exchangeName, routingKey);
+    publisherChannel = connection.createChannel();
+    publisherChannel.basicQos(queueConfig.getPublisherQos()); // Per publisher limit
+    exchangeName =
+        rabbitmqConfig.getPrefix()
+            + String.format(InjectTraceQueueService.EXCHANGE_KEY, queueConfig.getQueueName());
+    routingKey =
+        rabbitmqConfig.getPrefix()
+            + String.format(InjectTraceQueueService.ROUTING_KEY, queueConfig.getQueueName());
+    queueName =
+        rabbitmqConfig.getPrefix()
+            + String.format(InjectTraceQueueService.QUEUE_NAME, queueConfig.getQueueName());
+    publisherChannel.exchangeDeclare(exchangeName, "topic", true);
+    publisherChannel.queueDeclare(queueName, true, false, false, null);
+    publisherChannel.queueBind(queueName, exchangeName, routingKey);
 
-    setupConsumer();
+    createConsumer();
 
-    setupScheduledBatchProcessing();
-  }
-
-  private void setupConsumer() throws IOException {
-    Channel consumerChannel = connection.createChannel();
-    // Limiter le nombre de messages non acquittés
-    consumerChannel.basicQos(30);
-
-    DeliverCallback deliverCallback =
-        (consumerTag, delivery) -> {
-          try {
-            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-            log.info(" [x] Received '" + message + "'");
-
-            // Acquitter le message immédiatement
-            consumerChannel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
-
-            InjectExecutionCallback injectExecutionCallback =
-                mapper.readValue(message, InjectExecutionCallback.class);
-            synchronized (batchLock) {
-              queue.put(injectExecutionCallback);
-            }
-
-            if (queue.size() > 200) {
-              processBufferedBatch();
-            }
-          } catch (Exception e) {
-            log.log(Level.SEVERE, "Error processing message: {}", e.getMessage());
-            // Rejeter le message et le renvoyer à la file
-            consumerChannel.basicNack(delivery.getEnvelope().getDeliveryTag(), false, true);
-          }
-        };
-
-    CancelCallback cancelCallback =
-        consumerTag -> log.log(Level.WARNING, "Consumer {} was cancelled", consumerTag);
-
-    consumerChannel.basicConsume(
-        queueName, false, "consumer", false, false, null, deliverCallback, cancelCallback);
-  }
-
-  private void setupScheduledBatchProcessing() {
     queue = new LinkedBlockingQueue<>();
 
-    scheduledExecutor = Executors.newScheduledThreadPool(1);
+    ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(1);
     scheduledExecutor.scheduleAtFixedRate(
-        this::processBufferedBatch, 10000, 10000, TimeUnit.MILLISECONDS);
+        this::processBufferedBatch,
+        this.queueConfig.getConsumerFrequency(),
+        this.queueConfig.getConsumerFrequency(),
+        TimeUnit.MILLISECONDS);
+  }
+
+  private void createConsumer() throws IOException {
+    for (int i = 0; i < queueConfig.getConsumerNumber(); ++i) {
+      Channel consumerChannel = connection.createChannel();
+      // Limiter le nombre de messages non acquittés
+      consumerChannel.basicQos(queueConfig.getConsumerQos());
+
+      DeliverCallback deliverCallback =
+          (consumerTag, delivery) -> {
+            try {
+              String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+              log.info(" [x] Received '" + message + "'");
+
+              // Acquitter le message immédiatement
+              consumerChannel.basicAck(delivery.getEnvelope().getDeliveryTag(), true);
+
+              T element = mapper.readValue(message, clazz);
+              queue.put(element);
+
+              if (queue.size() > this.queueConfig.getMaxSize()) {
+                processBufferedBatch();
+              }
+            } catch (Exception e) {
+              log.error("Error processing message: {}", e.getMessage());
+              // Rejeter le message et le renvoyer à la file
+              consumerChannel.basicNack(delivery.getEnvelope().getDeliveryTag(), false, true);
+            }
+          };
+
+      CancelCallback cancelCallback =
+          consumerTag -> log.warn("Consumer {} was cancelled", consumerTag);
+
+      consumerChannel.basicConsume(
+          queueName,
+          false,
+          String.format("consumer-%s-%d", queueConfig.getQueueName(), i),
+          false,
+          false,
+          null,
+          deliverCallback,
+          cancelCallback);
+    }
   }
 
   /** Traite les messages en attente dans le tampon */
   private void processBufferedBatch() {
-    // Éviter les traitements simultanés
-    if (!processingBatch.compareAndSet(false, true)) {
-      return;
-    }
-
     try {
-      List<InjectExecutionCallback> currentBatch;
-
-      // Récupérer et vider le tampon
-      synchronized (batchLock) {
-        if (queue.isEmpty()) {
-          return;
-        }
-
-        currentBatch = new ArrayList<>(queue);
-        queue.clear();
-      }
+      List<T> currentBatch = new ArrayList<>();
+      queue.drainTo(currentBatch, queueConfig.getMaxSize());
 
       if (!currentBatch.isEmpty()) {
-        log.log(Level.INFO, "Processing batch of {} callbacks", currentBatch.size());
-        injectStatusService.handleInjectExecutionCallbackList(currentBatch);
+        log.info("Processing batch of {} callbacks", currentBatch.size());
+        queueExecution.perform(currentBatch);
+      }
+
+      if (queue.size() > this.queueConfig.getMaxSize()) {
+        processBufferedBatch();
       }
     } catch (Exception e) {
-      log.log(Level.SEVERE, "Error processing batch: {}", e.getMessage());
-    } finally {
-      processingBatch.set(false);
+      log.error("Error processing batch: {}", e.getMessage());
     }
   }
 
-  public void publish(String publishedJson) throws IOException, TimeoutException {
-    channel.basicPublish(exchangeName, routingKey, null, publishedJson.getBytes());
+  public void publish(String publishedJson) throws IOException {
+    publisherChannel.basicPublish(exchangeName, routingKey, null, publishedJson.getBytes());
   }
 }
---
+++ openbas-framework/src/main/java/io/openbas/config/QueueConfig.java
@@ -0,0 +1,25 @@
+package io.openbas.config;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class QueueConfig {
+  @JsonProperty("consumer.number")
+  private int consumerNumber;
+
+  @JsonProperty("consumer.frequency")
+  private int consumerFrequency;
+
+  @JsonProperty("queue-name")
+  private String queueName;
+
+  @JsonProperty("max-size")
+  private int maxSize;
+
+  @JsonProperty("consumer-qos")
+  private int consumerQos;
+
+  @JsonProperty("publisher-qos")
+  private int publisherQos;
+}
