c44cde25a972c68d59bd2bfca8780f7858871fdb
Gael Leblan <gael.leblan@filigran.io>
2025-05-20 14:26:57 +0200

    [backend] Batching inject execution traces

--- openbas-api/src/main/java/io/openbas/rest/inject/form/InjectExecutionCallback.java
+++ openbas-api/src/main/java/io/openbas/rest/inject/form/InjectExecutionCallback.java
@@ -1,15 +1,11 @@
 package io.openbas.rest.inject.form;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class InjectExecutionCallback {
   @JsonProperty("agent_id")
   private String agentId;
