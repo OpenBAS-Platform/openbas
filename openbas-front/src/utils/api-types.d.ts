03ade55ba46e781568e41239e0aacf60a0bfbfa7
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-05-21 11:19:45 +0200

    [backend] Wip

--- openbas-front/src/utils/api-types.d.ts
+++ openbas-front/src/utils/api-types.d.ts
@@ -1314,6 +1314,7 @@ export interface ExecutionTrace {
     | "ASSET_AGENTLESS"
     | "AGENT_INACTIVE"
     | "INFO";
+  execution_structured_message?: object;
   /** @format date-time */
   execution_time?: string;
   execution_trace_id: string;
