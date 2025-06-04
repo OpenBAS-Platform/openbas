c196e8d20ded2a20fcce8a6d71849976acb92bdc
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-06-04 16:27:35 +0200

    [backend/frontend] Add endpoint for simulation/scenario testing

--- openbas-api/src/test/java/io/openbas/rest/scenario/ScenarioInjectTestApiTest.java
+++ openbas-api/src/test/java/io/openbas/rest/scenario/ScenarioInjectTestApiTest.java
@@ -1,14 +1,25 @@
 package io.openbas.rest.scenario;
 
+import static io.openbas.utils.JsonUtils.asJsonString;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import io.openbas.IntegrationTest;
-import io.openbas.database.model.*;
-import io.openbas.utils.fixtures.*;
-import io.openbas.utils.fixtures.composers.*;
+import io.openbas.database.model.Inject;
+import io.openbas.database.model.InjectTestStatus;
+import io.openbas.database.model.Scenario;
+import io.openbas.rest.inject.form.InjectBulkProcessingInput;
+import io.openbas.utils.fixtures.InjectFixture;
+import io.openbas.utils.fixtures.InjectTestStatusFixture;
+import io.openbas.utils.fixtures.ScenarioFixture;
+import io.openbas.utils.fixtures.composers.InjectComposer;
+import io.openbas.utils.fixtures.composers.InjectTestStatusComposer;
+import io.openbas.utils.fixtures.composers.ScenarioComposer;
+import io.openbas.utils.pagination.SearchPaginationInput;
+import java.util.List;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -20,68 +31,74 @@ import org.springframework.test.web.servlet.MockMvc;
 public class ScenarioInjectTestApiTest extends IntegrationTest {
 
   @Autowired private MockMvc mvc;
-  @Autowired private ExerciseComposer exerciseComposer;
+  @Autowired private ScenarioComposer scenarioComposer;
   @Autowired private InjectComposer injectComposer;
   @Autowired private InjectTestStatusComposer injectTestStatusComposer;
 
-  private String exerciseId;
+  private Scenario scenario;
   private Inject inject1, inject2;
-  private InjectStatus testStatus1, testStatus2;
+  private InjectTestStatus injectTestStatus1, injectTestStatus2;
 
   @BeforeAll
   void setupData() {
-    exerciseId =
-        exerciseComposer.forExercise(ExerciseFixture.getExercise()).persist().get().getId();
+    InjectTestStatusComposer.Composer injectTestStatus =
+        injectTestStatusComposer.forInjectTestStatus(
+            InjectTestStatusFixture.createSuccessInjectStatus());
 
-    inject1 = injectComposer.forInject(InjectFixture.getDefaultInject()).persist().get();
+    injectTestStatus1 = injectTestStatus.persist().get();
+    injectTestStatus2 = injectTestStatus.persist().get();
 
-    inject2 = injectComposer.forInject(InjectFixture.getDefaultInject()).persist().get();
+    InjectComposer.Composer injectComposer1 =
+        injectComposer
+            .forInject(InjectFixture.getDefaultInject())
+            .withInjectTestStatus(injectTestStatus);
+    InjectComposer.Composer injectComposer2 =
+        injectComposer
+            .forInject(InjectFixture.getDefaultInject())
+            .withInjectTestStatus(injectTestStatus);
 
-    //    testStatus1 =
-    //        injectTestStatusComposer
-    //            .forInjectTestStatus(InjectStatusFixture.createPendingInjectStatus())
-    //            .withInject(inject1)
-    //            .persist()
-    //            .get();
+    inject1 = injectComposer1.persist().get();
+    inject2 = injectComposer2.persist().get();
 
-    //    testStatus2 =
-    //        injectTestStatusComposer
-    //            .forInjectTestStatus(InjectStatusFixture.createPendingInjectStatus())
-    //            .withInject(inject2)
-    //            .persist()
-    //            .get();
+    scenario =
+        scenarioComposer
+            .forScenario(ScenarioFixture.getScenario())
+            .withInjects(List.of(injectComposer1, injectComposer2))
+            .persist()
+            .get();
   }
 
   @Nested
-  @WithMockUser(roles = "EXERCISE_PLANNER")
-  @DisplayName("As ExercisePlanner")
-  class ExercisePlannerAccess {
+  @WithMockUser(roles = "SCENARIO_PLANNER")
+  @DisplayName("As ScenarioPlanner")
+  class ScenarioPlannerAccess {
 
     @Test
     @DisplayName("Should return paginated inject test results when inject tests exist")
     void should_return_paginated_results_when_inject_tests_exist() throws Exception {
-      String requestJson =
-          """
-                {
-                  "page": 0,
-                  "size": 10
-                }
-            """;
-
+      SearchPaginationInput searchPaginationInput = new SearchPaginationInput();
       String response =
           mvc.perform(
-                  post("/api/exercises/{exerciseId}/injects/test", exerciseId)
+                  post("/api/scenarios/{scenarioId}/injects/test/search", scenario.getId())
                       .contentType(MediaType.APPLICATION_JSON)
-                      .content(requestJson))
+                      .content(asJsonString(searchPaginationInput)))
               .andExpect(status().isOk())
               .andReturn()
               .getResponse()
               .getContentAsString();
 
-      //      assertThatJson(response)
-      //          .inPath("$.content[*].id")
-      //          .asArray()
-      //          .contains(testStatus1.getId(), testStatus2.getId());
+      assertThatJson(response)
+          .inPath("$.content[*].id")
+          .isArray()
+          .contains(injectTestStatus1.getId(), injectTestStatus2.getId());
+    }
+
+    @Test
+    @DisplayName("Should return test status using test id")
+    void should_return_test_status_by_testId() throws Exception {
+      mvc.perform(get("/api/scenarios/injects/test/{testId}", scenario.getId(), inject1.getId()))
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.inject_id").value(inject1.getId()));
     }
 
     @Test
@@ -89,37 +106,24 @@ public class ScenarioInjectTestApiTest extends IntegrationTest {
     void should_return_test_status_when_testing_specific_inject() throws Exception {
       mvc.perform(
               get(
-                  "/api/exercises/{exerciseId}/injects/{injectId}/test",
-                  exerciseId,
+                  "/api/scenarios/{scenarioId}/injects/{injectId}/test",
+                  scenario.getId(),
                   inject1.getId()))
           .andExpect(status().isOk())
           .andExpect(jsonPath("$.inject_id").value(inject1.getId()));
     }
 
     @Test
-    @DisplayName("Should return inject test status when fetching by ID")
-    void should_return_inject_test_status_when_fetching_by_id() throws Exception {
-      mvc.perform(get("/api/exercises/injects/test/{testId}", testStatus1.getId()))
-          .andExpect(status().isOk())
-          .andExpect(jsonPath("$.id").value(testStatus1.getId()));
-    }
-
-    @Test
     @DisplayName("Should return test statuses when performing bulk test with inject IDs")
     void should_return_test_statuses_when_bulk_testing_with_inject_ids() throws Exception {
-      String requestJson =
-          String.format(
-              """
-                {
-                  "inject_ids_to_process": ["%s"]
-                }
-            """,
-              inject1.getId());
+      InjectBulkProcessingInput input = new InjectBulkProcessingInput();
+      input.setInjectIDsToProcess(List.of(inject1.getId(), inject2.getId()));
+      input.setSimulationOrScenarioId(scenario.getId());
 
       mvc.perform(
-              post("/api/exercises/{exerciseId}/injects/test", exerciseId)
+              post("/api/scenarios/{scenarioId}/injects/{injectId}/test", scenario.getId())
                   .contentType(MediaType.APPLICATION_JSON)
-                  .content(requestJson))
+                  .content(asJsonString(input)))
           .andExpect(status().isOk())
           .andExpect(jsonPath("$").isArray());
     }
@@ -129,12 +133,12 @@ public class ScenarioInjectTestApiTest extends IntegrationTest {
     void should_return_404_when_fetching_deleted_inject_test_status() throws Exception {
       mvc.perform(
               delete(
-                  "/api/exercises/{exerciseId}/injects/test/{testId}",
-                  exerciseId,
-                  testStatus2.getId()))
+                  "/api/scenarios/{scenarioId}/injects/test/{testId}",
+                  scenario.getId(),
+                  injectTestStatus2.getId()))
           .andExpect(status().isOk());
 
-      mvc.perform(get("/api/exercises/injects/test/{testId}", testStatus2.getId()))
+      mvc.perform(get("/api/exercises/injects/test/{testId}", injectTestStatus2.getId()))
           .andExpect(status().isNotFound());
     }
   }
@@ -145,43 +149,56 @@ public class ScenarioInjectTestApiTest extends IntegrationTest {
   class UnauthorizedUserAccess {
 
     @Test
-    @DisplayName("Should return 403 when testing inject without planner role")
-    void should_return_403_when_testing_inject_without_planner_role() throws Exception {
+    @DisplayName("Should return 200 when search a paginated inject test results")
+    void should_return_200_when_search_paginated_results() throws Exception {
+      SearchPaginationInput searchPaginationInput = new SearchPaginationInput();
+      mvc.perform(
+              post("/api/scenarios/{scenarioId}/injects/test/search", scenario.getId())
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .content(asJsonString(searchPaginationInput)))
+          .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Should return 200 when search by id")
+    void should_return_200_when_search_by_testId() throws Exception {
+      mvc.perform(get("/api/scenarios/injects/test/{testId}", scenario.getId(), inject1.getId()))
+          .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Should return 403 when testing a specific inject")
+    void should_return_403_when_testing_specific_inject() throws Exception {
       mvc.perform(
               get(
-                  "/api/exercises/{exerciseId}/injects/{injectId}/test",
-                  exerciseId,
+                  "/api/scenarios/{scenarioId}/injects/{injectId}/test",
+                  scenario.getId(),
                   inject1.getId()))
           .andExpect(status().isForbidden());
     }
 
     @Test
-    @DisplayName("Should return 403 when deleting inject test without planner role")
-    void should_return_403_when_deleting_inject_test_without_planner_role() throws Exception {
+    @DisplayName("Should return 403 when performing bulk test with inject IDs")
+    void should_return_403_when_bulk_testing_with_inject_ids() throws Exception {
+      InjectBulkProcessingInput input = new InjectBulkProcessingInput();
+      input.setInjectIDsToProcess(List.of(inject1.getId(), inject2.getId()));
+      input.setSimulationOrScenarioId(scenario.getId());
+
       mvc.perform(
-              delete(
-                  "/api/exercises/{exerciseId}/injects/test/{testId}",
-                  exerciseId,
-                  testStatus1.getId()))
+              post("/api/scenarios/{scenarioId}/injects/{injectId}/test", scenario.getId())
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .content(asJsonString(input)))
           .andExpect(status().isForbidden());
     }
 
     @Test
-    @DisplayName("Should return 403 when bulk testing without planner role")
-    void should_return_403_when_bulk_testing_without_planner_role() throws Exception {
-      String json =
-          String.format(
-              """
-                {
-                  "inject_ids_to_process": ["%s"]
-                }
-            """,
-              inject1.getId());
-
+    @DisplayName("Should return 403 when fetching a deleted inject test status")
+    void should_return_403_when_fetching_deleted_inject_test_status() throws Exception {
       mvc.perform(
-              post("/api/exercises/{exerciseId}/injects/test", exerciseId)
-                  .contentType(MediaType.APPLICATION_JSON)
-                  .content(json))
+              delete(
+                  "/api/scenarios/{scenarioId}/injects/test/{testId}",
+                  scenario.getId(),
+                  injectTestStatus2.getId()))
           .andExpect(status().isForbidden());
     }
   }
