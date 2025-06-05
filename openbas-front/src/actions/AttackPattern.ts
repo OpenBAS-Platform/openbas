dbff87b6f37fa8e1c3fa33e3ac2249ed4b869c6d
Marine LM <marine.lemezo@filigran.io>
2025-06-05 14:37:53 +0200

    [frontend/backend] add ai assistant
    Signed-off-by: Marine LM <marine.lemezo@filigran.io>

--- openbas-front/src/actions/AttackPattern.ts
+++ openbas-front/src/actions/AttackPattern.ts
@@ -1,7 +1,12 @@
 import { type Dispatch } from 'redux';
 
 import { delReferential, getReferential, postReferential, putReferential, simpleCall, simplePostCall } from '../utils/Action';
-import { type AttackPattern, type AttackPatternCreateInput, type AttackPatternUpdateInput, type SearchPaginationInput } from '../utils/api-types';
+import {
+  type AttackPattern,
+  type AttackPatternCreateInput,
+  type AttackPatternUpdateInput,
+  type SearchPaginationInput,
+} from '../utils/api-types';
 import * as schema from './Schema';
 
 const ATTACK_PATTERN_URI = '/api/attack_patterns';
@@ -14,6 +19,15 @@ export const searchAttackPatterns = (paginationInput: SearchPaginationInput) => 
   const data = paginationInput;
   const uri = `${ATTACK_PATTERN_URI}/search`;
   return simplePostCall(uri, data);
+};
+
+export const searchAttackPatternsWithAIWebservice = (files: File[], text: string) => {
+  const formData = new FormData();
+  files.forEach((file) => {
+    formData.append('files', file);
+  });
+  formData.append('text', new Blob([JSON.stringify({ text })], { type: 'application/json' }));
+  return simplePostCall(`${ATTACK_PATTERN_URI}/search-with-ai`, formData);
 };
 
 export const updateAttackPattern = (attackPatternId: AttackPattern['attack_pattern_id'], data: AttackPatternUpdateInput) => (dispatch: Dispatch) => {
---
+++ openbas-front/src/admin/components/scenarios/scenario/scenario_assistant/ScenarioAssistantDrawer.tsx
@@ -0,0 +1,269 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { AutoAwesomeOutlined } from '@mui/icons-material';
+import { Button, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { FormProvider, useForm, useWatch } from 'react-hook-form';
+import { makeStyles } from 'tss-react/mui';
+import { z } from 'zod';
+
+import { findEndpoints } from '../../../../../actions/assets/endpoint-actions';
+import type { LoggedHelper } from '../../../../../actions/helper';
+import Drawer from '../../../../../components/common/Drawer';
+import AttackPatternFieldController from '../../../../../components/fields/AttackPatternFieldController';
+import TextFieldController from '../../../../../components/fields/TextFieldController';
+import { useFormatter } from '../../../../../components/i18n';
+import { useHelper } from '../../../../../store';
+import {
+  type EndpointOutput,
+  type InjectAssistantInput,
+} from '../../../../../utils/api-types';
+import AssetGroupPopover from '../../../assets/asset_groups/AssetGroupPopover';
+import AssetGroupsList from '../../../assets/asset_groups/AssetGroupsList';
+import EndpointPopover from '../../../assets/endpoints/EndpointPopover';
+import EndpointsList from '../../../assets/endpoints/EndpointsList';
+import EEChip from '../../../common/entreprise_edition/EEChip';
+import InjectAddAssetGroups from '../../../simulations/simulation/injects/asset_groups/InjectAddAssetGroups';
+import InjectAddEndpoints from '../../../simulations/simulation/injects/endpoints/InjectAddEndpoints';
+import AttackPatternAIAssistantModal from './AttackPatternAIAssistantModal';
+import SelectTTPsDrawer from './SelectTTPsDrawer';
+
+const useStyles = makeStyles()(theme => ({
+  formContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: theme.spacing(2),
+  },
+  titleWithButtonContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    marginTop: theme.spacing(1),
+    gap: theme.spacing(1),
+  },
+  useArianeButton: {
+    marginLeft: 'auto',
+    color: theme.palette.ai.main,
+  },
+  selectTTPsButton: { borderColor: theme.palette.divider },
+}));
+
+interface Props {
+  open: boolean;
+  onClose: () => void;
+  onSubmit: (input: InjectAssistantInput) => void;
+}
+
+const ScenarioAssistantDrawer = ({ open, onClose, onSubmit }: Props) => {
+  const { t } = useFormatter();
+  const { classes } = useStyles();
+  const [openMitreFilterDrawer, setOpenMitreFilterDrawer] = useState(false);
+  const [openArianeAIAssistantModal, setOpenArianeAIAssistantModal] = useState(false);
+  const { settings } = useHelper((helper: LoggedHelper) => ({ settings: helper.getPlatformSettings() }));
+
+  const schema = z.object({
+    asset_group_ids: z.string().array().optional(),
+    asset_ids: z.string().array().optional(),
+    attack_pattern_ids: z.array(z.string()).min(1, { message: t('Should not be empty') }),
+    inject_by_ttp_number: z.coerce.number().min(0).max(5),
+  }).superRefine((data, ctx) => {
+    const hasAssetGroupIds = data.asset_group_ids && data.asset_group_ids.length > 0;
+    const hasAssetIds = data.asset_ids && data.asset_ids.length > 0;
+    if (!hasAssetGroupIds && !hasAssetIds) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: t('Should have at least one asset or one asset group'),
+        path: ['asset_group_ids'],
+      });
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: t('Should have at least one asset or one asset group'),
+        path: ['asset_ids'],
+      });
+    }
+  });
+
+  const methods = useForm<InjectAssistantInput>({
+    mode: 'all',
+    resolver: zodResolver(schema),
+    defaultValues: {
+      asset_group_ids: [],
+      asset_ids: [],
+      attack_pattern_ids: [],
+      inject_by_ttp_number: 1,
+    },
+  });
+
+  const {
+    control,
+    handleSubmit,
+    setValue,
+    reset,
+    getValues,
+    formState: { errors, isDirty, isSubmitting },
+  } = methods;
+
+  const onCloseMitreDrawer = () => {
+    setOpenMitreFilterDrawer(false);
+  };
+  const onCloseArianneAIAssistantModal = () => {
+    setOpenArianeAIAssistantModal(false);
+  };
+
+  const onCloseDrawer = () => {
+    reset();
+    onClose();
+  };
+
+  // -- Assets
+  const assetIds = useWatch({
+    control,
+    name: 'asset_ids',
+  }) as string[];
+  const [endpoints, setEndpoints] = useState<EndpointOutput[]>([]);
+  useEffect(() => {
+    if (assetIds.length > 0) {
+      findEndpoints(assetIds).then(result => setEndpoints(result.data));
+    } else {
+      setEndpoints([]);
+    }
+  }, [assetIds]);
+  const onAssetChange = (assetIds: string[]) => setValue('asset_ids', assetIds);
+  const removeAsset = (assetId: string) => setValue('asset_ids', assetIds.filter(id => id !== assetId));
+
+  // -- Asset groups
+  const assetGroupIds = useWatch({
+    control,
+    name: 'asset_group_ids',
+  }) as string[];
+  const onAssetGroupChange = (assetGroupIds: string[]) => setValue('asset_group_ids', assetGroupIds);
+  const removeAssetGroup = (assetGroupId: string) => setValue('asset_group_ids', assetGroupIds.filter(id => id !== assetGroupId));
+
+  // -- Attack patterns
+  const onUpdateAttackPattern = (attackPatternIds: string[]) => {
+    setValue('attack_pattern_ids', attackPatternIds, { shouldDirty: true });
+  };
+  const onUpdateAttackPatternWidget = (selectedAttackPatternIds: Set<string>) => {
+    onUpdateAttackPattern(Array.from(selectedAttackPatternIds));
+    onCloseMitreDrawer();
+  };
+  const onUpdateAttackPatternAIModal = (attackPatternIds: string[]) => {
+    onUpdateAttackPattern(attackPatternIds);
+    onCloseArianneAIAssistantModal();
+  };
+
+  return (
+    <Drawer
+      open={open}
+      handleClose={onCloseDrawer}
+      title={t('Scenario assistant')}
+    >
+      <>
+        <FormProvider {...methods}>
+          <form
+            noValidate // disabled tooltip
+            onSubmit={handleSubmit(onSubmit)}
+            className={classes.formContainer}
+          >
+            <Typography variant="h5">
+              {t('Target')}
+              *
+            </Typography>
+            <div>
+              <Typography variant="h3">{t('Assets')}</Typography>
+              <EndpointsList
+                endpoints={endpoints}
+                renderActions={endpoint => (
+                  <EndpointPopover
+                    inline
+                    endpoint={endpoint}
+                    removeFromContextLabel="Remove"
+                    onRemoveFromContext={removeAsset}
+                  />
+                )}
+              />
+              <InjectAddEndpoints
+                endpointIds={assetIds}
+                onSubmit={onAssetChange}
+                errorLabel={errors?.asset_ids?.message ?? null}
+              />
+            </div>
+
+            <div>
+              <Typography variant="h3">{t('Asset groups')}</Typography>
+              <AssetGroupsList
+                assetGroupIds={assetGroupIds}
+                renderActions={assetGroup => (
+                  <AssetGroupPopover
+                    assetGroup={assetGroup}
+                    inline
+                    onRemoveAssetGroupFromList={removeAssetGroup}
+                    removeAssetGroupFromListMessage="Remove"
+                    actions={['remove']}
+                  />
+                )}
+              />
+              <InjectAddAssetGroups
+                assetGroupIds={assetGroupIds}
+                onSubmit={onAssetGroupChange}
+                errorLabel={errors?.asset_group_ids?.message ?? null}
+              />
+            </div>
+
+            <div className={classes.titleWithButtonContainer}>
+              <Typography variant="h5">{t('Scenario coverage')}</Typography>
+              <Button
+                variant="outlined"
+                color="inherit"
+                size="small"
+                className={classes.useArianeButton}
+                onClick={() => setOpenArianeAIAssistantModal(true)}
+                startIcon={<AutoAwesomeOutlined fontSize="small" />}
+                endIcon={!settings.platform_license?.license_is_validated ? <span><EEChip /></span> : <></>}
+                disabled={!settings.platform_license?.license_is_validated}
+              >
+                {t('Use Ariane')}
+              </Button>
+              <Button
+                variant="outlined"
+                color="inherit"
+                size="small"
+                className={classes.selectTTPsButton}
+                onClick={() => setOpenMitreFilterDrawer(true)}
+              >
+                {t('Select TTPs')}
+              </Button>
+            </div>
+            <AttackPatternFieldController
+              hideAddButton
+              name="attack_pattern_ids"
+              label={t('Cover the following TTPs')}
+              required
+            />
+            <TextFieldController type="number" label={t('Number of injects by TTP')} name="inject_by_ttp_number" required />
+            <Button
+              variant="contained"
+              color="secondary"
+              type="submit"
+              style={{ marginLeft: 'auto' }}
+              disabled={isSubmitting || !isDirty}
+            >
+              {t('Create Injects')}
+            </Button>
+          </form>
+        </FormProvider>
+        <SelectTTPsDrawer
+          open={openMitreFilterDrawer}
+          onClose={onCloseMitreDrawer}
+          initialSelectedTTPIds={getValues('attack_pattern_ids')}
+          onUpdateAttackPattern={onUpdateAttackPatternWidget}
+        />
+        <AttackPatternAIAssistantModal
+          open={openArianeAIAssistantModal}
+          onClose={onCloseArianneAIAssistantModal}
+          onAttackPatternIdsFind={onUpdateAttackPatternAIModal}
+        />
+      </>
+    </Drawer>
+  );
+};
+export default ScenarioAssistantDrawer;
