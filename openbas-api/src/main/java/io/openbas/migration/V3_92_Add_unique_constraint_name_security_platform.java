6661ade994a7e2987694146f903744c5823a40c4
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-06-09 09:43:09 +0200

    [backend] Deduplication of security platforms

---
+++ openbas-api/src/main/java/io/openbas/migration/V3_92_Add_unique_constraint_name_security_platform.java
@@ -0,0 +1,19 @@
+package io.openbas.migration;
+
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+@Component
+public class V3_92_Add_unique_constraint_name_security_platform extends BaseJavaMigration {
+
+  @Override
+  public void migrate(Context context) throws Exception {
+    Connection connection = context.getConnection();
+    Statement select = connection.createStatement();
+    select.execute("ALTER TABLE execution_traces ADD COLUMN execution_structured_output TEXT;");
+  }
+}
