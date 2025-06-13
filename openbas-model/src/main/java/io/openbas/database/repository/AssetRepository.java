b337112a9c198ecc6d2727a4aa44335584b4edf9
Antoine MAZEAS <antoine.mazeas@filigran.io>
2025-06-13 16:36:34 +0200

    fix dead code
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- openbas-model/src/main/java/io/openbas/database/repository/AssetRepository.java
+++ openbas-model/src/main/java/io/openbas/database/repository/AssetRepository.java
@@ -18,32 +18,6 @@ public interface AssetRepository
   @Query("select a from Asset a where a.type IN :types")
   List<Asset> findByType(@Param("types") final List<String> types);
 
-  /**
-   * Returns the raw assets having the ids passed in parameter
-   *
-   * @param ids the ids
-   * @return the list of raw assets
-   */
-  @Query(
-      value =
-          "SELECT asset_id, asset_name, asset_type, endpoint_platform, "
-              + "asset_created_at, asset_updated_at "
-              + "FROM assets "
-              + "WHERE asset_id IN :ids ;",
-      nativeQuery = true)
-  List<RawAsset> rawByIds(@Param("ids") List<String> ids);
-
-  @Query(
-      value =
-          "SELECT DISTINCT a.asset_id, a.asset_name, a.asset_type, a.endpoint_platform, "
-              + "a.asset_created_at, a.asset_updated_at "
-              + "FROM assets a "
-              + "LEFT JOIN injects_assets ia ON a.asset_id = ia.asset_id "
-              + "WHERE a.asset_id IN (:assetIds) OR ia.inject_id IN (:injectIds) ;",
-      nativeQuery = true)
-  List<RawAsset> rawByIdsOrInjectIds(
-      @Param("assetIds") Set<String> assetIds, @Param("injectIds") Set<String> injectIds);
-
   @Query(
       value =
           "SELECT DISTINCT i.inject_exercise, a.asset_id, a.asset_name "
