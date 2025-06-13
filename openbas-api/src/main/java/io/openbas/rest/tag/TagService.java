89d211661f775a628a5dca8434251a436a5bad22
Antoine MAZEAS <antoine.mazeas@filigran.io>
2025-06-13 11:08:26 +0200

    fix tag updated at + expose finding endpoint side
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- openbas-api/src/main/java/io/openbas/rest/tag/TagService.java
+++ openbas-api/src/main/java/io/openbas/rest/tag/TagService.java
@@ -1,12 +1,16 @@
 package io.openbas.rest.tag;
 
 import static io.openbas.helper.StreamHelper.fromIterable;
+import static java.time.Instant.now;
 
 import io.openbas.database.model.Tag;
 import io.openbas.database.repository.TagRepository;
+import io.openbas.rest.exception.ElementNotFoundException;
 import io.openbas.rest.tag.form.TagCreateInput;
 import java.util.List;
 import java.util.Optional;
+
+import io.openbas.rest.tag.form.TagUpdateInput;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
@@ -32,5 +36,12 @@ public class TagService {
       newTag.setUpdateAttributes(input);
       return tagRepository.save(newTag);
     }
+  }
+
+  public Tag updateTag(String tagId, TagUpdateInput input) {
+    Tag tag = tagRepository.findById(tagId).orElseThrow(ElementNotFoundException::new);
+    tag.setUpdateAttributes(input);
+    tag.setUpdatedAt(now());
+    return tagRepository.save(tag);
   }
 }
