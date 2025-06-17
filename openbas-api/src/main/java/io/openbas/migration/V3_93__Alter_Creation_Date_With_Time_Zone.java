e9d9220ee37d6e191e7062810f18673cc94f5605
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-06-17 09:33:35 +0200

    [backend] Add TimeZone on date for attack_patterns & injects_expectations

---
+++ openbas-api/src/main/java/io/openbas/migration/V3_93__Alter_Creation_Date_With_Time_Zone.java
@@ -0,0 +1,25 @@
+package io.openbas.migration;
+
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+import org.springframework.stereotype.Component;
+
+import java.sql.Statement;
+
+@Component
+public class V3_93__Alter_Creation_Date_With_Time_Zone extends BaseJavaMigration {
+
+  @Override
+  public void migrate(Context context) throws Exception {
+    try (Statement statement = context.getConnection().createStatement()) {
+      statement.execute(
+          """
+                  ALTER TABLE attack_patterns ALTER COLUMN attack_pattern_created_at type timestamp with time zone using attack_pattern_created_at::timestamp with time zone;
+                  ALTER TABLE attack_patterns ALTER COLUMN attack_pattern_updated_at type timestamp with time zone using attack_pattern_updated_at::timestamp with time zone;
+              
+                  ALTER TABLE injects_expectations ALTER COLUMN inject_expectation_created_at type timestamp with time zone using inject_expectation_created_at::timestamp with time zone;
+                  ALTER TABLE injects_expectations ALTER COLUMN inject_expectation_updated_at type timestamp with time zone using inject_expectation_updated_at::timestamp with time zone;
+              """);
+    }
+  }
+}
