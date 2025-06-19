1954ce695a079b3c909c17f9af8decee6eee5090
Antoine MAZEAS <antoine.mazeas@filigran.io>
2025-06-19 15:27:09 +0200

    enable default width for non specific columns in endpoint
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- openbas-front/src/admin/components/workspaces/custom_dashboards/widgets/viz/list/elements/endpoint/EndpointElementStyles.ts
+++ openbas-front/src/admin/components/workspaces/custom_dashboards/widgets/viz/list/elements/endpoint/EndpointElementStyles.ts
@@ -1,6 +1,6 @@
 import type { CSSProperties } from '@mui/material/styles';
 
-const EndpointElementStyles: Record<string, CSSProperties> = {
+const EndpointElementStyles = new Proxy({
   endpoint_name: { width: '25%' },
   endpoint_active: { width: '10%' },
   endpoint_agents_privilege: { width: '12%' },
@@ -16,20 +16,6 @@ const EndpointElementStyles: Record<string, CSSProperties> = {
     alignItems: 'center',
   },
   base_tags_side: { width: '25%' },
-  base_entity: { display: 'none' },
-  base_id: { display: 'none' },
-  base_representative: { display: 'none' },
-  base_restrictions: { display: 'none' },
-  base_dependencies: { display: 'none' },
-  endpoint_ips: { display: 'none' },
-  endpoint_mac_addresses: { display: 'none' },
-  base_created_at: { display: 'none' },
-  base_updated_at: { display: 'none' },
-  endpoint_description: { display: 'none' },
-  endpoint_external_reference: { display: 'none' },
-  endpoint_hostname: { display: 'none' },
-  endpoint_seen_ip: { display: 'none' },
-  base_findings_side: { display: 'none' },
-};
+}, { get: (target: Record<string, CSSProperties>, name: string) => name in target ? target[name] : { width: '10%' } });
 
 export default EndpointElementStyles;
