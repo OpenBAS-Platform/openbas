c1b7a8c8725a27c2e13208e29f8ceb2b44d480b9
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-06-25 09:52:54 +0200

    [backend/frontend] Add CVE entity

--- openbas-model/src/main/java/io/openbas/database/repository/CweRepository.java
+++ openbas-model/src/main/java/io/openbas/database/repository/CweRepository.java
@@ -1,9 +1,9 @@
 package io.openbas.database.repository;
 
-import io.openbas.database.model.Cve;
+import io.openbas.database.model.Cwe;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CveRepository extends CrudRepository<Cve, String>, JpaSpecificationExecutor<Cve> {}
+public interface CweRepository extends CrudRepository<Cwe, String>, JpaSpecificationExecutor<Cwe> {}
