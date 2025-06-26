cdb11d0e76794f437655cb35a1fd719692869dab
Gael Leblan <gael.leblan@filigran.io>
2025-06-26 15:17:39 +0200

    [backend/frontend] Add EOL field on endpoints

--- openbas-api/src/main/java/io/openbas/migration/V3_97__Add_Eol_Column.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_97__Add_Eol_Column.java
@@ -7,7 +7,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V3_96__Add_Eol_Column extends BaseJavaMigration {
+public class V3_97__Add_Eol_Column extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
