bdeacb3cc8011f7569b64b7eed92c740534ace5a
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-06-26 18:16:14 +0200

    [backend] Fix wrong TTPs indexation on inject expectation

---
+++ openbas-api/src/main/java/io/openbas/migration/V3_97__Migrate_Inject_Expectation_TTPs_format.java
@@ -0,0 +1,20 @@
+package io.openbas.migration;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+import org.springframework.stereotype.Component;
+
+@Component
+public class V3_97__Migrate_Inject_Expectation_TTPs_format extends BaseJavaMigration {
+
+  @Override
+  public void migrate(Context context) throws Exception {
+    Connection connection = context.getConnection();
+    try (Statement statement = connection.createStatement()) {
+      statement.executeUpdate(
+          "DELETE FROM indexing_status WHERE indexing_status_type = 'expectation-inject'");
+    }
+  }
+}
