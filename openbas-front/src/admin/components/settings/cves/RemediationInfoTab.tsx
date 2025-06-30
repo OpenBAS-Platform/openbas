e3c492ce66fa519c62d6c4a7e439e970eefcdddd
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-06-30 11:14:39 +0200

    [backend/frontend] Refact Findings views

--- openbas-front/src/admin/components/settings/cves/RemediationInfoTab.tsx
+++ openbas-front/src/admin/components/settings/cves/RemediationInfoTab.tsx
@@ -12,9 +12,14 @@ const RemediationInfoTab = ({ cve }: Props) => {
 
   return (
     <div style={{ padding: theme.spacing(2, 1, 0, 0) }}>
-      <Typography variant="body2" gutterBottom>
-        {cve?.cve_remediation ?? t('There is no information yet on a vulnerability remediation for this CVE.')}
+      <Typography variant="subtitle2" gutterBottom>
+        {t('Vulnerability Remediation')}
       </Typography>
+      <pre>
+        <Typography variant="body2" gutterBottom>
+          {cve?.cve_remediation ?? t('There is no information yet on a vulnerability remediation for this CVE.')}
+        </Typography>
+      </pre>
     </div>
   );
 };
