9909ef1974f2f038d4cec6e711a29a39ccc39054
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-07-01 10:43:33 +0200

    Rebase

--- openbas-api/src/main/java/io/openbas/migration/V3_99__Migrate_Inject_Expectation_TTPs_format.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_99__Migrate_Inject_Expectation_TTPs_format.java
@@ -7,7 +7,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V3_98__Migrate_Inject_Expectation_TTPs_format extends BaseJavaMigration {
+public class V3_99__Migrate_Inject_Expectation_TTPs_format extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
