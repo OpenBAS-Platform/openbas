128798e00d4cfe5435cb486f8c4faa01fb1e03e6
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-07-01 13:18:11 +0200

    [backend/frontend] Refact Findings views

--- openbas-api/src/main/java/io/openbas/migration/V4_01__Cves_Default_data.java
+++ openbas-api/src/main/java/io/openbas/migration/V4_01__Cves_Default_data.java
@@ -8,7 +8,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V3_98__Cves_Default_data extends BaseJavaMigration {
+public class V4_01__Cves_Default_data extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
