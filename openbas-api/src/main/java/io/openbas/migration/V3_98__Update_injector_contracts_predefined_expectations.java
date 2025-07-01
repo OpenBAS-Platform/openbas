5fd04bb8d4ac63e95117005a99c84b5bb8ec6dad
Johanah LEKEU <johanah.lekeu@filigran.io>
2025-07-01 16:10:50 +0200

    Finalize migration

--- openbas-api/src/main/java/io/openbas/migration/V3_98__Update_injector_contracts_predefined_expectations.java
+++ openbas-api/src/main/java/io/openbas/migration/V3_98__Update_injector_contracts_predefined_expectations.java
@@ -1,12 +1,15 @@
 package io.openbas.migration;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
@@ -18,36 +21,48 @@ public class V3_98__Update_injector_contracts_predefined_expectations extends Ba
     Statement select = context.getConnection().createStatement();
     ResultSet results =
         select.executeQuery(
-            "SELECT injector_contract_content FROM injectors_contracts WHERE injector_contract_payload is not null");
+            "SELECT injector_contract_content,injector_contract_id FROM injectors_contracts WHERE injector_contract_payload is not null");
 
     PreparedStatement statement =
         context
             .getConnection()
             .prepareStatement(
-                "UPDATE injectors_contracts SET injector_contract_content = ?::json WHERE injector_contract_payload is not null");
+                "UPDATE injectors_contracts SET injector_contract_content = ? WHERE injector_contract_id=?");
 
     ObjectMapper mapper = new ObjectMapper();
 
     while (results.next()) {
-      ArrayNode fields = mapper.readTree((JsonParser) results.getArray("fields"));
-      fields.forEach(
-          (field) -> {
-            String key = field.get("key").asText();
-            if (key.equals("expectations")) {
-              ArrayNode predefinedExpectations = (ArrayNode) field.get("predefinedExpectations");
-              ObjectNode node = mapper.createObjectNode();
-              node.put("expectation_type", "VULNERABILITY");
-              node.put("expectation_name", "Expect asset to not be vulnerable");
-              node.put("expectation_description", "");
-              node.put("expectation_score", 100.0);
-              node.put("expectation_expectation_group", false);
-              node.put("expectation_expiration_time", 21600);
-              predefinedExpectations.add(node);
-            }
-          });
-      String content = mapper.writeValueAsString(results);
-      statement.setString(1, content);
-      statement.addBatch();
+      String content = results.getString("injector_contract_content");
+      ObjectNode contractContent = mapper.readValue(content, ObjectNode.class);
+      String contractId = results.getString("injector_contract_id");
+      int fieldIndex = 0;
+      if (contractContent != null) {
+        ArrayNode fields = (ArrayNode) contractContent.get("fields");
+        for (JsonNode field : fields) {
+          String key = field.get("key").asText();
+          if (key.equals("expectations")) {
+            ArrayNode predefinedExpectations = (ArrayNode) field.get("predefinedExpectations");
+            ObjectNode node = mapper.createObjectNode();
+            node.put("expectation_type", "VULNERABILITY");
+            node.put("expectation_name", "Expect asset to not be vulnerable");
+            node.put("expectation_description", "");
+            node.put("expectation_score", 100.0);
+            node.put("expectation_expectation_group", false);
+            node.put("expectation_expiration_time", 21600);
+            predefinedExpectations.add(node);
+            ObjectNode expectations = (ObjectNode) field;
+            expectations.set("predefinedExpectations", predefinedExpectations);
+            fields.set(fieldIndex, expectations);
+            break;
+          }
+          fieldIndex++;
+        }
+        contractContent.set("fields", fields);
+        String contentToSave = mapper.writeValueAsString(contractContent);
+        statement.setString(1, contentToSave);
+        statement.setString(2, contractId);
+        statement.addBatch();
+      }
     }
     statement.executeBatch();
   }
