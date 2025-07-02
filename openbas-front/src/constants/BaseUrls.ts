a16aecb50401894cf3c526764fd993863a3e5280
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-07-02 16:13:05 +0200

    [frontend] Add path

--- openbas-front/src/constants/BaseUrls.ts
+++ openbas-front/src/constants/BaseUrls.ts
@@ -1,6 +1,8 @@
 // ADMIN ROUTES
 
+export const ADMIN_BASE_URL = `/admin`;
 export const ATOMIC_BASE_URL = '/admin/atomic_testings';
 export const SIMULATION_BASE_URL = '/admin/simulations';
 export const SCENARIO_BASE_URL = '/admin/scenarios';
 export const ENDPOINT_BASE_URL = '/admin/assets/endpoints';
+export const PAYLOAD_BASE_URL = '/admin/payloads';
