86911c7447aea1454721f2abdde6e31945ee96fd
Antoine MAZEAS <antoine.mazeas@filigran.io>
2025-07-02 11:54:30 +0200

    arrow should have same link as main button
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- openbas-front/src/admin/components/workspaces/custom_dashboards/widgets/viz/list/elements/vulnerableendpoint/VulnerableEndpointElementSecondaryAction.tsx
+++ openbas-front/src/admin/components/workspaces/custom_dashboards/widgets/viz/list/elements/vulnerableendpoint/VulnerableEndpointElementSecondaryAction.tsx
@@ -1,13 +1,37 @@
 import { KeyboardArrowRight } from '@mui/icons-material';
+import qs from 'qs';
 import { Link } from 'react-router';
 
+import { buildSearchPagination } from '../../../../../../../../../components/common/queryable/QueryableUtils';
 import { SIMULATION_BASE_URL } from '../../../../../../../../../constants/BaseUrls';
 import { type EsVulnerableEndpoint } from '../../../../../../../../../utils/api-types';
 
 type Props = { element: EsVulnerableEndpoint };
 
 const VulnerableEndpointElementSecondaryAction = (props: Props) => {
-  const findingsTabUrl = `${SIMULATION_BASE_URL}/${props.element.base_simulation_side}/findings`;
+  const craftedFilter = btoa(qs.stringify({
+    ...buildSearchPagination({
+      filterGroup: {
+        mode: 'and',
+        filters: [
+          {
+            key: 'finding_assets',
+            operator: 'eq',
+            mode: 'or',
+            values: [props.element.vulnerable_endpoint_id ?? ''],
+          },
+          {
+            key: 'finding_type',
+            operator: 'eq',
+            mode: 'or',
+            values: ['CVE'],
+          },
+        ],
+      },
+    }),
+    key: `simulation-findings_${props.element.base_simulation_side}`,
+  }, { allowEmptyArrays: true }));
+  const findingsTabUrl = `${SIMULATION_BASE_URL}/${props.element.base_simulation_side}/findings?query=${craftedFilter}`;
   return (
     <Link to={findingsTabUrl} className="noDrag"><KeyboardArrowRight color="action" /></Link>
   );
