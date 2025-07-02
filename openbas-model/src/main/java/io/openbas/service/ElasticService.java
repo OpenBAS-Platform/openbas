5da52c72652e1a5e81e3468e2e775edf3b601e53
Gael Leblan <gael.leblan@filigran.io>
2025-07-03 00:46:43 +0200

    [backend] Remove unnecessary stuff

--- openbas-model/src/main/java/io/openbas/service/ElasticService.java
+++ openbas-model/src/main/java/io/openbas/service/ElasticService.java
@@ -5,7 +5,10 @@ import static java.util.Optional.ofNullable;
 import static org.springframework.util.StringUtils.hasText;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.elasticsearch._types.*;
+import co.elastic.clients.elasticsearch._types.FieldSort;
+import co.elastic.clients.elasticsearch._types.FieldValue;
+import co.elastic.clients.elasticsearch._types.SortOptions;
+import co.elastic.clients.elasticsearch._types.SortOrder;
 import co.elastic.clients.elasticsearch._types.aggregations.*;
 import co.elastic.clients.elasticsearch._types.query_dsl.*;
 import co.elastic.clients.elasticsearch.core.*;
@@ -371,7 +374,6 @@ public class ElasticService implements EngineService {
           BoolQuery.of(b -> b.should(directId, dependenciesId).minimumShouldMatch("1"))._toQuery();
       elasticClient.deleteByQuery(
           new DeleteByQueryRequest.Builder()
-              .conflicts(Conflicts.Proceed)
               .index(engineConfig.getIndexPrefix() + "*")
               .query(query)
               .build());
