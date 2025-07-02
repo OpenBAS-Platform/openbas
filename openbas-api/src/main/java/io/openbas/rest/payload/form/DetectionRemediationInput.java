49b5ad11c73708c04b344b64bbc254842d2e50e2
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-07-02 10:47:59 +0200

    [backend/frontend] Add detectionRemediation

--- openbas-api/src/main/java/io/openbas/rest/payload/form/DetectionRemediationInput.java
+++ openbas-api/src/main/java/io/openbas/rest/payload/form/DetectionRemediationInput.java
@@ -3,7 +3,14 @@ package io.openbas.rest.payload.form;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+@Builder
+@Getter
+@Setter
+@Schema(description = "Common base input for CVE creation and update")
 public class DetectionRemediationInput {
 
   @JsonProperty("detection_remediation_id")
