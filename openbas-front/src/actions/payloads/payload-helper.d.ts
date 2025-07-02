e3df8d387425033fb1d53635473c2b49d4aa3cec
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-07-02 16:03:31 +0200

    [frontend] Improv payload helper

--- openbas-front/src/actions/payloads/payload-helper.d.ts
+++ openbas-front/src/actions/payloads/payload-helper.d.ts
@@ -1,6 +1,7 @@
 import { type Payload } from '../../utils/api-types';
 
 export interface PayloadHelper {
+  getPayload: (string: payloadId) => Payload;
   getPayloads: () => Payload[];
   getPayloadsMap: () => Record<string, Payload>;
 }
