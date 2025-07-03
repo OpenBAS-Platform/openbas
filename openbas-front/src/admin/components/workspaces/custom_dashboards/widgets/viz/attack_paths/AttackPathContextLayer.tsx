0e06b3f372ccea55b8bff1482c200736324ed461
Marine LM <marine.lemezo@filigran.io>
2025-07-03 11:27:52 +0200

    [frontend] remove useless import
    Signed-off-by: Marine LM <marine.lemezo@filigran.io>

--- openbas-front/src/admin/components/workspaces/custom_dashboards/widgets/viz/attack_paths/AttackPathContextLayer.tsx
+++ openbas-front/src/admin/components/workspaces/custom_dashboards/widgets/viz/attack_paths/AttackPathContextLayer.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { fetchExercise } from '../../../../../../../actions/Exercise';
 import { type ExercisesHelper } from '../../../../../../../actions/exercises/exercise-helper';
