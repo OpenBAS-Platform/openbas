789b905308a1b3b0a6c6dd36788bab667c968978
Antoine MAZEAS <antoine.mazeas@filigran.io>
2025-07-03 11:32:08 +0200

    align icon
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- openbas-front/src/admin/components/workspaces/custom_dashboards/widgets/viz/list/elements/vulnerableendpoint/VulnerableEndpointElementStyles.ts
+++ openbas-front/src/admin/components/workspaces/custom_dashboards/widgets/viz/list/elements/vulnerableendpoint/VulnerableEndpointElementStyles.ts
@@ -1,14 +1,10 @@
 import type { CSSProperties } from '@mui/material/styles';
 
 const VulnerableEndpointElementStyles: Record<string, CSSProperties> = {
-  endpoint_platform: {
+  vulnerable_endpoint_platform: {
     display: 'flex',
     alignItems: 'center',
-  },
-  endpoint_agents_executor: {
-    display: 'flex',
-    alignItems: 'center',
-  },
+  }
 };
 
 export default VulnerableEndpointElementStyles;
