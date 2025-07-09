3665a5f22db73f0d081c9268d7ba1bbf4faeab69
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-07-09 12:14:38 +0200

    [frontend] Remove exclusion from obas agent installation

--- openbas-front/src/admin/components/agents/InstructionSelector.tsx
+++ openbas-front/src/admin/components/agents/InstructionSelector.tsx
@@ -143,31 +143,13 @@ nohup ${agentFolder ?? '/opt/openbas-caldera-agent'}/openbas-caldera-agent -serv
       }
       return `iex (iwr ${buildInstallationUrl(settings.platform_base_url + '/api/agent/installer/openbas/windows')}).Content`;
     };
-    const buildExclusionPath = (advanced: string, standard: string, other: string) => {
-      let result = other;
-      if (agentFolder) {
-        return agentFolder; // If agentFolder is truthy, return it immediately
-      }
-      if (activeTab === 1 && selectedOption !== USER) {
-        result = advanced;
-      } else if (activeTab === 0) {
-        result = standard;
-      }
-      return result;
-    };
 
     switch (platform) {
       case WINDOWS:
         return {
           icon: <Powershell />,
           label: 'powershell',
-          exclusions: `${buildExclusionPath('C:\\Program Files (x86)\\Filigran\\OBAS Agent', 'C:\\Filigran', 'C:\\Filigran')}
-
-Caldera injector hashes:
-MD5: 68c1795fb45cb9b522d6cf48443fdc37
-SHA1: 5f87d06f818ff8cba9e11e8cd1c6f9d990eca0f8
-SHA256: 6b180913acb8cdac3fb8d3154a2f6a0bed13c056a477f4f94c4679414ec13b9f
-SHA512: 6185b7253eedfa6253f26cd85c4bcfaf05195219b6ab06b43d9b07279d7d0cdd3c957bd58d36058d7cde405bc8c5084f3ac060a6080bfc18a843738d3bee87fd`,
+          exclusions: '',
           displayedCode: buildUrlScript2Windows(),
           code: buildUrlScript2Windows(),
         };
@@ -175,13 +157,7 @@ SHA512: 6185b7253eedfa6253f26cd85c4bcfaf05195219b6ab06b43d9b07279d7d0cdd3c957bd5
         return {
           icon: <Bash />,
           label: 'sh',
-          exclusions: `${buildExclusionPath('/opt/openbas-agent', '$HOME/.local/openbas-agent-session', '/opt/openbas-agent-service-USER')}
-          
-Caldera injector hashes:
-MD5: d604c952bb3c6d96621594d39992c499
-SHA1: 5b6087f87f5f2ae129f888bba799611836eb39a2
-SHA256: 98d1e64445bbef46a36d4724699a386646de78881a1b6f2b346122c76d696c12
-SHA512: ca07dc1d0a5297e29327e483f4f35dadb254d96a16a5c33da5ad048e6965a3863d621518a2be40f1a42226c68cbf5e779382a37ee5baa7dd7c538ec73ce059e8`,
+          exclusions: '',
           displayedCode: `curl -s ${buildInstallationUrl(settings.platform_agent_url + '/api/agent/installer/openbas/linux')} ${buildExtraParams(' | sudo sh -s -- --user USER --group GROUP', '| sh', '| sudo sh')}`,
           code: `curl -s ${buildInstallationUrl(settings.platform_agent_url + '/api/agent/installer/openbas/linux')} ${buildExtraParams(' | sudo sh -s -- --user USER --group GROUP', '| sh', '| sudo sh')}`,
         };
@@ -189,13 +165,7 @@ SHA512: ca07dc1d0a5297e29327e483f4f35dadb254d96a16a5c33da5ad048e6965a3863d621518
         return {
           icon: <TerminalOutlined />,
           label: 'sh',
-          exclusions: `${buildExclusionPath('/opt/openbas-agent', '$HOME/.local/openbas-agent-session', '/opt/openbas-agent-service-USER')}
-          
-Caldera injector hashes:
-MD5: 1132906cc40001f51673108847b88d0c
-SHA1: 3177df4a8fa13a2e13ce63670c579955ad55df3f
-SHA256: 2b4397160925bf6b9dcca0949073fd9b2fc590ab641ea1d1c3d7d36048ed674a
-SHA512: f1c8cf0c41c7d193bcb2aad21d7a739c785902c3231e15986b2eb37f911824a802f50cb2dbb509deba1c7a2a535fb7b34cf100303c61a6087102948628133747`,
+          exclusions: '',
           displayedCode: `curl -s ${buildInstallationUrl(settings.platform_agent_url + '/api/agent/installer/openbas/macos')} ${buildExtraParams(' | sudo sh -s -- --user USER --group GROUP', '| sh', '| sudo sh')}`,
           code: `curl -s ${buildInstallationUrl(settings.platform_agent_url + '/api/agent/installer/openbas/macos')} ${buildExtraParams(' | sudo sh -s -- --user USER --group GROUP', '| sh', '| sudo sh')}`,
         };
@@ -203,13 +173,7 @@ SHA512: f1c8cf0c41c7d193bcb2aad21d7a739c785902c3231e15986b2eb37f911824a802f50cb2
         return {
           icon: <Bash />,
           label: 'sh',
-          exclusions: `${buildExclusionPath('/opt/openbas-agent', '$HOME/.local/openbas-agent-session', '/opt/openbas-agent-service-USER')}
-
-Caldera injector hashes:
-MD5: d604c952bb3c6d96621594d39992c499
-SHA1: 5b6087f87f5f2ae129f888bba799611836eb39a2
-SHA256: 98d1e64445bbef46a36d4724699a386646de78881a1b6f2b346122c76d696c12
-SHA512: ca07dc1d0a5297e29327e483f4f35dadb254d96a16a5c33da5ad048e6965a3863d621518a2be40f1a42226c68cbf5e779382a37ee5baa7dd7c538ec73ce059e8`,
+          exclusions: '',
           displayedCode: `curl -s ${buildInstallationUrl(settings.platform_agent_url + '/api/agent/installer/openbas/linux')} ${buildExtraParams(' | sudo sh -s -- --user USER --group GROUP', '| sh', '| sudo sh')}`,
           code: `curl -s ${buildInstallationUrl(settings.platform_agent_url + '/api/agent/installer/openbas/linux')} ${buildExtraParams(' | sudo sh -s -- --user USER --group GROUP', '| sh', '| sudo sh')}`,
         };
@@ -235,6 +199,13 @@ SHA512: ca07dc1d0a5297e29327e483f4f35dadb254d96a16a5c33da5ad048e6965a3863d621518
           <MenuItem value="arm64">{t('arm64')}</MenuItem>
         </Select>
       </FormControl>
+    );
+  };
+  const stepInstallationTitle = () => {
+    return (
+      <Typography variant="h2" style={{ marginTop: theme.spacing(3) }}>
+        {t('Installation procedure')}
+      </Typography>
     );
   };
   const stepOneInstallationTitle = () => {
@@ -347,7 +318,7 @@ SHA512: ca07dc1d0a5297e29327e483f4f35dadb254d96a16a5c33da5ad048e6965a3863d621518
           <p>
             {`${t('Install the agent using your own user account.')} ${platform === WINDOWS ? t('It can be run as administrator or as a standard user, depending on the PowerShell elevation.') : t('This installation requires only local standard privileges.')}`}
           </p>
-          {stepOneInstallationTitle()}
+          {stepInstallationTitle()}
           {buildInstallationScriptsAndActionButtons()}
         </>
       ));
@@ -368,7 +339,7 @@ SHA512: ca07dc1d0a5297e29327e483f4f35dadb254d96a16a5c33da5ad048e6965a3863d621518
         <p>
           {`${t('Install the agent as a user or a system service. This installation requires local administrator privileges.')}`}
         </p>
-        {stepOneInstallationTitle()}
+        {stepInstallationTitle()}
         <div>
           <RadioGroup
             value={selectedOption}
@@ -430,7 +401,6 @@ SHA512: ca07dc1d0a5297e29327e483f4f35dadb254d96a16a5c33da5ad048e6965a3863d621518
               </Tabs>
               {activeTab === 0 && (buildStandardInstallation())}
               {activeTab === 1 && (buildAdvancedInstallation())}
-              {buildStepTwoExclusions()}
             </div>
           )}
         </div>
