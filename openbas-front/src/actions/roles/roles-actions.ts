da9204cc849952dc7c67ce60db049f5c1d1c3226
Camille Roux <camille.roux@filigran.io>
2025-07-15 14:57:41 +0200

    [frontend] RBAC creation of roles screen

--- openbas-front/src/actions/roles/roles-actions.ts
+++ openbas-front/src/actions/roles/roles-actions.ts
@@ -1,10 +1,16 @@
-import { simplePostCall } from '../../utils/Action';
-import { type SearchPaginationInput } from '../../utils/api-types';
+import type { Dispatch } from 'redux';
+
+import { delReferential, simplePostCall } from '../../utils/Action';
+import { type Role, type SearchPaginationInput } from '../../utils/api-types';
 
 const ROLES_URI = '/api/roles';
-// eslint-disable-next-line import/prefer-default-export
-export const searchRoles = ({ paginationInput }: { paginationInput: SearchPaginationInput }) => {
+export const searchRoles = (paginationInput: SearchPaginationInput) => {
   const data = paginationInput;
   const uri = `${ROLES_URI}/search`;
   return simplePostCall(uri, data);
+};
+
+export const deleteRoles = (roleId: Role['role_id']) => (dispatch: Dispatch) => {
+  const uri = `${ROLES_URI}/${roleId}`;
+  return delReferential(uri, 'roles', roleId)(dispatch);
 };
