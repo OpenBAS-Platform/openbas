89f481ed0d4ed95b57f5b461457ca5470d262212
Antoine MAZEAS <antoine.mazeas@filigran.io>
2025-07-16 09:42:10 +0200

    add migration for list widgets
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

---
+++ openbas-api/src/main/java/io/openbas/migration/V4_10__List_Widget_config_update.java
@@ -0,0 +1,31 @@
+package io.openbas.migration;
+
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+@Component
+public class V4_10__List_Widget_config_update extends BaseJavaMigration {
+
+  @Override
+  public void migrate(Context context) throws Exception {
+    Connection connection = context.getConnection();
+    try (Statement statement = connection.createStatement()) {
+
+      // 1. Add new column for collector_type
+      statement.execute(
+          """
+            UPDATE widgets
+            SET widget_config = widget_config || CONCAT('{"perspective":', (widget_config ->> 'series')::jsonb ->> 0, '}')::jsonb
+            WHERE widget_config @> '{"widget_configuration_type": "list"}';
+            
+            UPDATE widgets
+            SET widget_config = widget_config - 'series'
+            WHERE widget_config @> '{"widget_configuration_type": "list"}';
+            """);
+    }
+  }
+}
