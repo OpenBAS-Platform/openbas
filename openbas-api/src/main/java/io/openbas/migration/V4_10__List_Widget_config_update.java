3ff02ea7051a20b75cbded68b403277a998882f9
Antoine MAZEAS <antoine.mazeas@filigran.io>
2025-07-16 09:42:32 +0200

    lint
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- openbas-api/src/main/java/io/openbas/migration/V4_10__List_Widget_config_update.java
+++ openbas-api/src/main/java/io/openbas/migration/V4_10__List_Widget_config_update.java
@@ -1,11 +1,10 @@
 package io.openbas.migration;
 
+import java.sql.Connection;
+import java.sql.Statement;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
-
-import java.sql.Connection;
-import java.sql.Statement;
 
 @Component
 public class V4_10__List_Widget_config_update extends BaseJavaMigration {
@@ -21,7 +20,7 @@ public class V4_10__List_Widget_config_update extends BaseJavaMigration {
             UPDATE widgets
             SET widget_config = widget_config || CONCAT('{"perspective":', (widget_config ->> 'series')::jsonb ->> 0, '}')::jsonb
             WHERE widget_config @> '{"widget_configuration_type": "list"}';
-            
+
             UPDATE widgets
             SET widget_config = widget_config - 'series'
             WHERE widget_config @> '{"widget_configuration_type": "list"}';
