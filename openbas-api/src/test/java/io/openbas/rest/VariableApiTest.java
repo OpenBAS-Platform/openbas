43493d2c229344d8956e9fcfc4a1abf560f21eb7
Gael Leblan <gael.leblan@filigran.io>
2025-07-18 00:31:04 +0200

    [backend] Trying to handle composers

--- openbas-api/src/test/java/io/openbas/rest/VariableApiTest.java
+++ openbas-api/src/test/java/io/openbas/rest/VariableApiTest.java
@@ -16,7 +16,6 @@ import io.openbas.database.model.Variable;
 import io.openbas.database.repository.ScenarioRepository;
 import io.openbas.database.repository.VariableRepository;
 import io.openbas.service.ScenarioService;
-import io.openbas.utils.mockUser.WithMockObserverUser;
 import io.openbas.utils.mockUser.WithMockPlannerUser;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -95,7 +94,7 @@ public class VariableApiTest extends IntegrationTest {
   @DisplayName("Retrieve variables for scenario")
   @Test
   @Order(2)
-  @WithMockObserverUser
+  @WithMockPlannerUser
   void retrieveVariableForScenarioTest() throws Exception {
     // -- EXECUTE --
     String response =
