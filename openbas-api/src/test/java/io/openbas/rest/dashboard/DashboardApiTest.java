e502cc3debb6b234b90699e8d01225f68d9ec5af
Gael Leblan <gael.leblan@filigran.io>
2025-07-17 19:04:55 +0200

    [backend] Fixing tests

--- openbas-api/src/test/java/io/openbas/rest/dashboard/DashboardApiTest.java
+++ openbas-api/src/test/java/io/openbas/rest/dashboard/DashboardApiTest.java
@@ -11,6 +11,7 @@ import io.openbas.IntegrationTest;
 import io.openbas.database.model.Endpoint;
 import io.openbas.database.model.Filters;
 import io.openbas.database.model.Widget;
+import io.openbas.database.repository.ExerciseRepository;
 import io.openbas.driver.ElasticDriver;
 import io.openbas.engine.EsEngine;
 import io.openbas.engine.EsModel;
@@ -53,6 +54,7 @@ class DashboardApiTest extends IntegrationTest {
   @Autowired private InjectComposer injectComposer;
   @Autowired private FindingComposer findingComposer;
   @Autowired private CustomDashboardParameterComposer customDashboardParameterComposer;
+  @Autowired private ExerciseRepository exerciseRepository;
 
   @BeforeEach
   void setup() throws IOException {
@@ -70,6 +72,7 @@ class DashboardApiTest extends IntegrationTest {
   @AfterAll
   void afterAll() {
     globalTeardown();
+    exerciseRepository.deleteAll();
   }
 
   @Nested
