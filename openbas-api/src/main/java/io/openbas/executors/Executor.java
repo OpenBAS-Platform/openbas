93cee736ad99893f38b0a0f89462f97f1e5ee09b
damgouj <damien.goujard@filigran.io>
2025-07-21 10:59:41 +0200

    [backend] Delete debug logs (#2797)

--- openbas-api/src/main/java/io/openbas/executors/Executor.java
+++ openbas-api/src/main/java/io/openbas/executors/Executor.java
@@ -55,24 +55,15 @@ public class Executor {
 
   private InjectStatus executeInternal(ExecutableInject executableInject, Injector injector) {
     Inject inject = executableInject.getInjection().getInject();
-    // [issue/2797] logs -> we try to understand why we can't reproduce on dev and
-    // test-feature-branch env so those
-    // logs are for tests on prerelease env with the scenario "20250318 REP-YGN Fermeture temporaire
-    // KUC - test Dam"
-    log.info("[issue/2797] executeInternal 1: " + inject.getId());
     io.openbas.executors.Injector executor =
         this.context.getBean(injector.getType(), io.openbas.executors.Injector.class);
-    log.info("[issue/2797] executeInternal 2: " + inject.getId());
     Execution execution = executor.executeInjection(executableInject);
-    log.info("[issue/2797] executeInternal 3: " + inject.getId());
     // After execution, expectations are already created
     // Injection status is filled after complete execution
     // Report inject execution
     InjectStatus injectStatus =
         this.injectStatusRepository.findByInjectId(inject.getId()).orElseThrow();
-    log.info("[issue/2797] executeInternal 4: " + inject.getId());
     InjectStatus completeStatus = injectStatusService.fromExecution(execution, injectStatus);
-    log.info("[issue/2797] executeInternal 5: " + inject.getId());
     return injectStatusRepository.save(completeStatus);
   }
 
