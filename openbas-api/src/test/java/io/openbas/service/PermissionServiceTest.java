42e5a77b6188adafff1b720e8e81b15299ea3a83
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-07-22 14:58:57 +0200

    [backend] Migrate to junit 5

--- openbas-api/src/test/java/io/openbas/service/PermissionServiceTest.java
+++ openbas-api/src/test/java/io/openbas/service/PermissionServiceTest.java
@@ -1,7 +1,7 @@
 package io.openbas.service;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 import io.openbas.database.model.Action;
