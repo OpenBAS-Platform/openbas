ecac9fabc8b718534f7a72b9a219015ee2bbaf14
Camille Roux <camille.roux@filigran.io>
2025-07-23 14:11:40 +0200

    [frontend] RBAC creation of roles screen

--- openbas-front/src/admin/components/settings/roles/Roles.tsx
+++ openbas-front/src/admin/components/settings/roles/Roles.tsx
@@ -1,6 +1,4 @@
-import { HelpOutlineOutlined, SecurityOutlined } from '@mui/icons-material';
-import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import { useSearchParams } from 'react-router';
 import { makeStyles } from 'tss-react/mui';
 
@@ -9,15 +7,13 @@ import Breadcrumbs from '../../../../components/Breadcrumbs';
 import { initSorting } from '../../../../components/common/queryable/Page';
 import PaginationComponentV2 from '../../../../components/common/queryable/pagination/PaginationComponentV2';
 import { buildSearchPagination } from '../../../../components/common/queryable/QueryableUtils';
-import SortHeadersComponentV2 from '../../../../components/common/queryable/sort/SortHeadersComponentV2';
-import useBodyItemsStyles from '../../../../components/common/queryable/style/style';
 import { useQueryableWithLocalStorage } from '../../../../components/common/queryable/useQueryableWithLocalStorage';
 import { useFormatter } from '../../../../components/i18n';
-import PaginatedListLoader from '../../../../components/PaginatedListLoader';
 import type { Role, SearchPaginationInput } from '../../../../utils/api-types';
 import SecurityMenu from '../SecurityMenu';
 import CreateRole from './CreateRole';
 import RolePopover from './RolePopover';
+import RolesList from './RolesList';
 
 const useStyles = makeStyles()(() => ({
   itemHead: { textTransform: 'uppercase' },
@@ -26,37 +22,9 @@ const useStyles = makeStyles()(() => ({
   bodyItems: { flexGrow: 1 },
 }));
 
-const inlineStyles = {
-  role_name: { width: '50%' },
-  role_creation_date: { width: '25%' },
-  role_modification_date: { width: '25%' },
-};
-
 const Roles = () => {
   const { classes } = useStyles();
   const { t } = useFormatter();
-
-  const bodyItemsStyles = useBodyItemsStyles();
-
-  // Headers
-  const headers = useMemo(() => [
-    {
-      field: 'role_name',
-      label: t('Name'),
-      isSortable: true,
-    },
-    {
-      field: 'role_creation_date',
-      label: t('Platform creation date'),
-      isSortable: true,
-    },
-    {
-      field: 'role_modification_date',
-      label: t('Modification date'),
-      isSortable: true,
-    },
-  ], []);
-
   // Query param
   const [searchParams] = useSearchParams();
   const [search] = searchParams.getAll('search');
@@ -68,6 +36,14 @@ const Roles = () => {
     sorts: initSorting('role_name', 'ASC'),
     textSearch: search,
   }));
+
+  const secondaryAction = (role: Role) => (
+    <RolePopover
+      onDelete={result => setRoles(roles.filter(r => (r.role_id !== result)))}
+      onUpdate={result => setRoles(roles.map(r => (r.role_id !== result.role_id ? r : result)))}
+      role={{ ...role }}
+    />
+  );
 
   const searchRolesToLoad = (input: SearchPaginationInput) => {
     setLoading(true);
@@ -95,66 +71,7 @@ const Roles = () => {
           disablePagination
           disableFilters
         />
-        <List>
-          <ListItem
-            classes={{ root: classes.itemHead }}
-            divider={false}
-            style={{ paddingTop: 0 }}
-            secondaryAction={<>&nbsp;</>}
-          >
-            <ListItemIcon />
-            <ListItemText
-              primary={(
-                <SortHeadersComponentV2
-                  headers={headers}
-                  inlineStylesHeaders={inlineStyles}
-                  sortHelpers={queryableHelpers.sortHelpers}
-                />
-              )}
-            />
-          </ListItem>
-          {loading
-            ? <PaginatedListLoader Icon={HelpOutlineOutlined} headers={headers} headerStyles={inlineStyles} />
-            : (
-                <div>
-                  {roles.map((role: Role) => (
-                    <ListItem
-                      key={role.role_id}
-                      classes={{ root: classes.item }}
-                      divider={true}
-                    >
-                      <ListItemIcon>
-                        <SecurityOutlined sx={{ color: '#96F426' }} />
-                      </ListItemIcon>
-                      <ListItemText primary={(
-                        <div style={bodyItemsStyles.bodyItems}>
-                          {Object.keys(inlineStyles).map(key => (
-                            <div
-                              key={key}
-                              style={{
-                                ...bodyItemsStyles.bodyItem,
-                                ...inlineStyles[key as keyof typeof inlineStyles],
-                              }}
-                            >
-                              {role[key as keyof typeof role]}
-                            </div>
-                          ))}
-
-                        </div>
-
-                      )}
-                      />
-                      <RolePopover
-                        onDelete={result => setRoles(roles.filter(r => (r.role_id !== result)))}
-                        onUpdate={result => setRoles(roles.map(r => (r.role_id !== result.role_id ? r : result)))}
-                        role={{ ...role }}
-                      />
-                    </ListItem>
-                  ),
-                  )}
-                </div>
-              )}
-        </List>
+        <RolesList roles={roles} queryableHelpers={queryableHelpers} loading={loading} secondaryAction={secondaryAction} />
         <CreateRole onCreate={(result: Role) => setRoles([...roles, result])} />
 
       </div>
--- openbas-model/src/main/java/io/openbas/database/model/Role.java
+++ openbas-model/src/main/java/io/openbas/database/model/Role.java
@@ -1,10 +1,15 @@
 package io.openbas.database.model;
 
+import static java.time.Instant.now;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.openbas.annotation.Queryable;
 import io.openbas.database.audit.ModelBaseListener;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 import lombok.Data;
@@ -36,4 +41,18 @@ public class Role implements Base {
   @Enumerated(EnumType.STRING)
   @Column(name = "capability")
   private Set<Capability> capabilities = new HashSet<>();
+
+  @Queryable(sortable = true)
+  @Column(name = "role_created_at")
+  @JsonProperty("role_created_at")
+  @NotNull
+  @Schema(description = "Creation date of the role", accessMode = Schema.AccessMode.READ_ONLY)
+  private Instant createdAt = now();
+
+  @Queryable(sortable = true)
+  @Column(name = "role_updated_at")
+  @JsonProperty("role_updated_at")
+  @NotNull
+  @Schema(description = "Update date of the role", accessMode = Schema.AccessMode.READ_ONLY)
+  private Instant updatedAt = now();
 }
