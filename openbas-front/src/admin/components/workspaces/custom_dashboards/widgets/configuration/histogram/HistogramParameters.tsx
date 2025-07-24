86ed502468d8b28985db830626ff6117902eee85
Damien Goujard <damien.goujard@filigran.io>
2025-07-24 10:33:31 +0200

    [frontend] Number widget form

--- openbas-front/src/admin/components/workspaces/custom_dashboards/widgets/configuration/histogram/HistogramParameters.tsx
+++ openbas-front/src/admin/components/workspaces/custom_dashboards/widgets/configuration/histogram/HistogramParameters.tsx
@@ -173,7 +173,7 @@ const HistogramParameters = ({ widgetType, control, setValue }: Props) => {
           )}
         />
       )}
-      {mode === 'temporal' && (
+      {mode === 'temporal' && widgetType !== 'number' && (
         <>
           <Controller
             control={control}
