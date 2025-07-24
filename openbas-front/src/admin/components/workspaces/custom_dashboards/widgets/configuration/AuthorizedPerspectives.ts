8a847b9ac769573b8c68b5bd7b2143fe27d82081
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-07-24 09:11:02 +0200

    [frontend] Rollback whitelist filters on perspectives (#3545)

--- openbas-front/src/admin/components/workspaces/custom_dashboards/widgets/configuration/AuthorizedPerspectives.ts
+++ openbas-front/src/admin/components/workspaces/custom_dashboards/widgets/configuration/AuthorizedPerspectives.ts
@@ -1,5 +1,5 @@
 const getAuthorizedPerspectives = (): Map<string, string[]> => new Map([
-  ['expectation-inject', ['base_created_at', 'inject_expectation_status', 'inject_expectation_type', 'base_updated_at', 'base_simulation_side']],
+  ['expectation-inject', ['base_created_at', 'inject_expectation_status', 'inject_expectation_type', 'base_updated_at', 'base_simulation_side', 'base_agent_side', 'base_asset_side', 'base_asset_group_side']],
   ['finding', ['base_created_at', 'finding_type', 'base_updated_at', 'base_endpoint_side', 'base_simulation_side']],
   ['endpoint', ['endpoint_arch', 'endpoint_platform', 'endpoint_ips', 'endpoint_hostname']],
   ['vulnerable-endpoint', ['vulnerable_endpoint_architecture', 'vulnerable_endpoint_agents_active_status', 'vulnerable_endpoint_agents_privileges', 'vulnerable_endpoint_platform', 'base_simulation_side']],
