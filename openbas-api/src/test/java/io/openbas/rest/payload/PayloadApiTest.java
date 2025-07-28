715a377886f4bba90154cb5c2fc51fe90b317da3
Hedi Tarchouni <hedi.tarchouni@filigran.io>
2025-07-28 10:27:03 +0200

    test

--- openbas-api/src/test/java/io/openbas/rest/payload/PayloadApiTest.java
+++ openbas-api/src/test/java/io/openbas/rest/payload/PayloadApiTest.java
@@ -45,7 +45,6 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
