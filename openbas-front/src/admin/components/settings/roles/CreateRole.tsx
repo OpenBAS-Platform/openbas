0c8150e2ca6d39e7bb58bbca39508a80855f52e6
Camille Roux <camille.roux@filigran.io>
2025-07-29 11:43:23 +0200

    [frontend] RBAC creation of roles screen

--- openbas-front/src/admin/components/settings/roles/CreateRole.tsx
+++ openbas-front/src/admin/components/settings/roles/CreateRole.tsx
@@ -1,10 +1,45 @@
-import { useState } from 'react';
+import { useState } from 'react';
 
 import { createRole } from '../../../../actions/roles/roles-actions';
-import { type RoleResult } from '../../../../actions/roles/roles-helper';
+import { type RoleResult } from '../../../../actions/roles/roles-helper';
 import ButtonCreate from '../../../../components/common/ButtonCreate';
 import Drawer from '../../../../components/common/Drawer';
 import { useFormatter } from '../../../../components/i18n';
-import type { RoleOutput } from '../../../../utils/api-types';
+import type { RoleOutput } from '../../../../utils/api-types';
 import { useAppDispatch } from '../../../../utils/hooks';
-import RoleForm, { type RoleCreateInput } from './RoleForm';interface CreateRoleProps { onCreate?: (result: RoleOutput) => void }const CreateRole = ({ onCreate }: CreateRoleProps) => {  // Standard hooks  const [open, setOpen] = useState(false);  const { t } = useFormatter();  const dispatch = useAppDispatch();  const onSubmit = (data: RoleCreateInput) => {    dispatch(createRole(data))      .then((result: RoleResult) => {        if (onCreate) {          const roleCreated = result.entities.roles[result.result];          onCreate(roleCreated);        }        return (result.result ? setOpen(false) : result);      });  };  return (    <>      <ButtonCreate onClick={() => setOpen(true)} style={{ right: 230 }} />      <Drawer        open={open}        handleClose={() => setOpen(false)}        title={t('Create a new role')}      >        <RoleForm onSubmit={onSubmit} handleClose={() => setOpen(false)} editing={false} />      </Drawer>    </>  );};export default CreateRole;
+import RoleForm, { type RoleCreateInput } from './RoleForm';
+
+interface CreateRoleProps { onCreate?: (result: RoleOutput) => void }
+
+const CreateRole = ({ onCreate }: CreateRoleProps) => {
+  // Standard hooks
+  const [open, setOpen] = useState(false);
+  const { t } = useFormatter();
+
+  const dispatch = useAppDispatch();
+  const onSubmit = (data: RoleCreateInput) => {
+    dispatch(createRole(data))
+      .then((result: RoleResult) => {
+        if (onCreate) {
+          const roleCreated = result.entities.roles[result.result];
+          onCreate(roleCreated);
+        }
+        return (result.result ? setOpen(false) : result);
+      });
+  };
+
+  return (
+    <>
+      <ButtonCreate onClick={() => setOpen(true)} style={{ right: 230 }} />
+      <Drawer
+        open={open}
+        handleClose={() => setOpen(false)}
+        title={t('Create a new role')}
+      >
+        <RoleForm onSubmit={onSubmit} handleClose={() => setOpen(false)} editing={false} />
+      </Drawer>
+    </>
+  );
+};
+
+export default CreateRole;
