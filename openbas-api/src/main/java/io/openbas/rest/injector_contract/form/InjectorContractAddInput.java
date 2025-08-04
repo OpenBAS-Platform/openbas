38e7b239b93472b2576e62d49dcef0b82c8b687e
Antoine MAZEAS <antoine.mazeas@filigran.io>
2025-08-04 15:48:44 +0200

    remove boilerplate; use lombok generators
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- openbas-api/src/main/java/io/openbas/rest/injector_contract/form/InjectorContractAddInput.java
+++ openbas-api/src/main/java/io/openbas/rest/injector_contract/form/InjectorContractAddInput.java
@@ -25,23 +25,18 @@ public class InjectorContractAddInput {
   @JsonProperty("injector_id")
   private String injectorId;
 
-  @Getter
   @JsonProperty("contract_manual")
   private boolean manual = false;
 
-  @Getter
   @JsonProperty("contract_labels")
   private Map<String, String> labels;
 
-  @Getter
   @JsonProperty("contract_attack_patterns_ids")
   private List<String> attackPatternsIds = new ArrayList<>();
 
-  @Getter
   @JsonProperty("contract_attack_patterns_external_ids")
   private List<String> attackPatternsExternalIds = new ArrayList<>();
 
-  @Getter
   @NotBlank(message = MANDATORY_MESSAGE)
   @JsonProperty("contract_content")
   private String content;
@@ -49,79 +44,6 @@ public class InjectorContractAddInput {
   @JsonProperty("is_atomic_testing")
   private boolean isAtomicTesting = true;
 
-  @Getter
   @JsonProperty("contract_platforms")
   private String[] platforms = new String[0];
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getInjectorId() {
-    return injectorId;
-  }
-
-  public void setInjectorId(String injectorId) {
-    this.injectorId = injectorId;
-  }
-
-  public boolean isManual() {
-    return manual;
-  }
-
-  public void setManual(boolean manual) {
-    this.manual = manual;
-  }
-
-  public Map<String, String> getLabels() {
-    return labels;
-  }
-
-  public void setLabels(Map<String, String> labels) {
-    this.labels = labels;
-  }
-
-  public List<String> getAttackPatternsIds() {
-    return attackPatternsIds;
-  }
-
-  public void setAttackPatternsIds(List<String> attackPatternsIds) {
-    this.attackPatternsIds = attackPatternsIds;
-  }
-
-  public List<String> getAttackPatternsExternalIds() {
-    return attackPatternsExternalIds;
-  }
-
-  public void setAttackPatternsExternalIds(List<String> attackPatternsExternalIds) {
-    this.attackPatternsExternalIds = attackPatternsExternalIds;
-  }
-
-  public String getContent() {
-    return content;
-  }
-
-  public void setContent(String content) {
-    this.content = content;
-  }
-
-  public boolean isAtomicTesting() {
-    return isAtomicTesting;
-  }
-
-  public void setAtomicTesting(boolean atomicTesting) {
-    isAtomicTesting = atomicTesting;
-  }
-
-  public String[] getPlatforms() {
-    return platforms;
-  }
-
-  public void setPlatforms(String[] platforms) {
-    this.platforms = platforms;
-  }
 }
