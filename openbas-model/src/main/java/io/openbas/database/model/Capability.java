c929c94f17246d2508415186bbba657ed46e2c60
Hedi <hedi.tarchouni@filigran.io>
2025-08-06 10:05:40 +0200

    [backend] Issue/375 RBAC Capa hierarchy  (#375)

--- openbas-model/src/main/java/io/openbas/database/model/Capability.java
+++ openbas-model/src/main/java/io/openbas/database/model/Capability.java
@@ -7,72 +7,78 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
+import lombok.Getter;
 
 public enum Capability {
 
   // Superuser
-  BYPASS(pair(null, null)),
+  BYPASS(null, pair(null, null)),
 
   // Atomic Testing
-  ACCESS_ATOMIC_TESTING(pair(ResourceType.ATOMIC_TESTING, Action.READ)),
-  MANAGE_ATOMIC_TESTING(pair(ResourceType.ATOMIC_TESTING, Action.WRITE)),
-  DELETE_ATOMIC_TESTING(pair(ResourceType.ATOMIC_TESTING, Action.DELETE)),
-  LAUNCH_ATOMIC_TESTING(pair(ResourceType.ATOMIC_TESTING, Action.LAUNCH)),
+  ACCESS_ATOMIC_TESTING(null, pair(ResourceType.ATOMIC_TESTING, Action.READ)),
+  MANAGE_ATOMIC_TESTING(ACCESS_ATOMIC_TESTING, pair(ResourceType.ATOMIC_TESTING, Action.WRITE)),
+  DELETE_ATOMIC_TESTING(MANAGE_ATOMIC_TESTING, pair(ResourceType.ATOMIC_TESTING, Action.DELETE)),
+  LAUNCH_ATOMIC_TESTING(MANAGE_ATOMIC_TESTING, pair(ResourceType.ATOMIC_TESTING, Action.LAUNCH)),
 
   // Teams & Players
   MANAGE_TEAMS_AND_PLAYERS(
-      pair(ResourceType.TEAM, Action.WRITE), pair(ResourceType.PLAYER, Action.WRITE)),
+      null, pair(ResourceType.TEAM, Action.WRITE), pair(ResourceType.PLAYER, Action.WRITE)),
   DELETE_TEAMS_AND_PLAYERS(
-      pair(ResourceType.TEAM, Action.DELETE), pair(ResourceType.PLAYER, Action.DELETE)),
+      MANAGE_TEAMS_AND_PLAYERS,
+      pair(ResourceType.TEAM, Action.DELETE),
+      pair(ResourceType.PLAYER, Action.DELETE)),
 
   // Assets (Endpoints, Groups)
-  ACCESS_ASSETS(pair(ResourceType.ASSET, Action.READ)),
-  MANAGE_ASSETS(pair(ResourceType.ASSET, Action.WRITE)),
-  DELETE_ASSETS(pair(ResourceType.ASSET, Action.DELETE)),
+  ACCESS_ASSETS(null, pair(ResourceType.ASSET, Action.READ)),
+  MANAGE_ASSETS(ACCESS_ASSETS, pair(ResourceType.ASSET, Action.WRITE)),
+  DELETE_ASSETS(MANAGE_ASSETS, pair(ResourceType.ASSET, Action.DELETE)),
 
   // Payloads
-  ACCESS_PAYLOADS(pair(ResourceType.PAYLOAD, Action.READ)),
-  MANAGE_PAYLOADS(pair(ResourceType.PAYLOAD, Action.WRITE)),
-  DELETE_PAYLOADS(pair(ResourceType.PAYLOAD, Action.DELETE)),
+  ACCESS_PAYLOADS(null, pair(ResourceType.PAYLOAD, Action.READ)),
+  MANAGE_PAYLOADS(ACCESS_PAYLOADS, pair(ResourceType.PAYLOAD, Action.WRITE)),
+  DELETE_PAYLOADS(MANAGE_PAYLOADS, pair(ResourceType.PAYLOAD, Action.DELETE)),
 
   // Dashboards
-  ACCESS_DASHBOARDS(pair(ResourceType.DASHBOARD, Action.READ)),
-  MANAGE_DASHBOARDS(pair(ResourceType.DASHBOARD, Action.WRITE)),
-  DELETE_DASHBOARDS(pair(ResourceType.DASHBOARD, Action.DELETE)),
+  ACCESS_DASHBOARDS(null, pair(ResourceType.DASHBOARD, Action.READ)),
+  MANAGE_DASHBOARDS(ACCESS_DASHBOARDS, pair(ResourceType.DASHBOARD, Action.WRITE)),
+  DELETE_DASHBOARDS(MANAGE_DASHBOARDS, pair(ResourceType.DASHBOARD, Action.DELETE)),
 
   // Findings
-  ACCESS_FINDINGS(pair(ResourceType.FINDING, Action.READ)),
-  MANAGE_FINDINGS(pair(ResourceType.FINDING, Action.WRITE)),
-  DELETE_FINDINGS(pair(ResourceType.FINDING, Action.DELETE)),
+  ACCESS_FINDINGS(null, pair(ResourceType.FINDING, Action.READ)),
+  MANAGE_FINDINGS(ACCESS_FINDINGS, pair(ResourceType.FINDING, Action.WRITE)),
+  DELETE_FINDINGS(MANAGE_FINDINGS, pair(ResourceType.FINDING, Action.DELETE)),
 
   // Documents
-  ACCESS_DOCUMENTS(pair(ResourceType.DOCUMENT, Action.READ)),
-  MANAGE_DOCUMENTS(pair(ResourceType.DOCUMENT, Action.WRITE)),
-  DELETE_DOCUMENTS(pair(ResourceType.DOCUMENT, Action.DELETE)),
+  ACCESS_DOCUMENTS(null, pair(ResourceType.DOCUMENT, Action.READ)),
+  MANAGE_DOCUMENTS(ACCESS_DOCUMENTS, pair(ResourceType.DOCUMENT, Action.WRITE)),
+  DELETE_DOCUMENTS(MANAGE_DOCUMENTS, pair(ResourceType.DOCUMENT, Action.DELETE)),
 
   // Channels
-  ACCESS_CHANNELS(pair(ResourceType.CHANNEL, Action.READ)),
-  MANAGE_CHANNELS(pair(ResourceType.CHANNEL, Action.WRITE)),
-  DELETE_CHANNELS(pair(ResourceType.CHANNEL, Action.DELETE)),
+  ACCESS_CHANNELS(null, pair(ResourceType.CHANNEL, Action.READ)),
+  MANAGE_CHANNELS(ACCESS_CHANNELS, pair(ResourceType.CHANNEL, Action.WRITE)),
+  DELETE_CHANNELS(MANAGE_CHANNELS, pair(ResourceType.CHANNEL, Action.DELETE)),
 
   // Challenges
-  ACCESS_CHALLENGES(pair(ResourceType.CHALLENGE, Action.READ)),
-  MANAGE_CHALLENGES(pair(ResourceType.CHALLENGE, Action.WRITE)),
-  DELETE_CHALLENGES(pair(ResourceType.CHALLENGE, Action.DELETE)),
+  ACCESS_CHALLENGES(null, pair(ResourceType.CHALLENGE, Action.READ)),
+  MANAGE_CHALLENGES(ACCESS_CHALLENGES, pair(ResourceType.CHALLENGE, Action.WRITE)),
+  DELETE_CHALLENGES(MANAGE_CHALLENGES, pair(ResourceType.CHALLENGE, Action.DELETE)),
 
   // Lessons Learned
-  ACCESS_LESSONS_LEARNED(pair(ResourceType.LESSON_LEARNED, Action.READ)),
-  MANAGE_LESSONS_LEARNED(pair(ResourceType.LESSON_LEARNED, Action.WRITE)),
-  DELETE_LESSONS_LEARNED(pair(ResourceType.LESSON_LEARNED, Action.DELETE)),
+  ACCESS_LESSONS_LEARNED(null, pair(ResourceType.LESSON_LEARNED, Action.READ)),
+  MANAGE_LESSONS_LEARNED(ACCESS_LESSONS_LEARNED, pair(ResourceType.LESSON_LEARNED, Action.WRITE)),
+  DELETE_LESSONS_LEARNED(MANAGE_LESSONS_LEARNED, pair(ResourceType.LESSON_LEARNED, Action.DELETE)),
 
   // Security Platforms
-  ACCESS_SECURITY_PLATFORMS(pair(ResourceType.SECURITY_PLATFORM, Action.READ)),
-  MANAGE_SECURITY_PLATFORMS(pair(ResourceType.SECURITY_PLATFORM, Action.WRITE)),
-  DELETE_SECURITY_PLATFORMS(pair(ResourceType.SECURITY_PLATFORM, Action.DELETE)),
+  ACCESS_SECURITY_PLATFORMS(null, pair(ResourceType.SECURITY_PLATFORM, Action.READ)),
+  MANAGE_SECURITY_PLATFORMS(
+      ACCESS_SECURITY_PLATFORMS, pair(ResourceType.SECURITY_PLATFORM, Action.WRITE)),
+  DELETE_SECURITY_PLATFORMS(
+      MANAGE_SECURITY_PLATFORMS, pair(ResourceType.SECURITY_PLATFORM, Action.DELETE)),
 
   // Platform Settings
-  ACCESS_PLATFORM_SETTINGS(pair(ResourceType.PLATFORM_SETTING, Action.READ)),
-  MANAGE_PLATFORM_SETTINGS(pair(ResourceType.PLATFORM_SETTING, Action.WRITE));
+  ACCESS_PLATFORM_SETTINGS(null, pair(ResourceType.PLATFORM_SETTING, Action.READ)),
+  MANAGE_PLATFORM_SETTINGS(
+      ACCESS_PLATFORM_SETTINGS, pair(ResourceType.PLATFORM_SETTING, Action.WRITE));
 
   private record ResourceTypeActionPair(ResourceType resource, Action action) {}
 
@@ -82,7 +88,10 @@ public enum Capability {
 
   private final Set<ResourceTypeActionPair> pairs;
 
-  Capability(ResourceTypeActionPair... pairs) {
+  @Getter private final Capability parent;
+
+  Capability(Capability parent, ResourceTypeActionPair... pairs) {
+    this.parent = parent;
     this.pairs = Set.of(pairs);
   }
 
