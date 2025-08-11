ef8c1b7cab619a8776626140665a14fad97e45d0
Damien Goujard <damien.goujard@filigran.io>
2025-08-11 09:12:29 +0200

    [backend] Fix migration

--- openbas-api/src/main/java/io/openbas/migration/V4_16__Force_es_reindex_injects.java
+++ openbas-api/src/main/java/io/openbas/migration/V4_16__Force_es_reindex_injects.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V4_14__Force_es_reindex_injects extends BaseJavaMigration {
+public class V4_16__Force_es_reindex_injects extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
