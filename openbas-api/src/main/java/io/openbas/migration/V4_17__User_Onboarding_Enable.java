581f2a31a1f25d0a88283ade85bdb4d1fb28c2bc
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-08-14 11:08:37 +0200

    [backend] Change migration number

--- openbas-api/src/main/java/io/openbas/migration/V4_17__User_Onboarding_Enable.java
+++ openbas-api/src/main/java/io/openbas/migration/V4_17__User_Onboarding_Enable.java
@@ -7,7 +7,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V4_16__User_Onboarding_Enable extends BaseJavaMigration {
+public class V4_17__User_Onboarding_Enable extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
