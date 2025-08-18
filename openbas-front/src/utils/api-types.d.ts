4858fee3981be0146a2289d57090d8b2c61f2ce8
Damien Goujard <damien.goujard@filigran.io>
2025-08-18 16:12:38 +0200

    [backend/frontend] Add execution date for widget list

--- openbas-front/src/utils/api-types.d.ts
+++ openbas-front/src/utils/api-types.d.ts
@@ -1760,6 +1760,8 @@ export interface EsInject {
   base_teams_side?: string[];
   /** @format date-time */
   base_updated_at?: string;
+  /** @format date-time */
+  inject_execution_date?: string;
   inject_status?: string;
   inject_title?: string;
 }
