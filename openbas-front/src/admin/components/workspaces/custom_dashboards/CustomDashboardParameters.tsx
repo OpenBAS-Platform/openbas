bca02417b1abf53f1fede3bb394b3c1f66335ff2
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-08-19 18:13:33 +0200

    Update openbas-front/src/admin/components/workspaces/custom_dashboards/CustomDashboardParameters.tsx

--- openbas-front/src/admin/components/workspaces/custom_dashboards/CustomDashboardParameters.tsx
+++ openbas-front/src/admin/components/workspaces/custom_dashboards/CustomDashboardParameters.tsx
@@ -53,7 +53,7 @@ const CustomDashboardParameters: FunctionComponent = () => {
               </div>
             );
           default:
-            return (<></>);
+            return null;
         }
       })}
     </div>
