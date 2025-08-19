090536b194194615408a356edb0d09c6027a7226
Gael Leblan <gael.leblan@filigran.io>
2025-08-19 15:25:22 +0200

    [backend] Adding possibility to provide instance ID (#3644)

---
+++ openbas-api/src/test/java/io/openbas/AppTest.java
@@ -0,0 +1,80 @@
+package io.openbas;
+
+import static io.openbas.database.model.SettingKeys.PLATFORM_INSTANCE;
+import static org.junit.jupiter.api.Assertions.*;
+
+import io.openbas.database.repository.SettingRepository;
+import java.util.UUID;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+class AppTest extends IntegrationTest {
+
+  @Autowired private SettingRepository settingRepository;
+
+  @BeforeAll
+  static void setup() {
+    // Setting another port we're going to launch new instances of the application
+    System.setProperty("server.port", "8089");
+  }
+
+  @DisplayName("Should throw an exception when having an incorrect instance id")
+  @Test
+  void shouldThrowExceptionWithInvalidInstanceId() {
+    System.setProperty("openbas.instance-id", "pouet");
+    settingRepository
+        .findByKey(PLATFORM_INSTANCE.key())
+        .ifPresent(setting -> settingRepository.delete(setting));
+    ConfigurableApplicationContext context = null;
+    try {
+      context = SpringApplication.run(App.class);
+    } catch (Exception e) {
+      assertEquals("Invalid UUID string: pouet", e.getCause().getMessage());
+      return;
+    } finally {
+      if (context != null) {
+        context.close();
+      }
+    }
+    fail();
+  }
+
+  @DisplayName("Should update the instance id with the one in the properties")
+  @Test
+  void shouldUpdateInstanceIdWithProperty() {
+    String uuid = UUID.randomUUID().toString();
+    System.setProperty("openbas.instance-id", uuid);
+    ConfigurableApplicationContext context = SpringApplication.run(App.class);
+    assertTrue(settingRepository.findByKey(PLATFORM_INSTANCE.key()).isPresent());
+    assertEquals(uuid, settingRepository.findByKey(PLATFORM_INSTANCE.key()).get().getValue());
+    context.close();
+  }
+
+  @DisplayName("Should update the instance id when there is none in db")
+  @Test
+  void shouldUpdateInstanceIdWhenNoneInDb() {
+    settingRepository
+        .findByKey(PLATFORM_INSTANCE.key())
+        .ifPresent(setting -> settingRepository.delete(setting));
+    ConfigurableApplicationContext context = SpringApplication.run(App.class);
+    assertTrue(settingRepository.findByKey(PLATFORM_INSTANCE.key()).isPresent());
+    assertNotNull(settingRepository.findByKey(PLATFORM_INSTANCE.key()).get().getValue());
+    context.close();
+  }
+
+  @DisplayName(
+      "Should update the instance id when there is none in db and the specified one is blank")
+  @Test
+  void shouldUpdateInstanceIdWhenNoneInDbAndSpecifiedOneIsBlank() {
+    System.setProperty("openbas.instance-id", "");
+    settingRepository
+        .findByKey(PLATFORM_INSTANCE.key())
+        .ifPresent(setting -> settingRepository.delete(setting));
+    ConfigurableApplicationContext context = SpringApplication.run(App.class);
+    assertTrue(settingRepository.findByKey(PLATFORM_INSTANCE.key()).isPresent());
+    assertNotNull(settingRepository.findByKey(PLATFORM_INSTANCE.key()).get().getValue());
+    context.close();
+  }
+}
