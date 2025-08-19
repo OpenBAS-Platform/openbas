dbe352a82570399e3b4ff6e1222058e11924235c
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-08-19 15:46:34 +0200

    [backend] Add tests for stix import

--- openbas-api/src/main/java/io/openbas/rest/StixApi.java
+++ openbas-api/src/main/java/io/openbas/rest/StixApi.java
@@ -3,6 +3,7 @@ package io.openbas.rest;
 import io.openbas.rest.helper.RestBehavior;
 import io.openbas.service.StixService;
 import io.openbas.stix.parsing.ParsingException;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -10,13 +11,14 @@ import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/stix")
 @RequiredArgsConstructor
+@Tag(name = "STIX API", description = "Operations related to STIX bundles")
 public class StixApi extends RestBehavior {
+
+  public static final String STIX_API = "/api/stix";
 
   private final StixService stixService;
 
