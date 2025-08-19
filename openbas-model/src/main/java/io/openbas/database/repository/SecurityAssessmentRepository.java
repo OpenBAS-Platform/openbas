ddd73a0bd2874200d94a45ef560a4f3c2414e44b
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-08-19 15:04:15 +0200

    [backend] Add stix model

--- openbas-model/src/main/java/io/openbas/database/repository/SecurityAssessmentRepository.java
+++ openbas-model/src/main/java/io/openbas/database/repository/SecurityAssessmentRepository.java
@@ -1,16 +1,15 @@
 package io.openbas.database.repository;
 
 import io.openbas.database.model.SecurityAssessment;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface SecurityAssessmentRepository
     extends CrudRepository<SecurityAssessment, String>,
-    JpaSpecificationExecutor<SecurityAssessment> {
+        JpaSpecificationExecutor<SecurityAssessment> {
 
   Optional<SecurityAssessment> findByExternalId(String id);
 }
