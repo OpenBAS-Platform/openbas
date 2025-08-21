2b787c8e3db44bb9ade6d243481a1ef2eb1df7e9
Yann <yann.pauly@filigran.io>
2025-08-21 17:04:28 +0200

    [backend] feat: RBAC on exports

--- openbas-model/src/main/java/io/openbas/database/model/Grantable.java
+++ openbas-model/src/main/java/io/openbas/database/model/Grantable.java
@@ -5,6 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to mark a resource type that can be granted permissions. This is used to link a class
+ * to an RBAC ResourceType and to specify on which Grant field the link is done. For example, for
+ * the Scenario class, the annotation would be: @Grantable(grantFieldName = "scenario"). Where
+ * "scenario" is the name of the field in the Grant entity that corresponds to this resource type.
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Grantable {
