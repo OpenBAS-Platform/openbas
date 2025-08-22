8b2197dc9c3cb6a3deef9e7d64a5696cfb5bb140
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-08-22 16:09:44 +0200

    [frontend] Fix lint

--- openbas-front/src/admin/components/workspaces/custom_dashboards/CustomDashboard.tsx
+++ openbas-front/src/admin/components/workspaces/custom_dashboards/CustomDashboard.tsx
@@ -7,11 +7,9 @@ import RGL, { type Layout, WidthProvider } from 'react-grid-layout';
 import { updateCustomDashboardWidgetLayout } from '../../../../actions/custom_dashboards/customdashboardwidget-action';
 import { ErrorBoundary } from '../../../../components/Error';
 import { useFormatter } from '../../../../components/i18n';
-import Loader from '../../../../components/Loader';
 import { type Widget } from '../../../../utils/api-types-custom';
 import { CustomDashboardContext } from './CustomDashboardContext';
 import CustomDashboardHeader from './CustomDashboardHeader';
-import { LAST_QUARTER_TIME_RANGE } from './widgets/configuration/common/TimeRangeUtils';
 import WidgetCreation from './widgets/WidgetCreation';
 import WidgetPopover from './widgets/WidgetPopover';
 import { getWidgetTitle } from './widgets/WidgetUtils';
@@ -23,25 +21,14 @@ const CustomDashboardComponent: FunctionComponent<{ readOnly: boolean }> = ({ re
   const { t } = useFormatter();
   const ReactGridLayout = useMemo(() => WidthProvider(RGL), []);
   const [fullscreenWidgets, setFullscreenWidgets] = useState<Record<Widget['widget_id'], boolean | never>>({});
-  const [loading, setLoading] = useState(true);
-  const { customDashboard, setCustomDashboard, customDashboardParameters, setCustomDashboardParameters } = useContext(CustomDashboardContext);
+
+  const { customDashboard, setCustomDashboard } = useContext(CustomDashboardContext);
 
   const [idToResize, setIdToResize] = useState<string | null>(null);
   const handleResize = (updatedWidget: string | null) => setIdToResize(updatedWidget);
 
   useEffect(() => {
     window.dispatchEvent(new Event('resize'));
-    const params: Record<string, string> = {};
-    customDashboard?.custom_dashboard_parameters?.forEach((p: {
-      custom_dashboards_parameter_type: string;
-      custom_dashboards_parameter_id: string;
-    }) => {
-    });
-    setCustomDashboardParameters(prev => ({
-      ...prev,
-      ...params,
-    }));
-    setLoading(false);
   }, [customDashboard]);
 
   const handleWidgetUpdate = (widget: Widget) => {
@@ -101,106 +88,106 @@ const CustomDashboardComponent: FunctionComponent<{ readOnly: boolean }> = ({ re
 
   // Not perfect to use negative margin here, but I don't find a better solution to align items
   return (
-          <div id="container" style={{ margin: theme.spacing(-2.5) }}>
-            <ReactGridLayout
-              className="layout"
-              margin={[20, 20]}
-              rowHeight={50}
-              cols={12}
-              draggableCancel=".noDrag"
-              isDraggable={!readOnly}
-              isResizable={!readOnly}
-              onLayoutChange={onLayoutChange}
-              onResizeStart={(_, { i }) => handleResize(i)}
-              onResizeStop={() => handleResize(null)}
-            >
-              {customDashboard?.custom_dashboard_widgets?.map((widget) => {
-                const layout = {
-                  i: widget.widget_id,
-                  x: widget.widget_layout?.widget_layout_x,
-                  y: widget.widget_layout?.widget_layout_y,
-                  w: widget.widget_layout?.widget_layout_w,
-                  h: widget.widget_layout?.widget_layout_h,
-                };
-                const setFullscreen = (fullscreen: boolean) => setFullscreenWidgets({
-                  ...fullscreenWidgets,
-                  [widget.widget_id]: fullscreen,
-                });
-                return (
-                  <Paper
-                    key={widget.widget_id}
-                    data-grid={layout}
-                    style={{
-                      borderRadius: 4,
-                      display: 'flex',
-                      flexDirection: 'column',
+      <div id="container" style={{ margin: theme.spacing(-2.5) }}>
+        <ReactGridLayout
+          className="layout"
+          margin={[20, 20]}
+          rowHeight={50}
+          cols={12}
+          draggableCancel=".noDrag"
+          isDraggable={!readOnly}
+          isResizable={!readOnly}
+          onLayoutChange={onLayoutChange}
+          onResizeStart={(_, { i }) => handleResize(i)}
+          onResizeStop={() => handleResize(null)}
+        >
+          {customDashboard?.custom_dashboard_widgets?.map((widget) => {
+            const layout = {
+              i: widget.widget_id,
+              x: widget.widget_layout?.widget_layout_x,
+              y: widget.widget_layout?.widget_layout_y,
+              w: widget.widget_layout?.widget_layout_w,
+              h: widget.widget_layout?.widget_layout_h,
+            };
+            const setFullscreen = (fullscreen: boolean) => setFullscreenWidgets({
+              ...fullscreenWidgets,
+              [widget.widget_id]: fullscreen,
+            });
+            return (
+              <Paper
+                key={widget.widget_id}
+                data-grid={layout}
+                style={{
+                  borderRadius: 4,
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}
+                variant="outlined"
+              >
+                <Box
+                  display="flex"
+                  flexDirection="row"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Typography
+                    variant="h4"
+                    sx={{
+                      margin: 0,
+                      paddingLeft: theme.spacing(2),
+                      paddingTop: theme.spacing(2.5),
+                      textTransform: 'uppercase',
                     }}
-                    variant="outlined"
                   >
+                    {getWidgetTitle(widget.widget_config.title, widget.widget_type, t)}
+                  </Typography>
+                  <Box
+                    display="flex"
+                    flexDirection="row"
+                  >
+                    {widget.widget_type === 'security-coverage' && (
+                      <IconButton
+                        color="primary"
+                        className="noDrag"
+                        onClick={() => setFullscreen(true)}
+                        size="small"
+                      >
+                        <OpenInFullOutlined fontSize="small" />
+                      </IconButton>
+                    )}
+                    {!readOnly && (
+                      <WidgetPopover
+                        className="noDrag"
+                        customDashboardId={customDashboard.custom_dashboard_id}
+                        widget={widget}
+                        onUpdate={widget => handleWidgetUpdate(widget)}
+                        onDelete={widgetId => handleWidgetDelete(widgetId)}
+                      />
+                    )}
+                  </Box>
+                </Box>
+                <ErrorBoundary>
+                  {widget.widget_id === idToResize ? (<div />) : (
                     <Box
+                      flex={1}
                       display="flex"
-                      flexDirection="row"
-                      justifyContent="space-between"
-                      alignItems="center"
+                      flexDirection="column"
+                      minHeight={0}
+                      padding={theme.spacing(1, 2, 2)}
+                      overflow={'number' === widget.widget_type ? 'hidden' : 'auto'}
                     >
-                      <Typography
-                        variant="h4"
-                        sx={{
-                          margin: 0,
-                          paddingLeft: theme.spacing(2),
-                          paddingTop: theme.spacing(2.5),
-                          textTransform: 'uppercase',
-                        }}
-                      >
-                        {getWidgetTitle(widget.widget_config.title, widget.widget_type, t)}
-                      </Typography>
-                      <Box
-                        display="flex"
-                        flexDirection="row"
-                      >
-                        {widget.widget_type === 'security-coverage' && (
-                          <IconButton
-                            color="primary"
-                            className="noDrag"
-                            onClick={() => setFullscreen(true)}
-                            size="small"
-                          >
-                            <OpenInFullOutlined fontSize="small" />
-                          </IconButton>
-                        )}
-                        {!readOnly && (
-                          <WidgetPopover
-                            className="noDrag"
-                            customDashboardId={customDashboard.custom_dashboard_id}
-                            widget={widget}
-                            onUpdate={widget => handleWidgetUpdate(widget)}
-                            onDelete={widgetId => handleWidgetDelete(widgetId)}
-                          />
-                        )}
-                      </Box>
+                      <WidgetViz
+                        widget={widget}
+                        fullscreen={fullscreenWidgets[widget.widget_id]}
+                        setFullscreen={setFullscreen}
+                      />
                     </Box>
-                    <ErrorBoundary>
-                      {widget.widget_id === idToResize ? (<div />) : (
-                        <Box
-                          flex={1}
-                          display="flex"
-                          flexDirection="column"
-                          minHeight={0}
-                          padding={theme.spacing(1, 2, 2)}
-                          overflow={'number' === widget.widget_type ? 'hidden' : 'auto'}
-                        >
-                          <WidgetViz
-                            widget={widget}
-                            fullscreen={fullscreenWidgets[widget.widget_id]}
-                            setFullscreen={setFullscreen}
-                          />
-                        </Box>
-                      )}
-                    </ErrorBoundary>
-                  </Paper>
-                );
-              })}
-            </ReactGridLayout>
+                  )}
+                </ErrorBoundary>
+              </Paper>
+            );
+          })}
+        </ReactGridLayout>
     </div>
   );
 };
