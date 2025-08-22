39955f0b2ef20bddc976db740e9d72022b694636
Damien Goujard <damien.goujard@filigran.io>
2025-08-22 12:02:44 +0200

    [backend] Add tests for Security Platform Api

---
+++ openbas-api/src/test/java/io/openbas/rest/SecurityPlatformApiTest.java
@@ -0,0 +1,121 @@
+package io.openbas.rest;
+
+import static io.openbas.rest.asset.security_platforms.SecurityPlatformApi.SECURITY_PLATFORM_URI;
+import static io.openbas.utils.JsonUtils.asJsonString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import io.openbas.IntegrationTest;
+import io.openbas.database.model.SecurityPlatform;
+import io.openbas.database.repository.SecurityPlatformRepository;
+import io.openbas.utils.mockUser.WithMockAdminUser;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import org.json.JSONArray;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@TestInstance(PER_CLASS)
+@Transactional
+class SecurityPlatformApiTest extends IntegrationTest {
+
+  private static final String SECURITY_PLATFORM_NAME = "My Security Platform ";
+
+  @Autowired private MockMvc mvc;
+  @Autowired private SecurityPlatformRepository securityPlatformRepository;
+
+  // Options tests
+
+  private List<SecurityPlatform> prepareOptionsSecurityPlatformTestData() {
+    SecurityPlatform securityPlatformTest1 = new SecurityPlatform();
+    securityPlatformTest1.setName(SECURITY_PLATFORM_NAME + "1");
+    securityPlatformTest1.setSecurityPlatformType(SecurityPlatform.SECURITY_PLATFORM_TYPE.SIEM);
+    SecurityPlatform securityPlatform1 =
+        this.securityPlatformRepository.save(securityPlatformTest1);
+    SecurityPlatform securityPlatformTest2 = new SecurityPlatform();
+    securityPlatformTest2.setName(SECURITY_PLATFORM_NAME + "2");
+    securityPlatformTest2.setSecurityPlatformType(SecurityPlatform.SECURITY_PLATFORM_TYPE.SIEM);
+    SecurityPlatform securityPlatform2 =
+        this.securityPlatformRepository.save(securityPlatformTest2);
+    return List.of(securityPlatform1, securityPlatform2);
+  }
+
+  Stream<Arguments> optionsByNameTestParameters() {
+    return Stream.of(
+        Arguments.of("toto", 0),
+        Arguments.of(SECURITY_PLATFORM_NAME, 2),
+        Arguments.of(SECURITY_PLATFORM_NAME + "1", 1));
+  }
+
+  @DisplayName("Test optionsByName")
+  @ParameterizedTest
+  @MethodSource("optionsByNameTestParameters")
+  @WithMockAdminUser
+  void optionsByNameTest(String searchText, Integer expectedNumberOfResults) throws Exception {
+    // --PREPARE--
+    prepareOptionsSecurityPlatformTestData();
+
+    // --EXECUTE--;
+    String response =
+        mvc.perform(
+                get(SECURITY_PLATFORM_URI + "/options")
+                    .queryParam("searchText", searchText)
+                    .accept(MediaType.APPLICATION_JSON))
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+    JSONArray jsonArray = new JSONArray(response);
+
+    // --ASSERT--
+    assertEquals(expectedNumberOfResults, jsonArray.length());
+  }
+
+  Stream<Arguments> optionsByIdTestParameters() {
+    return Stream.of(
+        Arguments.of(0, 0), // Case 1: 0 ID given
+        Arguments.of(1, 1), // Case 1: 1 ID given
+        Arguments.of(2, 2) // Case 2: 2 IDs given
+        );
+  }
+
+  @DisplayName("Test optionsById")
+  @ParameterizedTest
+  @MethodSource("optionsByIdTestParameters")
+  @WithMockAdminUser
+  void optionsByIdTest(Integer numberOfSecurityPlatformToProvide, Integer expectedNumberOfResults)
+      throws Exception {
+    List<SecurityPlatform> securityPlatforms = prepareOptionsSecurityPlatformTestData();
+
+    List<String> securityPlatformIdsToSearch = new ArrayList<>();
+    for (int i = 0; i < numberOfSecurityPlatformToProvide; i++) {
+      securityPlatformIdsToSearch.add(securityPlatforms.get(i).getId());
+    }
+
+    // --EXECUTE--;
+    String response =
+        mvc.perform(
+                post(SECURITY_PLATFORM_URI + "/options")
+                    .content(asJsonString(securityPlatformIdsToSearch))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+    JSONArray jsonArray = new JSONArray(response);
+
+    // --ASSERT--
+    assertEquals(expectedNumberOfResults, jsonArray.length());
+  }
+}
