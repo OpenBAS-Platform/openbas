cca1887833d39ea8819565ccc3e2afca3d49a49a
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-08-25 10:08:02 +0200

    [backend] Clean code

--- openbas-model/src/main/java/io/openbas/stix/objects/ObjectBase.java
+++ openbas-model/src/main/java/io/openbas/stix/objects/ObjectBase.java
@@ -3,9 +3,12 @@ package io.openbas.stix.objects;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.openbas.stix.parsing.ParsingException;
 import io.openbas.stix.parsing.StixSerialisable;
 import io.openbas.stix.types.BaseType;
+import java.time.Instant;
 import java.util.Map;
+import java.util.function.Consumer;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,5 +30,31 @@ public class ObjectBase implements StixSerialisable {
       node.set(entry.getKey(), entry.getValue().toStix(mapper));
     }
     return node;
+  }
+
+  public String getRequiredProperty(String propName) throws ParsingException {
+    if (!this.hasProperty(propName) || this.getProperty(propName).getValue() == null) {
+      throw new ParsingException("Missing required property: " + propName);
+    }
+    return this.getProperty(propName).getValue().toString();
+  }
+
+  public String getOptionalProperty(String propName, String defaultValue) {
+    if (this.hasProperty(propName) && this.getProperty(propName).getValue() != null) {
+      return this.getProperty(propName).getValue().toString();
+    }
+    return defaultValue;
+  }
+
+  public void setIfPresent(String propName, Consumer<String> setter) {
+    if (this.hasProperty(propName) && this.getProperty(propName).getValue() != null) {
+      setter.accept(this.getProperty(propName).getValue().toString());
+    }
+  }
+
+  public void setInstantIfPresent(String propName, Consumer<Instant> setter) {
+    if (this.hasProperty(propName) && this.getProperty(propName).getValue() != null) {
+      setter.accept(Instant.parse(this.getProperty(propName).getValue().toString()));
+    }
   }
 }
