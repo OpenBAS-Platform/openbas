0216111d610db2a9141b8ad43de4ff2ce6e56107
Hedi Tarchouni <hedi.tarchouni@filigran.io>
2025-08-27 15:14:19 +0200

    [backend] check if the user has the capa before loading options

--- openbas-front/src/components/common/queryable/filter/useRetrieveOptions.tsx
+++ openbas-front/src/components/common/queryable/filter/useRetrieveOptions.tsx
@@ -1,5 +1,5 @@
 import type { AxiosResponse } from 'axios';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 
 import { searchAssetGroupByIdAsOption } from '../../../../actions/asset_groups/assetgroup-action';
 import { searchEndpointByIdAsOption } from '../../../../actions/assets/endpoint-actions';
@@ -15,10 +15,13 @@ import { searchSimulationByIdAsOptions } from '../../../../actions/simulations/s
 import { searchTagByIdAsOption } from '../../../../actions/tags/tag-action';
 import { searchTeamByIdAsOption } from '../../../../actions/teams/team-actions';
 import { type GroupOption, type Option } from '../../../../utils/Option';
+import { AbilityContext } from '../../../../utils/permissions/PermissionsProvider';
+import { ACTIONS, SUBJECTS } from '../../../../utils/permissions/types';
 import { CUSTOM_DASHBOARD, SCENARIOS, SIMULATIONS } from './constants';
 
 const useRetrieveOptions = () => {
   const [options, setOptions] = useState<Option[]>([]);
+  const ability = useContext(AbilityContext);
 
   const handleOptions = (response: AxiosResponse<GroupOption[] | Option[]>, filterDefaultValues: GroupOption[]) => {
     if (filterDefaultValues && filterDefaultValues.length > 0) {
@@ -33,6 +36,7 @@ const useRetrieveOptions = () => {
 
   const searchOptions = (filterKey: string, ids: string[], defaultValues: GroupOption[] = []) => {
     const filterDefaultValues = defaultValues.filter(v => ids.includes(v.id));
+
     switch (filterKey) {
       case SIMULATIONS:
       case 'base_simulation_side':
@@ -62,16 +66,25 @@ const useRetrieveOptions = () => {
         });
         break;
       case 'target_asset_groups':
-        searchTargetOptionsById('ASSETS_GROUPS', ids).then((response) => {
-          setOptions(response.data);
-        });
+        // TODO allow to fetch for a specific resource if no capa issue/3864
+        if (ability.can(ACTIONS.ACCESS, SUBJECTS.ASSETS)) {
+          searchTargetOptionsById('ASSETS_GROUPS', ids).then((response) => {
+            setOptions(response.data);
+          });
+        } else {
+          setOptions([]);
+        }
         break;
       case 'target_assets':
       case 'target_endpoint':
       case 'base_endpoint_side':
-        searchTargetOptionsById('ASSETS', ids).then((response) => {
-          setOptions(response.data);
-        });
+        if (ability.can(ACTIONS.ACCESS, SUBJECTS.ASSETS)) {
+          searchTargetOptionsById('ASSETS', ids).then((response) => {
+            setOptions(response.data);
+          });
+        } else {
+          setOptions([]);
+        }
         break;
       case 'target_teams':
         searchTargetOptionsById('TEAMS', ids).then((response) => {
@@ -96,16 +109,24 @@ const useRetrieveOptions = () => {
       case 'finding_asset_groups':
       case 'inject_asset_groups':
       case 'base_asset_groups_side':
-        searchAssetGroupByIdAsOption(ids).then((response) => {
-          setOptions(response.data);
-        });
+        if (ability.can(ACTIONS.ACCESS, SUBJECTS.ASSETS)) {
+          searchAssetGroupByIdAsOption(ids).then((response) => {
+            setOptions(response.data);
+          });
+        } else {
+          setOptions([]);
+        }
         break;
       case 'finding_assets':
       case 'inject_assets':
       case 'base_assets_side':
-        searchEndpointByIdAsOption(ids).then((response) => {
-          setOptions(response.data);
-        });
+        if (ability.can(ACTIONS.ACCESS, SUBJECTS.ASSETS)) {
+          searchEndpointByIdAsOption(ids).then((response) => {
+            setOptions(response.data);
+          });
+        } else {
+          setOptions([]);
+        }
         break;
       case 'inject_teams':
       case 'base_teams_side':
@@ -137,9 +158,13 @@ const useRetrieveOptions = () => {
         });
         break;
       case CUSTOM_DASHBOARD:
-        searchCustomDashboardByIdAsOptions(ids).then((response) => {
-          setOptions(response.data);
-        });
+        if (ability.can(ACTIONS.ACCESS, SUBJECTS.DASHBOARDS)) {
+          searchCustomDashboardByIdAsOptions(ids).then((response) => {
+            setOptions(response.data);
+          });
+        } else {
+          setOptions([]);
+        }
         break;
       default:
         setOptions(ids.map(id => ({
