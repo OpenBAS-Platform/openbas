ec2a2927ee7e37242351d53761152b8a25da970b
Damien Goujard <damien.goujard@filigran.io>
2025-09-01 09:11:16 +0200

    [backend] Fix tests

--- openbas-api/src/main/java/io/openbas/migration/V4_21__Force_es_reindex.java
+++ openbas-api/src/main/java/io/openbas/migration/V4_21__Force_es_reindex.java
@@ -13,7 +13,7 @@ public class V4_21__Force_es_reindex extends BaseJavaMigration {
     try (Statement statement = context.getConnection().createStatement()) {
       // re-index in ES
       statement.executeUpdate(
-          "DELETE FROM indexing_status WHERE indexing_status_type in ('expectation-inject', 'simulation', 'scenario', 'endpoint', 'inject', 'vulnerable-endpoint', 'finding', 'security-platform');");
+          "DELETE FROM indexing_status WHERE indexing_status_type in ('expectation-inject', 'simulation', 'scenario', 'endpoint', 'inject', 'vulnerable-endpoint', 'finding', 'security-platform', 'tag');");
     }
   }
 }
