803e8dad0e76d79d9c0c8c6e7a5b7a46cb1c77cd
Hedi Tarchouni <hedi.tarchouni@filigran.io>
2025-09-01 17:43:35 +0200

    fix test

--- openbas-api/src/test/java/io/openbas/service/GrantServiceTest.java
+++ openbas-api/src/test/java/io/openbas/service/GrantServiceTest.java
@@ -1,8 +1,13 @@
 package io.openbas.service;
 
+import static io.openbas.database.model.Grant.GRANT_TYPE.LAUNCHER;
+import static io.openbas.database.model.Grant.GRANT_TYPE.OBSERVER;
+import static io.openbas.database.model.Grant.GRANT_TYPE.PLANNER;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import io.openbas.IntegrationTest;
@@ -10,6 +15,7 @@ import io.openbas.database.model.Grant;
 import io.openbas.database.model.User;
 import io.openbas.database.repository.GrantRepository;
 import io.openbas.utils.fixtures.UserFixture;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -29,7 +35,10 @@ public class GrantServiceTest extends IntegrationTest {
   public void test_hasReadGrant_WHEN_has_read_grant() {
     User user = UserFixture.getUser();
     user.setId(USER_ID);
-    when(grantRepository.existsByUserIdAndResourceIdAndNameIn(any(), any(), any()))
+    when(grantRepository.existsByUserIdAndResourceIdAndNameIn(
+            eq(RESOURCE_ID),
+            eq(USER_ID),
+            any()))
         .thenReturn(true);
 
     assertTrue(grantService.hasReadGrant(RESOURCE_ID, user));
