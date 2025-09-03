f3c005d7306fc8a000f9ad83b46c8636d43db14d
Guillaume Paris <guillaume.paris@filigran.io>
2025-09-03 09:14:49 +0200

    up migration

--- openbas-api/src/main/java/io/openbas/migration/V4_21__Rename_asset_group_contract.java
+++ openbas-api/src/main/java/io/openbas/migration/V4_21__Rename_asset_group_contract.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V4_16__Rename_asset_group_contract extends BaseJavaMigration {
+public class V4_21__Rename_asset_group_contract extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
