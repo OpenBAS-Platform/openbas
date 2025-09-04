2124464fb63efa35866e5caf871a8e297d89fe3a
Hedi Tarchouni <hedi.tarchouni@filigran.io>
2025-09-04 16:35:44 +0200

    lint

--- openbas-front/src/admin/components/scenarios/scenario/analysis/ScenarioAnalysisWrapper.tsx
+++ openbas-front/src/admin/components/scenarios/scenario/analysis/ScenarioAnalysisWrapper.tsx
@@ -9,14 +9,18 @@ import {
   seriesByScenario,
 } from '../../../../../actions/scenarios/scenario-actions';
 import { type CustomDashboard, type Scenario } from '../../../../../utils/api-types';
-import { CustomDashboardContext, type ParameterOption } from '../../../workspaces/custom_dashboards/CustomDashboardContext';
+import {
+  CustomDashboardContext,
+  type CustomDashboardContextType,
+  type ParameterOption
+} from '../../../workspaces/custom_dashboards/CustomDashboardContext';
 import ScenarioAnalysis from './ScenarioAnalysis';
 
 const ScenarioAnalysisWrapper = () => {
   const { scenarioId } = useParams() as { scenarioId: Scenario['scenario_id'] };
   const [customDashboardValue, setCustomDashboardValue] = useState<CustomDashboard>();
   const [parameters, setParameters] = useLocalStorage<Record<string, ParameterOption>>('custom-dashboard-scenario-' + scenarioId, Object.fromEntries(new Map()));
-  const contextValue = useMemo(() => ({
+  const contextValue: CustomDashboardContextType = useMemo(() => ({
     customDashboard: customDashboardValue,
     setCustomDashboard: setCustomDashboardValue,
     customDashboardParameters: parameters,
