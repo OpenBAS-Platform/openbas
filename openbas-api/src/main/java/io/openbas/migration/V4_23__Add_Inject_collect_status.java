30d76f6e5bef117255ba7817c42d5e1cd9c0efe9
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-09-05 16:45:27 +0200

    [backend] Migration

--- openbas-api/src/main/java/io/openbas/migration/V4_23__Add_Inject_collect_status.java
+++ openbas-api/src/main/java/io/openbas/migration/V4_23__Add_Inject_collect_status.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V4_22__Add_Inject_collect_status extends BaseJavaMigration {
+public class V4_23__Add_Inject_collect_status extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
