967e8da44e426623e59dbda43bbfcfcc62070702
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-09-05 10:04:54 +0200

    [backend] rename migration file

--- openbas-api/src/main/java/io/openbas/migration/V4_22__Add_octi_stix_objects.java
+++ openbas-api/src/main/java/io/openbas/migration/V4_22__Add_octi_stix_objects.java
@@ -7,7 +7,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V4_21__Add_octi_stix_objects extends BaseJavaMigration {
+public class V4_22__Add_octi_stix_objects extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
