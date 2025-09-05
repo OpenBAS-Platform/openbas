0b0b7e73725ad1f59bcba7ec1d761e659736da5e
Hedi Tarchouni <hedi.tarchouni@filigran.io>
2025-09-05 14:19:22 +0200

    fix lint

--- openbas-front/src/admin/components/workspaces/custom_dashboards/CustomDashboardContext.tsx
+++ openbas-front/src/admin/components/workspaces/custom_dashboards/CustomDashboardContext.tsx
@@ -2,7 +2,8 @@ import { createContext } from 'react';
 
 import { attackPaths, count, entities, series } from '../../../../actions/dashboards/dashboard-action';
 import { type SearchOptionsConfig } from '../../../../components/common/queryable/filter/useSearchOptions';
-import { type CustomDashboard } from '../../../../utils/api-types';
+import {type CustomDashboard, type EsAttackPath, EsBase, EsSeries} from '../../../../utils/api-types';
+import {AxiosResponse} from "axios";
 
 export interface ParameterOption {
   value: string;
@@ -15,14 +16,10 @@ export interface CustomDashboardContextType {
   setCustomDashboard: React.Dispatch<React.SetStateAction<CustomDashboard | undefined>>;
   customDashboardParameters: Record<string, ParameterOption>;
   setCustomDashboardParameters: React.Dispatch<React.SetStateAction<Record<string, ParameterOption>>>;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  fetchCount: (widgetId: string, params: Record<string, string | undefined>) => Promise<any>;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  fetchSeries: (widgetId: string, params: Record<string, string | undefined>) => Promise<any>;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  fetchEntities: (widgetId: string, params: Record<string, string | undefined>) => Promise<any>;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  fetchAttackPaths: (widgetId: string, params: Record<string, string | undefined>) => Promise<any>;
+  fetchCount: (widgetId: string, params: Record<string, string | undefined>) => Promise<AxiosResponse<number>>;
+  fetchSeries: (widgetId: string, params: Record<string, string | undefined>) => Promise<AxiosResponse<EsSeries[]>>;
+  fetchEntities: (widgetId: string, params: Record<string, string | undefined>) => Promise<AxiosResponse<EsBase[]>>;
+  fetchAttackPaths: (widgetId: string, params: Record<string, string | undefined>) => Promise<AxiosResponse<EsAttackPath[]>>;
 }
 
 export const CustomDashboardContext = createContext<CustomDashboardContextType>({
