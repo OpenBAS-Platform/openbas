12e9119b91b242232181b003c909d7b6282b4b49
savacano28 <stephanya.casanova@filigran.io>
2026-09-07 12:28:15 +0200

    feat: add lesson for chained scenarios/simulations

--- openaev-api/src/test/java/io/openaev/utils/fixtures/composers/ChallengeComposer.java
+++ openaev-api/src/test/java/io/openaev/utils/fixtures/composers/ChallengeComposer.java
@@ -5,6 +5,7 @@ import io.openaev.database.model.Document;
 import io.openaev.database.model.Tag;
 import io.openaev.database.repository.ChallengeRepository;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,6 +49,9 @@ public class ChallengeComposer extends ComposerBase<Challenge> {
     public Composer persist() {
       this.tagComposers.forEach(TagComposer.Composer::persist);
       this.documentComposers.forEach(DocumentComposer.Composer::persist);
+      challenge.setFlags(new ArrayList<>(challenge.getFlags()));
+      challenge.setDocuments(new ArrayList<>(challenge.getDocuments()));
+      challenge.setTags(new HashSet<>(challenge.getTags()));
       challengeRepository.save(challenge);
       return this;
     }
