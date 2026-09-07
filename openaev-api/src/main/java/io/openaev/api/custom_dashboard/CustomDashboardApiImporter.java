01e86346043afac8f1ecbae5564b0f716c1af6be
Corinne Krych <corinne.krych@filigran.io>
2026-09-07 13:07:58 +0200

    chore(Multi-tenancy): add TxCTx for CustomDashboardApiImporter

--- openaev-api/src/main/java/io/openaev/api/custom_dashboard/CustomDashboardApiImporter.java
+++ openaev-api/src/main/java/io/openaev/api/custom_dashboard/CustomDashboardApiImporter.java
@@ -1,6 +1,7 @@
 package io.openaev.api.custom_dashboard;
 
 import io.openaev.aop.AccessControl;
+import io.openaev.context.TxCtx;
 import io.openaev.database.model.Action;
 import io.openaev.database.model.CustomDashboard;
 import io.openaev.database.model.ResourceType;
@@ -43,6 +44,7 @@ public class CustomDashboardApiImporter extends RestBehavior {
   @Transactional
   @AccessControl(actionPerformed = Action.WRITE, resourceType = ResourceType.DASHBOARD)
   public ResponseEntity<JsonApiDocument<ResourceObject>> importJson(
+      TxCtx ctx,
       @RequestPart("file") @NotNull MultipartFile file) throws IOException {
     return ResponseEntity.ok(
         zipJsonApi
