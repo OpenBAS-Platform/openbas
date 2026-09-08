43586ede14953c36e47095180548f70adbe5fd9e
Corinne Krych <corinne.krych@filigran.io>
2026-09-08 14:36:10 +0200

    chore(tag): spotless

--- openaev-api/src/main/java/io/openaev/rest/exercise/ExerciseApi.java
+++ openaev-api/src/main/java/io/openaev/rest/exercise/ExerciseApi.java
@@ -17,8 +17,8 @@ import io.openaev.api.expectations.ExpectationsDriftService;
 import io.openaev.api.expectations.dto.ExpectationsDriftDismissInput;
 import io.openaev.api.expectations.dto.ExpectationsDriftOutput;
 import io.openaev.api.expectations.dto.ExpectationsRealignOutput;
-import io.openaev.config.cache.LicenseCacheManager;
 import io.openaev.config.RequireTenantSelector;
+import io.openaev.config.cache.LicenseCacheManager;
 import io.openaev.context.TenantContext;
 import io.openaev.context.TxCtx;
 import io.openaev.database.model.*;
@@ -1041,8 +1041,7 @@ public class ExerciseApi extends RestBehavior {
   @Transactional
   @AccessControl(actionPerformed = Action.CREATE, resourceType = ResourceType.SIMULATION)
   public ImportResult exerciseImport(
-      @RequireTenantSelector TxCtx ctx, @RequestPart("file") MultipartFile file)
-      throws Exception {
+      @RequireTenantSelector TxCtx ctx, @RequestPart("file") MultipartFile file) throws Exception {
     return importService.handleFileImport(ctx, file, null, null);
   }
 
