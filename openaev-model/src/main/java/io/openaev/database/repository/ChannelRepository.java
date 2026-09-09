1ab1082f0b1ffcca2308c7605c79636c09538bc0
Corinne Krych <corinne.krych@filigran.io>
2026-09-09 14:14:03 +0200

    chore(challenge): spotless

--- openaev-model/src/main/java/io/openaev/database/repository/ChannelRepository.java
+++ openaev-model/src/main/java/io/openaev/database/repository/ChannelRepository.java
@@ -1,19 +1,15 @@
 package io.openaev.database.repository;
 
 import io.openaev.database.model.Channel;
-import jakarta.validation.constraints.NotNull;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChannelRepository
     extends CrudRepository<Channel, String>, JpaSpecificationExecutor<Channel> {
-  
+
   List<Channel> findByNameIgnoreCase(String name);
 
   List<Channel> findByNameIgnoreCaseAndTenantId(String name, String tenantId);
