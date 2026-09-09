50d6afab69d9b92f06f0ae6b542227e2ac28924e
savacano28 <stephanya.casanova@filigran.io>
2026-09-09 15:21:59 +0200

    feat: support scheduled execution of chained scenarios

--- openaev-api/src/test/java/io/openaev/scheduler/jobs/ScenarioExecutionJobTest.java
+++ openaev-api/src/test/java/io/openaev/scheduler/jobs/ScenarioExecutionJobTest.java
@@ -62,13 +62,13 @@ class ScenarioExecutionJobTest extends IntegrationTest {
   static String EXERCISE_ID;
 
   @AfterEach
-void after() {
-  entityManager.unwrap(org.hibernate.Session.class).disableFilter("tenantFilter");
-  entityManager.clear();
-  workflowRepository.deleteAll();
-  exerciseRepository.deleteAll();
-  scenarioRepository.deleteAll();
-}
+  void after() {
+    entityManager.unwrap(org.hibernate.Session.class).disableFilter("tenantFilter");
+    entityManager.clear();
+    workflowRepository.deleteAll();
+    exerciseRepository.deleteAll();
+    scenarioRepository.deleteAll();
+  }
 
   @Nested
   @DisplayName("When using cron-based recurrence")
