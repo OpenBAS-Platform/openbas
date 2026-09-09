a628df5d4162fcbc49131b754c38349cf504d2f7
Damien Goujard <damien.goujard@filigran.io>
2026-09-09 09:23:26 +0200

    [backend] Fix

--- openaev-api/src/main/java/io/openaev/service/user_events/UserPasswordSetupRequestedEvent.java
+++ openaev-api/src/main/java/io/openaev/service/user_events/UserPasswordSetupRequestedEvent.java
@@ -1,8 +1,4 @@
 package io.openaev.service.user_events;
 
-/**
- * Event published when a user must receive a password setup or reset email.
- */
-public record UserPasswordSetupRequestedEvent(String email, String lang) {
-
-}
+/** Event published when a user must receive a password setup or reset email. */
+public record UserPasswordSetupRequestedEvent(String email, String lang) {}
