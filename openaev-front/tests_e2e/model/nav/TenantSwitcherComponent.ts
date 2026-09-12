OpenAEV-Platform/openaev

feat(nav): replace the left menu with the design system Navbar (#7150)
Co-authored-by: Copilot App <223556219+Copilot@users.noreply.github.com>

openaev-front/tests_e2e/model/nav/TenantSwitcherComponent.ts
@@ -1,11 +1,19 @@
-import { type Page } from '@playwright/test';
+import { type Locator, type Page } from '@playwright/test';
 
 import { TIMEOUT } from '../../utils/constants';
 
 /**
  * Wraps the LeftBarTenantSwitcher component interactions.
- * The switcher is a MenuItem in the left navigation bar that opens a Popover
- * listing all accessible tenants.
+ *
+ * The switcher is a rail row (`data-testid="tenant-switcher"`) that opens the
+ * design system `Menu`; the panel is portalled to the end of the document, so
+ * its rows are located globally and not under `.app-navbar`. Its rows are real
+ * links (switching tenant is a URL navigation), so this object clicks anchors,
+ * not menu buttons.
+ *
+ * The trigger keeps its test id in both rail states, and without a validated
+ * Enterprise Edition licence it is the same row minus the menu — it only opens
+ * the upsell dialog and lists nothing.
  */
 class TenantSwitcherComponent {
   constructor(private page: Page) {}
@@ -15,39 +23,49 @@ class TenantSwitcherComponent {
   }
 
   /**
-   * Opens the tenant-switcher popover by clicking the icon-based menu item.
+   * The trigger row. It carries its own test id in both rail states; the
+   * tenant name is only used to disambiguate when a caller passes one (the
+   * label stays in the DOM, visually hidden, when the rail is collapsed).
+   */
+  private triggerFor(currentTenantName?: string): Locator {
+    if (!currentTenantName) {
+      return this.switcher;
+    }
+    return this.switcher.filter({ hasText: currentTenantName }).first();
+  }
+
+  /**
+   * Opens the tenant list.
    * Works regardless of whether the left bar is expanded or collapsed.
    */
-  async openSwitcher(_currentTenantName?: string): Promise<void> {
-    const switcher = this.switcher;
+  async openSwitcher(currentTenantName?: string): Promise<void> {
+    const trigger = this.triggerFor(currentTenantName);
 
-    await switcher.waitFor({
+    await trigger.waitFor({
       state: 'visible',
       timeout: TIMEOUT,
     });
-    await switcher.click();
+    await trigger.click();
 
-    await this.page.locator('.MuiPopover-root').last().waitFor({
+    await this.popoverTenantItems.first().waitFor({
       state: 'visible',
       timeout: TIMEOUT,
     });
   }
 
   /**
-   * All tenant menu items inside the currently open switcher popover.
-   * Call {@link openSwitcher} first to open the popover.
+   * All tenant rows of the open switcher — anchors inside the portalled menu
+   * panel. Call {@link openSwitcher} first.
    */
   get popoverTenantItems() {
-    return this.page.locator('.MuiPopover-root').last().getByRole('menuitem');
+    return this.page.getByTestId('tenant-switcher-option');
   }
 
   /**
-   * Clicks a specific tenant by name from the open switcher popover.
+   * Clicks a specific tenant by name in the open switcher.
    */
   async selectTenantByName(tenantName: string): Promise<void> {
-    const popover = this.page.locator('.MuiPopover-root').last();
-    await popover.waitFor({ state: 'visible' });
-    await popover.getByRole('menuitem').filter({ hasText: tenantName }).click();
+    await this.popoverTenantItems.filter({ hasText: tenantName }).first().click();
   }
 }
 export default TenantSwitcherComponent;

OpenBAS-Platform/openbas

openaev-front/tests_e2e/model/nav/TenantSwitcherComponent.ts
@@ -1,11 +1,19 @@
-import { expect, type Page } from '@playwright/test';
+import { expect, type Locator, type Page } from '@playwright/test';
 
 import { TIMEOUT } from '../../utils/constants';
 
 /**
  * Wraps the LeftBarTenantSwitcher component interactions.
- * The switcher is a MenuItem in the left navigation bar that opens a Popover
- * listing all accessible tenants.
+ *
+ * The switcher is a rail row (`data-testid="tenant-switcher"`) that opens the
+ * design system `Menu`; the panel is portalled to the end of the document, so
+ * its rows are located globally and not under `.app-navbar`. Its rows are real
+ * links (switching tenant is a URL navigation), so this object clicks anchors,
+ * not menu buttons.
+ *
+ * The trigger keeps its test id in both rail states, and without a validated
+ * Enterprise Edition licence it is the same row minus the menu — it only opens
+ * the upsell dialog and lists nothing.
  */
 class TenantSwitcherComponent {
   constructor(private page: Page) {}
@@ -19,17 +27,29 @@ class TenantSwitcherComponent {
   }
 
   /**
-   * Opens the tenant-switcher popover by clicking the icon-based menu item.
+   * The trigger row. It carries its own test id in both rail states; the
+   * tenant name is only used to disambiguate when a caller passes one (the
+   * label stays in the DOM, visually hidden, when the rail is collapsed).
+   */
+  private triggerFor(currentTenantName?: string): Locator {
+    if (!currentTenantName) {
+      return this.switcher;
+    }
+    return this.switcher.filter({ hasText: currentTenantName }).first();
+  }
+
+  /**
+   * Opens the tenant list.
    * Works regardless of whether the left bar is expanded or collapsed.
    */
-  async openSwitcher(_currentTenantName?: string): Promise<void> {
-    const switcher = this.switcher;
+  async openSwitcher(currentTenantName?: string): Promise<void> {
+    const trigger = this.triggerFor(currentTenantName);
 
-    await switcher.waitFor({
+    await trigger.waitFor({
       state: 'attached',
       timeout: TIMEOUT,
     });
-    await switcher.waitFor({
+    await trigger.waitFor({
       state: 'visible',
       timeout: TIMEOUT,
     });
@@ -38,7 +58,7 @@ class TenantSwitcherComponent {
       if (await this.switcherPopover.isVisible().catch(() => false)) {
         return;
       }
-      await switcher.click();
+      await trigger.click();
       await this.switcherPopover.waitFor({
         state: 'visible',
         timeout: TIMEOUT,
@@ -50,20 +70,20 @@ class TenantSwitcherComponent {
   }
 
   /**
-   * All tenant menu items inside the currently open switcher popover.
-   * Call {@link openSwitcher} first to open the popover.
+   * All tenant rows of the open switcher — anchors inside the portalled menu
+   * panel. Call {@link openSwitcher} first.
    */
   get popoverTenantItems() {
-    return this.switcherPopover.getByRole('menuitem');
+    return this.switcherPopover.getByTestId('tenant-switcher-option');
   }
 
   /**
-   * Clicks a specific tenant by name from the open switcher popover.
+   * Clicks a specific tenant by name in the open switcher.
    */
   async selectTenantByName(tenantName: string): Promise<void> {
     const popover = this.switcherPopover;
     await popover.waitFor({ state: 'visible' });
-    await popover.getByRole('menuitem').filter({ hasText: tenantName }).click();
+    await this.popoverTenantItems.filter({ hasText: tenantName }).first().click();
   }
 }
 export default TenantSwitcherComponent;